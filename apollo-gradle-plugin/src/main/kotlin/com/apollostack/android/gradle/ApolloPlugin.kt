58e74fdeb76d131b63d3f0833f7a4e9b866b3046
Felipe Lima <felipe.lima@gmail.com>
2016-09-29 23:07:18 -0700

    Rename the plugin class

--- apollo-gradle-plugin/src/main/kotlin/com/apollostack/android/gradle/ApolloPlugin.kt
+++ apollo-gradle-plugin/src/main/kotlin/com/apollostack/android/gradle/ApolloPlugin.kt
@@ -3,7 +3,7 @@ package com.apollostack.android.gradle
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-class SqlDelightPlugin : Plugin<Project> {
+class ApolloPlugin : Plugin<Project> {
   override fun apply(project: Project) {
   }
 }
