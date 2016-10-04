92ffafe16a65a4c14a2bed536a98e8285580abba
Felipe Lima <felipe.lima@gmail.com>
2016-10-03 20:46:38 -0700

    Adds initial project files for Gradle plugin (#1)
    * Adds initial project files for Gradle plugin
    
    * Rename the plugin class
    
    * Code review fixes

---
+++ apollo-gradle-plugin/src/main/kotlin/com/apollostack/android/gradle/ApolloPlugin.kt
@@ -0,0 +1,9 @@
+package com.apollostack.android.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class ApolloPlugin : Plugin<Project> {
+  override fun apply(project: Project) {
+  }
+}
