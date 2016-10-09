7b3eac3d9a0f193623869efed345583150b8888b
Felipe Lima <felipe.lima@gmail.com>
2016-10-09 14:06:04 -0700

    Adds compiler files

---
+++ apollo-gradle-plugin/src/main/kotlin/com/apollostack/android/gradle/ApolloTask.kt
@@ -0,0 +1,28 @@
+package com.apollostack.android.gradle
+
+import com.apollostack.compiler.GraphqlCompiler
+import com.apollostack.android.VERSION
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.SourceTask
+import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.incremental.IncrementalTaskInputs
+import java.io.File
+
+open class ApolloTask : SourceTask() {
+  @Suppress("unused") // Required to invalidate the task on version updates.
+  @Input fun pluginVersion() = VERSION
+
+  @get:OutputDirectory var outputDirectory: File? = null
+
+  var buildDirectory: File? = null
+    set(value) {
+      field = value
+      outputDirectory = GraphqlCompiler.OUTPUT_DIRECTORY.fold(buildDirectory, ::File)
+    }
+
+  @TaskAction
+  fun execute(inputs: IncrementalTaskInputs) {
+
+  }
+}
