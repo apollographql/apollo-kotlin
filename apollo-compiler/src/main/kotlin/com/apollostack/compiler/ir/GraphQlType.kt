111c3b52238ac3310e812eb82e5316ca22a1113e
Marwan Ahmed <mrwnad@gmail.com>
2016-12-08 11:03:05 -0500

    refactor test methods, fix query name in a file
    This moves the file creation logic in the test files to utils and fixes a typo
    in the query name for one of the test IRs.

--- apollo-compiler/src/main/kotlin/com/apollostack/compiler/ir/GraphQlType.kt
+++ apollo-compiler/src/main/kotlin/com/apollostack/compiler/ir/GraphQlType.kt
@@ -27,7 +27,7 @@ sealed class GraphQlType(val nullable: Boolean) {
   companion object {
     private val NULLABLE_ANNOTATION = AnnotationSpec.builder(Nullable::class.java).build()
     private val LIST_TYPE = ClassName.get(List::class.java)
-    private val GRAPHQLTYPE_TO_JAVA_TIPE = mapOf(
+    private val GRAPHQLTYPE_TO_JAVA_TYPE = mapOf(
         GraphQlString::class.java to ClassName.get(String::class.java),
         GraphQlId::class.java to TypeName.LONG,
         GraphQlInt::class.java to TypeName.INT,
@@ -54,7 +54,7 @@ sealed class GraphQlType(val nullable: Boolean) {
             graphQlTypeToJavaTypeName(type.listType, false, false))
         is GraphQlUnknown -> ClassName.get("", type.typeName)
         else ->
-          GRAPHQLTYPE_TO_JAVA_TIPE[type.javaClass]!!.let {
+          GRAPHQLTYPE_TO_JAVA_TYPE[type.javaClass]!!.let {
             if (primitive) it else it.box()
           }
       }
