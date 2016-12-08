3adacc1e205db9f1591ed9476320dcc1888ffc52
Felipe Lima <felipe.lima@gmail.com>
2016-12-07 20:06:58 -0800

    Address code review feedback

--- apollo-compiler/src/main/kotlin/com/apollostack/compiler/OperationTypeSpecBuilder.kt
+++ apollo-compiler/src/main/kotlin/com/apollostack/compiler/OperationTypeSpecBuilder.kt
@@ -3,14 +3,20 @@ package com.apollostack.compiler
 import com.apollostack.compiler.ir.CodeGenerator
 import com.apollostack.compiler.ir.Field
 import com.apollostack.compiler.ir.Fragment
+import com.squareup.javapoet.ClassName
+import com.squareup.javapoet.MethodSpec
 import com.squareup.javapoet.TypeSpec
 import javax.lang.model.element.Modifier
 
+/**
+ * TODO: Move this code to the class Operation so it's consistent with other classes implementing
+ * the CodeGenerator interface.
+ */
 class OperationTypeSpecBuilder(
     val operationName: String,
     val fields: List<Field>,
-    val allFragments: List<Fragment>) : CodeGenerator {
-
+    val allFragments: List<Fragment>
+) : CodeGenerator {
   override fun toTypeSpec(): TypeSpec = buildTypeSpecs(operationName, fields, allFragments)
 
   private fun buildTypeSpecs(typeName: String, fields: List<Field>, fragments: List<Fragment>) =
@@ -25,10 +31,12 @@ class OperationTypeSpecBuilder(
   private fun innerFragments(fields: List<Field>, fragments: List<Fragment>): List<TypeSpec> =
       fields.filter(Field::hasFragments)
           .map { field ->
+            // TODO: This is generating the same "Fragments" interface for every field that
+            // references a fragment.
             TypeSpec.interfaceBuilder(field.normalizedName())
                 .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
-                .addMethod(Fragment.genericAccessorMethodSpec())
-                .addType(Fragment.genericInterfaceSpec(fragments))
+                .addMethod(fragmentAccessorMethodSpec())
+                .addType(fragmentInterfaceSpec(fragments))
                 .build()
           }
 
@@ -40,4 +48,21 @@ class OperationTypeSpecBuilder(
   /** Filters all fragments to only the ones referenced by the provided field */
   private fun referencedFragments(field: Field) =
       allFragments.filter { field.fragmentSpreads?.contains(it.fragmentName) ?: false }
+
+  companion object {
+    private val INTERFACE_NAME = "Fragments"
+
+    fun fragmentAccessorMethodSpec(): MethodSpec =
+        MethodSpec.methodBuilder("fragments")
+            .returns(ClassName.get("", INTERFACE_NAME))
+            .addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT)
+            .build()
+
+    /** Returns a generic `Fragments` interface with methods for each of the provided fragments */
+    fun fragmentInterfaceSpec(fragments: List<Fragment>): TypeSpec =
+        TypeSpec.interfaceBuilder(INTERFACE_NAME)
+            .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+            .addMethods(fragments.map(Fragment::toMethodSpec))
+            .build()
+  }
 }
--- apollo-compiler/src/main/kotlin/com/apollostack/compiler/ir/Fragment.kt
+++ apollo-compiler/src/main/kotlin/com/apollostack/compiler/ir/Fragment.kt
@@ -14,7 +14,7 @@ data class Fragment(
     val inlineFragments: List<String>,
     val fragmentsReferenced: List<String>) : CodeGenerator {
   /** Returns a Java method that returns the interface represented by this Fragment object. */
-  private fun toMethodSpec() =
+  fun toMethodSpec(): MethodSpec =
       MethodSpec.methodBuilder(fragmentName.decapitalize())
           .returns(ClassName.get("", fragmentName.capitalize()))
           .addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT)
@@ -26,21 +26,4 @@ data class Fragment(
           .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
           .addMethods(fields.map(Field::toMethodSpec))
           .build()
-
-  companion object {
-    private val INTERFACE_NAME = "Fragments"
-
-    fun genericAccessorMethodSpec(): MethodSpec =
-        MethodSpec.methodBuilder("fragments")
-            .returns(ClassName.get("", INTERFACE_NAME))
-            .addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT)
-            .build()
-
-    /** Returns a generic `Fragments` interface with methods for each of the provided fragments */
-    fun genericInterfaceSpec(fragments: List<Fragment>): TypeSpec =
-        TypeSpec.interfaceBuilder(INTERFACE_NAME)
-            .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
-            .addMethods(fragments.map(Fragment::toMethodSpec))
-            .build()
-  }
 }
