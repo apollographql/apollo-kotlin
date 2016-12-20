ede8e048b0c68bb9b34099d4cedfc9eee0a8a665
Ivan Savytskyi <ivan.savitsky@gmail.com>
2016-12-19 20:43:19 -0500

    Remove garbage

--- apollo-sample/src/main/java/com/example/apollostack/sample/MainActivity.java
+++ apollo-sample/src/main/java/com/example/apollostack/sample/MainActivity.java
@@ -7,15 +7,5 @@ public class MainActivity extends AppCompatActivity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-
-//    ReviewInput.builder()
-//        .commentary("test")
-//        .stars(5)
-//        .favoriteColor(ColorInput.builder()
-//            .red(255)
-//            .green(255)
-//            .blue(255)
-//            .build()
-//        ).build();
   }
 }
