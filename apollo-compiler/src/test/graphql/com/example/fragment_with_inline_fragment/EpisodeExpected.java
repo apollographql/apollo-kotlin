1718f4000ffdda8a5d90aa59039ae07389b27be0
Marwan Ahmed <mrwnad@gmail.com>
2016-12-21 16:44:00 -0500

    refactor the compiler tests into parameterized tests

---
+++ apollo-compiler/src/test/graphql/com/example/fragment_with_inline_fragment/EpisodeExpected.java
@@ -0,0 +1,21 @@
+package com.example.fragment_with_inline_fragment;
+
+/**
+ * The episodes in the Star Wars trilogy
+ */
+public enum Episode {
+  /**
+   * Star Wars Episode IV: A New Hope, released in 1977.
+   */
+  NEWHOPE,
+
+  /**
+   * Star Wars Episode V: The Empire Strikes Back, released in 1980.
+   */
+  EMPIRE,
+
+  /**
+   * Star Wars Episode VI: Return of the Jedi, released in 1983.
+   */
+  JEDI
+}
