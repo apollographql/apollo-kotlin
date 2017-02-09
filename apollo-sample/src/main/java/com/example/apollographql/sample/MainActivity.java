71425976a5b4e864803e5e8bd32cdc00d660beff
Juni Kim <73hKrn@gmail.com>
2017-02-08 19:31:17 -0800

    Implement Upvote Click handling
    Clicking on the upvote button emits an event to the Observer owned by
    the caller of the PostsAdapter constructor. To better facilitate list
    manipulation, the PostsAdapter was changed to own the backing List
    itself, rather than holding a reference to a List owned by some other
    object.

--- apollo-sample/src/main/java/com/example/apollographql/sample/MainActivity.java
+++ apollo-sample/src/main/java/com/example/apollographql/sample/MainActivity.java
@@ -11,11 +11,14 @@ import android.widget.Toast;
 
 import com.apollographql.android.api.graphql.Response;
 import com.example.AllPosts;
+import com.example.Upvote;
 
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subjects.Subject;
 
 public class MainActivity extends AppCompatActivity {
   private static final String TAG = "MainActivity";
@@ -25,33 +28,59 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
     final TextView txtResponse = (TextView) findViewById(R.id.txt_response);
     final RecyclerView responses = (RecyclerView) findViewById(R.id.responses);
+
+    final SampleApplication application = (SampleApplication) getApplication();
+
+    final Subject<Integer> upvoteObserver = PublishSubject.create();
+
+    final PostsAdapter adapter = new PostsAdapter(upvoteObserver);
+    responses.setAdapter(adapter);
     responses.setLayoutManager(new LinearLayoutManager(this));
 
-    SampleApplication application = (SampleApplication) getApplication();
+    upvoteObserver.subscribe(new SimpleObserver<Integer>() {
+      @Override public void onNext(Integer postId) {
+        final Upvote.Variables variables = Upvote.Variables.builder()
+            .postId(postId)
+            .build();
+
+        application.postsService()
+            .upvote(new Upvote(variables))
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(new SimpleObserver<Response<Upvote.Data>>() {
+              @Override public void onNext(Response<Upvote.Data> response) {
+                adapter.upvotePost(response.data().upvotePost());
+              }
+            });
+      }
+    });
+
     application.postsService()
         .allPosts(new AllPosts())
         .subscribeOn(Schedulers.io())
         .observeOn(AndroidSchedulers.mainThread())
-        .subscribe(new Observer<Response<AllPosts.Data>>() {
-          @Override public void onSubscribe(Disposable d) {
-          }
-
+        .subscribe(new SimpleObserver<Response<AllPosts.Data>>() {
           @Override public void onNext(Response<AllPosts.Data> response) {
             txtResponse.setVisibility(View.GONE);
             responses.setVisibility(View.VISIBLE);
-            responses.setAdapter(new PostsAdapter(response.data().posts()));
-          }
 
-          @Override public void onError(Throwable e) {
-            Log.e(TAG, "", e);
-            Toast.makeText(MainActivity.this, "onError(): " + e.getMessage(), Toast.LENGTH_LONG)
-                .show();
-          }
-
-          @Override public void onComplete() {
+            adapter.allPosts(response.data().posts());
           }
         });
   }
 
+  private abstract class SimpleObserver<T> implements Observer<T> {
+    @Override public void onSubscribe(Disposable d) {
+    }
 
+    @Override public void onError(Throwable e) {
+      Log.e(TAG, "", e);
+      Toast.makeText(MainActivity.this, "onError(): " + e.getMessage(), Toast.LENGTH_LONG)
+          .show();
+    }
+
+    @Override public void onComplete() {
+
+    }
+  }
 }
--- apollo-sample/src/main/java/com/example/apollographql/sample/PostsAdapter.java
+++ apollo-sample/src/main/java/com/example/apollographql/sample/PostsAdapter.java
@@ -9,37 +9,50 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.AllPosts;
+import com.example.Upvote;
 
 import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Nonnull;
 
 import fragment.PostDetails;
+import io.reactivex.Observer;
 
 class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> {
-  @Nonnull private final List<? extends AllPosts.Data.Post> posts;
+  @Nonnull private final Observer<Integer> upvoteObserver;
+  @Nonnull private final List<AllPosts.Data.Post> posts;
   @Nonnull private final NumberFormat numberFormat = NumberFormat.getIntegerInstance();
+  @Nonnull private final View.OnClickListener upvoteClickedListener = new View.OnClickListener() {
+    @Override public void onClick(View v) {
+      upvoteObserver.onNext((Integer)v.getTag());
+    }
+  };
 
-  PostsAdapter(@Nonnull List<? extends AllPosts.Data.Post> posts) {
-    this.posts = posts;
+  PostsAdapter(@Nonnull Observer<Integer> upvoteObserver) {
+    this.upvoteObserver = upvoteObserver;
+    this.posts = new ArrayList<>();
   }
 
   @Override public ViewHolder onCreateViewHolder(@Nonnull ViewGroup parent, int viewType) {
     final View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.listitem_post, parent, false);
-    return new ViewHolder(itemView);
+    final ViewHolder viewHolder = new ViewHolder(itemView);
+    viewHolder.upvote.setOnClickListener(upvoteClickedListener);
+    return viewHolder;
   }
 
   @Override public void onBindViewHolder(@Nonnull ViewHolder holder, int position) {
     final AllPosts.Data.Post post = posts.get(position);
     final PostDetails details = post.fragments().postDetails();
 
+    holder.upvote.setTag(details.id());
+
     holder.title.setText(details.title());
 
     final PostDetails.Author author = details.author();
     final String byline = (author != null) ? getByline(holder.byline.getResources(), author) : null;
     holder.byline.setText(byline);
-
 
     holder.voteCount.setText(numberFormat.format(details.votes()));
   }
@@ -50,6 +63,31 @@ class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> {
 
   @Nonnull private static String getByline(@Nonnull Resources resources, @Nonnull PostDetails.Author author) {
     return resources.getString(R.string.byline_format, author.firstName(), author.lastName());
+  }
+
+  void allPosts(@Nonnull List<? extends AllPosts.Data.Post> posts) {
+    this.posts.addAll(posts);
+  }
+
+  void upvotePost(@Nonnull Upvote.Data.UpvotePost upvotePost) {
+    for (int index = 0, max = posts.size(); index < max; ++index) {
+      final AllPosts.Data.Post post = posts.get(index);
+      final PostDetails details = post.fragments().postDetails();
+      final PostDetails upvoteDetails = upvotePost.fragments().postDetails();
+      if (details.id() == upvoteDetails.id()) {
+        final PostDetails newDetails = PostDetails.CREATOR.create(
+            upvoteDetails.id(),
+            upvoteDetails.title(),
+            upvoteDetails.votes(),
+            upvoteDetails.author());
+        final AllPosts.Data.Post newData = new AllPosts.Data.Post(new AllPosts.Data.Post.Fragments(newDetails));
+        posts.remove(index);
+        posts.add(index, newData);
+
+        notifyItemChanged(index);
+        break;
+      }
+    }
   }
 
   static class ViewHolder extends RecyclerView.ViewHolder {
