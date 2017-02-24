07026db8409bb8d4918fab5effe6a222fde54fe8
Plummer <brian.r.plummer@gmail.com>
2017-02-24 14:59:21 -0500

    fix sample to use apollo-runtime and okhttp

--- apollo-sample/src/main/java/com/example/apollographql/sample/MainActivity.java
+++ apollo-sample/src/main/java/com/example/apollographql/sample/MainActivity.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.apollographql.android.api.graphql.Response;
 import com.example.FeedQuery;
 import com.example.type.FeedType;
 
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
@@ -18,25 +21,31 @@ import io.reactivex.schedulers.Schedulers;
 public class MainActivity extends AppCompatActivity {
   private static final String TAG = "MainActivity";
 
+  private SampleApplication application;
+
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     final TextView txtResponse = (TextView) findViewById(R.id.txt_response);
-    SampleApplication application = (SampleApplication) getApplication();
-    application.githuntApiService()
-        .githuntFeed(new FeedQuery(FeedQuery.Variables.builder()
-                .limit(10)
-                .type(FeedType.HOT)
-                .build()))
+    application = (SampleApplication) getApplication();
+
+    Observable<Response<FeedQuery.Data>> query = Observable.fromCallable(new Callable<Response<FeedQuery.Data>>() {
+      @Override public Response<FeedQuery.Data> call() throws Exception {
+        return executeFeedQuery();
+      }
+    });
+
+    query
         .subscribeOn(Schedulers.io())
         .observeOn(AndroidSchedulers.mainThread())
         .subscribe(new Observer<Response<FeedQuery.Data>>() {
           @Override public void onSubscribe(Disposable d) {
+
           }
 
-          @Override public void onNext(Response<FeedQuery.Data> response) {
+          @Override public void onNext(Response<FeedQuery.Data> dataResponse) {
             StringBuffer buffer = new StringBuffer();
-            for (FeedQuery.Data.Feed feed : response.data().feed()) {
+            for (FeedQuery.Data.Feed feed : dataResponse.data().feed()) {
               buffer.append("name:" + feed.repository().fragments().repositoryFragment().name());
               buffer.append(" owner: " + feed.repository().fragments().repositoryFragment().owner().login());
               buffer.append(" postedBy: " + feed.postedBy().login());
@@ -47,15 +56,19 @@ public class MainActivity extends AppCompatActivity {
           }
 
           @Override public void onError(Throwable e) {
-            Log.e(TAG, "", e);
-            Toast.makeText(MainActivity.this, "onError(): " + e.getMessage(), Toast.LENGTH_LONG)
-                .show();
+            Log.e(TAG, e.getMessage(), e);
           }
 
           @Override public void onComplete() {
+
           }
         });
   }
 
-
+  public Response<FeedQuery.Data> executeFeedQuery() throws IOException {
+    return application.apolloClient().newCall(new FeedQuery(FeedQuery.Variables.builder()
+        .limit(10)
+        .type(FeedType.HOT)
+        .build())).execute();
+  }
 }
