aa8be6eadd64ef6c0719e82767717092f0a203ef
Plummer <brian.r.plummer@gmail.com>
2017-02-24 15:58:11 -0500

    ditch rxJava, use enqueue

--- apollo-sample/src/main/java/com/example/apollographql/sample/MainActivity.java
+++ apollo-sample/src/main/java/com/example/apollographql/sample/MainActivity.java
@@ -5,18 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.apollographql.android.ApolloCall;
 import com.apollographql.android.api.graphql.Response;
 import com.example.FeedQuery;
 import com.example.type.FeedType;
 
-import java.io.IOException;
-import java.util.concurrent.Callable;
-
-import io.reactivex.Observable;
-import io.reactivex.Observer;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
+import javax.annotation.Nonnull;
 
 public class MainActivity extends AppCompatActivity {
   private static final String TAG = "MainActivity";
@@ -29,46 +23,35 @@ public class MainActivity extends AppCompatActivity {
     final TextView txtResponse = (TextView) findViewById(R.id.txt_response);
     application = (SampleApplication) getApplication();
 
-    Observable<Response<FeedQuery.Data>> query = Observable.fromCallable(new Callable<Response<FeedQuery.Data>>() {
-      @Override public Response<FeedQuery.Data> call() throws Exception {
-        return executeFeedQuery();
-      }
-    });
-
-    query
-        .subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-        .subscribe(new Observer<Response<FeedQuery.Data>>() {
-          @Override public void onSubscribe(Disposable d) {
-
-          }
-
-          @Override public void onNext(Response<FeedQuery.Data> dataResponse) {
-            StringBuffer buffer = new StringBuffer();
-            for (FeedQuery.Data.Feed feed : dataResponse.data().feed()) {
-              buffer.append("name:" + feed.repository().fragments().repositoryFragment().name());
-              buffer.append(" owner: " + feed.repository().fragments().repositoryFragment().owner().login());
-              buffer.append(" postedBy: " + feed.postedBy().login());
-              buffer.append("\n~~~~~~~~~~~");
-              buffer.append("\n\n");
-            }
-            txtResponse.setText(buffer.toString());
-          }
-
-          @Override public void onError(Throwable e) {
-            Log.e(TAG, e.getMessage(), e);
-          }
-
-          @Override public void onComplete() {
-
-          }
-        });
-  }
-
-  public Response<FeedQuery.Data> executeFeedQuery() throws IOException {
-    return application.apolloClient().newCall(new FeedQuery(FeedQuery.Variables.builder()
+    application.apolloClient().newCall(new FeedQuery(FeedQuery.Variables.builder()
         .limit(10)
         .type(FeedType.HOT)
-        .build())).execute();
+        .build())).enqueue(new ApolloCall.Callback<FeedQuery.Data>() {
+
+      @Override public void onResponse(@Nonnull Response<FeedQuery.Data> dataResponse) {
+
+        final StringBuffer buffer = new StringBuffer();
+        for (FeedQuery.Data.Feed feed : dataResponse.data().feed()) {
+          buffer.append("name:" + feed.repository().fragments().repositoryFragment().name());
+          buffer.append(" owner: " + feed.repository().fragments().repositoryFragment().owner().login());
+          buffer.append(" postedBy: " + feed.postedBy().login());
+          buffer.append("\n~~~~~~~~~~~");
+          buffer.append("\n\n");
+        }
+
+        MainActivity.this.runOnUiThread(new Runnable() {
+          @Override public void run() {
+            txtResponse.setText(buffer.toString());
+          }
+        });
+
+      }
+
+      @Override public void onFailure(@Nonnull Exception e) {
+        Log.e(TAG, e.getMessage(), e);
+      }
+    });
   }
+
 }
+
