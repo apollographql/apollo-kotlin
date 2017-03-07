ce9a30f355856611785d71b4a04e6ca62290109d
Ivan Savytskyi <ivan.savitsky@gmail.com>
2017-03-07 14:29:05 -0500

    Update

--- apollo-runtime/src/main/java/com/apollographql/android/cache/normalized/CacheKeyResolver.java
+++ apollo-runtime/src/main/java/com/apollographql/android/cache/normalized/CacheKeyResolver.java
@@ -10,13 +10,13 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 public abstract class CacheKeyResolver {
-  public final static CacheKeyResolver DEFAULT = new CacheKeyResolver() {
+  public static final CacheKeyResolver DEFAULT = new CacheKeyResolver() {
     @Nullable @Override public String resolve(@Nonnull Map<String, Object> jsonObject) {
       return null;
     }
   };
-  private final static String QUERY_ROOT_KEY = "QUERY_ROOT";
-  private final static String MUTATION_ROOT_KEY = "MUTATION_ROOT";
+  private static final String QUERY_ROOT_KEY = "QUERY_ROOT";
+  private static final String MUTATION_ROOT_KEY = "MUTATION_ROOT";
 
   public static String rootKeyForOperation(@Nonnull Operation operation) {
     if (operation instanceof Query) {
