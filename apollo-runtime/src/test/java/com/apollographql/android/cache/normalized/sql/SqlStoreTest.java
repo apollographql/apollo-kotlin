a0addb8e25405b3e4ce929890264eb904137d834
digitalbuddha <mnakhimovich@gmail.com>
2017-03-08 13:35:29 -0500

    test scaffolding

--- apollo-runtime/src/test/java/com/apollographql/android/cache/normalized/sql/SqlStoreTest.java
+++ apollo-runtime/src/test/java/com/apollographql/android/cache/normalized/sql/SqlStoreTest.java
@@ -1,20 +1,9 @@
 package com.apollographql.android.cache.normalized.sql;
 
-import android.database.sqlite.SQLiteDatabase;
-import android.provider.Contacts;
 import android.test.AndroidTestCase;
 import android.test.RenamingDelegatingContext;
 
-import com.apollographql.android.cache.normalized.Record;
-
 import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
-
-import static android.provider.Contacts.SettingsColumns.KEY;
-import static com.google.common.truth.Truth.assertThat;
-import static org.mockito.Mockito.when;
 
 /**
  * Created by 206847 on 3/8/17.
@@ -23,7 +12,7 @@ import static org.mockito.Mockito.when;
 public class SqlStoreTest extends AndroidTestCase {
 
   public static final String KEY = "key";
-  public static final String RECORD="record";
+  public static final String RECORD = "record";
   private SqlStore sqlStore;
 
   @Before
