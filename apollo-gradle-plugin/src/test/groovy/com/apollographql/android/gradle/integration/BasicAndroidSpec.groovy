airbnb/apollo-android

add apollo-runtime dependency through the plugin

apollo-gradle-plugin/src/test/groovy/com/apollographql/android/gradle/integration/BasicAndroidSpec.groovy
@@ -23,7 +23,7 @@ class BasicAndroidSpec extends Specification {
     def result = GradleRunner.create()
         .withProjectDir(testProjectDir)
         .withPluginClasspath()
-        .withArguments("build", "-Dapollographql.skipApi=true")
+        .withArguments("build", "-Dapollographql.skipRuntime=true")
         .forwardStdError(new OutputStreamWriter(System.err))
         .build()
 

apollo-gradle-plugin/src/test/groovy/com/apollographql/android/gradle/integration/FlavoredMultiSchemaSpec.groovy
@@ -71,7 +71,7 @@ class FlavoredMultiSchemaSpec extends Specification {
     def result = GradleRunner.create()
         .withProjectDir(testProjectDir)
         .withPluginClasspath()
-        .withArguments("build", "-Dapollographql.skipApi=true")
+        .withArguments("build", "-Dapollographql.skipRuntime=true")
         .forwardStdError(new OutputStreamWriter(System.err))
         .build()
 

apollographql/apollo-kotlin

apollo-gradle-plugin/src/test/groovy/com/apollographql/android/gradle/integration/BasicAndroidSpec.groovy
@@ -23,7 +23,7 @@ class BasicAndroidSpec extends Specification {
     def result = GradleRunner.create()
         .withProjectDir(testProjectDir)
         .withPluginClasspath()
-        .withArguments("build", "-Dapollographql.skipApi=true")
+        .withArguments("build", "-Dapollographql.skipRuntimeDep=true")
         .forwardStdError(new OutputStreamWriter(System.err))
         .build()
 

apollo-gradle-plugin/src/test/groovy/com/apollographql/android/gradle/integration/FlavoredMultiSchemaSpec.groovy
@@ -71,7 +71,7 @@ class FlavoredMultiSchemaSpec extends Specification {
     def result = GradleRunner.create()
         .withProjectDir(testProjectDir)
         .withPluginClasspath()
-        .withArguments("build", "-Dapollographql.skipApi=true")
+        .withArguments("build", "-Dapollographql.skipRuntimeDep=true")
         .forwardStdError(new OutputStreamWriter(System.err))
         .build()
 