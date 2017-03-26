2fb43f24df5de29e6647ae9ee92925c22e5dd197
Ivan Savytskyi <ivan.savitsky@gmail.com>
2017-03-26 16:31:30 -0400

    Add Jsr305 dep back to runtime Fix semantics of converting async calls to Rx

--- apollo-rxsupport/src/main/java/com.apollographql.android.rx/RxApollo.java
+++ apollo-rxsupport/src/main/java/com.apollographql.android.rx/RxApollo.java
@@ -3,20 +3,19 @@ package com.apollographql.android.rx;
 
 import com.apollographql.android.ApolloCall;
 import com.apollographql.android.ApolloWatcher;
-import com.apollographql.android.Cancelable;
 import com.apollographql.android.api.graphql.Response;
 
 import java.io.IOException;
 
 import javax.annotation.Nonnull;
 
+import rx.Emitter;
 import rx.Observable;
 import rx.Single;
-import rx.SingleSubscriber;
-import rx.Subscriber;
+import rx.SingleEmitter;
 import rx.exceptions.Exceptions;
-import rx.functions.Action0;
-import rx.subscriptions.Subscriptions;
+import rx.functions.Action1;
+import rx.functions.Cancellable;
 
 import static com.apollographql.android.api.graphql.util.Utils.checkNotNull;
 
@@ -26,65 +25,47 @@ public final class RxApollo {
   }
 
   @Nonnull
-  public static <T> Observable<T> from(@Nonnull
-  final ApolloWatcher<T> watcher) {
+  public static <T> Observable<T> from(@Nonnull final ApolloWatcher<T> watcher) {
     checkNotNull(watcher);
-    return Observable.create(new Observable.OnSubscribe<T>() {
-      @Override public void call(final Subscriber<? super T> subscriber) {
-        cancelOnUnsubscribe(subscriber, watcher);
+    return Observable.fromEmitter(new Action1<Emitter<T>>() {
+      @Override public void call(final Emitter<T> emitter) {
+        emitter.setCancellation(new Cancellable() {
+          @Override public void cancel() throws Exception {
+            watcher.cancel();
+          }
+        });
         watcher.enqueueAndWatch(new ApolloCall.Callback<T>() {
           @Override public void onResponse(@Nonnull Response<T> response) {
-            if (!subscriber.isUnsubscribed()) {
-              subscriber.onNext(response.data());
-            }
+            emitter.onNext(response.data());
           }
 
           @Override public void onFailure(@Nonnull Throwable e) {
             Exceptions.throwIfFatal(e);
-            if (!subscriber.isUnsubscribed()) {
-              subscriber.onError(e);
-            }
+            emitter.onError(e);
+          }
+        });
+      }
+    }, Emitter.BackpressureMode.LATEST);
+  }
+
+  @Nonnull public static <T> Single<T> from(@Nonnull final ApolloCall<T> call) {
+    checkNotNull(call);
+    return Single.fromEmitter(new Action1<SingleEmitter<T>>() {
+      @Override public void call(SingleEmitter<T> emitter) {
+        emitter.setCancellation(new Cancellable() {
+          @Override public void cancel() throws Exception {
+            call.cancel();
           }
         });
 
-      }
-    });
-  }
-
-  @Nonnull public static <T> Single<T> from(@Nonnull
-  final ApolloCall<T> call) {
-    checkNotNull(call);
-    return Single.create(new Single.OnSubscribe<T>() {
-      @Override public void call(SingleSubscriber<? super T> subscriber) {
-        cancelOnSingleUnsubscribe(subscriber, call);
         try {
           Response<T> response = call.execute();
-          if (!subscriber.isUnsubscribed()) {
-            subscriber.onSuccess(response.data());
-          }
+          emitter.onSuccess(response.data());
         } catch (IOException e) {
           Exceptions.throwIfFatal(e);
-          if (!subscriber.isUnsubscribed()) {
-            subscriber.onError(e);
-          }
+          emitter.onError(e);
         }
       }
     });
-  }
-
-  private static <T> void cancelOnUnsubscribe(Subscriber<? super T> subscriber, final Cancelable toCancel) {
-    subscriber.add(Subscriptions.create(new Action0() {
-      @Override public void call() {
-        toCancel.cancel();
-      }
-    }));
-  }
-
-  private static <T> void cancelOnSingleUnsubscribe(SingleSubscriber<? super T> subscriber, final Cancelable toCancel) {
-    subscriber.add(Subscriptions.create(new Action0() {
-      @Override public void call() {
-        toCancel.cancel();
-      }
-    }));
   }
 }
