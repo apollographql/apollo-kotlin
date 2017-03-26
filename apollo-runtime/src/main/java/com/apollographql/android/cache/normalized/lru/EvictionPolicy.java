b59bc904082a8672f46502e3f05f4637663d31ed
Ben Schwab <ben.schwab@airbnb.com>
2017-03-25 21:14:48 -0700

    remove auto sizing option

--- apollo-runtime/src/main/java/com/apollographql/android/cache/normalized/lru/EvictionPolicy.java
+++ apollo-runtime/src/main/java/com/apollographql/android/cache/normalized/lru/EvictionPolicy.java
@@ -1,14 +1,8 @@
 package com.apollographql.android.cache.normalized.lru;
 
-import android.app.ActivityManager;
-import android.content.Context;
-
 import com.apollographql.android.api.graphql.internal.Optional;
 
 import java.util.concurrent.TimeUnit;
-
-import static android.content.Context.ACTIVITY_SERVICE;
-import static android.content.pm.ApplicationInfo.FLAG_LARGE_HEAP;
 
 public final class EvictionPolicy {
 
@@ -60,11 +54,6 @@ public final class EvictionPolicy {
     private Optional<Long> expireAfterWrite = Optional.absent();
     private Optional<TimeUnit> expireAfterWriteTimeUnit = Optional.absent();
 
-    public EvictionPolicy.Builder deviceBasedMaxSize(Context context) {
-      this.maxSizeBytes = Optional.of(calculateDeviceMemoryCacheSize(context));
-      return this;
-    }
-
     public EvictionPolicy.Builder maxSizeBytes(long maxSizeBytes) {
       this.maxSizeBytes = Optional.of(maxSizeBytes);
       return this;
@@ -92,17 +81,6 @@ public final class EvictionPolicy {
           expireAfterWrite, expireAfterWriteTimeUnit);
     }
 
-  }
-
-  /**
-   * Derived from https://github.com/square/picasso cache sizing.
-   */
-  private static long calculateDeviceMemoryCacheSize(Context context) {
-    ActivityManager activityManager = (ActivityManager) context.getSystemService(ACTIVITY_SERVICE);
-    boolean largeHeap = (context.getApplicationInfo().flags & FLAG_LARGE_HEAP) != 0;
-    int memoryClass = largeHeap ? activityManager.getLargeMemoryClass() : activityManager.getMemoryClass();
-    // Target ~15% of the available heap.
-    return (int) (1024L * 1024L * memoryClass / 7);
   }
 
   private EvictionPolicy(Optional<Long> maxSizeBytes, Optional<Long> maxEntries, Optional<Long> expireAfterAccess,
