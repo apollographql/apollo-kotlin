c9760a71b9bb56463cb8025f8c4021f86558c9fe
Ivan Savytskyi <ivan.savitsky@gmail.com>
2017-03-27 13:06:54 -0400

    Small fixes (#353)

--- apollo-rxsupport/src/main/java/com.apollographql.android.rx/RxApollo.java
+++ apollo-rxsupport/src/main/java/com.apollographql.android.rx/RxApollo.java
@@ -10,12 +10,14 @@ import java.io.IOException;
 
 import javax.annotation.Nonnull;
 
+import rx.Emitter;
 import rx.Observable;
 import rx.Single;
 import rx.SingleSubscriber;
-import rx.Subscriber;
 import rx.exceptions.Exceptions;
 import rx.functions.Action0;
+import rx.functions.Action1;
+import rx.functions.Cancellable;
 import rx.subscriptions.Subscriptions;
 
 import static com.apollographql.android.api.graphql.util.Utils.checkNotNull;
@@ -26,34 +28,37 @@ public final class RxApollo {
   }
 
   @Nonnull
-  public static <T> Observable<T> from(@Nonnull
-  final ApolloWatcher<T> watcher) {
-    checkNotNull(watcher);
-    return Observable.create(new Observable.OnSubscribe<T>() {
-      @Override public void call(final Subscriber<? super T> subscriber) {
-        cancelOnUnsubscribe(subscriber, watcher);
+  public static <T> Observable<T> from(@Nonnull final ApolloWatcher<T> watcher) {
+    return from(watcher, Emitter.BackpressureMode.LATEST);
+  }
+
+  @Nonnull public static <T> Observable<T> from(@Nonnull final ApolloWatcher<T> watcher,
+      @Nonnull Emitter.BackpressureMode backpressureMode) {
+    checkNotNull(backpressureMode, "backpressureMode == null");
+    checkNotNull(watcher, "watcher == null");
+    return Observable.create(new Action1<Emitter<T>>() {
+      @Override public void call(final Emitter<T> emitter) {
+        emitter.setCancellation(new Cancellable() {
+          @Override public void cancel() throws Exception {
+            watcher.cancel();
+          }
+        });
         watcher.enqueueAndWatch(new ApolloCall.Callback<T>() {
           @Override public void onResponse(@Nonnull Response<T> response) {
-            if (!subscriber.isUnsubscribed()) {
-              subscriber.onNext(response.data());
-            }
+            emitter.onNext(response.data());
           }
 
           @Override public void onFailure(@Nonnull Throwable e) {
             Exceptions.throwIfFatal(e);
-            if (!subscriber.isUnsubscribed()) {
-              subscriber.onError(e);
-            }
+            emitter.onError(e);
           }
         });
-
       }
-    });
+    }, backpressureMode);
   }
 
-  @Nonnull public static <T> Single<T> from(@Nonnull
-  final ApolloCall<T> call) {
-    checkNotNull(call);
+  @Nonnull public static <T> Single<T> from(@Nonnull final ApolloCall<T> call) {
+    checkNotNull(call, "call == null");
     return Single.create(new Single.OnSubscribe<T>() {
       @Override public void call(SingleSubscriber<? super T> subscriber) {
         cancelOnSingleUnsubscribe(subscriber, call);
@@ -70,14 +75,6 @@ public final class RxApollo {
         }
       }
     });
-  }
-
-  private static <T> void cancelOnUnsubscribe(Subscriber<? super T> subscriber, final Cancelable toCancel) {
-    subscriber.add(Subscriptions.create(new Action0() {
-      @Override public void call() {
-        toCancel.cancel();
-      }
-    }));
   }
 
   private static <T> void cancelOnSingleUnsubscribe(SingleSubscriber<? super T> subscriber, final Cancelable toCancel) {
