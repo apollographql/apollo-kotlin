1599bec71d1f1af21c0a51251a34dc2a3c50e403
Ivan Savytskyi <ivan.savitsky@gmail.com>
2017-03-28 16:30:36 -0400

    Response body empty when HttpException throw (#365)

--- apollo-runtime/src/main/java/com/apollographql/android/impl/util/HttpException.java
+++ apollo-runtime/src/main/java/com/apollographql/android/impl/util/HttpException.java
@@ -1,15 +1,37 @@
 package com.apollographql.android.impl.util;
 
+import javax.annotation.Nullable;
+
 import okhttp3.Response;
 
 public class HttpException extends RuntimeException {
-  private final okhttp3.Response rawResponse;
+  private final int code;
+  private final String message;
+  private final transient Response rawResponse;
 
-  public HttpException(okhttp3.Response rawResponse) {
+  public HttpException(@Nullable okhttp3.Response rawResponse) {
+    super(formatMessage(rawResponse));
+    this.code = rawResponse != null ? rawResponse.code() : 0;
+    this.message = rawResponse != null ? rawResponse.message() : "";
     this.rawResponse = rawResponse;
   }
 
-  public Response rawResponse() {
+  public int code() {
+    return code;
+  }
+
+  public String message() {
+    return message;
+  }
+
+  @Nullable public Response rawResponse() {
     return rawResponse;
+  }
+
+  private static String formatMessage(Response response) {
+    if (response == null) {
+      return "Empty HTTP response";
+    }
+    return "HTTP " + response.code() + " " + response.message();
   }
 }
---
+++ apollo-runtime/src/test/java/com/apollographql/android/impl/InterceptorTest.java
@@ -0,0 +1,65 @@
+package com.apollographql.android.impl;
+
+import com.apollographql.android.api.graphql.Query;
+import com.apollographql.android.api.graphql.ResponseFieldMapper;
+import com.apollographql.android.api.graphql.ResponseReader;
+import com.apollographql.android.impl.util.HttpException;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+
+import static com.google.common.truth.Truth.assertThat;
+import static junit.framework.Assert.fail;
+
+public class InterceptorTest {
+  private ApolloClient apolloClient;
+  @Rule public final MockWebServer server = new MockWebServer();
+
+  @Before public void setUp() {
+    apolloClient = ApolloClient.builder()
+        .serverUrl(server.url("/"))
+        .okHttpClient(new OkHttpClient.Builder().build())
+        .build();
+  }
+
+  @Test public void httpException() throws Exception {
+    server.enqueue(new MockResponse().setResponseCode(401).setBody("Unauthorized request!"));
+
+    try {
+      apolloClient.newCall(new Query() {
+        @Override public String queryDocument() {
+          return "";
+        }
+
+        @Override public Variables variables() {
+          return EMPTY_VARIABLES;
+        }
+
+        @Override public ResponseFieldMapper<Data> responseFieldMapper() {
+          return new ResponseFieldMapper<Data>() {
+            @Override public Data map(ResponseReader responseReader) throws IOException {
+              return null;
+            }
+          };
+        }
+
+        @Override public Object wrapData(Data data) {
+          return data;
+        }
+      }).execute();
+      fail("Expected HttpException");
+    } catch (HttpException httpException) {
+      assertThat(httpException.code()).isEqualTo(401);
+      assertThat(httpException.message()).isEqualTo("Client Error");
+      assertThat(httpException.rawResponse().body().string()).isEqualTo("Unauthorized request!");
+      assertThat(httpException.getMessage()).isEqualTo("HTTP 401 Client Error");
+    }
+  }
+}
