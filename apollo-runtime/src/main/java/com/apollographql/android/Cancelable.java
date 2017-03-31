e5aefb4e1fa24b8d4b65b0624c5fc2a92d0653b4
Vishesh <vishesh.2710@gmail.com>
2017-03-31 20:55:02 +0530

    Add docs for Cancelable interface

--- apollo-runtime/src/main/java/com/apollographql/android/Cancelable.java
+++ apollo-runtime/src/main/java/com/apollographql/android/Cancelable.java
@@ -1,5 +1,11 @@
 package com.apollographql.android;
 
+/**
+ * Represents an operation which can be cancelled.
+ */
 public interface Cancelable {
+  /**
+   * Cancels the operation.
+   */
   void cancel();
 }
