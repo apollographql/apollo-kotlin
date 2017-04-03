b500b814caec4cd97b7b74eb413396e1584810ad
Vishesh Vadhera <vishesh.2710@gmail.com>
2017-04-03 08:58:35 +0530

    Add requested docs change to ApolloPrefetch class

--- apollo-runtime/src/main/java/com/apollographql/apollo/ApolloPrefetch.java
+++ apollo-runtime/src/main/java/com/apollographql/apollo/ApolloPrefetch.java
@@ -12,13 +12,17 @@ import javax.annotation.Nullable;
 import okhttp3.Response;
 
 /**
- * <p>ApolloPrefetch is an abstraction for a request that has been prepared for execution. It fetches the graph
- * response from the server on successful completion but <b>doesn't</b> inflate the response into models. Instead it
- * stores the raw response in the request/response cache and defers the parsing to a later time.</p>
+ * <p>ApolloPrefetch is an abstraction for a request that has been prepared for execution. It represents a single
+ * request/response pair and cannot be executed twice, though it can be cancelled. It fetches the graph response from
+ * the server on successful completion but <b>doesn't</b> inflate the response into models. Instead it stores the raw
+ * response in the request/response cache and defers the parsing to a later time.</p>
  *
  *
- * <p>Use this object for use cases when the data needs to be fetched, but is not required for immediate
- * consumption. e.g.background update/syncing.</p>
+ * <p>Use this object for use cases when the data needs to be fetched, but is not required for immediate consumption.
+ * e.g.background update/syncing.</p>
+ *
+ * <p>Note: In order to execute the request again, call the {@link ApolloPrefetch#clone()} method which creates a new
+ * {@link ApolloPrefetch} object.</p>
  */
 public interface ApolloPrefetch extends Cancelable {
 
@@ -33,8 +37,6 @@ public interface ApolloPrefetch extends Cancelable {
 
   /**
    * Schedules the request to be executed at some point in the future.
-   * The dispatcher defines when the request will run: usually immediately unless there are several other requests
-   * currently being executed.
    *
    * @param callback Callback which will handle the success response or a failure exception
    * @throws IllegalStateException when the call has already been executed
@@ -42,8 +44,8 @@ public interface ApolloPrefetch extends Cancelable {
   @Nonnull ApolloPrefetch enqueue(@Nullable Callback callback);
 
   /**
-   * Creates a new, identical ApolloPrefetch to this one which can be enqueued or executed even if this one has
-   * already been executed.
+   * Creates a new, identical ApolloPrefetch to this one which can be enqueued or executed even if this one has already
+   * been executed.
    *
    * @return The cloned ApolloPrefetch object
    */
@@ -60,14 +62,13 @@ public interface ApolloPrefetch extends Cancelable {
     public abstract void onSuccess();
 
     /**
-     * Gets called when a network exception occurs while communicating with the server, or an unexpected
-     * exception occurs while creating the request or processing the response.
+     * Gets called when an unexpected exception occurs while creating the request or processing the response.
      */
     public abstract void onFailure(@Nonnull ApolloException e);
 
     /**
-     * Gets called when an http request error takes place either due to client error (status code >= 400) or due
-     * to server error (status code >= 500).
+     * Gets called when an http request error takes place. This is the case when the returned http status code doesn't
+     * lie in the range 200 (inclusive) and 300 (exclusive).
      */
     public void onHttpError(@Nonnull ApolloHttpException e) {
       onFailure(e);
