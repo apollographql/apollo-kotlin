ddf8946907b9e4c9511777738af0f2dd937a8cbf
Ben Schwab <ben.schwab@airbnb.com>
2017-04-10 11:30:29 -0700

    Fix in memory cache

--- apollo-integration/src/test/java/com/apollographql/apollo/InMemoryNormalizedCache.java
+++ apollo-integration/src/test/java/com/apollographql/apollo/InMemoryNormalizedCache.java
@@ -1,6 +1,7 @@
 package com.apollographql.apollo;
 
 import com.apollographql.apollo.cache.normalized.NormalizedCache;
+import com.apollographql.apollo.cache.normalized.NormalizedCacheFactory;
 import com.apollographql.apollo.cache.normalized.Record;
 import com.apollographql.apollo.cache.normalized.RecordSet;
 import com.apollographql.apollo.cache.normalized.sql.RecordFieldAdapter;
@@ -11,7 +12,8 @@ import java.util.Set;
 
 import javax.annotation.Nonnull;
 
-public final class InMemoryNormalizedCache extends NormalizedCache {
+public final class InMemoryNormalizedCache extends NormalizedCache
+    implements NormalizedCacheFactory<InMemoryNormalizedCache> {
 
   private RecordSet recordSet;
 
@@ -46,5 +48,9 @@ public final class InMemoryNormalizedCache extends NormalizedCache {
 
   public Collection<Record> allRecords() {
     return recordSet.allRecords();
+  }
+
+  @Override public InMemoryNormalizedCache createNormalizedCache(RecordFieldAdapter recordFieldAdapter) {
+    return new InMemoryNormalizedCache(recordFieldAdapter);
   }
 }
