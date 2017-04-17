4d8af528ecb8b5684d2a9f3dd6a09ca85e2c3a80
Vishesh Vadhera <vishesh.2710@gmail.com>
2017-04-17 23:54:35 +0530

    First test for Interceptor. Yay!

--- apollo-integration/src/test/java/com/apollographql/apollo/InterceptorTest.java
+++ apollo-integration/src/test/java/com/apollographql/apollo/InterceptorTest.java
@@ -1,30 +1,91 @@
 package com.apollographql.apollo;
 
 
+import com.apollographql.android.impl.normalizer.EpisodeHeroName;
+import com.apollographql.android.impl.normalizer.type.Episode;
+import com.apollographql.apollo.api.Operation;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.cache.normalized.Record;
 import com.apollographql.apollo.exception.ApolloException;
+import com.apollographql.apollo.interceptor.ApolloInterceptor;
+import com.apollographql.apollo.interceptor.ApolloInterceptorChain;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.concurrent.ExecutorService;
 
+import javax.annotation.Nonnull;
+
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
+import okhttp3.ResponseBody;
 import okhttp3.mockwebserver.MockWebServer;
+
+import static com.apollographql.apollo.interceptor.ApolloInterceptor.InterceptorResponse;
+import static com.google.common.truth.Truth.assertThat;
 
 public class InterceptorTest {
 
-  private ApolloClient apolloClient;
+  private ApolloClient client;
   private MockWebServer mockWebServer;
+  private OkHttpClient okHttpClient;
 
   @Before
   public void setup() {
     mockWebServer = new MockWebServer();
-    OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
+    okHttpClient = new OkHttpClient.Builder().build();
   }
 
   @Test
   public void applicationInterceptorCanShortCircuitResponses() throws IOException, ApolloException {
+    mockWebServer.shutdown();
 
+    EpisodeHeroName query = EpisodeHeroName.builder().episode(Episode.EMPIRE).build();
+
+    okhttp3.Request request = new okhttp3.Request.Builder()
+        .url(mockWebServer.url("/"))
+        .build();
+
+    okhttp3.Response okHttpResponse = new okhttp3.Response.Builder()
+        .request(request)
+        .protocol(Protocol.HTTP_2)
+        .code(200)
+        .message("Intercepted")
+        .body(ResponseBody.create(MediaType.parse("text/plain; charset=utf-8"), "dummy"))
+        .build();
+
+    Response<EpisodeHeroName.Data> apolloResponse = new Response<>(query);
+
+    final InterceptorResponse expectedResponse = new InterceptorResponse(okHttpResponse,
+        apolloResponse, Collections.<Record>emptyList());
+
+    client = ApolloClient.builder()
+        .serverUrl(mockWebServer.url("/"))
+        .okHttpClient(okHttpClient)
+        .applicationInterceptor(new ApolloInterceptor() {
+          @Nonnull @Override
+          public InterceptorResponse intercept(Operation operation, ApolloInterceptorChain chain) throws ApolloException {
+            return expectedResponse;
+          }
+
+          @Override
+          public void interceptAsync(@Nonnull Operation operation, @Nonnull ApolloInterceptorChain chain, @Nonnull ExecutorService dispatcher, @Nonnull CallBack callBack) {
+            //No op
+          }
+
+          @Override public void dispose() {
+            //No op
+          }
+        })
+        .build();
+
+    Response<EpisodeHeroName.Data> actualResponse = client.newCall(query).execute();
+
+    assertThat(expectedResponse.parsedResponse.get()).isEqualTo(actualResponse);
   }
 
 }
