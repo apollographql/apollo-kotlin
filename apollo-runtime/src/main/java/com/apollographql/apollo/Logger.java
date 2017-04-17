3feb9a2f9e32356192f351c28abbcf9052f1f3c5
Vishesh Vadhera <vishesh.2710@gmail.com>
2017-04-17 21:01:28 +0530

    Java Docs (#417)

--- apollo-runtime/src/main/java/com/apollographql/apollo/Logger.java
+++ apollo-runtime/src/main/java/com/apollographql/apollo/Logger.java
@@ -4,10 +4,21 @@ import com.apollographql.apollo.api.internal.Optional;
 
 import javax.annotation.Nonnull;
 
+/**
+ * Logger to use for logging by the {@link ApolloClient}
+ */
 public interface Logger {
   int DEBUG = 3;
   int WARN = 5;
   int ERROR = 6;
 
+  /**
+   * Logs the message to the appropriate channel (file, console, etc)
+   *
+   * @param priority the priority to set
+   * @param message message to log
+   * @param t Optional throwable to log
+   * @param args extra arguments to pass to the logged message.
+   */
   void log(int priority, @Nonnull String message, @Nonnull Optional<Throwable> t, @Nonnull Object... args);
 }
