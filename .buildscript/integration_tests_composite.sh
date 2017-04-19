e4236e84a98cb366f39a63dc5a9de084bd5d6944
Marwan <mrwnad@gmail.com>
2017-04-18 21:12:36 -0400

    cleanup shell script

--- .buildscript/integration_tests_composite.sh
+++ .buildscript/integration_tests_composite.sh
@@ -5,7 +5,24 @@
 # This is used for the integration tests to run against the project version of
 # the gradle plugin
 
-cp -fr ../gradle/composite/apollo-integration-build.gradle ../apollo-integration/build.gradle
-cp -fr ../gradle/composite/apollo-integration-settings.gradle ../apollo-integration/settings.gradle
-cp -fr ../gradle/composite/root-settings.gradle ../settings.gradle
+cd "${0%/*}" && cd ..
 
+# backup old build files
+echo "Backing up build scripts..."
+mv apollo-integration/build.gradle gradle/composite/apollo-integration-build-backup.gradle
+mv settings.gradle gradle/composite/root-settings-backup.gradle
+
+# switch apollo-integration to a composite build
+echo "Converting apollo-integration to a composite build..."
+cp gradle/composite/apollo-integration-build.gradle apollo-integration/build.gradle
+cp gradle/composite/apollo-integration-settings.gradle apollo-integration/settings.gradle
+cp gradle/composite/root-settings.gradle settings.gradle
+
+echo "Running integration tests"
+./gradlew -p apollo-integration build
+
+# restore files
+echo "Restoring old build scripts"
+mv gradle/composite/root-settings-backup.gradle settings.gradle
+mv gradle/composite/apollo-integration-build-backup.gradle apollo-integration/build.gradle
+rm apollo-integration/settings.gradle
