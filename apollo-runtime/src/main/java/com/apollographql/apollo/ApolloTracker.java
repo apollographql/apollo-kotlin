76780b331297cfc93fc36835ba98e9a4361c24a7
Vishesh <vishesh.2710@gmail.com>
2017-04-24 17:29:29 +0530

    Execute callback on calls finished

--- apollo-runtime/src/main/java/com/apollographql/apollo/ApolloTracker.java
+++ apollo-runtime/src/main/java/com/apollographql/apollo/ApolloTracker.java
@@ -8,18 +8,14 @@ import java.util.Deque;
 
 public class ApolloTracker {
 
-  private Runnable idleRunnable;
+  private Runnable idleCallback;
 
-  private final Deque<RealApolloCall> runningSyncCalls = new ArrayDeque<>();
+  private final Deque<ApolloCall> runningSyncCalls = new ArrayDeque<>();
   private final Deque<ApolloCall.Callback> runningAsyncCalls = new ArrayDeque<>();
-  private final Deque<RealApolloPrefetch> runningSyncPrefetches = new ArrayDeque<>();
+  private final Deque<ApolloPrefetch> runningSyncPrefetches = new ArrayDeque<>();
   private final Deque<ApolloPrefetch.Callback> runningAsyncPrefetches = new ArrayDeque<>();
 
   public ApolloTracker() {
-  }
-
-  public void execute(Runnable runnable) {
-
   }
 
   public void syncPrefetchInProgress(RealApolloPrefetch realApolloPrefetch) {
@@ -39,34 +35,71 @@ public class ApolloTracker {
   }
 
   public void syncPrefetchFinished(RealApolloPrefetch realApolloPrefetch) {
+    Runnable idleCallback;
+    int runningCallsCount;
     synchronized (this) {
       if (!runningSyncPrefetches.remove(realApolloPrefetch)) {
         throw new AssertionError("Prefetch call wasn't in progress");
       }
+      runningCallsCount = getRunningCallsCount();
+      idleCallback = this.idleCallback;
     }
+    executeCallBackIfCallsAreFinished(runningCallsCount, idleCallback);
   }
 
   public void syncCallFinished(RealApolloCall tRealApolloCall) {
+    Runnable idleCallback;
+    int runningCallsCount;
     synchronized (this) {
       if (!runningSyncCalls.remove(tRealApolloCall)) {
         throw new AssertionError("Call wasn't in progress");
       }
+      runningCallsCount = getRunningCallsCount();
+      idleCallback = this.idleCallback;
     }
+    executeCallBackIfCallsAreFinished(runningCallsCount, idleCallback);
   }
 
   public void asyncPrefetchFinished(ApolloPrefetch.Callback responseCallback) {
+    Runnable idleCallback;
+    int runningCallsCount;
     synchronized (this) {
       if (!runningAsyncPrefetches.remove(responseCallback)) {
         throw new AssertionError("Prefetch call wasn't in progress");
       }
+      runningCallsCount = getRunningCallsCount();
+      idleCallback = this.idleCallback;
     }
+    executeCallBackIfCallsAreFinished(runningCallsCount, idleCallback);
   }
 
   public void asyncCallFinished(ApolloCall.Callback callback) {
+    Runnable idleCallback;
+    int runningCallsCount;
     synchronized (this) {
       if (!runningAsyncCalls.remove(callback)) {
         throw new AssertionError("Call wasn't in progress");
       }
+      runningCallsCount = getRunningCallsCount();
+      idleCallback = this.idleCallback;
     }
+    executeCallBackIfCallsAreFinished(runningCallsCount, idleCallback);
+  }
+
+  public synchronized void setIdleCallback(Runnable idleCallback) {
+    this.idleCallback = idleCallback;
+  }
+
+  private void executeCallBackIfCallsAreFinished(int runningCallsCount, Runnable idleCallback) {
+    if (runningCallsCount == 0 && idleCallback != null) {
+      idleCallback.run();
+    }
+  }
+
+  private int getRunningCallsCount() {
+    return runningAsyncCalls.size() +
+        runningSyncCalls.size() +
+        runningSyncPrefetches.size() +
+        runningAsyncPrefetches.size();
   }
 }
