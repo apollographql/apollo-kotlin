853adac766b69fcd0ee17b4f6a598be9429cf262
Vishesh <vishesh.2710@gmail.com>
2017-05-04 13:01:57 +0530

    Fix imports

--- apollo-compiler/src/main/kotlin/com/apollographql/apollo/compiler/Annotations.kt
+++ apollo-compiler/src/main/kotlin/com/apollographql/apollo/compiler/Annotations.kt
@@ -4,15 +4,16 @@ import com.squareup.javapoet.AnnotationSpec
 import com.squareup.javapoet.CodeBlock
 import javax.annotation.Generated
 import javax.annotation.Nonnull
-import javax.annotation.Nullable
+import javax.annotation.Nullable;
+
 
 object Annotations {
-  val NULLABLE: com.squareup.javapoet.AnnotationSpec = com.squareup.javapoet.AnnotationSpec.builder(
-      javax.annotation.Nullable::class.java).build()
-  val NONNULL: com.squareup.javapoet.AnnotationSpec = com.squareup.javapoet.AnnotationSpec.builder(
-      javax.annotation.Nonnull::class.java).build()
-  val OVERRIDE: com.squareup.javapoet.AnnotationSpec = com.squareup.javapoet.AnnotationSpec.builder(Override::class.java).build()
-  val GENERATED_BY_APOLLO: com.squareup.javapoet.AnnotationSpec = com.squareup.javapoet.AnnotationSpec.builder(
-      javax.annotation.Generated::class.java)
-      .addMember("value", com.squareup.javapoet.CodeBlock.of("\$S", "Apollo GraphQL")).build()
+  val NULLABLE: AnnotationSpec = AnnotationSpec.builder(
+      Nullable::class.java).build()
+  val NONNULL: AnnotationSpec = AnnotationSpec.builder(
+      Nonnull::class.java).build()
+  val OVERRIDE: AnnotationSpec = AnnotationSpec.builder(Override::class.java).build()
+  val GENERATED_BY_APOLLO: AnnotationSpec = AnnotationSpec.builder(
+      Generated::class.java)
+      .addMember("value", CodeBlock.of("\$S", "Apollo GraphQL")).build()
 }
