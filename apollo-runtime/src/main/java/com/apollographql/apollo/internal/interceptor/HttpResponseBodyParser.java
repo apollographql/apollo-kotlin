43e6d8f05e9a32cc31e0eb0bbd94b5657ca90308
Ivan Savytskyi <ivan.savitsky@gmail.com>
2017-05-27 15:29:20 -0400

    Add custom GraphQL error attributes
    Some implementations of GraphQL server extends generic Error type and include custom attributes.
    One example is https://docs.scaphold.io/errors/ where error can be:
    ```
    {
      "data":{"loginUser":null},
      "errors":[{
        "message":"Could not find a user with that username",
        "locations":[{"line":1,"column":53}],
        "path":["loginUser"],
        "name":"GraphQLError"}
      ]}
    ```

--- apollo-runtime/src/main/java/com/apollographql/apollo/internal/interceptor/HttpResponseBodyParser.java
+++ apollo-runtime/src/main/java/com/apollographql/apollo/internal/interceptor/HttpResponseBodyParser.java
@@ -1,18 +1,21 @@
 package com.apollographql.apollo.internal.interceptor;
 
+import com.apollographql.apollo.CustomTypeAdapter;
 import com.apollographql.apollo.api.Error;
 import com.apollographql.apollo.api.Operation;
 import com.apollographql.apollo.api.Response;
 import com.apollographql.apollo.api.ResponseFieldMapper;
 import com.apollographql.apollo.api.ScalarType;
-import com.apollographql.apollo.CustomTypeAdapter;
 import com.apollographql.apollo.internal.cache.normalized.ResponseNormalizer;
 import com.apollographql.apollo.internal.field.MapFieldValueResolver;
 import com.apollographql.apollo.internal.json.BufferedSourceJsonReader;
-import com.apollographql.apollo.internal.reader.RealResponseReader;
 import com.apollographql.apollo.internal.json.ResponseJsonStreamReader;
+import com.apollographql.apollo.internal.reader.RealResponseReader;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -49,7 +52,7 @@ final class HttpResponseBodyParser<D extends Operation.Data, W> {
           //noinspection unchecked
           data = (D) responseStreamReader.nextObject(true, new ResponseJsonStreamReader.ObjectReader<Object>() {
             @Override public Object read(ResponseJsonStreamReader reader) throws IOException {
-              Map<String, Object> buffer = reader.buffer();
+              Map<String, Object> buffer = reader.toMap();
               RealResponseReader<Map<String, Object>> realResponseReader = new RealResponseReader<>(
                   operation.variables(), buffer, new MapFieldValueResolver(), customTypeAdapters,
                   networkResponseNormalizer);
@@ -63,7 +66,11 @@ final class HttpResponseBodyParser<D extends Operation.Data, W> {
         }
       }
       jsonReader.endObject();
-      return new Response<>(operation, operation.wrapData(data), errors, networkResponseNormalizer.dependentKeys());
+      return Response.<W>builder(operation)
+          .data(operation.wrapData(data))
+          .errors(errors)
+          .dependentKeys(networkResponseNormalizer.dependentKeys())
+          .build();
     } finally {
       if (jsonReader != null) {
         jsonReader.close();
@@ -83,42 +90,40 @@ final class HttpResponseBodyParser<D extends Operation.Data, W> {
     });
   }
 
-  private Error readError(ResponseJsonStreamReader reader) throws IOException {
+  @SuppressWarnings("unchecked") private Error readError(ResponseJsonStreamReader reader) throws IOException {
     String message = null;
-    List<Error.Location> locations = null;
-    while (reader.hasNext()) {
-      String name = reader.nextName();
-      if ("message".equals(name)) {
-        message = reader.nextString(false);
-      } else if ("locations".equals(name)) {
-        locations = reader.nextList(true, new ResponseJsonStreamReader.ListReader<Error.Location>() {
-          @Override public Error.Location read(ResponseJsonStreamReader reader) throws IOException {
-            return reader.nextObject(false, new ResponseJsonStreamReader.ObjectReader<Error.Location>() {
-              @Override public Error.Location read(ResponseJsonStreamReader reader) throws IOException {
-                return readErrorLocation(reader);
-              }
-            });
+    final List<Error.Location> locations = new ArrayList<>();
+    final Map<String, Object> customAttributes = new HashMap<>();
+    for (Map.Entry<String, Object> entry : reader.toMap().entrySet()) {
+      if ("message".equals(entry.getKey())) {
+        message = entry.getValue().toString();
+      } else if ("locations".equals(entry.getKey())) {
+        List<Map<String, Object>> locationItems = (List<Map<String, Object>>) entry.getValue();
+        if (locationItems != null) {
+          for (Map<String, Object> item : locationItems) {
+            locations.add(readErrorLocation(item));
           }
-        });
+        }
       } else {
-        reader.skipNext();
+        if (entry.getValue() != null) {
+          customAttributes.put(entry.getKey(), entry.getValue());
+        }
       }
     }
-    return new Error(message, locations);
+    return new Error(message, locations, customAttributes);
   }
 
   @SuppressWarnings("ConstantConditions")
-  private Error.Location readErrorLocation(ResponseJsonStreamReader reader) throws IOException {
+  private Error.Location readErrorLocation(Map<String, Object> data) throws IOException {
     long line = -1;
     long column = -1;
-    while (reader.hasNext()) {
-      String name = reader.nextName();
-      if ("line".equals(name)) {
-        line = reader.nextLong(false);
-      } else if ("column".equals(name)) {
-        column = reader.nextLong(false);
-      } else {
-        reader.skipNext();
+    if (data != null) {
+      for (Map.Entry<String, Object> entry : data.entrySet()) {
+        if ("line".equals(entry.getKey())) {
+          line = ((BigDecimal) entry.getValue()).longValue();
+        } else if ("column".equals(entry.getKey())) {
+          column = ((BigDecimal) entry.getValue()).longValue();
+        }
       }
     }
     return new Error.Location(line, column);
