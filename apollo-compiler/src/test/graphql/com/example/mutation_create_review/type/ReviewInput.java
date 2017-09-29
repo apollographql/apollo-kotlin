6530856c33dd55b67230e8d51472ea2eba6fd1f6
Ivan Savytskyi <ivan.savitsky@gmail.com>
2017-09-29 00:51:42 -0400

    Fix input list with default values generates non compiled code
    Part of #673

--- apollo-compiler/src/test/graphql/com/example/mutation_create_review/type/ReviewInput.java
+++ apollo-compiler/src/test/graphql/com/example/mutation_create_review/type/ReviewInput.java
@@ -9,6 +9,7 @@ import java.lang.Integer;
 import java.lang.Object;
 import java.lang.Override;
 import java.lang.String;
+import java.util.Arrays;
 import java.util.List;
 import javax.annotation.Generated;
 import javax.annotation.Nonnull;
@@ -32,10 +33,15 @@ public final class ReviewInput {
 
   private final Input<List<Episode>> listOfEnums;
 
+  private final Input<List<Integer>> listOfInt;
+
+  private final Input<List<String>> listOfString;
+
   ReviewInput(int stars, Input<Integer> nullableIntFieldWithDefaultValue, Input<String> commentary,
       @Nonnull ColorInput favoriteColor, Input<Episode> enumWithDefaultValue,
       Input<Episode> nullableEnum, Input<List<Object>> listOfCustomScalar,
-      Input<List<Episode>> listOfEnums) {
+      Input<List<Episode>> listOfEnums, Input<List<Integer>> listOfInt,
+      Input<List<String>> listOfString) {
     this.stars = stars;
     this.nullableIntFieldWithDefaultValue = nullableIntFieldWithDefaultValue;
     this.commentary = commentary;
@@ -44,6 +50,8 @@ public final class ReviewInput {
     this.nullableEnum = nullableEnum;
     this.listOfCustomScalar = listOfCustomScalar;
     this.listOfEnums = listOfEnums;
+    this.listOfInt = listOfInt;
+    this.listOfString = listOfString;
   }
 
   /**
@@ -102,6 +110,20 @@ public final class ReviewInput {
     return this.listOfEnums.value;
   }
 
+  /**
+   * for test purpose only
+   */
+  public @Nullable List<Integer> listOfInt() {
+    return this.listOfInt.value;
+  }
+
+  /**
+   * for test purpose only
+   */
+  public @Nullable List<String> listOfString() {
+    return this.listOfString.value;
+  }
+
   public static Builder builder() {
     return new Builder();
   }
@@ -144,6 +166,26 @@ public final class ReviewInput {
             }
           } : null);
         }
+        if (listOfInt.defined) {
+          writer.writeList("listOfInt", listOfInt.value != null ? new InputFieldWriter.ListWriter() {
+            @Override
+            public void write(InputFieldWriter.ListItemWriter listItemWriter) throws IOException {
+              for (Integer $item : listOfInt.value) {
+                listItemWriter.writeInt($item);
+              }
+            }
+          } : null);
+        }
+        if (listOfString.defined) {
+          writer.writeList("listOfString", listOfString.value != null ? new InputFieldWriter.ListWriter() {
+            @Override
+            public void write(InputFieldWriter.ListItemWriter listItemWriter) throws IOException {
+              for (String $item : listOfString.value) {
+                listItemWriter.writeString($item);
+              }
+            }
+          } : null);
+        }
       }
     };
   }
@@ -164,6 +206,10 @@ public final class ReviewInput {
     private Input<List<Object>> listOfCustomScalar = Input.absent();
 
     private Input<List<Episode>> listOfEnums = Input.absent();
+
+    private Input<List<Integer>> listOfInt = Input.fromNullable(Arrays.<Integer>asList(1, 2, 3));
+
+    private Input<List<String>> listOfString = Input.fromNullable(Arrays.<String>asList("test1", "test2", "test3"));
 
     Builder() {
     }
@@ -232,9 +278,25 @@ public final class ReviewInput {
       return this;
     }
 
+    /**
+     * for test purpose only
+     */
+    public Builder listOfInt(@Nullable List<Integer> listOfInt) {
+      this.listOfInt = Input.fromNullable(listOfInt);
+      return this;
+    }
+
+    /**
+     * for test purpose only
+     */
+    public Builder listOfString(@Nullable List<String> listOfString) {
+      this.listOfString = Input.fromNullable(listOfString);
+      return this;
+    }
+
     public ReviewInput build() {
       Utils.checkNotNull(favoriteColor, "favoriteColor == null");
-      return new ReviewInput(stars, nullableIntFieldWithDefaultValue, commentary, favoriteColor, enumWithDefaultValue, nullableEnum, listOfCustomScalar, listOfEnums);
+      return new ReviewInput(stars, nullableIntFieldWithDefaultValue, commentary, favoriteColor, enumWithDefaultValue, nullableEnum, listOfCustomScalar, listOfEnums, listOfInt, listOfString);
     }
   }
 }
