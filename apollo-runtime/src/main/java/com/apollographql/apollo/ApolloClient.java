f21655c4c7476ac9be80dc4c975e2893564b35ae
Abdul Hannan Kanji <hannanabdul55@gmail.com>
2017-10-24 16:04:03 -0700

    Fixed another typo in the method

--- apollo-runtime/src/main/java/com/apollographql/apollo/ApolloClient.java
+++ apollo-runtime/src/main/java/com/apollographql/apollo/ApolloClient.java
@@ -464,15 +464,15 @@ public final class ApolloClient implements ApolloQueryCall.Factory, ApolloMutati
     }
 
     private static okhttp3.Call.Factory addHttpCacheInterceptorIfNeeded(Call.Factory callFactory,
-        Interceptor httCacheInterceptor) {
+        Interceptor httpCacheInterceptor) {
       if (callFactory instanceof OkHttpClient) {
         OkHttpClient client = (OkHttpClient) callFactory;
         for (Interceptor interceptor : client.interceptors()) {
-          if (interceptor.getClass().equals(httCacheInterceptor.getClass())) {
+          if (interceptor.getClass().equals(httpCacheInterceptor.getClass())) {
             return callFactory;
           }
         }
-        return client.newBuilder().addInterceptor(httCacheInterceptor).build();
+        return client.newBuilder().addInterceptor(httpCacheInterceptor).build();
       }
       return callFactory;
     }
