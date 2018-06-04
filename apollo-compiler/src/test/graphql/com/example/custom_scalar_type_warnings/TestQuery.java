ae77d49bf4fa9e2a7efe8b7ee06db12521dd0394
Chris Dawson <cdawson@ebay.com>
2018-06-04 16:05:48 -0700

    Fix wrong directory structure for tests and comparison file

--- apollo-compiler/src/test/graphql/com/example/custom_scalar_type_warnings/TestQuery.java
+++ apollo-compiler/src/test/graphql/com/example/custom_scalar_type_warnings/TestQuery.java
@@ -21,8 +21,8 @@ import org.jetbrains.annotations.Nullable;
 @Generated("Apollo GraphQL")
 public final class TestQuery implements Query<TestQuery.Data, Optional<TestQuery.Data>, Operation.Variables> {
   public static final String OPERATION_DEFINITION = "query TestQuery {\n"
-    + "  links\n"
-    + "}";
+      + "  links\n"
+      + "}";
 
   public static final String OPERATION_ID = "261d102d2c930a0f9f444325bd349452a77a8c138cc1278825eb48971a8b88cf";
 
