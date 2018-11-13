d74c099944185f8732bb49e356ab0d0e86a80aaf
Ivan Savytskyi <ivan.savitsky@gmail.com>
2018-11-13 00:32:32 -0500

    Prepare next development version

--- apollo-compiler/src/test/graphql/com/example/mutation_create_review_semantic_naming/type/ReviewInput.java
+++ apollo-compiler/src/test/graphql/com/example/mutation_create_review_semantic_naming/type/ReviewInput.java
@@ -472,7 +472,8 @@ public final class ReviewInput implements InputType {
     /**
      * for test purpose only
      */
-    public Builder nullableIntFieldWithDefaultValue(@Nullable Integer nullableIntFieldWithDefaultValue) {
+    public Builder nullableIntFieldWithDefaultValue(
+        @Nullable Integer nullableIntFieldWithDefaultValue) {
       this.nullableIntFieldWithDefaultValue = Input.fromNullable(nullableIntFieldWithDefaultValue);
       return this;
     }
@@ -600,7 +601,8 @@ public final class ReviewInput implements InputType {
     /**
      * for test purpose only
      */
-    public Builder nullableIntFieldWithDefaultValueInput(@NotNull Input<Integer> nullableIntFieldWithDefaultValue) {
+    public Builder nullableIntFieldWithDefaultValueInput(
+        @NotNull Input<Integer> nullableIntFieldWithDefaultValue) {
       this.nullableIntFieldWithDefaultValue = Utils.checkNotNull(nullableIntFieldWithDefaultValue, "nullableIntFieldWithDefaultValue == null");
       return this;
     }
@@ -704,7 +706,8 @@ public final class ReviewInput implements InputType {
     /**
      * for test purpose only
      */
-    public Builder listOfListOfObjectInput(@NotNull Input<List<List<ColorInput>>> listOfListOfObject) {
+    public Builder listOfListOfObjectInput(
+        @NotNull Input<List<List<ColorInput>>> listOfListOfObject) {
       this.listOfListOfObject = Utils.checkNotNull(listOfListOfObject, "listOfListOfObject == null");
       return this;
     }
