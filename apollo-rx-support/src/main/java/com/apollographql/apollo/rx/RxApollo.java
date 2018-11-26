37b9389f2736e132612720017c2ecfd639b0f0e2
OH JAE HWAN <ojh102@gmail.com>
2018-11-27 01:00:41 +0900

    Fix incorrect javadoc for RxApollo (#1122)

--- apollo-rx-support/src/main/java/com/apollographql/apollo/rx/RxApollo.java
+++ apollo-rx-support/src/main/java/com/apollographql/apollo/rx/RxApollo.java
@@ -174,11 +174,11 @@ public final class RxApollo {
   }
 
   /**
-   * Converts an {@link ApolloStoreOperation} to a Observable.
+   * Converts an {@link ApolloStoreOperation} to a Single.
    *
    * @param operation        the ApolloStoreOperation to convert
    * @param <T>              the value type
-   * @return the converted Observable
+   * @return the converted Single
    */
   @NotNull public static <T> Single<T> from(@NotNull final ApolloStoreOperation<T> operation) {
     checkNotNull(operation, "operation == null");
