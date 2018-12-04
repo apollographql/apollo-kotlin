3d6791bd1e32e1dba2880ad3131cba9ba65d02b8
EduardoExposito <eduardo.exposito@talea.de>
2018-12-04 08:40:24 +0000

    Fix identation

--- apollo-api/src/main/java/com/apollographql/apollo/api/Input.java
+++ apollo-api/src/main/java/com/apollographql/apollo/api/Input.java
@@ -14,68 +14,68 @@ import org.jetbrains.annotations.Nullable;
  * @param <V> the type of instance that can be contained
  */
 public final class Input<V> {
-    @Nullable
-    public final V value;
-    public final boolean defined;
+  @Nullable
+  public final V value;
+  public final boolean defined;
 
-    private Input(V value, boolean defined) {
-        this.value = value;
-        this.defined = defined;
+  private Input(V value, boolean defined) {
+    this.value = value;
+    this.defined = defined;
+  }
+
+  /**
+   * Creates a new {@link Input} instance that is defined in case if {@code value} is not-null
+   * and undefined otherwise.
+   *
+   * @param value to be wrapped
+   * @return a new {@link Input} instance
+   */
+  public static <V> Input<V> optional(@Nullable V value) {
+    if (value == null) {
+      return absent();
+    } else {
+      return fromNullable(value);
+    }
+  }
+
+  /**
+   * Creates a new {@link Input} instance that is always defined.
+   *
+   * @param value to be wrapped
+   * @return a new {@link Input} instance
+   */
+  public static <V> Input<V> fromNullable(@Nullable V value) {
+    return new Input<>(value, true);
+  }
+
+  /**
+   * Creates a new {@link Input} instance that is always undefined.
+   *
+   * @param value to be wrapped
+   * @return a new {@link Input} instance
+   */
+  public static <V> Input<V> absent() {
+    return new Input<>(null, false);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Input)) {
+      return false;
     }
 
-    /**
-     * Creates a new {@link Input} instance that is defined in case if {@code value} is not-null
-     * and undefined otherwise.
-     *
-     * @param value to be wrapped
-     * @return a new {@link Input} instance
-     */
-    public static <V> Input<V> optional(@Nullable V value) {
-        if (value == null) {
-            return absent();
-        } else {
-            return fromNullable(value);
-        }
-    }
+    Input<?> input = (Input<?>) o;
+    return defined == input.defined && (value != null && value.equals(input.value) ||
+            value == null && input.value == null);
+  }
 
-    /**
-     * Creates a new {@link Input} instance that is always defined.
-     *
-     * @param value to be wrapped
-     * @return a new {@link Input} instance
-     */
-    public static <V> Input<V> fromNullable(@Nullable V value) {
-        return new Input<>(value, true);
-    }
-
-    /**
-     * Creates a new {@link Input} instance that is always undefined.
-     *
-     * @param value to be wrapped
-     * @return a new {@link Input} instance
-     */
-    public static <V> Input<V> absent() {
-        return new Input<>(null, false);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Input)) {
-            return false;
-        }
-
-        Input<?> input = (Input<?>) o;
-        return defined == input.defined &&
-                (value != null && value.equals(input.value) || value == null && input.value == null);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = value != null ? value.hashCode() : 0;
-        result = 31 * result + (defined ? 1 : 0);
-        return result;
-    }
+  @Override
+  public int hashCode() {
+    int result = value != null ? value.hashCode() : 0;
+    result = 31 * result + (defined ? 1 : 0);
+    return result;
+  }
 }
