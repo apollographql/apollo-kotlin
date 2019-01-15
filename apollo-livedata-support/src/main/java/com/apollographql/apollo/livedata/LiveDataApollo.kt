df9f243ed10697f5fecf1531bfb0699756de87dc
skydoves <skydoves@naver.com>
2019-01-16 02:45:35 +0900

    removed AtomicBoolean on LiveDataApollo

--- apollo-livedata-support/src/main/java/com/apollographql/apollo/livedata/LiveDataApollo.kt
+++ apollo-livedata-support/src/main/java/com/apollographql/apollo/livedata/LiveDataApollo.kt
@@ -11,7 +11,6 @@ import com.apollographql.apollo.api.Response
 import com.apollographql.apollo.api.internal.Utils.checkNotNull
 import com.apollographql.apollo.cache.normalized.ApolloStoreOperation
 import com.apollographql.apollo.exception.ApolloException
-import java.util.concurrent.atomic.AtomicBoolean
 
 /**
  * The LiveDataApollo class provides methods for converting ApolloCall, ApolloPrefetch and ApolloWatcher types to LiveData
@@ -30,32 +29,17 @@ internal object LiveDataApollo {
     fun <T> from(@NonNull watcher: ApolloQueryWatcher<T>): LiveData<ApolloLiveDataResponse<T>> {
         checkNotNull(watcher, "watcher == null")
         return object : LiveData<ApolloLiveDataResponse<T>>() {
-            var started = AtomicBoolean(false)
-
-            @MainThread
-            override fun setValue(value: ApolloLiveDataResponse<T>?) {
-                started.set(true)
-                super.setValue(value)
-            }
-
-            override fun postValue(value: ApolloLiveDataResponse<T>?) {
-                started.set(true)
-                super.postValue(value)
-            }
-
             override fun onActive() {
                 super.onActive()
-                if (started.compareAndSet(false, true)) {
-                    watcher.enqueueAndWatch(object : ApolloCall.Callback<T>() {
-                        override fun onResponse(response: Response<T>) {
-                            postValue(ApolloLiveDataResponse.of { response })
-                        }
+                watcher.enqueueAndWatch(object : ApolloCall.Callback<T>() {
+                    override fun onResponse(response: Response<T>) {
+                        postValue(ApolloLiveDataResponse.of { response })
+                    }
 
-                        override fun onFailure(e: ApolloException) {
-                            postValue(ApolloLiveDataResponse.Failure.Exception(e))
-                        }
-                    })
-                }
+                    override fun onFailure(e: ApolloException) {
+                        postValue(ApolloLiveDataResponse.Failure.Exception(e))
+                    }
+                })
             }
         }
     }
@@ -72,38 +56,23 @@ internal object LiveDataApollo {
     fun <T> from(@NonNull call: ApolloCall<T>): LiveData<ApolloLiveDataResponse<T>> {
         checkNotNull(call, "call == null")
         return object : LiveData<ApolloLiveDataResponse<T>>() {
-            var started = AtomicBoolean(false)
-
-            @MainThread
-            override fun setValue(value: ApolloLiveDataResponse<T>?) {
-                started.set(true)
-                super.setValue(value)
-            }
-
-            override fun postValue(value: ApolloLiveDataResponse<T>?) {
-                started.set(true)
-                super.postValue(value)
-            }
-
             override fun onActive() {
                 super.onActive()
-                if (started.compareAndSet(false, true)) {
-                    call.enqueue(object : ApolloCall.Callback<T>() {
-                        override fun onResponse(response: Response<T>) {
-                            postValue(ApolloLiveDataResponse.of { response })
-                        }
+                call.enqueue(object : ApolloCall.Callback<T>() {
+                    override fun onResponse(response: Response<T>) {
+                        postValue(ApolloLiveDataResponse.of { response })
+                    }
 
-                        override fun onFailure(e: ApolloException) {
-                            postValue(ApolloLiveDataResponse.Failure.Exception(e))
-                        }
+                    override fun onFailure(e: ApolloException) {
+                        postValue(ApolloLiveDataResponse.Failure.Exception(e))
+                    }
 
-                        override fun onStatusEvent(event: ApolloCall.StatusEvent) {
-                            if (event == ApolloCall.StatusEvent.COMPLETED) {
-                                postValue(ApolloLiveDataResponse.Complete())
-                            }
+                    override fun onStatusEvent(event: ApolloCall.StatusEvent) {
+                        if (event == ApolloCall.StatusEvent.COMPLETED) {
+                            postValue(ApolloLiveDataResponse.Complete())
                         }
-                    })
-                }
+                    }
+                })
             }
         }
     }
@@ -118,22 +87,23 @@ internal object LiveDataApollo {
     fun <T> from(@NonNull prefetch: ApolloPrefetch): LiveData<ApolloLiveDataResponse<T>> {
         checkNotNull(prefetch, "prefetch == null")
         return object : LiveData<ApolloLiveDataResponse<T>>() {
-            var started = AtomicBoolean(false)
+            var started = false
 
             @MainThread
             override fun setValue(value: ApolloLiveDataResponse<T>?) {
-                started.set(true)
+                started = true
                 super.setValue(value)
             }
 
             override fun postValue(value: ApolloLiveDataResponse<T>?) {
-                started.set(true)
+                started = true
                 super.postValue(value)
             }
 
             override fun onActive() {
                 super.onActive()
-                if (started.compareAndSet(false, true)) {
+                if (!started) {
+                    started = true
                     prefetch.enqueue(object : ApolloPrefetch.Callback() {
                         override fun onSuccess() {
                             postValue(ApolloLiveDataResponse.Complete())
@@ -158,36 +128,21 @@ internal object LiveDataApollo {
     fun <T> from(@NonNull call: ApolloSubscriptionCall<T>): LiveData<ApolloLiveDataResponse<T>> {
         checkNotNull(call, "call == null")
         return object : LiveData<ApolloLiveDataResponse<T>>() {
-            var started = AtomicBoolean(false)
-
-            @MainThread
-            override fun setValue(value: ApolloLiveDataResponse<T>?) {
-                started.set(true)
-                super.setValue(value)
-            }
-
-            override fun postValue(value: ApolloLiveDataResponse<T>?) {
-                started.set(true)
-                super.postValue(value)
-            }
-
             override fun onActive() {
                 super.onActive()
-                if (started.compareAndSet(false, true)) {
-                    call.execute(object : ApolloSubscriptionCall.Callback<T> {
-                        override fun onResponse(response: Response<T>) {
-                            postValue(ApolloLiveDataResponse.of { response })
-                        }
+                call.execute(object : ApolloSubscriptionCall.Callback<T> {
+                    override fun onResponse(response: Response<T>) {
+                        postValue(ApolloLiveDataResponse.of { response })
+                    }
 
-                        override fun onFailure(e: ApolloException) {
-                            postValue(ApolloLiveDataResponse.Failure.Exception(e))
-                        }
+                    override fun onFailure(e: ApolloException) {
+                        postValue(ApolloLiveDataResponse.Failure.Exception(e))
+                    }
 
-                        override fun onCompleted() {
-                            postValue(ApolloLiveDataResponse.Complete())
-                        }
-                    })
-                }
+                    override fun onCompleted() {
+                        postValue(ApolloLiveDataResponse.Complete())
+                    }
+                })
             }
         }
     }
@@ -202,39 +157,34 @@ internal object LiveDataApollo {
     fun <T> from(@NonNull operation: ApolloStoreOperation<T>): LiveData<T?> {
         checkNotNull(operation, "operation == null")
         return object : LiveData<T?>() {
-            var started = AtomicBoolean(false)
-            var onPosted = AtomicBoolean(false)
+            var onPosted = false
 
             @MainThread
             override fun setValue(value: T?) {
-                started.set(true)
-                if (!onPosted.get()) {
+                if (!onPosted) {
                     super.setValue(value)
                 }
             }
 
             override fun postValue(value: T?) {
-                started.set(true)
-                if (!onPosted.get()) {
+                if (!onPosted) {
                     super.postValue(value)
                 }
             }
 
             override fun onActive() {
                 super.onActive()
-                if (started.compareAndSet(false, true)) {
-                    operation.enqueue(object : ApolloStoreOperation.Callback<T> {
-                        override fun onSuccess(result: T) {
-                            postValue(result)
-                            onPosted.set(true)
-                        }
+                operation.enqueue(object : ApolloStoreOperation.Callback<T> {
+                    override fun onSuccess(result: T) {
+                        postValue(result)
+                        onPosted = true
+                    }
 
-                        override fun onFailure(t: Throwable?) {
-                            postValue(null)
-                            onPosted.set(true)
-                        }
-                    })
-                }
+                    override fun onFailure(t: Throwable?) {
+                        postValue(null)
+                        onPosted = true
+                    }
+                })
             }
         }
     }
