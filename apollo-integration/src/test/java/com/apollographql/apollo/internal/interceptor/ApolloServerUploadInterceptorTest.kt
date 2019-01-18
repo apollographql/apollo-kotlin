4a0fed9c174b263f42ab3f42293b2c99f02790ed
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2019-01-18 15:31:55 +0700

    better integration test

--- apollo-integration/src/test/java/com/apollographql/apollo/internal/interceptor/ApolloServerUploadInterceptorTest.kt
+++ apollo-integration/src/test/java/com/apollographql/apollo/internal/interceptor/ApolloServerUploadInterceptorTest.kt
@@ -3,7 +3,8 @@ package com.apollographql.apollo.internal.interceptor
 import com.apollographql.apollo.api.GraphqlUpload
 import com.apollographql.apollo.api.internal.Optional
 import com.apollographql.apollo.cache.CacheHeaders
-import com.apollographql.apollo.integration.interceptor.UploadQuery
+import com.apollographql.apollo.integration.interceptor.UploadFileExampleMutation
+import com.apollographql.apollo.integration.interceptor.type.NestedObject
 import com.apollographql.apollo.internal.ApolloLogger
 import com.apollographql.apollo.response.ScalarTypeAdapters
 import com.google.common.base.Predicate
@@ -34,9 +35,24 @@ fun createGraphqlUpload(fileName: String, content: String, mimeType: String): Gr
 
 class ApolloServerUploadInterceptorTest {
     private val serverUrl = HttpUrl.parse("http://google.com")
-    private val file = createGraphqlUpload("test1.txt", "content_testOne", "text/plain")
-    private val query = UploadQuery.builder()
-            .file(file)
+    private val file0 = createGraphqlUpload("test0.txt", "content_testZero", "text/plain")
+    private val file1 = createGraphqlUpload("test1.png", "content_testOne", "image/png")
+    private val file2 = createGraphqlUpload("test2.jpg", "content_test2", "image/jpg")
+    private val nestedObject0 = NestedObject.builder()
+            .file(file0)
+            .fileList(listOf(file1, file2))
+            .build()
+    private val nestedObject1 = NestedObject.builder()
+            .file(file1)
+            .fileList(listOf(file0, file2))
+            .build()
+    private val nestedObject2 = NestedObject.builder()
+            .file(file2)
+            .fileList(listOf(file0, file1))
+            .recursiveNested(listOf(nestedObject0, nestedObject1))
+            .build()
+    private val query = UploadFileExampleMutation.builder()
+            .nested(nestedObject2)
             .build()
 
     @Test
@@ -47,18 +63,66 @@ class ApolloServerUploadInterceptorTest {
 ----graphql-multipart-upload-boundary-85763456--
 Content-Disposition: form-data; name="operations"
 Content-Type: application/json; charset=utf-8
-Content-Length: 129
+Content-Length: 290
 
-{"query":"query UploadQuery(${'$'}file: Upload) {  uploadQuery(file: ${'$'}file)}","operationName":"UploadQuery","variables":{"file":null}}
+{"query":"mutation uploadFileExample(${'$'}nested: NestedObject) {  uploadFileExample(nested: ${'$'}nested)}","operationName":"uploadFileExample","variables":{"nested":{"recursiveNested":[{"file":null,"fileList":[null,null]},{"file":null,"fileList":[null,null]}],"file":null,"fileList":[null,null]}}}
 ----graphql-multipart-upload-boundary-85763456--
 Content-Disposition: form-data; name="map"
 Content-Type: application/json; charset=utf-8
-Content-Length: 24
+Content-Length: 415
 
-{"0":["variables.file"]}
+{"0":["variables.nested.recursiveNested.0.file"],"1":["variables.nested.recursiveNested.0.fileList.0"],"2":["variables.nested.recursiveNested.0.fileList.1"],"3":["variables.nested.recursiveNested.1.file"],"4":["variables.nested.recursiveNested.1.fileList.0"],"5":["variables.nested.recursiveNested.1.fileList.1"],"6":["variables.nested.file"],"7":["variables.nested.fileList.0"],"8":["variables.nested.fileList.1"]}
 ----graphql-multipart-upload-boundary-85763456--
-Content-Disposition: form-data; name="0"; filename="test1.txt"
+Content-Disposition: form-data; name="0"; filename="test0.txt"
 Content-Type: text/plain
+Content-Length: 16
+
+content_testZero
+----graphql-multipart-upload-boundary-85763456--
+Content-Disposition: form-data; name="1"; filename="test1.png"
+Content-Type: image/png
+Content-Length: 15
+
+content_testOne
+----graphql-multipart-upload-boundary-85763456--
+Content-Disposition: form-data; name="2"; filename="test2.jpg"
+Content-Type: image/jpg
+Content-Length: 13
+
+content_test2
+----graphql-multipart-upload-boundary-85763456--
+Content-Disposition: form-data; name="3"; filename="test1.png"
+Content-Type: image/png
+Content-Length: 15
+
+content_testOne
+----graphql-multipart-upload-boundary-85763456--
+Content-Disposition: form-data; name="4"; filename="test0.txt"
+Content-Type: text/plain
+Content-Length: 16
+
+content_testZero
+----graphql-multipart-upload-boundary-85763456--
+Content-Disposition: form-data; name="5"; filename="test2.jpg"
+Content-Type: image/jpg
+Content-Length: 13
+
+content_test2
+----graphql-multipart-upload-boundary-85763456--
+Content-Disposition: form-data; name="6"; filename="test2.jpg"
+Content-Type: image/jpg
+Content-Length: 13
+
+content_test2
+----graphql-multipart-upload-boundary-85763456--
+Content-Disposition: form-data; name="7"; filename="test0.txt"
+Content-Type: text/plain
+Content-Length: 16
+
+content_testZero
+----graphql-multipart-upload-boundary-85763456--
+Content-Disposition: form-data; name="8"; filename="test1.png"
+Content-Type: image/png
 Content-Length: 15
 
 content_testOne
