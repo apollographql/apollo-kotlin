e9d89a55bb5cf98e567f1526ea3caac87798d456
gshcherb <gshcherb@gmail.com>
2019-03-04 14:19:14 -0800

    Added simple unit test to validate missing and null data field in payload

---
+++ apollo-runtime/src/test/java/com/apollographql/apollo/response/OperationResponseParserTest.java
@@ -0,0 +1,33 @@
+package com.apollographql.apollo.response;
+
+import com.apollographql.apollo.api.Operation;
+import com.apollographql.apollo.api.ResponseFieldMapper;
+import com.apollographql.apollo.api.ScalarType;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.Mockito.mock;
+
+public class OperationResponseParserTest {
+
+    abstract class TestOperation implements Operation<Operation.Data, String, Operation.Variables> {
+    }
+
+    @Test
+    public void validateNullDataFieldInPayload() {
+        OperationResponseParser<Operation.Data, String> parser = new OperationResponseParser<>(
+                mock(TestOperation.class),
+                mock(ResponseFieldMapper.class),
+                new ScalarTypeAdapters(new HashMap<ScalarType, CustomTypeAdapter>())
+        );
+
+        Map<String, Object> payload = new HashMap<>();
+        parser.parse(payload);
+
+        payload.put("data", null);
+        parser.parse(payload);
+    }
+}
