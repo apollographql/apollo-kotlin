sav007/apollo-android

Add first implementation of Kotlin code gen

apollo-compiler/src/test/graphql/com/example/hero_details_nullable/TestQuery.kt
@@ -1,0 +1,235 @@
+package com.example.hero_details_nullable
+
+import com.apollographql.apollo.api.Operation
+import com.apollographql.apollo.api.OperationName
+import com.apollographql.apollo.api.Query
+import com.apollographql.apollo.api.ResponseField
+import com.apollographql.apollo.api.ResponseFieldMapper
+import com.apollographql.apollo.api.ResponseFieldMarshaller
+import com.apollographql.apollo.api.ResponseReader
+import javax.annotation.Generated
+import kotlin.Array
+import kotlin.Int
+import kotlin.String
+import kotlin.Suppress
+import kotlin.collections.List
+
+@Generated("Apollo GraphQL")
+class TestQuery : Query<TestQuery.Data, TestQuery.Data, Operation.Variables> {
+    override fun operationId(): String = OPERATION_ID
+    override fun queryDocument(): String = QUERY_DOCUMENT
+    override fun wrapData(data: TestQuery.Data): TestQuery.Data = data
+    override fun variables(): Operation.Variables = Operation.EMPTY_VARIABLES
+    override fun name(): OperationName = OPERATION_NAME
+    override fun responseFieldMapper(): ResponseFieldMapper<TestQuery.Data> = ResponseFieldMapper {
+        TestQuery.Data(it)
+    }
+
+    /**
+     * @param name The name of the character */
+    data class Node(
+        val __typename: String,
+        val name: String
+    ) {
+        fun marshaller(): ResponseFieldMarshaller = ResponseFieldMarshaller {
+            it.writeString(RESPONSE_FIELDS[0], __typename)
+            it.writeString(RESPONSE_FIELDS[1], name)
+        }
+
+        companion object {
+            private val RESPONSE_FIELDS: Array<ResponseField> = arrayOf(
+                        ResponseField.forString("__typename", "__typename", null, false, null),
+                        ResponseField.forString("name", "name", null, false, null)
+                    )
+
+            operator fun invoke(reader: ResponseReader): Node {
+                val __typename = reader.readString(RESPONSE_FIELDS[0])
+                val name = reader.readString(RESPONSE_FIELDS[1])
+                return Node(
+                    __typename = __typename,
+                    name = name
+                )
+            }
+        }
+    }
+
+    /**
+     * @param node The character represented by this friendship edge */
+    data class Edges(
+        val __typename: String,
+        val node: Node?
+    ) {
+        fun marshaller(): ResponseFieldMarshaller = ResponseFieldMarshaller {
+            it.writeString(RESPONSE_FIELDS[0], __typename)
+            it.writeObject(RESPONSE_FIELDS[1], node?.marshaller())
+        }
+
+        companion object {
+            private val RESPONSE_FIELDS: Array<ResponseField> = arrayOf(
+                        ResponseField.forString("__typename", "__typename", null, false, null),
+                        ResponseField.forObject("node", "node", null, true, null)
+                    )
+
+            operator fun invoke(reader: ResponseReader): Edges {
+                val __typename = reader.readString(RESPONSE_FIELDS[0])
+                val node = reader.readObject<Node>(RESPONSE_FIELDS[1]) {
+                    Node(it)
+                }
+
+                return Edges(
+                    __typename = __typename,
+                    node = node
+                )
+            }
+        }
+    }
+
+    /**
+     * @param totalCount The total number of friends@param edges The edges for each of the character's friends. */
+    data class FriendsConnection(
+        val __typename: String,
+        val totalCount: Int?,
+        val edges: List<Edges>?
+    ) {
+        fun marshaller(): ResponseFieldMarshaller = ResponseFieldMarshaller {
+            it.writeString(RESPONSE_FIELDS[0], __typename)
+            it.writeInt(RESPONSE_FIELDS[1], totalCount)
+            it.writeList(RESPONSE_FIELDS[2], edges) { value, listItemWriter ->
+                        @Suppress("NAME_SHADOWING")
+                        value?.forEach { value ->
+                            listItemWriter.writeObject(value?.marshaller())}
+                    }
+
+        }
+
+        companion object {
+            private val RESPONSE_FIELDS: Array<ResponseField> = arrayOf(
+                        ResponseField.forString("__typename", "__typename", null, false, null),
+                        ResponseField.forInt("totalCount", "totalCount", null, true, null),
+                        ResponseField.forList("edges", "edges", null, true, null)
+                    )
+
+            operator fun invoke(reader: ResponseReader): FriendsConnection {
+                val __typename = reader.readString(RESPONSE_FIELDS[0])
+                val totalCount = reader.readInt(RESPONSE_FIELDS[1])
+                val edges = reader.readList<Edges>(RESPONSE_FIELDS[2]) {
+                    it.readObject<Edges> {
+                        Edges(it)
+                    }
+
+                }
+
+                return FriendsConnection(
+                    __typename = __typename,
+                    totalCount = totalCount,
+                    edges = edges
+                )
+            }
+        }
+    }
+
+    /**
+     * @param name The name of the character@param friendsConnection The friends of the character exposed as a connection with edges */
+    data class Hero(
+        val __typename: String,
+        val name: String,
+        val friendsConnection: FriendsConnection
+    ) {
+        fun marshaller(): ResponseFieldMarshaller = ResponseFieldMarshaller {
+            it.writeString(RESPONSE_FIELDS[0], __typename)
+            it.writeString(RESPONSE_FIELDS[1], name)
+            it.writeObject(RESPONSE_FIELDS[2], friendsConnection.marshaller())
+        }
+
+        companion object {
+            private val RESPONSE_FIELDS: Array<ResponseField> = arrayOf(
+                        ResponseField.forString("__typename", "__typename", null, false, null),
+                        ResponseField.forString("name", "name", null, false, null),
+                        ResponseField.forObject("friendsConnection", "friendsConnection", null, false, null)
+                    )
+
+            operator fun invoke(reader: ResponseReader): Hero {
+                val __typename = reader.readString(RESPONSE_FIELDS[0])
+                val name = reader.readString(RESPONSE_FIELDS[1])
+                val friendsConnection = reader.readObject<FriendsConnection>(RESPONSE_FIELDS[2]) {
+                    FriendsConnection(it)
+                }
+
+                return Hero(
+                    __typename = __typename,
+                    name = name,
+                    friendsConnection = friendsConnection
+                )
+            }
+        }
+    }
+
+    data class Data(val hero: Hero?) : Operation.Data {
+        override fun marshaller(): ResponseFieldMarshaller = ResponseFieldMarshaller {
+            it.writeObject(RESPONSE_FIELDS[0], hero?.marshaller())
+        }
+
+        companion object {
+            private val RESPONSE_FIELDS: Array<ResponseField> = arrayOf(
+                        ResponseField.forObject("hero", "hero", null, true, null)
+                    )
+
+            operator fun invoke(reader: ResponseReader): Data {
+                val hero = reader.readObject<Hero>(RESPONSE_FIELDS[0]) {
+                    Hero(it)
+                }
+
+                return Data(
+                    hero = hero
+                )
+            }
+        }
+    }
+
+    companion object {
+        val OPERATION_DEFINITION: String = """
+                |query TestQuery {
+                |  hero {
+                |    __typename
+                |    name
+                |    friendsConnection {
+                |      __typename
+                |      totalCount
+                |      edges {
+                |        __typename
+                |        node {
+                |          __typename
+                |          name
+                |        }
+                |      }
+                |    }
+                |  }
+                |}
+                """.trimMargin()
+
+        const val OPERATION_ID: String =
+                "bd31d2946fb8bd87d560a73a4e14753bd9ea366bc94001f86d5a5fef0b459bee"
+
+        val QUERY_DOCUMENT: String = """
+                |query TestQuery {
+                |  hero {
+                |    __typename
+                |    name
+                |    friendsConnection {
+                |      __typename
+                |      totalCount
+                |      edges {
+                |        __typename
+                |        node {
+                |          __typename
+                |          name
+                |        }
+                |      }
+                |    }
+                |  }
+                |}
+                """.trimMargin()
+
+        val OPERATION_NAME: OperationName = OperationName { "TestQuery" }
+    }
+}

apollographql/apollo-kotlin

apollo-compiler/src/test/graphql/com/example/hero_details_nullable/TestQuery.kt
@@ -1,0 +1,240 @@
+package com.example.hero_details_nullable
+
+import com.apollographql.apollo.api.Operation
+import com.apollographql.apollo.api.OperationName
+import com.apollographql.apollo.api.Query
+import com.apollographql.apollo.api.ResponseField
+import com.apollographql.apollo.api.ResponseFieldMapper
+import com.apollographql.apollo.api.ResponseFieldMarshaller
+import com.apollographql.apollo.api.ResponseReader
+import javax.annotation.Generated
+import kotlin.Array
+import kotlin.Int
+import kotlin.String
+import kotlin.Suppress
+import kotlin.collections.List
+
+@Generated("Apollo GraphQL")
+class TestQuery : Query<TestQuery.Data, TestQuery.Data, Operation.Variables> {
+    override fun operationId(): String = OPERATION_ID
+    override fun queryDocument(): String = QUERY_DOCUMENT
+    override fun wrapData(data: TestQuery.Data): TestQuery.Data = data
+    override fun variables(): Operation.Variables = Operation.EMPTY_VARIABLES
+    override fun name(): OperationName = OPERATION_NAME
+    override fun responseFieldMapper(): ResponseFieldMapper<TestQuery.Data> = ResponseFieldMapper {
+        TestQuery.Data(it)
+    }
+
+    /**
+     * @param name The name of the character
+     */
+    data class Node(
+        val __typename: String,
+        val name: String
+    ) {
+        fun marshaller(): ResponseFieldMarshaller = ResponseFieldMarshaller {
+            it.writeString(RESPONSE_FIELDS[0], __typename)
+            it.writeString(RESPONSE_FIELDS[1], name)
+        }
+
+        companion object {
+            private val RESPONSE_FIELDS: Array<ResponseField> = arrayOf(
+                    ResponseField.forString("__typename", "__typename", null, false, null),
+                    ResponseField.forString("name", "name", null, false, null)
+                    )
+
+            operator fun invoke(reader: ResponseReader): Node {
+                val __typename = reader.readString(RESPONSE_FIELDS[0])
+                val name = reader.readString(RESPONSE_FIELDS[1])
+                return Node(
+                    __typename = __typename,
+                    name = name
+                )
+            }
+        }
+    }
+
+    /**
+     * @param node The character represented by this friendship edge
+     */
+    data class Edge(
+        val __typename: String,
+        val node: Node?
+    ) {
+        fun marshaller(): ResponseFieldMarshaller = ResponseFieldMarshaller {
+            it.writeString(RESPONSE_FIELDS[0], __typename)
+            it.writeObject(RESPONSE_FIELDS[1], node?.marshaller())
+        }
+
+        companion object {
+            private val RESPONSE_FIELDS: Array<ResponseField> = arrayOf(
+                    ResponseField.forString("__typename", "__typename", null, false, null),
+                    ResponseField.forObject("node", "node", null, true, null)
+                    )
+
+            operator fun invoke(reader: ResponseReader): Edge {
+                val __typename = reader.readString(RESPONSE_FIELDS[0])
+                val node = reader.readObject<Node>(RESPONSE_FIELDS[1]) {
+                    Node(it)
+                }
+
+                return Edge(
+                    __typename = __typename,
+                    node = node
+                )
+            }
+        }
+    }
+
+    /**
+     * @param totalCount The total number of friends
+     * @param edges The edges for each of the character's friends.
+     */
+    data class FriendsConnection(
+        val __typename: String,
+        val totalCount: Int?,
+        val edges: List<Edge>?
+    ) {
+        fun marshaller(): ResponseFieldMarshaller = ResponseFieldMarshaller {
+            it.writeString(RESPONSE_FIELDS[0], __typename)
+            it.writeInt(RESPONSE_FIELDS[1], totalCount)
+            it.writeList(RESPONSE_FIELDS[2], edges) { value, listItemWriter ->
+                @Suppress("NAME_SHADOWING")
+                value?.forEach { value ->
+                    listItemWriter.writeObject(value?.marshaller())
+                }
+            }
+        }
+
+        companion object {
+            private val RESPONSE_FIELDS: Array<ResponseField> = arrayOf(
+                    ResponseField.forString("__typename", "__typename", null, false, null),
+                    ResponseField.forInt("totalCount", "totalCount", null, true, null),
+                    ResponseField.forList("edges", "edges", null, true, null)
+                    )
+
+            operator fun invoke(reader: ResponseReader): FriendsConnection {
+                val __typename = reader.readString(RESPONSE_FIELDS[0])
+                val totalCount = reader.readInt(RESPONSE_FIELDS[1])
+                val edges = reader.readList<Edge>(RESPONSE_FIELDS[2]) {
+                    it.readObject<Edge> {
+                        Edge(it)
+                    }
+
+                }
+                return FriendsConnection(
+                    __typename = __typename,
+                    totalCount = totalCount,
+                    edges = edges
+                )
+            }
+        }
+    }
+
+    /**
+     * @param name The name of the character
+     * @param friendsConnection The friends of the character exposed as a connection with edges
+     */
+    data class Hero(
+        val __typename: String,
+        val name: String,
+        val friendsConnection: FriendsConnection
+    ) {
+        fun marshaller(): ResponseFieldMarshaller = ResponseFieldMarshaller {
+            it.writeString(RESPONSE_FIELDS[0], __typename)
+            it.writeString(RESPONSE_FIELDS[1], name)
+            it.writeObject(RESPONSE_FIELDS[2], friendsConnection.marshaller())
+        }
+
+        companion object {
+            private val RESPONSE_FIELDS: Array<ResponseField> = arrayOf(
+                    ResponseField.forString("__typename", "__typename", null, false, null),
+                    ResponseField.forString("name", "name", null, false, null),
+                    ResponseField.forObject("friendsConnection", "friendsConnection", null, false, null)
+                    )
+
+            operator fun invoke(reader: ResponseReader): Hero {
+                val __typename = reader.readString(RESPONSE_FIELDS[0])
+                val name = reader.readString(RESPONSE_FIELDS[1])
+                val friendsConnection = reader.readObject<FriendsConnection>(RESPONSE_FIELDS[2]) {
+                    FriendsConnection(it)
+                }
+
+                return Hero(
+                    __typename = __typename,
+                    name = name,
+                    friendsConnection = friendsConnection
+                )
+            }
+        }
+    }
+
+    data class Data(val hero: Hero?) : Operation.Data {
+        override fun marshaller(): ResponseFieldMarshaller = ResponseFieldMarshaller {
+            it.writeObject(RESPONSE_FIELDS[0], hero?.marshaller())
+        }
+
+        companion object {
+            private val RESPONSE_FIELDS: Array<ResponseField> = arrayOf(
+                    ResponseField.forObject("hero", "hero", null, true, null)
+                    )
+
+            operator fun invoke(reader: ResponseReader): Data {
+                val hero = reader.readObject<Hero>(RESPONSE_FIELDS[0]) {
+                    Hero(it)
+                }
+
+                return Data(
+                    hero = hero
+                )
+            }
+        }
+    }
+
+    companion object {
+        val OPERATION_DEFINITION: String = """
+                |query TestQuery {
+                |  hero {
+                |    __typename
+                |    name
+                |    friendsConnection {
+                |      __typename
+                |      totalCount
+                |      edges {
+                |        __typename
+                |        node {
+                |          __typename
+                |          name
+                |        }
+                |      }
+                |    }
+                |  }
+                |}
+                """.trimMargin()
+
+        const val OPERATION_ID: String =
+                "bd31d2946fb8bd87d560a73a4e14753bd9ea366bc94001f86d5a5fef0b459bee"
+
+        val QUERY_DOCUMENT: String = """
+                |query TestQuery {
+                |  hero {
+                |    __typename
+                |    name
+                |    friendsConnection {
+                |      __typename
+                |      totalCount
+                |      edges {
+                |        __typename
+                |        node {
+                |          __typename
+                |          name
+                |        }
+                |      }
+                |    }
+                |  }
+                |}
+                """.trimMargin()
+
+        val OPERATION_NAME: OperationName = OperationName { "TestQuery" }
+    }
+}