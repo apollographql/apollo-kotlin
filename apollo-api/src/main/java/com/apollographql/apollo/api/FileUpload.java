980b8cbf94565d233ad839d971b4ea21e7cc8b07
chalermpong <s_pong_c@hotmail.com>
2019-05-14 00:44:03 +0700

    Made FileUpload class and its fields final (#640),(#1149)

--- apollo-api/src/main/java/com/apollographql/apollo/api/FileUpload.java
+++ apollo-api/src/main/java/com/apollographql/apollo/api/FileUpload.java
@@ -2,9 +2,9 @@ package com.apollographql.apollo.api;
 
 import java.io.File;
 
-public class FileUpload {
-    public String mimetype;
-    public File file;
+public final class FileUpload {
+    public final String mimetype;
+    public final File file;
 
     public FileUpload(String mimetype, File file) {
         this.mimetype = mimetype;
