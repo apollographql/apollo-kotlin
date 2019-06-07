95b0e854eb5953d95acceabb474a8e193b9af3f3
Martin Bonnin <martin@mbonnin.net>
2019-06-07 08:08:08 +0200

    cleanup the test when done

--- apollo-gradle-plugin/src/test/groovy/com/apollographql/apollo/gradle/integration/KotlinSpec.groovy
+++ apollo-gradle-plugin/src/test/groovy/com/apollographql/apollo/gradle/integration/KotlinSpec.groovy
@@ -35,4 +35,8 @@ class KotlinSpec extends Specification {
         then:
         result.task(":assemble").outcome == TaskOutcome.SUCCESS
     }
+
+    def cleanupSpec() {
+        FileUtils.deleteDirectory(testProjectDir)
+    }
 }
