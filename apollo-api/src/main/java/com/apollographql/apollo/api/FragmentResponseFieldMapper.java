57d9c43fd1566b5494ce0010c4f93d67d365c55e
Zac Sweers <zac.sweers@gmail.com>
2019-08-07 02:39:55 -0400

    Fix broken master checkstyles
    These appear to be failing master right now

--- apollo-api/src/main/java/com/apollographql/apollo/api/FragmentResponseFieldMapper.java
+++ apollo-api/src/main/java/com/apollographql/apollo/api/FragmentResponseFieldMapper.java
@@ -6,5 +6,5 @@ import java.io.IOException;
  * FragmentResponseFieldMapper is responsible for mapping the response back to a fragment of type T.
  */
 public interface FragmentResponseFieldMapper<T> {
-  T map(final ResponseReader responseReader, String conditionalType) throws IOException;
+  T map(ResponseReader responseReader, String conditionalType) throws IOException;
 }
