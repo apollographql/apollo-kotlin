2b87bc8bffbe72734150301b03a9966e9ab86619
Ivan Savytskyi <ivan.savitsky@gmail.com>
2019-08-23 17:36:07 -0400

    Fix issue with duplicated fragment definition in query (#1536)
    Closes https://github.com/apollographql/apollo-android/issues/1533

--- apollo-compiler/src/main/kotlin/com/apollographql/apollo/compiler/ir/Fragment.kt
+++ apollo-compiler/src/main/kotlin/com/apollographql/apollo/compiler/ir/Fragment.kt
@@ -17,7 +17,6 @@ data class Fragment(
     val fields: List<Field>,
     val fragmentSpreads: List<String>,
     val inlineFragments: List<InlineFragment>,
-    val fragmentsReferenced: List<String>,
     val filePath: String?
 ) : CodeGenerator {
 
