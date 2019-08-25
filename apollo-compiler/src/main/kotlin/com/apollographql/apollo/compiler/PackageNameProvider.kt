0cd95c541d91b916500e83e3a0c5410b0d959008
Ivan Savytskyi <ivan.savitsky@gmail.com>
2019-08-25 14:08:30 -0400

    Revert and fix Kotlin Docs

--- apollo-compiler/src/main/kotlin/com/apollographql/apollo/compiler/PackageNameProvider.kt
+++ apollo-compiler/src/main/kotlin/com/apollographql/apollo/compiler/PackageNameProvider.kt
@@ -1,7 +1,20 @@
 package com.apollographql.apollo.compiler
 
 class PackageNameProvider(
+    /**
+     * Location of the schema file.
+     *
+     * Used to format target package name if custom root package name is not provided
+     * or to format relative position for GraphQL operations.
+     */
     private val schemaFilePath: String,
+    /**
+     * Root package name for all classes:
+     *
+     * - fragments will use `rootPackageName.fragment`
+     * - types will use `rootPackageName.type`
+     * - operations, will use `rootPackageName.{relativePosition to schema location}`
+     */
     rootPackageName: String? = null
 ) {
   val packageName: String = rootPackageName?.takeIf { it.isNotEmpty() } ?: schemaFilePath.formatPackageName()
