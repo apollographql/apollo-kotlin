a8ab530bfa16ef3d2d19389a705835ab047a7903
Martin Bonnin <martin@mbonnin.net>
2019-09-14 01:16:13 +0200

    indent with 2 spaces like the rest of the codebase
    No functional change

--- apollo-coroutines-support/src/main/kotlin/com/apollographql/apollo/coroutines/CoroutinesExtensions.kt
+++ apollo-coroutines-support/src/main/kotlin/com/apollographql/apollo/coroutines/CoroutinesExtensions.kt
@@ -13,30 +13,30 @@ import kotlin.coroutines.resumeWithException
 
 private class ChannelCallback<T>(val channel: Channel<Response<T>>) : ApolloCall.Callback<T>() {
 
-    override fun onResponse(response: Response<T>) {
-        channel.offer(response)
-    }
+  override fun onResponse(response: Response<T>) {
+    channel.offer(response)
+  }
 
-    override fun onFailure(e: ApolloException) {
-        channel.close(e)
-    }
+  override fun onFailure(e: ApolloException) {
+    channel.close(e)
+  }
 
-    override fun onStatusEvent(event: ApolloCall.StatusEvent) {
-        if (event == ApolloCall.StatusEvent.COMPLETED) {
-            channel.close()
-        }
+  override fun onStatusEvent(event: ApolloCall.StatusEvent) {
+    if (event == ApolloCall.StatusEvent.COMPLETED) {
+      channel.close()
     }
+  }
 }
 
 private fun checkCapacity(capacity: Int) {
-    when (capacity) {
-        Channel.UNLIMITED,
-        Channel.CONFLATED -> return
-        else ->
-            // Everything else than UNLIMITED or CONFLATED does not guarantee that channel.offer() succeeds all the time.
-            // We don't support these use cases for now
-            throw IllegalArgumentException("Bad channel capacity ($capacity). Only UNLIMITED and CONFLATED are supported")
-    }
+  when (capacity) {
+    Channel.UNLIMITED,
+    Channel.CONFLATED -> return
+    else ->
+      // Everything else than UNLIMITED or CONFLATED does not guarantee that channel.offer() succeeds all the time.
+      // We don't support these use cases for now
+      throw IllegalArgumentException("Bad channel capacity ($capacity). Only UNLIMITED and CONFLATED are supported")
+  }
 }
 
 /**
@@ -51,15 +51,15 @@ private fun checkCapacity(capacity: Int) {
  * @return the converted channel
  */
 fun <T> ApolloCall<T>.toChannel(capacity: Int = Channel.UNLIMITED): Channel<Response<T>> {
-    checkCapacity(capacity)
-    val channel = Channel<Response<T>>(capacity)
+  checkCapacity(capacity)
+  val channel = Channel<Response<T>>(capacity)
 
-    channel.invokeOnClose {
-        cancel()
-    }
-    enqueue(ChannelCallback(channel))
+  channel.invokeOnClose {
+    cancel()
+  }
+  enqueue(ChannelCallback(channel))
 
-    return channel
+  return channel
 }
 
 /**
@@ -71,24 +71,24 @@ fun <T> ApolloCall<T>.toChannel(capacity: Int = Channel.UNLIMITED): Channel<Resp
  * @return the deferred
  */
 fun <T> ApolloCall<T>.toDeferred(): Deferred<Response<T>> {
-    val deferred = CompletableDeferred<Response<T>>()
+  val deferred = CompletableDeferred<Response<T>>()
 
-    deferred.invokeOnCompletion {
-        if (deferred.isCancelled) {
-            cancel()
-        }
+  deferred.invokeOnCompletion {
+    if (deferred.isCancelled) {
+      cancel()
     }
-    enqueue(object: ApolloCall.Callback<T>() {
-        override fun onResponse(response: Response<T>) {
-            deferred.complete(response)
-        }
+  }
+  enqueue(object : ApolloCall.Callback<T>() {
+    override fun onResponse(response: Response<T>) {
+      deferred.complete(response)
+    }
 
-        override fun onFailure(e: ApolloException) {
-            deferred.completeExceptionally(e)
-        }
-    })
+    override fun onFailure(e: ApolloException) {
+      deferred.completeExceptionally(e)
+    }
+  })
 
-    return deferred
+  return deferred
 }
 
 /**
@@ -102,15 +102,15 @@ fun <T> ApolloCall<T>.toDeferred(): Deferred<Response<T>> {
  * @return the converted channel
  */
 fun <T> ApolloQueryWatcher<T>.toChannel(capacity: Int = Channel.UNLIMITED): Channel<Response<T>> {
-    checkCapacity(capacity)
-    val channel = Channel<Response<T>>(capacity)
+  checkCapacity(capacity)
+  val channel = Channel<Response<T>>(capacity)
 
-    channel.invokeOnClose {
-        cancel()
-    }
-    enqueueAndWatch(ChannelCallback(channel))
+  channel.invokeOnClose {
+    cancel()
+  }
+  enqueueAndWatch(ChannelCallback(channel))
 
-    return channel
+  return channel
 }
 
 /**
@@ -124,34 +124,34 @@ fun <T> ApolloQueryWatcher<T>.toChannel(capacity: Int = Channel.UNLIMITED): Chan
  * @return the converted channel
  */
 fun <T> ApolloSubscriptionCall<T>.toChannel(capacity: Int = Channel.UNLIMITED): Channel<Response<T>> {
-    checkCapacity(capacity)
-    val channel = Channel<Response<T>>(capacity)
+  checkCapacity(capacity)
+  val channel = Channel<Response<T>>(capacity)
 
-    channel.invokeOnClose {
-        cancel()
+  channel.invokeOnClose {
+    cancel()
+  }
+  execute(object : ApolloSubscriptionCall.Callback<T> {
+    override fun onConnected() {
     }
-    execute(object : ApolloSubscriptionCall.Callback<T> {
-        override fun onConnected() {
-        }
 
-        override fun onResponse(response: Response<T>) {
-            channel.offer(response)
-        }
+    override fun onResponse(response: Response<T>) {
+      channel.offer(response)
+    }
 
-        override fun onFailure(e: ApolloException) {
-            channel.close(e)
-        }
+    override fun onFailure(e: ApolloException) {
+      channel.close(e)
+    }
 
-        override fun onCompleted() {
-            channel.close()
-        }
+    override fun onCompleted() {
+      channel.close()
+    }
 
-        override fun onTerminated() {
-            channel.close()
-        }
-    })
+    override fun onTerminated() {
+      channel.close()
+    }
+  })
 
-    return channel
+  return channel
 }
 
 /**
@@ -161,25 +161,25 @@ fun <T> ApolloSubscriptionCall<T>.toChannel(capacity: Int = Channel.UNLIMITED): 
  * @return the converted job
  */
 fun ApolloPrefetch.toJob(): Job {
-    val deferred = CompletableDeferred<Unit>()
+  val deferred = CompletableDeferred<Unit>()
 
-    deferred.invokeOnCompletion {
-        if (deferred.isCancelled) {
-            cancel()
-        }
+  deferred.invokeOnCompletion {
+    if (deferred.isCancelled) {
+      cancel()
+    }
+  }
+
+  enqueue(object : ApolloPrefetch.Callback() {
+    override fun onSuccess() {
+      deferred.complete(Unit)
     }
 
-    enqueue(object : ApolloPrefetch.Callback() {
-        override fun onSuccess() {
-            deferred.complete(Unit)
-        }
+    override fun onFailure(e: ApolloException) {
+      deferred.completeExceptionally(e)
+    }
+  })
 
-        override fun onFailure(e: ApolloException) {
-            deferred.completeExceptionally(e)
-        }
-    })
-
-    return deferred
+  return deferred
 }
 
 
