5a8278185807d948a42828d8a23dd925969d41fb
Steven Schoen <steven@stevenschoen.com>
2019-09-25 08:46:40 -0700

    Make SourceLocation a data class

--- apollo-compiler/src/main/kotlin/com/apollographql/apollo/compiler/ir/SourceLocation.kt
+++ apollo-compiler/src/main/kotlin/com/apollographql/apollo/compiler/ir/SourceLocation.kt
@@ -1,6 +1,6 @@
 package com.apollographql.apollo.compiler.ir
 
-class SourceLocation(val line: Int, val position: Int) {
+data class SourceLocation(val line: Int, val position: Int) {
   companion object {
     val UNKNOWN = SourceLocation(-1, -1)
   }
