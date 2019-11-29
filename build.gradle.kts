4ace36d9bdab2038386558ba4154d884b26388b3
Martin Bonnin <martin.bonnin@dailymotion.com>
2019-11-29 15:53:22 +0100

    introduce japicmp

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,14 +6,34 @@ buildscript {
   repositories {
     maven { url = uri("https://plugins.gradle.org/m2/") }
     google()
+    jcenter()
   }
 
   dependencies {
     classpath(groovy.util.Eval.x(project, "x.dep.android.plugin"))
     classpath(groovy.util.Eval.x(project, "x.dep.gradleErrorpronePlugin"))
+    classpath(groovy.util.Eval.x(project, "x.dep.gradleJapiCmpPlugin"))
     classpath(groovy.util.Eval.x(project, "x.dep.kotlin.plugin"))
   }
 }
+
+fun baselineJar(project: Project, version: String): File {
+  val group = project.property("GROUP")
+  val artifactId = project.property("POM_ARTIFACT_ID")
+  try {
+    val jarFile = "$artifactId-${version}.jar"
+    project.group = "virtual_group_for_japicmp" // Prevent it from resolving the current version.
+    // see https://github.com/apollographql/apollo-android/issues/1325
+    project.repositories.maven("https://dl.bintray.com/apollographql/android")
+    val dependency = project.dependencies.create("$group:$artifactId:$version@jar")
+    return project.configurations.detachedConfiguration(dependency).files
+        .first { (it.name == jarFile) }
+  } finally {
+    project.group = group!!
+  }
+}
+
+val rootJapiCmp = tasks.register("japicmp")
 
 subprojects {
   apply {
@@ -44,6 +64,24 @@ subprojects {
 
   configurations.named("errorprone") {
     resolutionStrategy.force(groovy.util.Eval.x(this@subprojects, "x.dep.errorProneCore"))
+  }
+
+  // TODO: Make this lazy
+  this@subprojects.afterEvaluate {
+    val jarTask = this@subprojects.tasks.findByName("jar") as? org.gradle.jvm.tasks.Jar
+    if (jarTask != null) {
+      val japiCmp = this@subprojects.tasks.register("japicmp", me.champeau.gradle.japicmp.JapicmpTask::class.java) {
+        oldClasspath = this@subprojects.files(baselineJar(this@subprojects, "1.2.1"))
+        newClasspath = this@subprojects.files(jarTask.archiveFile)
+        ignoreMissingClasses = true
+        packageExcludes = listOf("*.internal*")
+        onlyModified = true
+        txtOutputFile = this@subprojects.file("$buildDir/reports/japi.txt")
+      }
+      rootJapiCmp.configure {
+        dependsOn(japiCmp)
+      }
+    }
   }
 
   group = property("GROUP")!!
