31fcf78a4ba7141860759af845179903eaaaaf33
Martin Bonnin <martin.bonnin@dailymotion.com>
2019-12-01 15:24:25 +0100

    merge deploy.sh and deploy_snapshot.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
 #
-# Deploy a jar, source jar, and javadoc jar to Sonatype's snapshot repo.
+# Deploy a jar, source jar, and javadoc jar to Sonatype and/or Bintray
+
+set -e
+
 #
-./gradlew bintrayUpload
+# Deploy the snapshots to Sonatype's repo if the build happens on master
+#
+SLUG="apollographql/apollo-android"
+JDK="openjdk8"
+SNAPSHOT_BRANCH="master"
+
+if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
+  echo "Skipping snapshot deployment: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
+elif [ "$TRAVIS_JDK_VERSION" != "$JDK" ]; then
+  echo "Skipping snapshot deployment: wrong JDK. Expected '$JDK' but was '$TRAVIS_JDK_VERSION'."
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "Skipping snapshot deployment: was pull request."
+elif [ "$TRAVIS_BRANCH" != "$SNAPSHOT_BRANCH" ]; then
+  echo "Skipping snapshot deployment: wrong branch. Expected '$SNAPSHOT_BRANCH' but was '$TRAVIS_BRANCH'."
+else
+  echo "Deploying snapshot..."
+  ./gradlew uploadArchives -PSONATYPE_NEXUS_USERNAME="${SONATYPE_NEXUS_USERNAME}" -PSONATYPE_NEXUS_PASSWORD="${SONATYPE_NEXUS_PASSWORD}"
+  echo "Snapshot deployed!"
+fi
+
+#
+# Deploy the release to Bintray if the build happens on master
+#
+if [ "$TRAVIS_TAG" != "" ]; then
+  echo "Deploy to bintray..."
+  ./gradlew bintrayUpload
+  echo "Deployed to bintray!"
+fi
 
