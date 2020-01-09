0dfcaf300d239d099c924740b545eea87eace8df
Eric Kuck <eric@bluelinelabs.com>
2020-01-08 22:30:03 -0600

    Singularization updates (#1888)

--- apollo-compiler/src/main/kotlin/com/apollographql/apollo/compiler/Inflector.kt
+++ apollo-compiler/src/main/kotlin/com/apollographql/apollo/compiler/Inflector.kt
@@ -5,7 +5,7 @@ import java.util.regex.Pattern
 /**
  * Various string transformation utilities. Transforms singular words to plural and folder paths to packages
  *
- * The singularization methods were heavily based off rogueweb <link> https://code.google.com/archive/p/rogueweb/source</link>
+ * The singularization methods were heavily based off ruby on rails <link>https://github.com/rails/rails/blob/master/activesupport/lib/active_support/inflections.rb</link>
  */
 
 fun String.singularize(): String {
@@ -14,63 +14,68 @@ fun String.singularize(): String {
   if (exclude.contains(this.toLowerCase())) return this
 
   val capitalized = first().isUpperCase()
-  val irregular = irregular().firstOrNull { this.toLowerCase() == it.component2() }
+  val irregular = irregular.firstOrNull { this.toLowerCase() == it.component2() }
   if (irregular != null) return irregular.component1().let {
     if (capitalized) it.capitalize() else it
   }
 
-  if (singularizationRules().find { match(it.component1(), this) } == null) return this
+  val rule = singularizationRules.lastOrNull { it.component1().matcher(this).find() }
+  if (rule != null) {
+    return rule.component1().matcher(this).replaceAll(rule.component2())
+  }
 
-  val rule = singularizationRules().last { match(it.component1(), this) }
-  return Pattern.compile(rule.component1(), Pattern.CASE_INSENSITIVE).matcher(this).replaceAll(rule.component2())
+  return this
 }
 
-private fun singularizationRules(): List<Pair<String, String>> {
-  return listOf(
-      "s$" to "",
-      "(s|si|u)s$" to "$1s",
-      "(n)ews$" to "$1ews",
-      "([ti])a$" to "$1um",
-      "((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)ses$" to "$1$2sis",
-      "(^analy)ses$" to "$1sis",
-      "(^analy)sis$" to "$1sis",
-      "([^f])ves$" to "$1fe",
-      "(hive)s$" to "$1",
-      "(tive)s$" to "$1",
-      "([lr])ves$" to "$1f",
-      "([^aeiouy]|qu)ies$" to "$1y",
-      "(s)eries$" to "$1eries",
-      "(m)ovies$" to "$1ovie",
-      "(x|ch|ss|sh)es$" to "$1",
-      "([m|l])ice$" to "$1ouse",
-      "(bus)es$" to "$1",
-      "(o)es$" to "$1",
-      "(shoe)s$" to "$1",
-      "(cris|ax|test)is$" to "$1is",
-      "(cris|ax|test)es$" to "$1is",
-      "(octop|vir)i$" to "$1us",
-      "(octop|vir)us$" to "$1us",
-      "(alias|status)es$" to "$1",
-      "(alias|status)$" to "$1",
-      "^(ox)en" to "$1",
-      "(vert|ind)ices$" to "$1ex",
-      "(matr)ices$" to "$1ix",
-      "(quiz)zes$" to "$1")
-}
+private val singularizationRules = listOf(
+    "s$" to "",
+    "(ss)$" to "$1",
+    "([ti])a$" to "$1um",
+    "(^analy)(sis|ses)$" to "$1sis",
+    "([^f])ves$" to "$1fe",
+    "(hive)s$" to "$1",
+    "(tive)s$" to "$1",
+    "([lr])ves$" to "$1f",
+    "([^aeiouy]|qu)ies$" to "$1y",
+    "(s)eries$" to "$1eries",
+    "(m)ovies$" to "$1ovie",
+    "(x|ch|ss|sh)es$" to "$1",
+    "^(m|l)ice$" to "$1ouse",
+    "(bus)(es)?$" to "$1",
+    "(o)es$" to "$1",
+    "(shoe)s$" to "$1",
+    "(cris|test)(is|es)$" to "$1is",
+    "^(a)x[ie]s$" to "$1xis",
+    "(octop|vir)(us|i)$" to "$1us",
+    "(alias|status)(es)?$" to "$1",
+    "^(ox)en/$" to "$1",
+    "(vert|ind)ices$" to "$1ex",
+    "(matr)ices$" to "$1ix",
+    "(quiz)zes$" to "$1",
+    "(database)s$" to "$1"
+).map { Pattern.compile(it.first, Pattern.CASE_INSENSITIVE) to it.second }
 
-private fun irregular(): List<Pair<String, String>> {
-  return listOf(
-      "person" to "people",
-      "man" to "men",
-      "goose" to "geese",
-      "child" to "children",
-      "sex" to "sexes",
-      "move" to "moves")
-}
+private val irregular = listOf(
+    "person" to "people",
+    "man" to "men",
+    "child" to "children",
+    "sex" to "sexes",
+    "move" to "moves",
+    "zombie" to "zombies",
+    "goose" to "geese"
+)
 
-private val uncountable = listOf("equipment", "information", "rice", "money", "species", "series", "fish", "sheep")
+private val uncountable = listOf(
+    "equipment",
+    "information",
+    "rice",
+    "money",
+    "species",
+    "series",
+    "fish",
+    "sheep",
+    "jeans",
+    "police"
+)
+
 private val exclude = listOf("data")
-
-private fun match(pattern: String, word: String): Boolean {
-  return Pattern.compile(pattern, Pattern.CASE_INSENSITIVE).matcher(word).find()
-}
