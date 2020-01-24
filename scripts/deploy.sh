e73b002941a3760bd25bfcc1298a1c1c5aa84876
Martin Bonnin <martin@mbonnin.net>
2020-01-24 16:52:09 +0100

    deploy apollo-gradle-plugin to plugins.gradle.org

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -34,5 +34,8 @@ else
   echo "Deploy to bintray..."
   ./gradlew bintrayUpload -Pbintray.user="${BINTRAY_USER}" -Pbintray.apikey="${BINTRAY_API_KEY}"
   echo "Deployed to bintray!"
+  echo "Deploy to Gradle portal..."
+  ./gradlew :apollo-gradle-plugin:publishPlugin -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET
+  echo "Deployed to Gradle portal!"
 fi
 
