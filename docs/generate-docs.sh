bded1d2cea76851385137a402a90be452d69981d
Rafa <rmoreno.cesar@gmail.com>
2020-02-22 16:00:17 -0600

    Add generate-docs.sh instead of inlining those in the command

---
+++ docs/generate-docs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Set up documentation so README.md is reflected onto apollographql.com/docs/android
+
+
+echo -e  "---\ntitle: Get started\ndescription:\n---\n$(cat README.md | grep -v 'the official docs')" > docs/source/essentials/get-started.md
+
+gatsby build --prefix-paths
+mkdir -p docs/android
+mv public/* docs/android
+mv docs public/
+mv public/docs/android/_redirects public
