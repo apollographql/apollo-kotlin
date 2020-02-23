eb3450e38a1292a80220c8271e441009fafe3891
Rafa <rmoreno.cesar@gmail.com>
2020-02-22 21:29:26 -0600

    Add correct description and read README.md from correct source

--- docs/generate-docs.sh
+++ docs/generate-docs.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
-#
+#!/bin/bash#
 # Set up documentation so README.md is reflected onto apollographql.com/docs/android
 
-echo -e  "---\ntitle: Get started\ndescription:\n---\n$(cat README.md | grep -v 'the official docs')" > source/essentials/get-started.md
+echo -e  "---\ntitle: Get started\ndescription: Learn how to quickly set up Apollo Client in your Android app\n---\n\n$(cat ../README.md | grep -v 'the official docs')" > source/essentials/get-started.md
 
 gatsby build --prefix-paths
 mkdir -p docs/android
