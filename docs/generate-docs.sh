4bf81587a16fa99c4ad5cf2a762628446e2cadd3
Rafa <rmoreno.cesar@gmail.com>
2020-02-22 19:17:30 -0600

    Make script executable

--- docs/generate-docs.sh
+++ docs/generate-docs.sh
@@ -2,8 +2,7 @@
 #
 # Set up documentation so README.md is reflected onto apollographql.com/docs/android
 
-
-echo -e  "---\ntitle: Get started\ndescription:\n---\n$(cat README.md | grep -v 'the official docs')" > docs/source/essentials/get-started.md
+echo -e  "---\ntitle: Get started\ndescription:\n---\n$(cat README.md | grep -v 'the official docs')" > /source/essentials/get-started.md
 
 gatsby build --prefix-paths
 mkdir -p docs/android
