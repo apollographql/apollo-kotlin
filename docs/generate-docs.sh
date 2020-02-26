2fff64d9464599c75ec32b5008345db3d9bfd8ba
Rafa <rmoreno.cesar@gmail.com>
2020-02-25 19:22:41 -0600

    Add set -e to exit early

--- docs/generate-docs.sh
+++ docs/generate-docs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 ## Set up documentation so README.md is reflected onto apollographql.com/docs/android
 
+set -e
+
 echo -e "---\ntitle: Get started\ndescription: Learn how to quickly set up Apollo Client in your Android app\n---\n\n$(cat ../README.md | grep -v 'the official docs')" > source/essentials/get-started.md
 
 gatsby build --prefix-paths
