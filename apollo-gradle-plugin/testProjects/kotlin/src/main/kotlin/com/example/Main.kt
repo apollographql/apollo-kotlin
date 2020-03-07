32ed91184b2294ea99a89ad995fa4a968ec504b1
Martin Bonnin <martin@mbonnin.net>
2020-03-07 22:46:11 +0100

    remove unused test projects

--- apollo-gradle-plugin/testProjects/kotlin/src/main/kotlin/com/example/Main.kt
+++
@@ -1,9 +0,0 @@
-package com.example
-
-import com.example.DroidDetailsQuery
-
-class Main {
-  internal fun aMethodThatReferencesAGeneratedQuery(): DroidDetailsQuery {
-    return DroidDetailsQuery()
-  }
-}
--- apollo-gradle-plugin/testProjects/simple/src/main/java/com/example/Main.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-import com.example.DroidDetailsQuery;
-
-class Main {
-    DroidDetailsQuery aMethodThatReferencesAGeneratedQuery() {
-        return new DroidDetailsQuery();
-    }
-}
