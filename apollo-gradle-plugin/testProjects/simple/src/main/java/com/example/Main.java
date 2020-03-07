5c46eaa2e1281a72435d8f590617beac40414e74
Martin Bonnin <martin@mbonnin.net>
2020-03-07 21:34:54 +0100

    added a simple project

---
+++ apollo-gradle-plugin/testProjects/simple/src/main/java/com/example/Main.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import com.example.DroidDetailsQuery;
+
+class Main {
+    DroidDetailsQuery aMethodThatReferencesAGeneratedQuery() {
+        return new DroidDetailsQuery();
+    }
+}
