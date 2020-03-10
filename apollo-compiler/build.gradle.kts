ab99bc3f80629435b34657f79ce2af617ffd14f8
Said Tahsin Dane <tasomaniac@gmail.com>
2020-03-10 08:42:51 +0100

    I realized we are using both reflection based and code generation based Moshi parsing. I removed the reflection one since it is redundant (#2055)

--- apollo-compiler/build.gradle.kts
+++ apollo-compiler/build.gradle.kts
@@ -16,7 +16,6 @@ dependencies {
   add("antlr", groovy.util.Eval.x(project, "x.dep.antlr.antlr"))
   add("implementation", groovy.util.Eval.x(project, "x.dep.kotlin.stdLib"))
   add("implementation", groovy.util.Eval.x(project, "x.dep.moshi.adapters"))
-  add("implementation", groovy.util.Eval.x(project, "x.dep.moshi.kotlin"))
   add("implementation", groovy.util.Eval.x(project, "x.dep.moshi.moshi"))
   add("implementation", groovy.util.Eval.x(project, "x.dep.poet.java"))
   add("implementation", groovy.util.Eval.x(project, "x.dep.poet.kotlin"))
