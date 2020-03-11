df06210d025711714ee90777c02dfad513160b75
Said Tahsin Dane <tasomaniac@gmail.com>
2020-03-11 01:22:14 +0100

    Fix StackOverflow

--- apollo-api/src/jvmMain/kotlin/com/apollographql/apollo/api/internal/json/format.kt
+++ apollo-api/src/jvmMain/kotlin/com/apollographql/apollo/api/internal/json/format.kt
@@ -1,3 +1,3 @@
 package com.apollographql.apollo.api.internal.json
 
-actual fun String.Companion.format(format: String, vararg args: Any?): String = String.format(format, *args)
+actual fun String.Companion.format(format: String, vararg args: Any?): String = java.lang.String.format(format, *args)
