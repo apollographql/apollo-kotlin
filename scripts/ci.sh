19586d34009a865d1167793e8ea90d2bc6cb0c4c
Martin Bonnin <martin@mbonnin.net>
2020-03-22 16:12:29 +0100

    trying to make the emulator start

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -8,8 +8,8 @@ env
 
 export PATH="$ANDROID_HOME"/tools/bin:$PATH
 
-sdkmanager --install 'system-images;android-22;default;armeabi-v7a'
-avdmanager create avd --force -n test -k 'system-images;android-22;default;armeabi-v7a'
+sdkmanager --install 'system-images;android-22;default;armeabi-v7a' 'emulator'
+echo "no" |avdmanager create avd --force -n test -k 'system-images;android-22;default;armeabi-v7a'
 emulator -avd test -no-audio -no-window &
 
 ./gradlew clean build connectedCheck -x checkstyleTest --stacktrace --max-workers=2
