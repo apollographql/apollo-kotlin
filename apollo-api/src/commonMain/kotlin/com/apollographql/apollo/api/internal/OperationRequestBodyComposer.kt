72a5ce680ae008100685ccfbc3f2496170749e32
Ivan Savytskyi <ivan.savitsky@gmail.com>
2020-04-10 21:05:25 -0400

    Feedback + iOS sample

--- apollo-api/src/commonMain/kotlin/com/apollographql/apollo/api/internal/OperationRequestBodyComposer.kt
+++ apollo-api/src/commonMain/kotlin/com/apollographql/apollo/api/internal/OperationRequestBodyComposer.kt
@@ -3,6 +3,7 @@ package com.apollographql.apollo.api.internal
 import com.apollographql.apollo.api.Operation
 import com.apollographql.apollo.api.ScalarTypeAdapters
 import com.apollographql.apollo.api.internal.json.JsonWriter
+import com.apollographql.apollo.api.internal.json.use
 import okio.Buffer
 import okio.ByteString
 import kotlin.jvm.JvmStatic
@@ -16,31 +17,29 @@ object OperationRequestBodyComposer {
       withQueryDocument: Boolean,
       scalarTypeAdapters: ScalarTypeAdapters
   ): ByteString {
-    return Buffer().apply {
-      JsonWriter.of(this)
-          .apply { serializeNulls = true }
-          .beginObject()
-          .name("operationName").value(operation.name().name())
-          .name("variables").jsonValue(operation.variables().marshal(scalarTypeAdapters))
-          .apply {
-            if (autoPersistQueries) {
-              name("extensions")
-                  .beginObject()
-                  .name("persistedQuery")
-                  .beginObject()
-                  .name("version").value(1)
-                  .name("sha256Hash").value(operation.operationId())
-                  .endObject()
-                  .endObject()
-            }
-          }
-          .apply {
-            if (withQueryDocument) {
-              name("query").value(operation.queryDocument())
-            }
-          }
-          .endObject()
-          .close()
-    }.readByteString()
+    val buffer = Buffer()
+    JsonWriter.of(buffer).use { writer ->
+      with(writer) {
+        serializeNulls = true
+        beginObject()
+        name("operationName").value(operation.name().name())
+        name("variables").jsonValue(operation.variables().marshal(scalarTypeAdapters))
+        if (autoPersistQueries) {
+          name("extensions")
+          beginObject()
+          name("persistedQuery")
+          beginObject()
+          name("version").value(1)
+          name("sha256Hash").value(operation.operationId())
+          endObject()
+          endObject()
+        }
+        if (withQueryDocument) {
+          name("query").value(operation.queryDocument())
+        }
+        endObject()
+      }
+    }
+    return buffer.readByteString()
   }
 }
