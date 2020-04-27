406f2a80f87451a2ea8989d097049a0051aebb6d
Ellen Shapiro <designatednerd@gmail.com>
2020-04-27 15:14:09 -0500

    byeee whitespace

--- samples/multiplatform/kmp-ios-app/kmp-ios-app/AppDelegate.swift
+++ samples/multiplatform/kmp-ios-app/kmp-ios-app/AppDelegate.swift
@@ -10,8 +10,6 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -30,7 +28,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
