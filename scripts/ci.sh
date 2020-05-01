31370be559d4e3b640573a39434ee521acf2acbc
Said Tahsin Dane <tasomaniac@gmail.com>
2020-05-01 19:01:42 +0200

    Remove connectedCheck since not necessary

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -5,7 +5,7 @@ set -x
 
 export PATH="$ANDROID_HOME"/tools/bin:$PATH
 
-./gradlew clean build connectedCheck -x checkstyleTest --stacktrace --max-workers=2
+./gradlew clean build -x checkstyleTest --stacktrace --max-workers=2
 ./gradlew -p composite build
 
 ./gradlew publishIfNeeded -Pgradle.publish.key="$GRADLE_PUBLISH_KEY" -Pgradle.publish.secret="$GRADLE_PUBLISH_SECRET" --parallel
