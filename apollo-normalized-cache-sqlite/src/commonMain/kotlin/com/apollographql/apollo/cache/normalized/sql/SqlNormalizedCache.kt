9fb727f50888de7365aa30bd2528c0cde2d1853f
Luca Evangelista <pevangelista@morneaushepell.com>
2020-05-02 16:22:20 +0100

    format

--- apollo-normalized-cache-sqlite/src/commonMain/kotlin/com/apollographql/apollo/cache/normalized/sql/SqlNormalizedCache.kt
+++ apollo-normalized-cache-sqlite/src/commonMain/kotlin/com/apollographql/apollo/cache/normalized/sql/SqlNormalizedCache.kt
@@ -9,90 +9,91 @@ import com.apollographql.apollo.cache.normalized.RecordFieldJsonAdapter
 import okio.IOException
 
 class SqlNormalizedCache internal constructor(
-        private val recordFieldAdapter: RecordFieldJsonAdapter,
-        private val cacheQueries: CacheQueries
+    private val recordFieldAdapter: RecordFieldJsonAdapter,
+    private val cacheQueries: CacheQueries
 ) : NormalizedCache() {
 
-    override fun loadRecord(key: String, cacheHeaders: CacheHeaders): Record? {
-        val record = selectRecordForKey(key)
+  override fun loadRecord(key: String, cacheHeaders: CacheHeaders): Record? {
+    val record = selectRecordForKey(key)
 
-        if (record != null) {
-            if (cacheHeaders.hasHeader(EVICT_AFTER_READ)) {
-                deleteRecord(key)
-            }
-            return record
+    if (record != null) {
+      if (cacheHeaders.hasHeader(EVICT_AFTER_READ)) {
+        deleteRecord(key)
+      }
+      return record
+    }
+    return nextCache?.loadRecord(key, cacheHeaders)
+  }
+
+  override fun clearAll() {
+    nextCache?.clearAll()
+    cacheQueries.deleteAll()
+  }
+
+  override fun remove(cacheKey: CacheKey, cascade: Boolean): Boolean {
+    val result: Boolean = nextCache?.remove(cacheKey, cascade) ?: false
+    if (result) {
+      return true
+    }
+    return if (cascade) {
+      selectRecordForKey(cacheKey.key)
+          ?.referencedFields()
+          ?.all { remove(CacheKey(it.key), cascade = true) }
+          ?: false
+    } else {
+      deleteRecord(cacheKey.key)
+    }
+  }
+
+  override fun performMerge(apolloRecord: Record, cacheHeaders: CacheHeaders): Set<String> {
+    val oldRecord = selectRecordForKey(apolloRecord.key)
+    return if (oldRecord == null) {
+      cacheQueries.insert(apolloRecord.key, recordFieldAdapter.toJson(apolloRecord.fields))
+      emptySet()
+    } else {
+      oldRecord.mergeWith(apolloRecord).also {
+        if (it.isNotEmpty()) {
+          cacheQueries.update(oldRecord.fields["_id"] as Long?
+              ?: 0L, oldRecord.key, recordFieldAdapter.toJson(oldRecord.fields))
         }
-        return nextCache?.loadRecord(key, cacheHeaders)
+      }
     }
+  }
 
-    override fun clearAll() {
-        nextCache?.clearAll()
-        cacheQueries.deleteAll()
+  fun selectRecordForKey(key: String): Record? {
+    return try {
+      cacheQueries.recordForKey(key)
+          .executeAsList()
+          .firstOrNull()
+          ?.let {
+            Record.builder(it.key)
+                .addFields(recordFieldAdapter.from(it.record)!!)
+                .addField("_id", it._id)
+                .build()
+          }
+    } catch (e: IOException) {
+      null
     }
+  }
 
-    override fun remove(cacheKey: CacheKey, cascade: Boolean): Boolean {
-        val result: Boolean = nextCache?.remove(cacheKey, cascade) ?: false
-        if (result) {
-            return true
-        }
-        return if (cascade) {
-            selectRecordForKey(cacheKey.key)
-                    ?.referencedFields()
-                    ?.all { remove(CacheKey(it.key), cascade = true) }
-                    ?: false
-        } else {
-            deleteRecord(cacheKey.key)
-        }
+  private inline fun <T> Collection<T>.all(predicate: (T) -> Boolean): Boolean {
+    var result = true
+    for (element in this) {
+      result = result && predicate(element)
     }
+    return result
+  }
 
-    override fun performMerge(apolloRecord: Record, cacheHeaders: CacheHeaders): Set<String> {
-        val oldRecord = selectRecordForKey(apolloRecord.key)
-        return if (oldRecord == null) {
-            cacheQueries.insert(apolloRecord.key, recordFieldAdapter.toJson(apolloRecord.fields))
-            emptySet()
-        } else {
-            oldRecord.mergeWith(apolloRecord).also {
-                if (it.isNotEmpty()) {
-                    cacheQueries.update(oldRecord.fields["_id"] as Long? ?: 0L, oldRecord.key, recordFieldAdapter.toJson(oldRecord.fields))
-                }
-            }
-        }
+  fun deleteRecord(key: String): Boolean {
+    var changes = 0L
+    cacheQueries.transaction {
+      cacheQueries.delete(key)
+      changes = cacheQueries.changes().executeAsOne()
     }
+    return changes > 0
+  }
 
-    fun selectRecordForKey(key: String): Record? {
-        return try {
-            cacheQueries.recordForKey(key)
-                    .executeAsList()
-                    .firstOrNull()
-                    ?.let {
-                        Record.builder(it.key)
-                                .addFields(recordFieldAdapter.from(it.record)!!)
-                                .addField("_id", it._id)
-                                .build()
-                    }
-        } catch (e: IOException) {
-            null
-        }
-    }
-
-    private inline fun <T> Collection<T>.all(predicate: (T) -> Boolean): Boolean {
-        var result = true
-        for (element in this) {
-            result = result && predicate(element)
-        }
-        return result
-    }
-
-    fun deleteRecord(key: String): Boolean {
-        var changes = 0L
-        cacheQueries.transaction {
-            cacheQueries.delete(key)
-            changes = cacheQueries.changes().executeAsOne()
-        }
-        return changes > 0
-    }
-
-    fun createRecord(key: String, fields: String) {
-        cacheQueries.insert(key, fields)
-    }
+  fun createRecord(key: String, fields: String) {
+    cacheQueries.insert(key, fields)
+  }
 }
