e6e66d0f1c0171485bbf8b4d5b5ea0f4949ce11c
Martin Bonnin <martin@mbonnin.net>
2020-07-08 11:23:58 +0200

    Unbreak metalava (#2427)
    * unbreak metalava
    
    * exclude apollo-compiler and apollo-runtime-kotlin
    
    * remove the uneeded api.txt
    
    * adapt to #2422

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -7,7 +7,10 @@ export PATH="$ANDROID_HOME"/tools/bin:$PATH
 
 ./gradlew clean build --stacktrace --max-workers=2
 ./gradlew -p composite build
-./gradlew checkMetalava
+# check that the public API did not change with Metalava
+# apollo-compiler is for now considered an internal artifact consumed by the Gradle plugin so we allow API changes there.
+# apollo-runtime-kotlin is still under development. Include the check once it is stable enough.
+./gradlew checkMetalava -x apollo-compiler:checkMetalava -x apollo-runtime-kotlin:checkMetalava --parallel
 
 ./gradlew publishIfNeeded -Pgradle.publish.key="$GRADLE_PUBLISH_KEY" -Pgradle.publish.secret="$GRADLE_PUBLISH_SECRET" --parallel
 # this is a separate task because sonatype does not support --parallel
