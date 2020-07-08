10167fa716147e228252d7eb93afd2348b2fa020
Martin Bonnin <martin@mbonnin.net>
2020-07-08 15:49:24 +0200

    do not run in parallel for now. It might take a bit more time but it'll make errors more readble in CI

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -10,7 +10,7 @@ export PATH="$ANDROID_HOME"/tools/bin:$PATH
 # check that the public API did not change with Metalava
 # apollo-compiler is for now considered an internal artifact consumed by the Gradle plugin so we allow API changes there.
 # apollo-runtime-kotlin is still under development. Include the check once it is stable enough.
-./gradlew checkMetalava -x apollo-compiler:checkMetalava -x apollo-runtime-kotlin:checkMetalava --parallel
+./gradlew checkMetalava -x apollo-compiler:checkMetalava -x apollo-runtime-kotlin:checkMetalava
 
 ./gradlew publishIfNeeded -Pgradle.publish.key="$GRADLE_PUBLISH_KEY" -Pgradle.publish.secret="$GRADLE_PUBLISH_SECRET" --parallel
 # this is a separate task because sonatype does not support --parallel
