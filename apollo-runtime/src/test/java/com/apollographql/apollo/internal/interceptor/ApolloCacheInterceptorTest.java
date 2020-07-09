cb131b945cbf86ddcb14f19e825da25ceb65c437
Martin Bonnin <martin@mbonnin.net>
2020-07-09 15:21:26 +0200

    fix line wrap

--- apollo-runtime/src/test/java/com/apollographql/apollo/internal/interceptor/ApolloCacheInterceptorTest.java
+++ apollo-runtime/src/test/java/com/apollographql/apollo/internal/interceptor/ApolloCacheInterceptorTest.java
@@ -51,7 +51,13 @@ public class ApolloCacheInterceptorTest {
         .body(ResponseBody.create(MediaType.parse("text/plain; charset=utf-8"), "fakeResponse"))
         .build();
 
-    interceptor = new ApolloCacheInterceptor(apolloStore, mock(ResponseFieldMapper.class), mock(Executor.class), new ApolloLogger(logger), false);
+    interceptor = new ApolloCacheInterceptor(
+        apolloStore,
+        mock(ResponseFieldMapper.class),
+        mock(Executor.class),
+        new ApolloLogger(logger),
+        false
+    );
   }
 
   @Test
