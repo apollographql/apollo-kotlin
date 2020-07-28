f63b8dbd5333320cdab5f94c6b8721431a685af5
Martin Bonnin <martin@mbonnin.net>
2020-07-28 19:22:48 +0200

    add a link to the side bar

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -38,6 +38,7 @@ module.exports = {
             'essentials/normalized-cache',
             'essentials/http-cache',
             'advanced/persisted-queries',
+            'essentials/model-names',
           ],
           'Languages & Extensions': [
             'advanced/coroutines',
