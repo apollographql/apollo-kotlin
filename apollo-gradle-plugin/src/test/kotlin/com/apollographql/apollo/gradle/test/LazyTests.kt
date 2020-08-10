4ce8c19910053ceca1098562d4bb76c597c55ffe
Martin Bonnin <martin@mbonnin.net>
2020-08-10 17:51:48 +0200

    restore test

--- apollo-gradle-plugin/src/test/kotlin/com/apollographql/apollo/gradle/test/LazyTests.kt
+++ apollo-gradle-plugin/src/test/kotlin/com/apollographql/apollo/gradle/test/LazyTests.kt
@@ -25,4 +25,39 @@ configure<ApolloExtension> {
       TestUtils.executeGradle(dir, "tasks", "--all")
     }
   }
+
+
+  @Test
+  fun `configuring compilation unit input carries task dependencies`() {
+
+    val apolloConfiguration = """
+abstract class InstallGraphQLFilesTask: DefaultTask() {
+  @get:OutputDirectory
+  abstract val outputDir: DirectoryProperty
+  @TaskAction
+  fun taskAction() {
+    println("installing graphql files")
+    project.file( "src/main/graphql/com/example").copyRecursively(outputDir.asFile.get())
+  }
+}
+val installTask = tasks.register("installTask", InstallGraphQLFilesTask::class.java) {
+  outputDir.set(project.file("build/toto"))
+}
+configure<ApolloExtension> {
+  onCompilationUnit {
+    graphqlSourceDirectorySet.srcDir(installTask.flatMap { it.outputDir })
+  }
+}
+""".trimIndent()
+
+    TestUtils.withProject(
+        usesKotlinDsl = true,
+        plugins = listOf(TestUtils.javaPlugin, TestUtils.apolloPlugin),
+        apolloConfiguration = apolloConfiguration
+    ) { dir ->
+      val result = TestUtils.executeTask("generateApolloSources", dir)
+      Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateApolloSources")!!.outcome)
+      Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":installTask")?.outcome)
+    }
+  }
 }
