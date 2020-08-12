170466d47e226706ffddf78fe14bfa72d5db61f1
Martin Bonnin <martin@mbonnin.net>
2020-08-12 16:17:27 +0200

    [SDL] add a basic validation test (#2493)
    * add a validation in case someone tries to inherit a non-interface type
    
    * remove debug

--- apollo-compiler/src/test/kotlin/com/apollographql/apollo/compiler/GraphSdlParseTest.kt
+++ apollo-compiler/src/test/kotlin/com/apollographql/apollo/compiler/GraphSdlParseTest.kt
@@ -1,9 +1,14 @@
 package com.apollographql.apollo.compiler
 
+import com.apollographql.apollo.compiler.parser.error.ParseException
 import com.apollographql.apollo.compiler.parser.introspection.IntrospectionSchema
 import com.apollographql.apollo.compiler.parser.sdl.GraphSdlSchema
 import com.apollographql.apollo.compiler.parser.sdl.toIntrospectionSchema
+import com.google.common.truth.Truth.assertThat
+import com.squareup.moshi.JsonClass
+import com.squareup.moshi.Moshi
 import org.junit.Assert.assertEquals
+import org.junit.Assert.fail
 import org.junit.Test
 import java.io.File
 
@@ -11,7 +16,7 @@ class GraphSdlParseTest() {
 
   @Test
   fun `SDL schema parsed successfully and produced the same introspection schema`() {
-    val actualSchema = GraphSdlSchema(File("src/test/sdl/schema.graphql")).toIntrospectionSchema().normalize()
+    val actualSchema = GraphSdlSchema(File("src/test/sdl/schema.sdl")).toIntrospectionSchema().normalize()
     val expectedSchema = IntrospectionSchema(File("src/test/sdl/schema.json")).normalize()
     assertEquals(actualSchema.toString(), expectedSchema.toString())
   }
@@ -28,6 +33,16 @@ class GraphSdlParseTest() {
       is IntrospectionSchema.Type.Union -> copy(fields = fields?.sortedBy { field -> field.name })
       is IntrospectionSchema.Type.Enum -> this
       is IntrospectionSchema.Type.InputObject -> copy(inputFields = inputFields.sortedBy { field -> field.name })
+    }
+  }
+
+  @Test
+  fun `implementing an object fails`() {
+    try {
+      GraphSdlSchema(File("src/test/sdl/implements-object.sdl"))
+      fail("parse expected to fail but was successful")
+    } catch (e: ParseException) {
+      assertThat(e.message).contains("Object `Cat` cannot implement non-interface `Animal`")
     }
   }
 }
