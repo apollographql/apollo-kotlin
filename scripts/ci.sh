1320daa73d8acfcdba4529918a7ead2359a77de2
Martin Bonnin <martin@mbonnin.net>
2020-08-21 21:31:06 +0200

    do not release from CI (#2526)

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -10,6 +10,4 @@ export PATH="$ANDROID_HOME"/tools/bin:$PATH
 # check that the public API did not change with Metalava
 ./gradlew checkMetalava
 
-./gradlew publishIfNeeded -Pgradle.publish.key="$GRADLE_PUBLISH_KEY" -Pgradle.publish.secret="$GRADLE_PUBLISH_SECRET" --parallel
-# this is a separate task because sonatype does not support --parallel
-./gradlew publishToOssStagingIfNeeded
+./gradlew publishSnapshotsIfNeeded  --parallel
---
+++ scripts/publish.sh
@@ -0,0 +1,8 @@
+# Jcenter
+./gradlew publishAllPublicationsToBintrayRepository
+
+# Gradle Plugin Portal
+./gradlew :apollo-gradle-plugin:publishPlugins -Pgradle.publish.key="$GRADLE_PUBLISH_KEY" -Pgradle.publish.secret="$GRADLE_PUBLISH_SECRET"
+
+# MavenCentral (go to https://oss.sonatype.org/ to close and release the repository after this is run)
+./gradlew publishAllPublicationsToOssStagingRepository
