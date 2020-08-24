9d77d7606223401042ecb663532bfab380f0e7cd
Martin Bonnin <martin@mbonnin.net>
2020-08-24 15:44:47 +0200

    convert apollo-runtime to the mpp plugin

--- apollo-runtime/build.gradle.kts
+++ apollo-runtime/build.gradle.kts
@@ -1,20 +1,31 @@
 plugins {
-  `java-library`
-  kotlin("jvm")
+  kotlin("multiplatform")
 }
 
-dependencies {
-  api(project(":apollo-api"))
-  api(project(":apollo-normalized-cache"))
-  api(project(":apollo-http-cache-api"))
-  api(groovy.util.Eval.x(project, "x.dep.okHttp.okHttp"))
+kotlin {
+  jvm {
+    withJava()
+  }
 
-  add("testCompileOnly", groovy.util.Eval.x(project, "x.dep.jetbrainsAnnotations"))
-  add("testCompile", groovy.util.Eval.x(project, "x.dep.mockito"))
-  add("testImplementation", groovy.util.Eval.x(project, "x.dep.junit"))
-  add("testImplementation", groovy.util.Eval.x(project, "x.dep.truth"))
-  add("testImplementation", groovy.util.Eval.x(project, "x.dep.okHttp.mockWebServer"))
-  add("testImplementation", project(":apollo-rx2-support"))
+  sourceSets {
+    val jvmMain by getting {
+      dependencies {
+        api(project(":apollo-api"))
+        api(project(":apollo-normalized-cache"))
+        api(project(":apollo-http-cache-api"))
+        api(groovy.util.Eval.x(project, "x.dep.okHttp.okHttp"))
+      }
+    }
+    val jvmTest by getting {
+      dependencies {
+        api(groovy.util.Eval.x(project, "x.dep.mockito"))
+        implementation(groovy.util.Eval.x(project, "x.dep.junit"))
+        implementation(groovy.util.Eval.x(project, "x.dep.truth"))
+        implementation(groovy.util.Eval.x(project, "x.dep.okHttp.mockWebServer"))
+        implementation(project(":apollo-rx2-support"))
+      }
+    }
+  }
 }
 
 tasks.withType<Javadoc> {
