25b40e738787bac10008535406e443c2ebdd2978
Martin Bonnin <martin@mbonnin.net>
2020-09-10 11:35:29 +0200

    added a few comments

--- apollo-gradle-plugin/testProjects/multi-modules-diamond/leaf/build.gradle.kts
+++ apollo-gradle-plugin/testProjects/multi-modules-diamond/leaf/build.gradle.kts
@@ -10,10 +10,11 @@ dependencies {
   implementation(groovy.util.Eval.x(project, "x.dep.apollo.api"))
 
   implementation(project(":node1"))
-  apollo(project(":node1"))
   implementation(project(":node2"))
-  apollo(project(":node2"))
   implementation(project(":root"))
+
+  apollo(project(":node1"))
+  apollo(project(":node2"))
 }
 
 application {
