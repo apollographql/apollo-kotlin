d1f92d625f30daf3a61ceed96109cdeacce810fb
Martin Bonnin <martin@mbonnin.net>
2020-10-05 21:37:27 +0200

    add the samples back

--- composite/settings.gradle.kts
+++ composite/settings.gradle.kts
@@ -5,4 +5,14 @@ rootProject.name = "apollo-composite"
 include(":apollo-integration")
 project(":apollo-integration").projectDir = file("../apollo-integration")
 
+// Samples
+include(":kotlin-sample")
+project(":kotlin-sample").projectDir = file("../samples/kotlin-sample")
+include(":multiplatform")
+project(":multiplatform").projectDir = file("../samples/multiplatform")
+include(":multiplatform:kmp-android-app")
+project(":multiplatform:kmp-android-app").projectDir = file("../samples/multiplatform/kmp-android-app")
+include(":multiplatform:kmp-lib-sample")
+project(":multiplatform:kmp-lib-sample").projectDir = file("../samples/multiplatform/kmp-lib-sample")
+
 includeBuild("../")
