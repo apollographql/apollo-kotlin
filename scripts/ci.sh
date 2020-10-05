7645e2f92b7c1a10ffad0ad714b3642d9d182dbe
Martin Bonnin <martin@mbonnin.net>
2020-10-05 10:31:31 +0200

    disable metalava in the dev-3.x branch for now

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -8,6 +8,7 @@ export PATH="$ANDROID_HOME"/tools/bin:$PATH
 ./gradlew clean build --stacktrace --max-workers=2
 ./gradlew -p composite build
 # check that the public API did not change with Metalava
-./gradlew checkMetalava
+# reenable when the 3.x API is more stable
+# ./gradlew checkMetalava
 
 ./gradlew publishSnapshotsIfNeeded  --parallel
