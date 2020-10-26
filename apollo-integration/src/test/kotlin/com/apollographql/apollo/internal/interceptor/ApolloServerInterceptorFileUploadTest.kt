bba326d28fdac2b7d2ce23a8dc59b3a8d1a02661
Martin Bonnin <martin@mbonnin.net>
2020-10-26 10:42:26 +0100

    [Documentation] use the correct "image/jpeg" mimetype.  (#2699)
    * use the correct "image/jpeg" mimetype. Thanks @NitroG42 for noticing!
    
    * fix tests

--- apollo-integration/src/test/kotlin/com/apollographql/apollo/internal/interceptor/ApolloServerInterceptorFileUploadTest.kt
+++ apollo-integration/src/test/kotlin/com/apollographql/apollo/internal/interceptor/ApolloServerInterceptorFileUploadTest.kt
@@ -36,7 +36,7 @@ class ApolloServerInterceptorFileUploadTest {
   private val file1 = createFile("file1.jpg", "content_file1")
   private val file2 = createFile("file2.png", "content_file2")
   private val upload0 = FileUpload("plain/txt", file0)
-  private val upload1 = FileUpload("image/jpg", file1)
+  private val upload1 = FileUpload("image/jpeg", file1)
   private val upload2 = FileUpload("image/png", file2)
   private val uploadSource = object: FileUpload("image/png") {
     val content = "content_source".encodeUtf8()
@@ -246,7 +246,7 @@ class ApolloServerInterceptorFileUploadTest {
     val part1 = body.parts()[1]
     assertMapPart(part1, "expectedMapPartBodySingle.json")
     val part2 = body.parts()[2]
-    assertFileContentPart(part2, "0", "file1.jpg", "image/jpg")
+    assertFileContentPart(part2, "0", "file1.jpg", "image/jpeg")
   }
 
   private fun assertRequestBodyTwice(request: Request) {
@@ -262,7 +262,7 @@ class ApolloServerInterceptorFileUploadTest {
     val part1 = body.parts()[1]
     assertMapPart(part1, "expectedMapPartBodyTwice.json")
     val part2 = body.parts()[2]
-    assertFileContentPart(part2, "0", "file1.jpg", "image/jpg")
+    assertFileContentPart(part2, "0", "file1.jpg", "image/jpeg")
     val part3 = body.parts()[3]
     assertFileContentPart(part3, "1", "file2.png", "image/png")
   }
@@ -280,7 +280,7 @@ class ApolloServerInterceptorFileUploadTest {
     val part1 = body.parts()[1]
     assertMapPart(part1, "expectedMapPartBodyMultiple.json")
     val part2 = body.parts()[2]
-    assertFileContentPart(part2, "0", "file1.jpg", "image/jpg")
+    assertFileContentPart(part2, "0", "file1.jpg", "image/jpeg")
     val part3 = body.parts()[3]
     assertFileContentPart(part3, "1", "file2.png", "image/png")
   }
