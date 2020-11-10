b136871a93d5595005d9f32b2f10376831ab0d4b
Martin Bonnin <martin@mbonnin.net>
2020-11-10 19:57:05 +0100

    disable parsing of default variables for now

--- apollo-compiler/src/main/kotlin/com/apollographql/apollo/compiler/ir/Variable.kt
+++ apollo-compiler/src/main/kotlin/com/apollographql/apollo/compiler/ir/Variable.kt
@@ -3,7 +3,6 @@ package com.apollographql.apollo.compiler.ir
 data class Variable(
     val name: String,
     val type: String,
-    val defaultValue: Any?,
     val sourceLocation: SourceLocation
 ) {
   fun optional(): Boolean = !type.endsWith(suffix = "!")
