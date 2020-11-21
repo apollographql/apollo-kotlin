99d61817f79ed3aea7c182cdd93569e4874308cb
Martin Bonnin <martin@mbonnin.net>
2020-11-21 16:21:48 +0100

    Increase Gradle verbosity during integration test to investigate Github Action hangs

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -6,7 +6,9 @@ set -x
 export PATH="$ANDROID_HOME"/tools/bin:$PATH
 
 ./gradlew clean build --stacktrace --max-workers=2
-./gradlew -p composite build
+# run with -i to debug an issue in Github Actions randomly hanging in :apollo-integration:test
+# remove when it's working again
+./gradlew -p composite build -i
 # check that the public API did not change with Metalava
 ./gradlew checkMetalava
 
