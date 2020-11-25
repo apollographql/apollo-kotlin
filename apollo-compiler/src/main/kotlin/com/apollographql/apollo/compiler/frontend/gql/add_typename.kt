c181b9c37d87c0732c901691a302b0c5c5fcaec3
Martin Bonnin <martin@mbonnin.net>
2020-11-25 19:16:15 +0100

    add typename to fragments as well

--- apollo-compiler/src/main/kotlin/com/apollographql/apollo/compiler/frontend/gql/add_typename.kt
+++ apollo-compiler/src/main/kotlin/com/apollographql/apollo/compiler/frontend/gql/add_typename.kt
@@ -2,14 +2,14 @@ package com.apollographql.apollo.compiler.frontend.gql
 
 fun GQLOperationDefinition.withTypenameWhenNeeded(schema: Schema): GQLOperationDefinition {
   return copy(
-      selectionSet = selectionSet.withTypenameWhenNeeded(schema, true)
+      selectionSet = selectionSet.withTypenameWhenNeeded(schema)
   )
 }
 
 fun GQLFragmentDefinition.withTypenameWhenNeeded(schema: Schema): GQLFragmentDefinition {
   return copy(
       // Fragment spread are not root selections by definition since they must be included by another selection set
-      selectionSet = selectionSet.withTypenameWhenNeeded(schema, false)
+      selectionSet = selectionSet.withTypenameWhenNeeded(schema)
   )
 }
 
@@ -22,25 +22,25 @@ private val typeNameField = GQLField(
     alias = null
 )
 
-private fun GQLSelectionSet.withTypenameWhenNeeded(schema: Schema, isRootSelection: Boolean): GQLSelectionSet {
+private fun GQLSelectionSet.withTypenameWhenNeeded(schema: Schema): GQLSelectionSet {
 
   var newSelections = selections.map {
     when (it) {
       is GQLInlineFragment -> {
         it.copy(
-            selectionSet = it.selectionSet.withTypenameWhenNeeded(schema, false)
+            selectionSet = it.selectionSet.withTypenameWhenNeeded(schema)
         )
       }
       is GQLFragmentSpread -> it
       is GQLField -> it.copy(
-          selectionSet = it.selectionSet?.withTypenameWhenNeeded(schema, true)
+          selectionSet = it.selectionSet?.withTypenameWhenNeeded(schema)
       )
     }
   }
 
   val hasFragment = selections.filter { it is GQLFragmentSpread || it is GQLInlineFragment }.isNotEmpty()
 
-  newSelections = if (isRootSelection && hasFragment) {
+  newSelections = if (hasFragment) {
     // remove the __typename if it exists
     // and add it again at the top so we're guaranteed to have it at the beginning of json parsing
     listOf(typeNameField) + newSelections.filterNot { (it as? GQLField)?.name == "__typename" }
