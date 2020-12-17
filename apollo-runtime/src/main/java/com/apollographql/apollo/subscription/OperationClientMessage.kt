c8a5fa259ff75798ea6f83728554e321f46ad148
Nicklas Ansman Giertz <nicklas@ansman.se>
2020-12-16 20:04:41 -0500

    Rewrite OperationClientMessage to a sealed class

--- apollo-runtime/src/main/java/com/apollographql/apollo/subscription/OperationClientMessage.kt
+++ apollo-runtime/src/main/java/com/apollographql/apollo/subscription/OperationClientMessage.kt
@@ -1,133 +1,113 @@
-package com.apollographql.apollo.subscription;
+package com.apollographql.apollo.subscription
 
-import com.apollographql.apollo.api.ScalarTypeAdapters;
-import com.apollographql.apollo.api.Subscription;
-import com.apollographql.apollo.api.internal.json.JsonWriter;
-import com.apollographql.apollo.api.internal.json.Utils;
-import okio.Buffer;
-import org.jetbrains.annotations.NotNull;
+import com.apollographql.apollo.api.ScalarTypeAdapters
+import com.apollographql.apollo.api.Subscription
+import com.apollographql.apollo.api.internal.json.JsonWriter
+import com.apollographql.apollo.api.internal.json.Utils
+import com.apollographql.apollo.api.internal.json.writeObject
+import okio.Buffer
+import java.io.IOException
 
-import java.io.IOException;
-import java.util.Map;
-
-import static com.apollographql.apollo.api.internal.Utils.checkNotNull;
-
-@SuppressWarnings("WeakerAccess")
-public abstract class OperationClientMessage {
-  static final String JSON_KEY_ID = "id";
-  static final String JSON_KEY_TYPE = "type";
-  static final String JSON_KEY_PAYLOAD = "payload";
-
-  OperationClientMessage() {
-  }
-
-  public String toJsonString() {
-    try {
-      Buffer buffer = new Buffer();
-      JsonWriter writer = JsonWriter.of(buffer);
-      writer.beginObject();
-      writeToJson(writer);
-      writer.endObject();
-      writer.close();
-      return buffer.readUtf8();
-    } catch (IOException e) {
-      throw new RuntimeException("Failed to serialize to json", e);
-    }
-  }
-
-  public abstract void writeToJson(@NotNull JsonWriter writer) throws IOException;
-
-  public static final class Init extends OperationClientMessage {
-    private static final String TYPE = "connection_init";
-    private final Map<String, Object> connectionParams;
-
-    public Init(@NotNull Map<String, Object> connectionParams) {
-      this.connectionParams = checkNotNull(connectionParams, "connectionParams == null");
-    }
-
-    @Override public void writeToJson(@NotNull JsonWriter writer) throws IOException {
-      checkNotNull(writer, "writer == null");
-      writer.name(JSON_KEY_TYPE).value(TYPE);
-      if (!connectionParams.isEmpty()) {
-        writer.name(JSON_KEY_PAYLOAD);
-        Utils.writeToJson(connectionParams, writer);
-      }
-    }
-  }
-
-  public static final class Start extends OperationClientMessage {
-    private static final String TYPE = "start";
-    private static final String JSON_KEY_QUERY = "query";
-    private static final String JSON_KEY_VARIABLES = "variables";
-    private static final String JSON_KEY_OPERATION_NAME = "operationName";
-    private static final String JSON_KEY_EXTENSIONS = "extensions";
-    private static final String JSON_KEY_EXTENSIONS_PERSISTED_QUERY = "persistedQuery";
-    private static final String JSON_KEY_EXTENSIONS_PERSISTED_QUERY_VERSION = "version";
-    private static final String JSON_KEY_EXTENSIONS_PERSISTED_QUERY_HASH = "sha256Hash";
-    private final ScalarTypeAdapters scalarTypeAdapters;
-
-    public final String subscriptionId;
-    public final Subscription<?, ?, ?> subscription;
-    public final boolean autoPersistSubscription;
-    public final boolean sendSubscriptionDocument;
-
-    public Start(@NotNull String subscriptionId, @NotNull Subscription<?, ?, ?> subscription,
-        @NotNull ScalarTypeAdapters scalarTypeAdapters, boolean autoPersistSubscription, boolean sendSubscriptionDocument) {
-      this.subscriptionId = checkNotNull(subscriptionId, "subscriptionId == null");
-      this.subscription = checkNotNull(subscription, "subscription == null");
-      this.scalarTypeAdapters = checkNotNull(scalarTypeAdapters, "scalarTypeAdapters == null");
-      this.autoPersistSubscription = autoPersistSubscription;
-      this.sendSubscriptionDocument = sendSubscriptionDocument;
-    }
-
-    @Override public void writeToJson(@NotNull JsonWriter writer) throws IOException {
-      checkNotNull(writer, "writer == null");
-      writer.name(JSON_KEY_ID).value(subscriptionId);
-      writer.name(JSON_KEY_TYPE).value(TYPE);
-      writer.name(JSON_KEY_PAYLOAD).beginObject();
-      writer.name(JSON_KEY_VARIABLES).jsonValue(subscription.variables().marshal(scalarTypeAdapters));
-      writer.name(JSON_KEY_OPERATION_NAME).value(subscription.name().name());
-
-      if (!autoPersistSubscription || sendSubscriptionDocument) {
-        writer.name(JSON_KEY_QUERY).value(subscription.queryDocument());
+sealed class OperationClientMessage {
+  fun toJsonString(): String =
+      try {
+        val buffer = Buffer()
+        JsonWriter.of(buffer).use { writer ->
+          writer.writeObject {
+            writeToJson(this)
+          }
+        }
+        buffer.readUtf8()
+      } catch (e: IOException) {
+        throw RuntimeException("Failed to serialize to json", e)
       }
 
-      if (autoPersistSubscription) {
-        writer
-            .name(JSON_KEY_EXTENSIONS).beginObject()
-            .name(JSON_KEY_EXTENSIONS_PERSISTED_QUERY).beginObject()
-            .name(JSON_KEY_EXTENSIONS_PERSISTED_QUERY_VERSION).value(1)
-            .name(JSON_KEY_EXTENSIONS_PERSISTED_QUERY_HASH).value(subscription.operationId())
-            .endObject()
-            .endObject();
+  @Throws(IOException::class)
+  abstract fun writeToJson(writer: JsonWriter)
+
+  companion object {
+    const val JSON_KEY_ID = "id"
+    const val JSON_KEY_TYPE = "type"
+    const val JSON_KEY_PAYLOAD = "payload"
+  }
+
+  class Init(private val connectionParams: Map<String, Any?>) : OperationClientMessage() {
+    @Throws(IOException::class)
+    override fun writeToJson(writer: JsonWriter) {
+      writer.name(JSON_KEY_TYPE).value(TYPE)
+      if (connectionParams.isNotEmpty()) {
+        writer.name(JSON_KEY_PAYLOAD)
+        Utils.writeToJson(connectionParams, writer)
       }
+    }
 
-      writer.endObject();
+    companion object {
+      private const val TYPE = "connection_init"
     }
   }
 
-  public static final class Stop extends OperationClientMessage {
-    private static final String TYPE = "stop";
+  class Start(
+      val subscriptionId: String,
+      val subscription: Subscription<*, *, *>,
+      private val scalarTypeAdapters: ScalarTypeAdapters,
+      val autoPersistSubscription: Boolean,
+      val sendSubscriptionDocument: Boolean
+  ) : OperationClientMessage() {
 
-    public final String subscriptionId;
-
-    public Stop(@NotNull String subscriptionId) {
-      this.subscriptionId = checkNotNull(subscriptionId, "subscriptionId == null");
+    @Throws(IOException::class)
+    override fun writeToJson(writer: JsonWriter) {
+      writer.name(JSON_KEY_ID).value(subscriptionId)
+      writer.name(JSON_KEY_TYPE).value(TYPE)
+      writer.name(JSON_KEY_PAYLOAD).writeObject {
+        name(JSON_KEY_VARIABLES).jsonValue(subscription.variables().marshal(scalarTypeAdapters))
+        name(JSON_KEY_OPERATION_NAME).value(subscription.name().name())
+        if (!autoPersistSubscription || sendSubscriptionDocument) {
+          writer.name(JSON_KEY_QUERY).value(subscription.queryDocument())
+        }
+        if (autoPersistSubscription) {
+          name(JSON_KEY_EXTENSIONS).writeObject {
+            name(JSON_KEY_EXTENSIONS_PERSISTED_QUERY).writeObject {
+              name(JSON_KEY_EXTENSIONS_PERSISTED_QUERY_VERSION).value(1)
+              name(JSON_KEY_EXTENSIONS_PERSISTED_QUERY_HASH).value(subscription.operationId())
+            }
+          }
+        }
+      }
     }
 
-    @Override public void writeToJson(@NotNull JsonWriter writer) throws IOException {
-      checkNotNull(writer, "writer == null");
-      writer.name(JSON_KEY_ID).value(subscriptionId);
-      writer.name(JSON_KEY_TYPE).value(TYPE);
+    companion object {
+      private const val TYPE = "start"
+      private const val JSON_KEY_QUERY = "query"
+      private const val JSON_KEY_VARIABLES = "variables"
+      private const val JSON_KEY_OPERATION_NAME = "operationName"
+      private const val JSON_KEY_EXTENSIONS = "extensions"
+      private const val JSON_KEY_EXTENSIONS_PERSISTED_QUERY = "persistedQuery"
+      private const val JSON_KEY_EXTENSIONS_PERSISTED_QUERY_VERSION = "version"
+      private const val JSON_KEY_EXTENSIONS_PERSISTED_QUERY_HASH = "sha256Hash"
     }
   }
 
-  public static final class Terminate extends OperationClientMessage {
-    private static final String TYPE = "connection_terminate";
+  class Stop(val subscriptionId: String) : OperationClientMessage() {
 
-    @Override public void writeToJson(@NotNull JsonWriter writer) throws IOException {
-      checkNotNull(writer, "writer == null");
-      writer.name(JSON_KEY_TYPE).value(TYPE);
+    @Throws(IOException::class)
+    override fun writeToJson(writer: JsonWriter) {
+      writer.name(JSON_KEY_ID).value(subscriptionId)
+      writer.name(JSON_KEY_TYPE).value(TYPE)
+    }
+
+    companion object {
+      private const val TYPE = "stop"
+    }
+  }
+
+  class Terminate : OperationClientMessage() {
+    @Throws(IOException::class)
+    override fun writeToJson(writer: JsonWriter) {
+      writer.name(JSON_KEY_TYPE).value(TYPE)
+    }
+
+    companion object {
+      private const val TYPE = "connection_terminate"
     }
   }
 }
