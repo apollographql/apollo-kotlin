93f5371d3630492dba9e052bbbbd3a5644b91291
Martin Bonnin <martin@mbonnin.net>
2021-01-11 13:39:45 +0100

    Do not sync android modules in the composite project

--- composite/settings.gradle.kts
+++ composite/settings.gradle.kts
@@ -1,16 +1,18 @@
 rootProject.name = "apollo-composite"
 
 // Samples
-include(":java-sample")
-project(":java-sample").projectDir = file("../samples/java-sample")
-include(":kotlin-sample")
-project(":kotlin-sample").projectDir = file("../samples/kotlin-sample")
 include(":multiplatform")
 project(":multiplatform").projectDir = file("../samples/multiplatform")
-include(":multiplatform:kmp-android-app")
-project(":multiplatform:kmp-android-app").projectDir = file("../samples/multiplatform/kmp-android-app")
 include(":multiplatform:kmp-lib-sample")
 project(":multiplatform:kmp-lib-sample").projectDir = file("../samples/multiplatform/kmp-lib-sample")
+if (System.getProperty("idea.sync.active") == null) {
+    include(":java-sample")
+    project(":java-sample").projectDir = file("../samples/java-sample")
+    include(":kotlin-sample")
+    project(":kotlin-sample").projectDir = file("../samples/kotlin-sample")
+    include(":multiplatform:kmp-android-app")
+    project(":multiplatform:kmp-android-app").projectDir = file("../samples/multiplatform/kmp-android-app")
+}
 
 // Integration Tests
 include(":apollo-integration")
