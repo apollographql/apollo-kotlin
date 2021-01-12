7923178a3b10b407a8515dada81afa144063124c
Ivan Savytskyi <ivan.savitsky@gmail.com>
2021-01-11 21:25:09 -0500

    Update apollo-gradle-plugin/src/main/kotlin/com/apollographql/apollo/gradle/api/Service.kt
    Co-authored-by: Martin Bonnin <martin@mbonnin.net>

--- apollo-gradle-plugin/src/main/kotlin/com/apollographql/apollo/gradle/api/Service.kt
+++ apollo-gradle-plugin/src/main/kotlin/com/apollographql/apollo/gradle/api/Service.kt
@@ -201,8 +201,7 @@ interface Service {
    * Whether or not generate default implementation classes for GraphQL fragments.
    * Default value is `false`, means only interfaces are been generated.
    *
-   * One of the use cases where generated implementation models are useful, is writing fragment response data directly to the Apollo store.
-   * To perform this operation you need fragment implementation class.
+   * Most the the times, fragment implementations are not needed because you can easily access fragments interfaces and read all data from your queries. They are needed if you want to be able to build fragments outside of an operation. For an exemple to programmatically build a fragment that is reused in another part of your code or to read and write fragments to the cache.
    */
   val generateFragmentImplementations: Property<Boolean>
 
