d75b24f09997871bdc61ee85578e6b321692fff0
Martin Bonnin <martin@mbonnin.net>
2021-02-02 19:17:44 +0100

    fix tests

--- apollo-idling-resource/src/test/java/com/apollographql/apollo/test/espresso/ApolloIdlingResourceTest.kt
+++ apollo-idling-resource/src/test/java/com/apollographql/apollo/test/espresso/ApolloIdlingResourceTest.kt
@@ -7,6 +7,7 @@ import com.apollographql.apollo.api.Operation
 import com.apollographql.apollo.api.Operation.Companion.EMPTY_VARIABLES
 import com.apollographql.apollo.api.Query
 import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.api.ResponseField
 import com.apollographql.apollo.api.CustomScalarAdapters
 import com.apollographql.apollo.api.internal.OperationRequestBodyComposer.compose
 import com.apollographql.apollo.api.internal.ResponseAdapter
