618fa7df194395f3b1bbdf5a21aff30c03ac5524
Martin Bonnin <martin@mbonnin.net>
2021-03-03 10:43:50 +0100

    remove ReadableStore and WriteableStore

--- apollo-normalized-cache-api/src/commonMain/kotlin/com/apollographql/apollo3/cache/normalized/internal/ReadableStore.kt
+++
@@ -1,9 +0,0 @@
-package com.apollographql.apollo3.cache.normalized.internal
-
-import com.apollographql.apollo3.cache.CacheHeaders
-import com.apollographql.apollo3.cache.normalized.Record
-
-interface ReadableStore {
-  fun read(key: String, cacheHeaders: CacheHeaders): Record?
-  fun read(keys: Collection<String>, cacheHeaders: CacheHeaders): Collection<Record>
-}
--- apollo-normalized-cache-api/src/commonMain/kotlin/com/apollographql/apollo3/cache/normalized/internal/WriteableStore.kt
+++
@@ -1,9 +0,0 @@
-package com.apollographql.apollo3.cache.normalized.internal
-
-import com.apollographql.apollo3.cache.CacheHeaders
-import com.apollographql.apollo3.cache.normalized.Record
-
-interface WriteableStore : ReadableStore {
-  fun merge(records: Collection<Record>, cacheHeaders: CacheHeaders): Set<String>
-  fun merge(record: Record, cacheHeaders: CacheHeaders): Set<String>
-}
