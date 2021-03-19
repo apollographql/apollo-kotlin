bd9664a1ad703f2cc3c37afa83ea325993ac79bb
Martin Bonnin <martin@mbonnin.net>
2021-03-19 19:43:01 +0100

    cleanup tests

--- apollo-compiler/src/test/kotlin/com/apollographql/apollo3/compiler/IrTest.kt
+++ apollo-compiler/src/test/kotlin/com/apollographql/apollo3/compiler/IrTest.kt
@@ -3,12 +3,9 @@ package com.apollographql.apollo3.compiler
 import com.apollographql.apollo3.compiler.frontend.GQLFragmentDefinition
 import com.apollographql.apollo3.compiler.frontend.GQLOperationDefinition
 import com.apollographql.apollo3.compiler.frontend.GraphQLParser
-import com.apollographql.apollo3.compiler.unified.Edge
 import com.apollographql.apollo3.compiler.unified.IrBuilder
-import com.apollographql.apollo3.compiler.unified.transitiveReduce
 import org.junit.Test
 import java.io.File
-import java.lang.UnsupportedOperationException
 
 class IrTest {
   @Test
@@ -36,51 +33,6 @@ class IrTest {
       throw UnsupportedOperationException("Multiple operations are not supported")
     }
 
-    val irOperation = ir.operations.first()
-  }
-
-  @Test
-  fun testTransitiveReduce() {
-    /**
-     * 1 -> 2 -> 3;
-     * 1 -> 3; // should be removed
-     */
-    val input = listOf(
-        Edge("1", "2"),
-        Edge("2", "3"),
-        Edge("1", "3"),
-    )
-
-    val output = transitiveReduce(input)
-
-    check(
-        output == listOf(
-            Edge("1", "2"),
-            Edge("2", "3"),
-        )
-    )
-  }
-
-  data class Node(val value: String)
-
-  @Test
-  fun testTransitiveReduceUsesEquals() {
-    /**
-     * Same test as [testTransitiveReduce] but using data classes so that the test would fail if referential equality were used
-     */
-    val input = listOf(
-        Edge(Node("1"), Node("2")),
-        Edge(Node("2"), Node("3")),
-        Edge(Node("1"), Node("3")),
-    )
-
-    val output = transitiveReduce(input)
-
-    check(
-        output == listOf(
-            Edge(Node("1"),Node( "2")),
-            Edge(Node("2"),Node( "3")),
-        )
-    )
+    println(ir.operations.first())
   }
 }
