67410b2857e91676b9fd2be87a989200691ad0ad
Martin Bonnin <martin@mbonnin.net>
2021-03-29 10:42:15 +0200

    Gradle plugin: Shadow everything (#3019)
    * Gradle plugin: Shade everything
    
    This avoids conflicts with other dependencies like moshi that wouldn't have
    conflicted otherwise but depend on relocated libraries for some of their
    API
    
    'com.squareup.moshi.JsonReader com.squareup.moshi.JsonReader.of(com.apollographql.relocated.okio.BufferedSource)'
    
    * fix comment
    
    * be more granular in what we relocate
    
    * cleanup
    
    * do not expose the full antlr artifact as API of apollo-compiler
    
    * no need for toSet
    
    * actually,antlr is an implementation, not api dependency
    
    * no need for javax.json either

--- apollo-compiler/build.gradle.kts
+++ apollo-compiler/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
 
 dependencies {
   antlr(groovy.util.Eval.x(project, "x.dep.antlr.antlr"))
+  implementation(groovy.util.Eval.x(project, "x.dep.antlr.runtime"))
   implementation(groovy.util.Eval.x(project, "x.dep.moshi.adapters"))
   implementation(groovy.util.Eval.x(project, "x.dep.moshi.moshi"))
   implementation(groovy.util.Eval.x(project, "x.dep.poet.kotlin"))
@@ -19,6 +20,12 @@ dependencies {
   testImplementation(groovy.util.Eval.x(project, "x.dep.kotlinCompileTesting"))
   testImplementation(groovy.util.Eval.x(project, "x.dep.junit"))
   testImplementation(groovy.util.Eval.x(project, "x.dep.truth"))
+}
+
+// Only expose the antlr runtime dependency
+// See https://github.com/gradle/gradle/issues/820#issuecomment-288838412
+configurations[JavaPlugin.API_CONFIGURATION_NAME].let { apiConfiguration ->
+  apiConfiguration.setExtendsFrom(apiConfiguration.extendsFrom.filter { it.name != "antlr" })
 }
 
 abstract class GeneratePluginVersion : DefaultTask() {
