04b5cca4dfb2aabaa3173df8ab4358112d092deb
Martin Bonnin <martin@mbonnin.net>
2021-04-21 17:37:18 +0200

    when computing shapes, only take the user shapes into consideration

--- apollo-compiler/src/main/kotlin/com/apollographql/apollo3/compiler/unified/ir/TypeSet.kt
+++ apollo-compiler/src/main/kotlin/com/apollographql/apollo3/compiler/unified/ir/TypeSet.kt
@@ -13,34 +13,6 @@ internal typealias TypeSet = Set<String>
  */
 internal typealias PossibleTypes = Set<String>
 
-/**
- * Return the different possible shapes for all concrete types that satisfy [fieldType]
- *
- */
-internal fun computeShapes(schema: Schema, fieldType: String, typeConditions: Set<String>): Map<TypeSet, PossibleTypes> {
-  val possibleTypes = schema.typeDefinition(fieldType).possibleTypes(schema)
-
-  val typeConditionToPossibleTypes = typeConditions.map {
-    it to schema.typeDefinition(it).possibleTypes(schema).intersect(possibleTypes)
-  }
-
-  return schema.typeDefinitions.values.filterIsInstance<GQLObjectTypeDefinition>()
-      .map { it.name }
-      .map { concreteType ->
-        val matchedSupers = typeConditionToPossibleTypes.filter { it.second.contains(concreteType) }
-            .map { it.first }
-            .toSet()
-
-        matchedSupers to concreteType
-      }
-      .filter { it.first.isNotEmpty() }
-      .groupBy(
-          keySelector = { it.first },
-          valueTransform = { it.second }
-      )
-      .mapValues { it.value.toSet() }
-}
-
 internal fun TypeSet.implements(other: TypeSet) = intersect(other) == other
 
 internal fun subTypeCount(typeSet: TypeSet, candidates: Collection<TypeSet>): Int {
