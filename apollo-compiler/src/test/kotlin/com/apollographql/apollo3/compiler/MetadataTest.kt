7ff0c766c716b875b377e83d5ca5b413a4bb86ab
Martin Bonnin <martin@mbonnin.net>
2021-04-23 16:46:00 +0200

    update test fixtures

--- apollo-compiler/src/test/kotlin/com/apollographql/apollo3/compiler/MetadataTest.kt
+++ apollo-compiler/src/test/kotlin/com/apollographql/apollo3/compiler/MetadataTest.kt
@@ -97,7 +97,7 @@ class MetadataTest {
     alwaysGenerateTypesMatchingTest(emptySet())
 
     // Only scalar types are generated in the root
-    rootSourcesDir.assertContents("Schema.kt")
+    rootSourcesDir.assertContents("Types.kt")
 
     // Leaf contains its referenced types but not the unused ones
     leafSourcesDir.assertContents(
@@ -125,7 +125,7 @@ class MetadataTest {
     rootSourcesDir.assertContents(
         "Body1.kt",
         "Body1_InputAdapter.kt",
-        "Schema.kt",
+        "Types.kt",
         "Encoding.kt",
         "Encoding_ResponseAdapter.kt",
         "MessageInput1.kt",
@@ -180,7 +180,7 @@ class MetadataTest {
     // Root generates the fragment
     rootSourcesDir.assertContents(
         "CharacterFragment.kt",
-        "Schema.kt",
+        "Types.kt",
         "Episode.kt",
         "Episode_ResponseAdapter.kt"
     )
@@ -225,7 +225,7 @@ class MetadataTest {
 
     rootSourcesDir.assertContents(
         "CharacterFragment.kt",
-        "Schema.kt"
+        "Types.kt"
     )
 
     leafSourcesDir.assertContents(
