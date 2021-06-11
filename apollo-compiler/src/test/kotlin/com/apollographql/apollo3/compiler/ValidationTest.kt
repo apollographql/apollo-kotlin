a1c0f1a55699b01701edfaf706c240d3ad742bc6
Martin Bonnin <martin@mbonnin.net>
2021-06-11 17:25:47 +0200

    fix tests

--- apollo-compiler/src/test/kotlin/com/apollographql/apollo3/compiler/ValidationTest.kt
+++ apollo-compiler/src/test/kotlin/com/apollographql/apollo3/compiler/ValidationTest.kt
@@ -1,12 +1,12 @@
 package com.apollographql.apollo3.compiler
 
-import com.apollographql.apollo3.compiler.TestUtils.checkExpected
-import com.apollographql.apollo3.compiler.TestUtils.testParametersForGraphQLFilesIn
 import com.apollographql.apollo3.ast.Issue
 import com.apollographql.apollo3.ast.ParseResult
 import com.apollographql.apollo3.ast.parseAsGQLDocument
 import com.apollographql.apollo3.ast.validateAsOperations
-import com.apollographql.apollo3.ast.validateAsSchemaAndMerge
+import com.apollographql.apollo3.ast.validateAsSchema
+import com.apollographql.apollo3.compiler.TestUtils.checkExpected
+import com.apollographql.apollo3.compiler.TestUtils.testParametersForGraphQLFilesIn
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
@@ -36,7 +36,7 @@ class ValidationTest(name: String, private val graphQLFile: File) {
 
       when (parseResult) {
         is ParseResult.Error -> parseResult.issues
-        is ParseResult.Success -> parseResult.value.validateAsSchemaAndMerge()
+        is ParseResult.Success -> parseResult.value.validateAsSchema()
       }
     }
     issues.serialize()
