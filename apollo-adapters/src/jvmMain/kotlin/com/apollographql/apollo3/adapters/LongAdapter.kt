cd6a7f0be3da3d9a12cb50249b6182b567e18e56
Martin Bonnin <martin@mbonnin.net>
2021-06-17 15:39:00 +0200

    KDoc for adapters

--- apollo-adapters/src/jvmMain/kotlin/com/apollographql/apollo3/adapters/LongAdapter.kt
+++ apollo-adapters/src/jvmMain/kotlin/com/apollographql/apollo3/adapters/LongAdapter.kt
@@ -4,7 +4,14 @@ import com.apollographql.apollo3.api.Adapter
 import com.apollographql.apollo3.api.CustomScalarAdapters
 import com.apollographql.apollo3.api.json.JsonReader
 import com.apollographql.apollo3.api.json.JsonWriter
+import java.util.Date
 
+/**
+ * An [Adapter] that converts a Json number to/from a Java [Long]
+ *
+ * If the Json number does not fit in a [Long], an exception will be thrown
+ *
+ */
 object LongAdapter: Adapter<Long>  {
   override fun fromJson(reader: JsonReader, customScalarAdapters: CustomScalarAdapters): Long {
     return reader.nextString()!!.toLong()
