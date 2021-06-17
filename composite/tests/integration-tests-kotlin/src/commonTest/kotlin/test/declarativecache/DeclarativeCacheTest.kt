0c52f7d2954e4223482cdbe1883907deca453d1a
Martin Bonnin <martin@mbonnin.net>
2021-06-18 00:03:03 +0200

    added a test case that resolves a list of objects (#3177)

--- composite/tests/integration-tests-kotlin/src/commonTest/kotlin/test/declarativecache/DeclarativeCacheTest.kt
+++ composite/tests/integration-tests-kotlin/src/commonTest/kotlin/test/declarativecache/DeclarativeCacheTest.kt
@@ -1,11 +1,15 @@
 package test.declarativecache
 
+import com.apollographql.apollo3.api.CompiledField
 import com.apollographql.apollo3.api.CustomScalarAdapters
+import com.apollographql.apollo3.api.Executable
 import com.apollographql.apollo3.cache.normalized.ApolloStore
+import com.apollographql.apollo3.cache.normalized.CacheKey
 import com.apollographql.apollo3.cache.normalized.CacheResolver
 import com.apollographql.apollo3.cache.normalized.MemoryCacheFactory
 import com.apollographql.apollo3.testing.runWithMainLoop
 import declarativecache.GetBookQuery
+import declarativecache.GetBooksQuery
 import declarativecache.GetOtherBookQuery
 import declarativecache.GetOtherLibraryQuery
 import declarativecache.GetPromoBookQuery
@@ -61,5 +65,39 @@ class DeclarativeCacheTest {
     val data = store.readOperation(operation, CustomScalarAdapters.Empty)
 
     assertEquals("Promo", data?.book?.title)
+  }
+
+  @Test
+  fun canResolveList() = runWithMainLoop {
+    val cacheResolver = object : CacheResolver() {
+      override fun resolveField(field: CompiledField, variables: Executable.Variables, parent: Map<String, Any?>, parentKey: String): Any? {
+        if (field.name == "books") {
+          val isbns = field.resolveArgument("isbns", variables) as? List<String>
+          if (isbns != null) {
+            return isbns.map { buildCacheKey(field, listOf(it))}
+          }
+        }
+
+        return super.resolveField(field, variables, parent, parentKey)
+      }
+    }
+    val store = ApolloStore(MemoryCacheFactory(), cacheResolver)
+
+    val promoOperation = GetPromoBookQuery()
+    store.writeOperation(promoOperation, GetPromoBookQuery.Data(promoBook = GetPromoBookQuery.PromoBook(__typename =  "Book", title = "Title1", isbn = "1")))
+    store.writeOperation(promoOperation, GetPromoBookQuery.Data(promoBook = GetPromoBookQuery.PromoBook(__typename =  "Book", title = "Title2", isbn = "2")))
+    store.writeOperation(promoOperation, GetPromoBookQuery.Data(promoBook = GetPromoBookQuery.PromoBook(__typename =  "Book", title = "Title3", isbn = "3")))
+    store.writeOperation(promoOperation, GetPromoBookQuery.Data(promoBook = GetPromoBookQuery.PromoBook(__typename =  "Book", title = "Title4", isbn = "4")))
+
+    var operation = GetBooksQuery(isbns = listOf("4", "1"))
+    var data = store.readOperation(operation, CustomScalarAdapters.Empty)
+
+    assertEquals("Title4", data?.books?.get(0)?.title)
+    assertEquals("Title1", data?.books?.get(1)?.title)
+
+    operation = GetBooksQuery(isbns = listOf("3"))
+    data = store.readOperation(operation, CustomScalarAdapters.Empty)
+
+    assertEquals("Title3", data?.books?.get(0)?.title)
   }
 }
