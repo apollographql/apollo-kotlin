7901d3ff80c342b0153d1f606fa63194db56e11b
Martin Bonnin <martin@mbonnin.net>
2021-07-01 18:39:41 +0200

    first frop for 3.0 docs

--- apollo-gradle-plugin/src/main/kotlin/com/apollographql/apollo3/gradle/internal/DefaultIntrospection.kt
+++ apollo-gradle-plugin/src/main/kotlin/com/apollographql/apollo3/gradle/internal/DefaultIntrospection.kt
@@ -1,6 +1,7 @@
 package com.apollographql.apollo3.gradle.internal
 
 import com.apollographql.apollo3.gradle.api.Introspection
+import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.MapProperty
 import org.gradle.api.provider.Property
 
@@ -8,4 +9,5 @@ import org.gradle.api.provider.Property
 abstract class DefaultIntrospection: Introspection {
   abstract override val endpointUrl: Property<String>
   abstract override val headers: MapProperty<String, String>
+  abstract override val schemaFile: RegularFileProperty
 }
