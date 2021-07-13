f7caee8dfeef54d05e88a1fa1e6c7cac8552c2d8
Martin Bonnin <martin@mbonnin.net>
2021-07-13 12:19:31 +0200

    Added a graphql-ws + subscription test (#3229)
    * added a graphql-ws + subscription test
    
    * oops, revert to payload

--- composite/tests/websockets/src/commonTest/kotlin/GraphQLWsTest.kt
+++ composite/tests/websockets/src/commonTest/kotlin/GraphQLWsTest.kt
@@ -2,17 +2,19 @@ import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.network.ws.GraphQLWsProtocol
 import com.apollographql.apollo3.network.ws.WebSocketNetworkTransport
 import com.apollographql.apollo3.testing.runWithMainLoop
+import graphql.ws.GreetingsSubscription
 import graphql.ws.HelloQuery
+import kotlinx.coroutines.flow.toList
 import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
+// Ignored because it depends on a local server
+// See https://github.com/martinbonnin/graphql-ws-server
+@Ignore
 class GraphQLWsTest {
   @Test
-  @Ignore
-  // Ignored because it depends on a local server
-  // See https://github.com/martinbonnin/graphql-ws-server
-  fun test() {
+  fun queryOverWebSocket() {
     val apolloClient = ApolloClient(
         networkTransport = WebSocketNetworkTransport(
             serverUrl = "http://localhost:9090/graphql",
@@ -22,6 +24,21 @@ class GraphQLWsTest {
 
     runWithMainLoop {
       assertEquals("Hello World!", apolloClient.query(HelloQuery()).data?.hello)
+    }
+  }
+
+  @Test
+  fun subscriptionOverWebSocket() {
+    val apolloClient = ApolloClient(
+        networkTransport = WebSocketNetworkTransport(
+            serverUrl = "http://localhost:9090/graphql",
+            protocol = GraphQLWsProtocol()
+        )
+    )
+
+    runWithMainLoop {
+      val list = apolloClient.subscribe(GreetingsSubscription()).toList()
+      assertEquals(listOf("Hi", "Bonjour", "Hola", "Ciao", "Zdravo"), list.map { it.data?.greetings })
     }
   }
 }
