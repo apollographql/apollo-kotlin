920e3a7e5fae21e60df4d558d30451a175b6b015
Martin Bonnin <martin@mbonnin.net>
2021-07-16 17:56:42 +0200

    install android tools

--- docs/generate-docs.sh
+++ docs/generate-docs.sh
@@ -3,11 +3,22 @@
 
 set -e
 
+CWD=$(cwd)
+
 curl -s "https://get.sdkman.io" | bash
-source ~/.sdkman/bin/sdkman-init.sh
+source "$HOME"/.sdkman/bin/sdkman-init.sh
 
 sdk install java 11.0.11.hs-adpt
 sdk install kotlin 1.5.21
+
+export ANDROID_SDK_ROOT="$HOME"/android
+mkdir -p "$ANDROID_SDK_ROOT"/cmdline-tools/
+
+curl https://dl.google.com/android/repository/commandlinetools-linux-6858069_latest.zip > android-commandline-tools.zip
+unzip ../android-commandline-tools.zip
+mv cmdline-tools "$ANDROID_SDK_ROOT"/cmdline-tools/latest
+export PATH="$ANDROID_SDK_ROOT"/cmdline-tools/latest/bin:$PATH
+
 
 ../gradlew -p ../ dokkaGfm
 
