600e10a5c5ac62ff52e4fb4a5cc3d49573ea4033
Martin Bonnin <martin@mbonnin.net>
2021-08-07 20:04:15 +0200

    remove data class

--- apollo-runtime/src/commonMain/kotlin/com/apollographql/apollo3/network/http/HttpExecutionContext.kt
+++ apollo-runtime/src/commonMain/kotlin/com/apollographql/apollo3/network/http/HttpExecutionContext.kt
@@ -3,7 +3,7 @@ package com.apollographql.apollo3.network.http
 import com.apollographql.apollo3.api.ExecutionContext
 import com.apollographql.apollo3.api.http.HttpHeader
 
-data class HttpInfo(
+class HttpInfo(
     val statusCode: Int,
     val headers: List<HttpHeader>
 ) : ExecutionContext.Element {
