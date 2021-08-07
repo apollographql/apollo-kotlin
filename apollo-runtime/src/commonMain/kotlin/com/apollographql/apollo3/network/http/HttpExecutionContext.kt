de790f72a4994a3245c0e2e5124f45346a56b20b
Martin Bonnin <martin@mbonnin.net>
2021-08-07 19:48:07 +0200

    remove unused context

--- apollo-runtime/src/commonMain/kotlin/com/apollographql/apollo3/network/http/HttpExecutionContext.kt
+++ apollo-runtime/src/commonMain/kotlin/com/apollographql/apollo3/network/http/HttpExecutionContext.kt
@@ -3,17 +3,6 @@ package com.apollographql.apollo3.network.http
 import com.apollographql.apollo3.api.ExecutionContext
 import com.apollographql.apollo3.api.http.HttpHeader
 
-data class HttpRequestParameters(val headers: Map<String, String>) : ExecutionContext.Element{
-  override val key: ExecutionContext.Key<*>
-    get() = Key
-
-  companion object Key : ExecutionContext.Key<HttpRequestParameters>
-}
-
-fun HttpRequestParameters?.withHeader(name: String, value: String) = (this ?: HttpRequestParameters(emptyMap())).let {
-  it.copy(headers = it.headers + (name to value))
-}
-
 data class HttpResponseInfo(
     val statusCode: Int,
     val headers: List<HttpHeader>
