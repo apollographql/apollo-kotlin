7a7b6088318d93fc1b61acf99d9a3380754bf935
Martin Bonnin <martin@mbonnin.net>
2021-09-16 11:34:43 +0200

    also relax restrictions on the request

--- apollo-normalized-cache/src/commonMain/kotlin/com/apollographql/apollo3/cache/normalized/internal/ApolloCacheInterceptor.kt
+++ apollo-normalized-cache/src/commonMain/kotlin/com/apollographql/apollo3/cache/normalized/internal/ApolloCacheInterceptor.kt
@@ -44,7 +44,8 @@ internal class ApolloCacheInterceptor(
     val store: ApolloStore,
 ) : ApolloInterceptor {
   init {
-    // The store has a MutableSharedFlow that doesn't like being frozen
+    // The store has a MutableSharedFlow that doesn't like being frozen when using coroutines
+    // But is ok to freeze when using coroutines-native-mt (see https://github.com/apollographql/apollo-android/issues/3357)
     // ensureNeverFrozen(store)
   }
 
