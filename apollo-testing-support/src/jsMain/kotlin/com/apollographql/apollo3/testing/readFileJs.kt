2dad62e853fc91451f68c4a79d8cee3cb82b6792
Martin Bonnin <martin@mbonnin.net>
2021-10-07 11:37:06 +0200

    add comment about the current working directory

--- apollo-testing-support/src/jsMain/kotlin/com/apollographql/apollo3/testing/readFileJs.kt
+++ apollo-testing-support/src/jsMain/kotlin/com/apollographql/apollo3/testing/readFileJs.kt
@@ -4,17 +4,13 @@ import fs.readFileSync
 import kotlin.test.assertEquals
 
 actual fun readFile(path: String): String {
+  // Workaround for https://youtrack.jetbrains.com/issue/KT-49125
   val pathPrefix = "../../../../../tests/integration-tests/"
-  val pathPrefixKotlin = "../../../../../tests/integration-tests-kotlin/"
   val options = object: fs.`T$44` {
     override var encoding: String? = "utf8"
   }
 
-  return try {
-    readFileSync("$pathPrefix$path", options)
-  } catch (t: Throwable) {
-    readFileSync("$pathPrefixKotlin$path", options)
-  } as String
+  return readFileSync("$pathPrefix$path", options) as String
 }
 
 actual fun checkFile(actualText: String, path: String) {
