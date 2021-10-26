2bdaaffd82255ced3963b08d83e0895bbdc67b0a
Martin Bonnin <martin@mbonnin.net>
2021-10-26 21:35:40 +0200

    trigger some core dumps

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -5,16 +5,5 @@ set -x
 
 export PATH="$ANDROID_HOME"/tools/bin:$PATH
 
-./gradlew compileKotlinJs
-./gradlew --stop
+./gradlew -p tests appleTest
 
-./gradlew -p tests fullCheck
-
-# check that the public API did not change with Metalava
-# reenable when the 3.x API is more stable
-# ./gradlew metalavaCheckCompatibility
-
-./gradlew publishSnapshotsIfNeeded  --parallel
-
-./gradlew publishToOssStagingIfNeeded
-./gradlew publishToGradlePortalIfNeeded -Pgradle.publish.key="$GRADLE_PUBLISH_KEY" -Pgradle.publish.secret="$GRADLE_PUBLISH_SECRET"
