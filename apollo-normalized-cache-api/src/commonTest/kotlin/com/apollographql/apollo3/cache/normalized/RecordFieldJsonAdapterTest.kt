821fe604c5fb7f433e8e94f700a81050c67aecd1
Davide Casadei <davide.c@twinlogix.com>
2021-10-27 17:36:20 +0200

    Update RecordFieldJsonAdapterTest.kt

--- apollo-normalized-cache-api/src/commonTest/kotlin/com/apollographql/apollo3/cache/normalized/RecordFieldJsonAdapterTest.kt
+++ apollo-normalized-cache-api/src/commonTest/kotlin/com/apollographql/apollo3/cache/normalized/RecordFieldJsonAdapterTest.kt
@@ -4,7 +4,6 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
 import kotlin.test.assertTrue
-import kotlin.test.expect
 
 class RecordFieldJsonAdapterTest {
 
