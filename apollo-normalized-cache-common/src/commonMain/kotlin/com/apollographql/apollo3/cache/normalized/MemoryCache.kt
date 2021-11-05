584025441816279548cdfc0b6c1596b035d623d4
Martin Bonnin <martin@mbonnin.net>
2021-11-05 12:17:02 +0100

    first drop for cache docs

--- apollo-normalized-cache-common/src/commonMain/kotlin/com/apollographql/apollo3/cache/normalized/MemoryCache.kt
+++ apollo-normalized-cache-common/src/commonMain/kotlin/com/apollographql/apollo3/cache/normalized/MemoryCache.kt
@@ -10,7 +10,7 @@ import kotlin.reflect.KClass
 /**
  * Memory (multiplatform) cache implementation based on recently used property (LRU).
  *
- * [maxSizeBytes] - the maximum size of bytes the cache may occupy.
+ * [maxSizeBytes] - the maximum size in bytes the cache may occupy.
  * [expireAfterMillis] - after what timeout each entry in the cache treated as expired. By default there is no timeout.
  *
  * Expired entries removed from the cache only on cache miss ([loadRecord] operation) and not removed from the cache automatically
