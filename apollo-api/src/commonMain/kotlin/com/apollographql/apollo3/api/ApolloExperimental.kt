00944bf1e61a7f218221c283788bc9e8d02106ec
Benoit Lubek <BoD@JRAF.org>
2021-11-08 09:29:32 +0100

    Apply suggestions from code review
    Co-authored-by: Martin Bonnin <martin@mbonnin.net>

--- apollo-api/src/commonMain/kotlin/com/apollographql/apollo3/api/ApolloExperimental.kt
+++ apollo-api/src/commonMain/kotlin/com/apollographql/apollo3/api/ApolloExperimental.kt
@@ -2,7 +2,7 @@ package com.apollographql.apollo3.api
 
 @RequiresOptIn(
     level = RequiresOptIn.Level.WARNING,
-    message = "This API is experimental and subject to change, use with caution"
+    message = "This API is experimental and can be changed in a backwards-incompatible manner."
 )
 @Retention(AnnotationRetention.BINARY)
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
