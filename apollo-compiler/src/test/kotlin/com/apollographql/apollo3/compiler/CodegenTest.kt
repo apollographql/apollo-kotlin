d2392ffceaa53c5ec9aa61031ef67c35d5fba45e
Martin Bonnin <martin@mbonnin.net>
2021-11-18 10:58:08 +0100

    Kotlin 1.6 (#3563)
    * update Kotlin and Ksp
    
    * Duration is now stable
    
    * keep 1.5 compatibility
    
    * if using apiVersion=1.5, keep OptIn
    
    * udpate kotlin-compile-testing

--- apollo-compiler/src/test/kotlin/com/apollographql/apollo3/compiler/CodegenTest.kt
+++ apollo-compiler/src/test/kotlin/com/apollographql/apollo3/compiler/CodegenTest.kt
@@ -15,14 +15,14 @@ import org.junit.AfterClass
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.File
-import java.util.concurrent.TimeUnit
 import kotlin.time.Duration
+import kotlin.time.DurationUnit
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTime
 
 @RunWith(TestParameterInjector::class)
 @OptIn(ExperimentalTime::class)
-class CodegenTest() {
+class CodegenTest {
   private class Measurement(
       val name: String,
       val linesOfCode: Int,
@@ -255,7 +255,7 @@ class CodegenTest() {
                         "%-50s %20s %20s %20s",
                         measurement.name,
                         measurement.linesOfCode.toString(),
-                        measurement.codegenDuration.toLong(TimeUnit.MILLISECONDS).toString(),
+                        measurement.codegenDuration.toLong(DurationUnit.MILLISECONDS).toString(),
                         measurement.compileDuration.toString(),
                     )
                   }
