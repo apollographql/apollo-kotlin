31dd06736b92bf937f7e71d5863b7ef8a8ff0c56
Martin Bonnin <martin@mbonnin.net>
2021-12-09 16:56:52 +0100

    add a test for a HttpRequestComposer that writes extensions (#3671)

--- tests/integration-tests/src/commonTest/kotlin/test/BodyExtensionsTest.kt
+++ tests/integration-tests/src/commonTest/kotlin/test/BodyExtensionsTest.kt
@@ -3,7 +3,6 @@ package test
 import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.annotations.ApolloInternal
 import com.apollographql.apollo3.api.ApolloRequest
-import com.apollographql.apollo3.api.ExecutionContext
 import com.apollographql.apollo3.api.Operation
 import com.apollographql.apollo3.api.http.ByteStringHttpBody
 import com.apollographql.apollo3.api.http.HttpMethod
@@ -13,66 +12,63 @@ import com.apollographql.apollo3.api.json.internal.buildJsonByteString
 import com.apollographql.apollo3.api.json.internal.writeObject
 import com.apollographql.apollo3.api.json.jsonReader
 import com.apollographql.apollo3.api.json.readAny
+import com.apollographql.apollo3.integration.normalizer.HeroNameQuery
 import com.apollographql.apollo3.mockserver.MockResponse
 import com.apollographql.apollo3.mockserver.MockServer
 import com.apollographql.apollo3.network.http.HttpNetworkTransport
 import com.apollographql.apollo3.testing.runTest
 import okio.Buffer
-import org.junit.Test
-import reserved.GetRandomQuery
+import kotlin.test.Test
 import kotlin.test.assertEquals
 
-@ApolloInternal
-class NoQueryDocumentTest {
-  private val queryDocument = """
-    query GetRandomQuery {
-      random
-    }
-  """.trimIndent()
-  
+@OptIn(ApolloInternal::class)
+class WithExtensionsHttpRequestComposer(private val serverUrl: String): HttpRequestComposer {
+  override fun <D : Operation.Data> compose(apolloRequest: ApolloRequest<D>): HttpRequest {
+
+    return HttpRequest.Builder(HttpMethod.Post, serverUrl)
+        .body(
+            ByteStringHttpBody(
+                "application/json",
+                buildJsonByteString {
+                  writeObject {
+                    name("query")
+                    value(apolloRequest.operation.document())
+                    name("operationName")
+                    value(apolloRequest.operation.name())
+                    name("extensions")
+                    value("extension value")
+                  }
+                }
+            )
+        )
+        .build()
+  }
+}
+
+@OptIn(ApolloInternal::class)
+class BodyExtensionsTest {
   @Test
-  fun noQueryDocumentTest() = runTest {
+  fun bodyExtensions() = runTest {
     val mockServer = MockServer()
     val serverUrl = mockServer.url()
-
-    val composer = object : HttpRequestComposer {
-      override fun <D : Operation.Data> compose(apolloRequest: ApolloRequest<D>): HttpRequest {
-
-        return HttpRequest.Builder(HttpMethod.Post, serverUrl)
-            .body(
-                ByteStringHttpBody(
-                    "application/json",
-                    buildJsonByteString {
-                      writeObject {
-                        name("query")
-                        value(queryDocument)
-                        name("operationName")
-                        value("GetRandomQuery")
-                      }
-                    }
-                )
-            )
-            .build()
-      }
-    }
 
     val apolloClient = ApolloClient.Builder()
         .networkTransport(
             HttpNetworkTransport.Builder()
-                .httpRequestComposer(composer)
+                .httpRequestComposer(WithExtensionsHttpRequestComposer(serverUrl))
                 .build()
         )
         .build()
 
     mockServer.enqueue(MockResponse(statusCode = 500))
     kotlin.runCatching {
-      apolloClient.query(GetRandomQuery())
+      apolloClient.query(HeroNameQuery())
           .execute()
     }
 
     val request = mockServer.takeRequest()
     val asMap = Buffer().write(request.body).jsonReader().readAny() as Map<String, Any>
-    assertEquals(asMap["query"], queryDocument)
+    assertEquals(asMap["extensions"], "extension value")
 
     apolloClient.dispose()
     mockServer.stop()
