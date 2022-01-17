a966675c7487d4f93ca8749c836032e63b610c81
BoD <BoD@JRAF.org>
2022-01-17 10:55:20 +0100

    Throw if scalar mapping are defined in several modules

---
+++ apollo-gradle-plugin/testProjects/multi-modules-custom-scalar-defined-in-leaf/root/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+  kotlin("jvm")
+  id("com.apollographql.apollo3")
+}
+
+dependencies {
+  implementation(kotlin("stdlib"))
+  implementation(groovy.util.Eval.x(project, "x.dep.apollo.api"))
+
+  testImplementation(kotlin("test-junit"))
+}
+
+apollo {
+  packageName.set("com.library")
+  generateApolloMetadata.set(true)
+  mapScalar("Date", "java.util.Date")
+}
