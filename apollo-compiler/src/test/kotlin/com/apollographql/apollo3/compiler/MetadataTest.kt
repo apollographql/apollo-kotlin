cc268eb8e9e9f96b4ad7267f8a2c9ef082c74716
BoD <BoD@JRAF.org>
2022-01-18 12:31:26 +0100

    Fix MetadataTest

--- apollo-compiler/src/test/kotlin/com/apollographql/apollo3/compiler/MetadataTest.kt
+++ apollo-compiler/src/test/kotlin/com/apollographql/apollo3/compiler/MetadataTest.kt
@@ -114,7 +114,13 @@ class MetadataTest {
     alwaysGenerateTypesMatchingTest(emptySet())
 
     // Only scalar types are generated in the root
-    rootSourcesDir.assertContents()
+    rootSourcesDir.assertContents(
+        "Boolean.kt",
+        "Float.kt",
+        "ID.kt",
+        "Int.kt",
+        "String.kt"
+    )
 
     // Leaf contains its referenced types but not the unused ones
     leafSourcesDir.assertContents(
@@ -144,11 +150,16 @@ class MetadataTest {
     rootSourcesDir.assertContents(
         "Body1.kt",
         "Body1_InputAdapter.kt",
+        "Boolean.kt",
         "CustomScalar1.kt",
         "Encoding.kt",
         "Encoding_ResponseAdapter.kt",
+        "Float.kt",
+        "ID.kt",
+        "Int.kt",
         "MessageInput1.kt",
         "MessageInput1_InputAdapter.kt",
+        "String.kt",
         "User1.kt",
         "User1_InputAdapter.kt"
     )
@@ -198,11 +209,16 @@ class MetadataTest {
 
     // Root generates the fragment
     rootSourcesDir.assertContents(
+        "Boolean.kt",
         "Character.kt",
         "CharacterFragment.kt",
         "CharacterFragmentSelections.kt",
         "Episode.kt",
         "Episode_ResponseAdapter.kt",
+        "Float.kt",
+        "ID.kt",
+        "Int.kt",
+        "String.kt"
     )
 
     // Leaf contains the query but not the fragment
@@ -245,9 +261,14 @@ class MetadataTest {
     fragmentTest("fragment-multiple")
 
     rootSourcesDir.assertContents(
+        "Boolean.kt",
         "Character.kt",
         "CharacterFragment.kt",
         "CharacterFragmentSelections.kt",
+        "Float.kt",
+        "ID.kt",
+        "Int.kt",
+        "String.kt"
     )
 
     leafSourcesDir.assertContents(
