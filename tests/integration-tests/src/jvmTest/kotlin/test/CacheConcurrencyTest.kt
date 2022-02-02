0e006365429fec116e31532cc669eb4063a6f801
Martin Bonnin <martin@mbonnin.net>
2022-02-02 08:25:39 +0100

    make concurrency more explicit

--- tests/integration-tests/src/jvmTest/kotlin/test/CacheConcurrencyTest.kt
+++ tests/integration-tests/src/jvmTest/kotlin/test/CacheConcurrencyTest.kt
@@ -10,8 +10,10 @@ import com.apollographql.apollo3.testing.QueueTestNetworkTransport
 import com.apollographql.apollo3.testing.enqueueTestResponse
 import com.apollographql.apollo3.testing.runTest
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
+import java.util.concurrent.Executors
 import kotlin.test.Test
 
 @OptIn(ApolloExperimental::class)
@@ -20,18 +22,26 @@ class CacheConcurrencyTest {
   @Test
   fun storeConcurrently() = runTest {
     val store = ApolloStore(MemoryCacheFactory(maxSizeBytes = 1000))
-    val apolloClient = ApolloClient.Builder().networkTransport(QueueTestNetworkTransport()).store(store).build()
+    val executor = Executors.newFixedThreadPool(10)
+    val dispatcher = executor.asCoroutineDispatcher()
+
+    val apolloClient = ApolloClient.Builder()
+        .networkTransport(QueueTestNetworkTransport())
+        .store(store)
+        .requestedDispatcher(dispatcher)
+        .build()
 
     val concurrency = 100
 
     0.until(concurrency).map {
-      launch {
+      launch(dispatcher) {
         val query = CharacterNameByIdQuery((it / 2).toString())
         apolloClient.enqueueTestResponse(query, CharacterNameByIdQuery.Data(CharacterNameByIdQuery.Character(name = it.toString())))
         apolloClient.query(query).execute()
       }
     }.joinAll()
 
+    executor.shutdown()
     println(store.dump().values.toList()[1].map { (k, v) -> "$k -> ${v.fields}" }.joinToString("\n"))
   }
 }
