e5c8a30aa96ab0aa945ee29b77c4e6893c733ab0
BoD <BoD@JRAF.org>
2022-02-17 11:45:12 +0100

    Add methods to create Multipart Mixed chunks

--- apollo-mockserver/src/commonTest/kotlin/com/apollographql/apollo3/mockserver/test/WriteResponseTest.kt
+++ apollo-mockserver/src/commonTest/kotlin/com/apollographql/apollo3/mockserver/test/WriteResponseTest.kt
@@ -3,6 +3,7 @@ package com.apollographql.apollo3.mockserver.test
 import com.apollographql.apollo3.annotations.ApolloExperimental
 import com.apollographql.apollo3.mockserver.MockChunk
 import com.apollographql.apollo3.mockserver.MockResponse
+import com.apollographql.apollo3.mockserver.createMultipartMixedChunkedResponse
 import com.apollographql.apollo3.mockserver.writeResponse
 import com.apollographql.apollo3.testing.runTest
 import okio.Buffer
@@ -57,4 +58,43 @@ class WriteResponseTest {
         buffer.readUtf8()
     )
   }
+
+  @Test
+  fun writeMultipartChunkedResponse() = runTest {
+    val mockResponse = createMultipartMixedChunkedResponse(listOf(
+        """{"data":{"song":{"firstVerse":"Now I know my ABC's."}},"hasNext":true}""",
+        """{"data":{"secondVerse":"Next time won't you sing with me?"},"path":["song"],"hasNext":false}"""
+    ))
+
+    val buffer = Buffer()
+    writeResponse(buffer, mockResponse, "1.1")
+    assertEquals(
+        listOf(
+            "1.1 200",
+            """Content-Type: multipart/mixed; boundary="-"""",
+            "Transfer-Encoding: chunked",
+            "Connection: close",
+            "",
+            "97",
+            "---",
+            "Content-Length: 70",
+            "Content-Type: application/json; charset=utf-8",
+            "",
+            """{"data":{"song":{"firstVerse":"Now I know my ABC's."}},"hasNext":true}""",
+            "---",
+            "",
+            "aa",
+            "Content-Length: 92",
+            "Content-Type: application/json; charset=utf-8",
+            "",
+            """{"data":{"secondVerse":"Next time won't you sing with me?"},"path":["song"],"hasNext":false}""",
+            "-----",
+            "",
+            "0",
+            "",
+        ).joinToString("\r\n", postfix = "\r\n"),
+        buffer.readUtf8()
+    )
+  }
+
 }
