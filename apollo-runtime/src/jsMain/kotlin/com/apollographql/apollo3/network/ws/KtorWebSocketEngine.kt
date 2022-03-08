859d11ab277c225189d7571710029f2a78689a99
Nathan Shurtz <n.shurtz@fetchrewards.com>
2022-03-08 16:53:47 -0600

    #2463 Add Websocket support for JS targets

--- apollo-runtime/src/jsMain/kotlin/com/apollographql/apollo3/network/ws/KtorWebSocketEngine.kt
+++ apollo-runtime/src/jsMain/kotlin/com/apollographql/apollo3/network/ws/KtorWebSocketEngine.kt
@@ -1,8 +1,53 @@
 package com.apollographql.apollo3.network.ws
 
-actual class DefaultWebSocketEngine : WebSocketEngine {
-  override suspend fun open(url: String, headers: Map<String, String>): WebSocketConnection {
-    TODO("WebSocket are not supported on JS")
+import io.ktor.client.HttpClient
+import io.ktor.client.features.websocket.webSocketSession
+import io.ktor.client.request.headers
+import io.ktor.http.cio.websocket.CloseReason
+import io.ktor.http.cio.websocket.Frame
+import io.ktor.http.cio.websocket.close
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okio.ByteString
+
+actual class DefaultWebSocketEngine(
+    private val ktorClient: HttpClient,
+) : WebSocketEngine {
+  private val coroutineScope = CoroutineScope(Dispatchers.Main)
+  actual constructor(): this(ktorClient = HttpClient())
+
+  override suspend fun open(
+      url: String,
+      headers: Map<String, String>,
+  ): WebSocketConnection {
+    val session = ktorClient.webSocketSession {
+      headers {
+        headers.forEach { (key, value) ->
+          append(key, value)
+        }
+      }
+    }
+
+    return object : WebSocketConnection {
+      override suspend fun receive(): String {
+        return session.incoming.receive().data.decodeToString()
+      }
+
+      override fun send(data: ByteString) {
+        session.outgoing.trySend(Frame.Binary(false, data.toByteArray()))
+      }
+
+      override fun send(string: String) {
+          session.outgoing.trySend(Frame.Text(string))
+      }
+
+      override fun close() {
+        coroutineScope.launch {
+          session.close(CloseReason(CloseReason.Codes.NORMAL, ""))
+        }
+      }
+    }
   }
 }
 
