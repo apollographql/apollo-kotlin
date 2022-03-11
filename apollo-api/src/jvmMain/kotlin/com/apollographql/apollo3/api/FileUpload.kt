29bc65976d8470a4bc299acc59a04703c637eea3
Stylianos Gakis <stylianos.gakis98@gmail.com>
2022-03-11 19:10:07 +0200

    Fix depracation annotation referencing old function

--- apollo-api/src/jvmMain/kotlin/com/apollographql/apollo3/api/FileUpload.kt
+++ apollo-api/src/jvmMain/kotlin/com/apollographql/apollo3/api/FileUpload.kt
@@ -12,8 +12,13 @@ fun DefaultUpload.Builder.content(file: File): DefaultUpload.Builder {
   return content(file.source().buffer()).contentLength(file.length())
 }
 
-@Deprecated("This is a helper function to help migrating to 3.x " +
-    "and will be removed in a future version", ReplaceWith("FileUpload(File(filePath), mimetype)"))
+@Deprecated(
+  "This is a helper function to help migrating to 3.x and will be removed in a future version",
+  ReplaceWith(
+    "DefaultUpload.Builder().content(filePath).contentType(mimetype).build()",
+    imports = ["com.apollographql.apollo3.api.DefaultUpload"]
+  )
+)
 @ApolloDeprecatedSince(v3_0_0)
 fun create(mimetype: String, filePath: String): Upload {
   val file = File(filePath)
