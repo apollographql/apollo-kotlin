05930b8184a67d962505248cb6dc7d778c38942a
undermark5 <nsshurtz@gmail.com>
2022-03-15 05:03:07 -0500

    WebSocket support for JS targets (#3913)
    * #2463 Add Websocket support for JS targets
    
    * transition away from ktor (1.6.7 doesn't support js client websockets)
    
    * transition to ktor 1.6.7
    
    * update ktor 1.6.4 -> 1.6.7
    
    * Implement websocket creation based on ktor code
    
    * update comment around the ktor code
    
    Co-authored-by: Nathan Shurtz <n.shurtz@fetchrewards.com>

--- apollo-runtime/src/jsMain/kotlin/com/apollographql/apollo3/network/ws/KtorWebSocketEngine.kt
+++
@@ -1,8 +0,0 @@
-package com.apollographql.apollo3.network.ws
-
-actual class DefaultWebSocketEngine : WebSocketEngine {
-  override suspend fun open(url: String, headers: Map<String, String>): WebSocketConnection {
-    TODO("WebSocket are not supported on JS")
-  }
-}
-
