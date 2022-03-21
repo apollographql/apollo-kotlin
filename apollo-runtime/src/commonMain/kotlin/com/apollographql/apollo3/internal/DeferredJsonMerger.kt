d971dc1afe45fd42dbf6ad279f144bf1e38a456b
Benoit Lubek <BoD@JRAF.org>
2022-03-21 10:01:43 +0100

    Defer: `DeferredJsonMerger` (#3901)
    * Introduce DeferredJsonMerger
    
    * Add @defer to the builtin directives
    
    * Add label argument on @defer directives
    
    * Move DeferTest to its own project in integration tests
    
    * Deep merge maps in DeferredJsonMerger
    
    * Add clarification on multipleBodies test
    
    * Do not merge errors or extensions
    
    * Remove automatically adding unique "label" to @defer directives. Will replace this by a validation rule later.

---
+++ apollo-runtime/src/commonMain/kotlin/com/apollographql/apollo3/internal/DeferredJsonMerger.kt
@@ -0,0 +1,101 @@
+package com.apollographql.apollo3.internal
+
+import com.apollographql.apollo3.annotations.ApolloInternal
+import com.apollographql.apollo3.api.json.BufferedSourceJsonReader
+import com.apollographql.apollo3.api.json.readAny
+import okio.BufferedSource
+
+/**
+ * Utility class for merging GraphQL JSON payloads received in multiple chunks when using the `@defer` directive.
+ *
+ * Each call to [merge] will merge the given chunk into the [merged] Map, and will also update the [mergedFragmentLabels] Set with the
+ * value of its `label` field (if present).
+ *
+ * The fields in `data` are merged into the node found in [merged] at `path` (for the first call to [merge], the payload is
+ * copied to [merged] as-is).
+ *
+ * `errors` and `extensions` fields are not merged: they are copied as-is (if present) to the [merged] Map at each call to [merge].
+ */
+@ApolloInternal
+class DeferredJsonMerger {
+  private val _merged = mutableMapOf<String, Any?>()
+  val merged: Map<String, Any?> = _merged
+
+  private val _mergedFragmentLabels = mutableSetOf<String>()
+  val mergedFragmentLabels: Set<String> = _mergedFragmentLabels
+
+  fun merge(payload: BufferedSource): Map<String, Any?> {
+    val payloadMap = jsonToMap(payload)
+    if (merged.isEmpty()) {
+      // Initial payload, no merging needed
+      _merged += payloadMap
+      return merged
+    }
+
+    mergeData(payloadMap)
+    if (payloadMap.containsKey("errors")) {
+      _merged["errors"] = payloadMap["errors"]
+    } else {
+      _merged.remove("errors")
+    }
+    if (payloadMap.containsKey("extensions")) {
+      _merged["extensions"] = payloadMap["extensions"]
+    } else {
+      _merged.remove("extensions")
+    }
+
+    return merged
+  }
+
+  @Suppress("UNCHECKED_CAST")
+  private fun mergeData(payloadMap: Map<String, Any?>) {
+    val payloadData = payloadMap["data"] as Map<String, Any?>?
+    val payloadPath = payloadMap["path"] as List<Any>
+    val mergedData = merged["data"] as Map<String, Any?>
+
+    // payloadData can be null if there are errors
+    if (payloadData != null) {
+      val nodeToMergeInto = nodeAtPath(mergedData, payloadPath) as MutableMap<String, Any?>
+      deepMerge(nodeToMergeInto, payloadData)
+
+      (payloadMap["label"] as String?)?.let { _mergedFragmentLabels += it }
+    }
+  }
+
+  @Suppress("UNCHECKED_CAST")
+  private fun deepMerge(destination: MutableMap<String, Any?>, map: Map<String, Any?>) {
+    for ((key, value) in map) {
+      if (destination.containsKey(key) && destination[key] is MutableMap<*, *>) {
+        // Objects: merge recursively
+        val fieldDestination = destination[key] as MutableMap<String, Any?>
+        val fieldMap = value as? Map<String, Any?> ?: error("'$key' is an object in destination but not in map")
+        deepMerge(destination = fieldDestination, map = fieldMap)
+      } else {
+        // Other types: add / overwrite
+        destination[key] = value
+      }
+    }
+  }
+
+  @Suppress("UNCHECKED_CAST")
+  private fun jsonToMap(json: BufferedSource): Map<String, Any?> = BufferedSourceJsonReader(json).readAny() as Map<String, Any?>
+
+
+  /**
+   * Find the node in the [map] at the given [path].
+   * @param path The path to the node to find, as a list of either `String` (name of field in object) or `Int` (index of element in array).
+   */
+  private fun nodeAtPath(map: Map<String, Any?>, path: List<Any>): Any? {
+    var node: Any? = map
+    for (key in path) {
+      node = if (node is List<*>) {
+        node[key as Int]
+      } else {
+        @Suppress("UNCHECKED_CAST")
+        node as Map<String, Any?>
+        node[key]
+      }
+    }
+    return node
+  }
+}
