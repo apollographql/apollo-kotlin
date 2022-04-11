cea97c0d1f7dfc8a06d7b4075fd781e74aef9423
Martin Bonnin <martin@mbonnin.net>
2022-04-11 15:54:04 +0200

    📖  Add more scalar tests and improve migration guide (#4014)
    * Shuffle a bit the scalar tests and improve migration guide
    
    * Why can't we have Json5 !?

--- tests/scalar-adapters/build.gradle.kts
+++ tests/scalar-adapters/build.gradle.kts
@@ -5,18 +5,28 @@ plugins {
 
 dependencies {
   implementation("com.apollographql.apollo3:apollo-runtime")
+  implementation("com.apollographql.apollo3:apollo-adapters")
   testImplementation("com.apollographql.apollo3:apollo-testing-support")
   testImplementation(groovy.util.Eval.x(project, "x.dep.kotlinJunit"))
 }
 
 apollo {
   packageName.set("custom.scalars")
+  // builtin
   mapScalar("Long", "kotlin.Long")
   mapScalarToKotlinFloat("CustomFloat")
   mapScalar("Any", "kotlin.Any")
   mapScalar("GeoPoint", "kotlin.Any")
-  mapScalar("Address", "custom.scalars.Address")
   mapScalar("ID", "kotlin.Long")
-  mapScalar("Int", "kotlin.Int", "custom.scalars.MyIntAdapter()")
-  mapScalar("String", "kotlin.String", "custom.scalars.MyStringAdapter")
+  // runtime
+  mapScalar("Address", "custom.scalars.Address")
+  // compile time
+  mapScalar("Int", "custom.scalars.MyInt", "custom.scalars.MyIntAdapter()")
+  mapScalar("String", "custom.scalars.MyString", "custom.scalars.MyStringAdapter")
+  // decimal
+  mapScalar(
+      "Decimal",
+      "com.apollographql.apollo3.adapter.BigDecimal",
+      "com.apollographql.apollo3.adapter.BigDecimalAdapter"
+  )
 }
--- tests/scalar-adapters/src/main/kotlin/custom/scalars/MyIntAdapter.kt
+++ tests/scalar-adapters/src/main/kotlin/custom/scalars/MyIntAdapter.kt
@@ -5,12 +5,14 @@ import com.apollographql.apollo3.api.CustomScalarAdapters
 import com.apollographql.apollo3.api.json.JsonReader
 import com.apollographql.apollo3.api.json.JsonWriter
 
-class MyIntAdapter : Adapter<Int> {
-  override fun fromJson(reader: JsonReader, customScalarAdapters: CustomScalarAdapters): Int {
-    return reader.nextInt()
+class MyInt(val value: Int)
+
+class MyIntAdapter : Adapter<MyInt> {
+  override fun fromJson(reader: JsonReader, customScalarAdapters: CustomScalarAdapters): MyInt {
+    return MyInt(reader.nextInt())
   }
 
-  override fun toJson(writer: JsonWriter, customScalarAdapters: CustomScalarAdapters, value: Int) {
-    writer.value(value)
+  override fun toJson(writer: JsonWriter, customScalarAdapters: CustomScalarAdapters, value: MyInt) {
+    writer.value(value.value)
   }
 }
