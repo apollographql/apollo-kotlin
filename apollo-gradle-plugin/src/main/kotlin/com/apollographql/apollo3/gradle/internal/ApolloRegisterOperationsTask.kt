4af433103bb4cef918cefed86e8d3629451a0936
Martin Bonnin <martin@mbonnin.net>
2022-04-12 17:25:35 +0200

    Fix the Moshi adapter used for OperationOutput. Moshi cannot get the type parameters from the typealias automagically

--- apollo-gradle-plugin/src/main/kotlin/com/apollographql/apollo3/gradle/internal/ApolloRegisterOperationsTask.kt
+++ apollo-gradle-plugin/src/main/kotlin/com/apollographql/apollo3/gradle/internal/ApolloRegisterOperationsTask.kt
@@ -1,6 +1,15 @@
 package com.apollographql.apollo3.gradle.internal
 
+import com.apollographql.apollo3.compiler.MOSHI
 import com.apollographql.apollo3.compiler.fromJson
+import com.apollographql.apollo3.compiler.getJsonAdapter
+import com.apollographql.apollo3.compiler.operationoutput.OperationDescriptor
+import com.apollographql.apollo3.compiler.operationoutput.OperationOutput
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import okio.buffer
+import okio.source
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Property
@@ -27,7 +36,12 @@ abstract class ApolloRegisterOperationsTask: DefaultTask() {
         key = key.get() ?: error("key is required to register operations"),
         graphID = graph.get() ?: error("graphID is required to register operations"),
         graphVariant = graphVariant.get() ?: error("graphVariant is required to register operations"),
-        operationOutput = operationOutput.get().asFile.fromJson()
+        operationOutput = operationOutputAdapter().fromJson(operationOutput.get().asFile.source().buffer())!!
     )
+  }
+
+  private fun operationOutputAdapter(): JsonAdapter<OperationOutput> {
+    val type = Types.newParameterizedType(Map::class.java, String::class.java, OperationDescriptor::class.java)
+    return MOSHI.adapter<OperationOutput>(type)
   }
 }
---
+++ apollo-gradle-plugin/testProjects/register-operations/build.gradle.kts
@@ -0,0 +1,21 @@
+import com.apollographql.apollo3.gradle.api.ApolloExtension
+
+buildscript {
+  apply(from = "../../testProjects/buildscript.gradle.kts")
+}
+
+apply(plugin = "org.jetbrains.kotlin.jvm")
+apply(plugin = "com.apollographql.apollo3")
+
+dependencies {
+  add("implementation", groovy.util.Eval.x(project, "x.dep.apollo.api"))
+}
+
+configure<ApolloExtension> {
+  packageName.set("com.example")
+  registerOperations {
+    key.set("unused")
+    graph.set("unused")
+    graphVariant.set("current")
+  }
+}
