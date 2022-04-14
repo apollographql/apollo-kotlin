4a52e0780836b3032d989da15212e334a88af456
Martin Bonnin <martin@mbonnin.net>
2022-04-14 11:42:42 +0200

    Fix the Moshi adapter used for OperationOutput. Moshi cannot get the type parameters from the typealias automagically (#4022)

--- apollo-gradle-plugin/src/test/kotlin/com/apollographql/apollo3/gradle/test/RegisterOperationsTests.kt
+++ apollo-gradle-plugin/src/test/kotlin/com/apollographql/apollo3/gradle/test/RegisterOperationsTests.kt
@@ -1,8 +1,13 @@
 package com.apollographql.apollo3.gradle.test
 
 import com.apollographql.apollo3.gradle.internal.RegisterOperations.safelistingHash
+import com.apollographql.apollo3.gradle.util.TestUtils
 import com.google.common.truth.Truth
+import okhttp3.mockwebserver.MockResponse
+import org.gradle.testkit.runner.UnexpectedBuildFailure
+import org.junit.Assert
 import org.junit.Test
+import java.io.File
 
 class RegisterOperationsTests {
   @Test
@@ -45,5 +50,17 @@ class RegisterOperationsTests {
     """.trimIndent()
 
     Truth.assertThat(query.safelistingHash()).isEqualTo("33bcef0fa53cb2235e9f069c446d88ee5d425846d58dcb61d30563c9740fccdd")
+  }
+
+  @Test
+  fun `operation output is parsed correctly`() {
+    TestUtils.withTestProject("register-operations") { dir ->
+      try {
+        TestUtils.executeTask("registerServiceApolloOperations", dir)
+      } catch (e: UnexpectedBuildFailure) {
+        // because there's no API key, this test will fail but that'll at least verify that we reach that point
+        Truth.assertThat(e.message).contains("Cannot push operations")
+      }
+    }
   }
 }
