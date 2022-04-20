68bd4f02d2b4d73225d8950f6b7e8ac1d987440c
Martin Bonnin <martin@mbonnin.net>
2022-04-20 09:26:31 +0200

    remove workaround

--- tests/build.gradle.kts
+++ tests/build.gradle.kts
@@ -32,21 +32,6 @@ subprojects {
     }
   }
 
-  // Workaround for https://youtrack.jetbrains.com/issue/KT-51970
-  afterEvaluate {
-    afterEvaluate {
-      tasks.configureEach {
-        if (
-            name.startsWith("compile")
-            && name.endsWith("KotlinMetadata")
-        ) {
-          println("disabling ${this@subprojects}:$name")
-          enabled = false
-        }
-      }
-    }
-  }
-
   configureJavaAndKotlinCompilers()
 
   afterEvaluate {
