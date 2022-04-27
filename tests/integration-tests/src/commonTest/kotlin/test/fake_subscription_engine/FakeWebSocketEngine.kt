7c83db74f757fdffb3fb2e1fd3f2d4ee4f47fa6a
Martin Bonnin <martin@mbonnin.net>
2022-04-27 19:46:07 +0200

    ⚡  Ignore unknown websocket messages (#4066)
    * ignore unknown websocket messages
    
    * add a test
    
    * kdoc

---
+++ tests/integration-tests/src/commonTest/kotlin/test/fake_subscription_engine/FakeWebSocketEngine.kt
@@ -0,0 +1,33 @@
+package test.fake_subscription_engine
+
+import com.apollographql.apollo3.api.http.HttpHeader
+import com.apollographql.apollo3.network.ws.WebSocketConnection
+import com.apollographql.apollo3.network.ws.WebSocketEngine
+import okio.ByteString
+
+class FakeWebSocketEngine(val onReceive: suspend () -> String, val onSend: (String) -> Unit) : WebSocketEngine {
+  override suspend fun open(url: String, headers: List<HttpHeader>): WebSocketConnection {
+    return FakeWebSocketConnection(onReceive, onSend)
+  }
+
+  override suspend fun open(url: String, headers: Map<String, String>): WebSocketConnection {
+    return open(url, headers.entries.map { HttpHeader(it.key, it.value) })
+  }
+}
+
+class FakeWebSocketConnection(val onReceive: suspend () -> String, val onSend: (String) -> Unit) : WebSocketConnection {
+  override suspend fun receive(): String {
+    return onReceive()
+  }
+
+  override fun send(data: ByteString) {
+    onSend(data.utf8())
+  }
+
+  override fun send(string: String) {
+    onSend(string)
+  }
+
+  override fun close() {
+  }
+}
