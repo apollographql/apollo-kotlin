5cc55d3b1a8eabb31d534ff8fbc24db1bb79f6a9
Martin Bonnin <martin@mbonnin.net>
2022-05-13 19:16:37 +0200

    add ApolloDeprecatedSince

--- apollo-normalized-cache-api/src/commonMain/kotlin/com/apollographql/apollo3/cache/normalized/api/RecordFieldJsonAdapter.kt
+++ apollo-normalized-cache-api/src/commonMain/kotlin/com/apollographql/apollo3/cache/normalized/api/RecordFieldJsonAdapter.kt
@@ -1,17 +1,21 @@
 package com.apollographql.apollo3.cache.normalized.api
 
+import com.apollographql.apollo3.annotations.ApolloDeprecatedSince
 import com.apollographql.apollo3.annotations.ApolloInternal
 import com.apollographql.apollo3.cache.normalized.api.internal.JsonRecordSerializer
 
 @OptIn(ApolloInternal::class)
 @Deprecated("Use JsonRecordSerializer instead")
+@ApolloDeprecatedSince(ApolloDeprecatedSince.Version.v3_3_1)
 object RecordFieldJsonAdapter {
   @Deprecated("Use JsonRecordSerializer instead", ReplaceWith("JsonRecordSerializer.deserialize(json)"))
+  @ApolloDeprecatedSince(ApolloDeprecatedSince.Version.v3_3_1)
   fun fromJson(jsonFieldSource: String): Map<String, Any?> {
     return JsonRecordSerializer.deserialize("", jsonFieldSource).fields
   }
 
   @Deprecated("Use JsonRecordSerializer instead", ReplaceWith("JsonRecordSerializer.serialize(fields)"))
+  @ApolloDeprecatedSince(ApolloDeprecatedSince.Version.v3_3_1)
   fun toJson(fields: Map<String, Any?>): String {
     return JsonRecordSerializer.serialize(Record("", fields))
   }
