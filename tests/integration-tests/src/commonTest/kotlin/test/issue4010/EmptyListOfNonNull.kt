58f164198b6696cf13882f63369d15ba0b76ff2a
Martin Bonnin <martin@mbonnin.net>
2022-05-13 10:32:05 +0200

    add a test for issue 4010

---
+++ tests/integration-tests/src/commonTest/kotlin/test/issue4010/EmptyListOfNonNull.kt
@@ -0,0 +1,38 @@
+package test.issue4010
+
+import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.annotations.ApolloExperimental
+import com.apollographql.apollo3.mockserver.MockServer
+import com.apollographql.apollo3.mockserver.enqueue
+import com.apollographql.apollo3.testing.runTest
+import issue4010.ListQuery
+import schema.type.Animal
+import schema.type.Cat
+import schema.type.Crocodile
+import schema.type.Dog
+import schema.type.Pet
+import schema.type.__Schema
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class EmptyListOfNonNull {
+  @OptIn(ApolloExperimental::class)
+  @Test
+  fun test() = runTest {
+    val mockServer = MockServer()
+    mockServer.enqueue("""
+      {
+        "data": {
+          "list": []
+        }
+      }
+    """.trimIndent())
+    val apolloClient = ApolloClient.Builder()
+        .serverUrl(mockServer.url())
+        .build()
+
+    val response = apolloClient.query(ListQuery()).execute()
+
+    assertEquals(response.data?.list, emptyList())
+  }
+}
