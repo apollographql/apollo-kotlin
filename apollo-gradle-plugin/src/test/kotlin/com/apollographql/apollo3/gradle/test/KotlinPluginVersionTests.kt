c184d026265c01922787739b43736bb1a977f3a1
BoD <BoD@JRAF.org>
2022-05-17 17:49:04 +0200

    Fix README about minimum supported Kotlin version, and fix test

--- apollo-gradle-plugin/src/test/kotlin/com/apollographql/apollo3/gradle/test/KotlinPluginVersionTests.kt
+++ apollo-gradle-plugin/src/test/kotlin/com/apollographql/apollo3/gradle/test/KotlinPluginVersionTests.kt
@@ -29,12 +29,26 @@ class KotlinPluginVersionTests {
   }
 
   @Test
-  fun kotlin14Succeeds() {
+  fun kotlin14Fails() {
     TestUtils.withTestProject("kotlin-plugin-version") { dir ->
       dir.setKotlinPluginVersion("1.4.32")
-      val result = TestUtils.executeTask("help", dir)
-
-      Truth.assertThat(result.task(":help")!!.outcome).isEqualTo(TaskOutcome.SUCCESS)
+      try {
+        TestUtils.executeTask("build", dir)
+        fail("An exception was expected")
+      } catch (e: UnexpectedBuildFailure) {
+        Truth.assertThat(e.message).contains("Incompatible classes were found in dependencies. Remove them from the classpath or use '-Xskip-metadata-version-check' to suppress errors")
+      }
     }
   }
+
+  @Test
+  fun kotlin15Succeeds() {
+    TestUtils.withTestProject("kotlin-plugin-version") { dir ->
+      dir.setKotlinPluginVersion("1.5.31")
+      val result = TestUtils.executeTask("build", dir)
+
+      Truth.assertThat(result.task(":build")!!.outcome).isEqualTo(TaskOutcome.SUCCESS)
+    }
+  }
+
 }
