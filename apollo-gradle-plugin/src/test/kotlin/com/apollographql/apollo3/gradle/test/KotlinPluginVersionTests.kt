465c8366c25b779e97484e34176aa0ea7d14bd85
BoD <BoD@JRAF.org>
2022-05-17 20:59:48 +0200

    Remove Kotlin 1.3 and 1.4 tests

--- apollo-gradle-plugin/src/test/kotlin/com/apollographql/apollo3/gradle/test/KotlinPluginVersionTests.kt
+++ apollo-gradle-plugin/src/test/kotlin/com/apollographql/apollo3/gradle/test/KotlinPluginVersionTests.kt
@@ -2,9 +2,7 @@ package com.apollographql.apollo3.gradle.test
 
 import com.apollographql.apollo3.gradle.util.TestUtils
 import com.google.common.truth.Truth
-import junit.framework.Assert.fail
 import org.gradle.testkit.runner.TaskOutcome
-import org.gradle.testkit.runner.UnexpectedBuildFailure
 import org.junit.Test
 import java.io.File
 
@@ -14,33 +12,9 @@ class KotlinPluginVersionTests {
     gradleScript.writeText(gradleScript.readText().replace("KOTLIN_VERSION", version))
   }
 
-  @Test
-  fun kotlin13Fails() {
-    TestUtils.withTestProject("kotlin-plugin-version") { dir ->
-      dir.setKotlinPluginVersion("1.3.0")
-      // Kotlin 1.3 uses DefaultSourceDirectorySet() which is removed in recent Gradle versions
-      try {
-        TestUtils.executeGradleWithVersion(dir, "5.6", "generateApolloSources")
-        fail("An exception was expected")
-      } catch (e: UnexpectedBuildFailure) {
-        Truth.assertThat(e.message).contains("Apollo Kotlin requires Kotlin plugin version 1.4")
-      }
-    }
-  }
-
-  @Test
-  fun kotlin14Fails() {
-    TestUtils.withTestProject("kotlin-plugin-version") { dir ->
-      dir.setKotlinPluginVersion("1.4.32")
-      try {
-        TestUtils.executeTask("build", dir)
-        fail("An exception was expected")
-      } catch (e: UnexpectedBuildFailure) {
-        Truth.assertThat(e.message).contains("Incompatible classes were found in dependencies. Remove them from the classpath or use '-Xskip-metadata-version-check' to suppress errors")
-      }
-    }
-  }
-
+  /**
+   * Using older versions of Kotlin will fail, but 1.5 should still work.
+   */
   @Test
   fun kotlin15Succeeds() {
     TestUtils.withTestProject("kotlin-plugin-version") { dir ->
