b0febbd3862fd33561355e57809b42e85d386496
Martin Bonnin <martin@mbonnin.net>
2022-05-18 09:53:14 +0200

    remove stale file

--- apollo-normalized-cache-sqlite/src/appleTest/kotlin/com/apollographql/apollo3/cache/normalized/sql/CreateDriver.kt
+++
@@ -1,8 +0,0 @@
-package com.apollographql.apollo3.cache.normalized.sql
-
-import com.apollographql.apollo3.cache.internal.json.JsonDatabase
-import com.squareup.sqldelight.db.SqlDriver
-import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
-
-actual fun createDriver(): SqlDriver =
-    NativeSqliteDriver(JsonDatabase.Schema, "apollo.db")
