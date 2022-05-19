c23bec083f3dbe8d1203c63299b6d4171946e195
Stylianos Gakis <stylianos.gakis98@gmail.com>
2022-05-19 21:21:51 +0300

    Add Operation.Data.toJsonString() convenience function for the jvm (#4124)
    * Add Operation.Data.toJsonString() convenience function for the jvm
    
    * Run apidump
    
    * Simply use buildJsonString instead, and mark as experimental
    
    * Run apidump

--- apollo-api/src/jvmMain/kotlin/com/apollographql/apollo3/api/-Data.kt
+++ apollo-api/src/jvmMain/kotlin/com/apollographql/apollo3/api/-Data.kt
@@ -1,6 +1,8 @@
 package com.apollographql.apollo3.api
 
+import com.apollographql.apollo3.annotations.ApolloExperimental
 import com.apollographql.apollo3.api.json.JsonWriter
+import com.apollographql.apollo3.api.json.buildJsonString
 
 private fun Operation.Data.adapter(): Adapter<Operation.Data> {
   val name = this::class.java.name
@@ -25,3 +27,9 @@ fun Operation.Data.toJson(jsonWriter: JsonWriter, customScalarAdapters: CustomSc
   adapter().toJson(jsonWriter, customScalarAdapters, this)
 }
 
+@ApolloExperimental
+fun Operation.Data.toJsonString(customScalarAdapters: CustomScalarAdapters = CustomScalarAdapters.Empty, indent: String? = null): String {
+  return buildJsonString(indent) {
+    toJson(this, customScalarAdapters)
+  }
+}
