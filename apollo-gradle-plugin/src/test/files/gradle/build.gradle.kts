bd5f0299541f0734a1d50a9614cf3077db9ac07f
BoD <BoD@JRAF.org>
2022-05-25 11:13:25 +0200

    Fix gradle plugin tests with Kotlin 1.7

--- apollo-gradle-plugin/src/test/files/gradle/build.gradle.kts
+++ apollo-gradle-plugin/src/test/files/gradle/build.gradle.kts
@@ -1,4 +1,21 @@
 plugins {
-  kotlin("jvm")
   id("com.apollographql.apollo3")
 }
+
+buildscript {
+  apply(from = "../../../gradle/dependencies.gradle")
+
+  repositories {
+    maven {
+      url = uri("../../../build/localMaven")
+    }
+    gradlePluginPortal()
+  }
+
+  dependencies {
+    classpath(groovy.util.Eval.x(project, "x.dep.kotlinPlugin"))
+  }
+}
+
+apply(plugin = "org.jetbrains.kotlin.jvm")
+
