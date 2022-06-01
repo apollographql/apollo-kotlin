486020afc1ff99bcd5fa4957af19d5bfaffceb04
Martin Bonnin <martin@mbonnin.net>
2022-06-01 19:43:53 +0200

    remove kts script

--- apollo-cli/apollo-kotlin.main.kts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env kotlin
-
-@file:Repository("https://repo.maven.apache.org/maven2/")
-
-@file:DependsOn("com.apollographql.apollo3:apollo-cli:3.3.1-SNAPSHOT")
-
-import com.apollographql.apollo3.cli.main
-
-main(args)
