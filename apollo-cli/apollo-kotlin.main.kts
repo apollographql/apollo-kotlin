fde7197fcc94f99e25ba509bf74b28c30f255d93
Martin Bonnin <martin@mbonnin.net>
2022-06-01 19:36:01 +0200

    Update apollo-cli/apollo-kotlin.main.kts
    Co-authored-by: Benoit Lubek <BoD@JRAF.org>

--- apollo-cli/apollo-kotlin.main.kts
+++ apollo-cli/apollo-kotlin.main.kts
@@ -1,7 +1,6 @@
 #!/usr/bin/env kotlin
 
 @file:Repository("https://repo.maven.apache.org/maven2/")
-@file:Repository("file://Users/mbonnin/.m2/repository")
 
 @file:DependsOn("com.apollographql.apollo3:apollo-cli:3.3.1-SNAPSHOT")
 
