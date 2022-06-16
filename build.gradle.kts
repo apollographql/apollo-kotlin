8e35d80d86280c0fb41cc0644eae9ecc429011d9
Martin Bonnin <martin@mbonnin.net>
2022-06-16 14:11:19 +0200

    fail on modified files

--- build.gradle.kts
+++ build.gradle.kts
@@ -149,6 +149,28 @@ tasks.register("ciTestsNoGradle") {
 tasks.register("ciBuild") {
   description = "Execute the 'build' task in each subproject"
   dependsOn(subprojectTasks("build"))
+
+  doLast {
+    val modifiedFiles = runCommand("git", "status", "--porcelain")
+    if (modifiedFiles.isNotEmpty()) {
+      error("The CI modified local files. This is certainly an indication that they should have been included in the PR. Modified files:\n$modifiedFiles")
+    }
+  }
+}
+
+fun runCommand(vararg args: String): String {
+  val builder = ProcessBuilder(*args)
+      .redirectError(ProcessBuilder.Redirect.INHERIT)
+
+  val process = builder.start()
+  val ret = process.waitFor()
+
+  val output = process.inputStream.bufferedReader().readText()
+  if (ret != 0) {
+    throw java.lang.Exception("command ${args.joinToString(" ")} failed:\n$output")
+  }
+
+  return output
 }
 
 repositories {
