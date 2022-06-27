5414b8a236f75050346b243457ca4da0a90707dc
Martin Bonnin <martin@mbonnin.net>
2022-06-27 22:03:07 +0200

    make it possible to reuse a FileUpload

--- apollo-api/src/commonMain/kotlin/com/apollographql/apollo3/api/DefaultUpload.kt
+++ apollo-api/src/commonMain/kotlin/com/apollographql/apollo3/api/DefaultUpload.kt
@@ -1,70 +1,87 @@
 package com.apollographql.apollo3.api
 
+import com.apollographql.apollo3.annotations.ApolloDeprecatedSince
 import okio.BufferedSink
 import okio.BufferedSource
 import okio.ByteString
-import okio.ByteString.Companion.encodeUtf8
-import okio.ByteString.Companion.toByteString
+import okio.FileSystem
+import okio.Path
+import okio.buffer
 import okio.use
+import okio.utf8Size
 
 /**
- * An [Upload] that writes data from the provided content
- *
- * If the content is a [bufferedSource], the [DefaultUpload] will close it once uploaded
- * If the content is a [byteString], the [DefaultUpload] can be reused
+ * A default [Upload] that can upload from a wide variety of content
  */
 class DefaultUpload internal constructor(
-    private val bufferedSource: BufferedSource?,
-    private val byteString: ByteString?,
+    private val writeTo: (BufferedSink) -> Unit,
     override val contentType: String,
     override val contentLength: Long,
     override val fileName: String?,
 ) : Upload {
-  private var bufferedSourceConsumed = false
-
   override fun writeTo(sink: BufferedSink) {
-    if (bufferedSource != null) {
-      check(!bufferedSourceConsumed) { "Apollo: DefaultUpload body can only be read once. If you want to read it several times for logging or other purposes, either buffer it in memory or use your own `Upload` implementation." }
-      bufferedSource.use {
-        sink.writeAll(it)
-      }
-      bufferedSourceConsumed = true
-    } else if (byteString != null) {
-      sink.write(byteString)
-    } else {
-      error("No upload content found")
+    writeTo.invoke(sink)
+  }
+
+  fun newBuilder(): Builder {
+    val builder = Builder()
+        .content(writeTo)
+        .contentType(contentType)
+        .contentLength(contentLength)
+
+    if (fileName != null) {
+      builder.fileName(fileName)
     }
+    return builder
   }
 
   class Builder {
-    private var bufferedSource: BufferedSource? = null
-    private var byteString: ByteString? = null
+    private var writeTo: ((BufferedSink) -> Unit)? = null
     private var contentType: String? = null
     private var contentLength: Long = -1
     private var fileName: String? = null
-    private val hasContent: Boolean
-      get() = bufferedSource != null || byteString != null
 
+    @Deprecated("This API is dangerous because the resulting upload can only be used once and can also lead to resource leaks.",
+        ReplaceWith("content {sink ->\nval source = openSource()\nsource.use {sink.writeAll(it)}\n}"))
+    @ApolloDeprecatedSince(ApolloDeprecatedSince.Version.v3_3_3)
     fun content(content: BufferedSource): Builder = apply {
-      check(!hasContent) { "content() can only be called once" }
-      this.bufferedSource = content
+      check(writeTo == null) { "content() can only be called once" }
+      var consumed = false
+      this.writeTo = { sink ->
+        check(!consumed) { "Apollo: DefaultUpload body can only be read once. If you want to read it several times for logging or other purposes, either buffer it in memory or use your own `Upload` implementation." }
+        content.use {
+          sink.writeAll(it)
+        }
+        consumed = true
+      }
+    }
+
+    fun content(writeTo: (BufferedSink) -> Unit): Builder = apply {
+      check(this.writeTo == null) { "content() can only be called once" }
+      this.writeTo = writeTo
     }
 
     fun content(content: String): Builder = apply {
-      check(!hasContent) { "content() can only be called once" }
-      this.byteString = content.encodeUtf8()
-      this.contentLength = content.length.toLong()
+      check(writeTo == null) { "content() can only be called once" }
+      this.writeTo = { sink ->
+        sink.writeUtf8(content)
+      }
+      contentLength = content.utf8Size()
     }
 
     fun content(byteString: ByteString): Builder = apply {
-      check(!hasContent) { "content() can only be called once" }
-      this.byteString = byteString
+      check(writeTo == null) { "content() can only be called once" }
+      this.writeTo = { sink ->
+        sink.write(byteString)
+      }
       this.contentLength = byteString.size.toLong()
     }
 
     fun content(byteArray: ByteArray): Builder = apply {
-      check(!hasContent) { "content() can only be called once" }
-      this.byteString = byteArray.toByteString()
+      check(writeTo == null) { "content() can only be called once" }
+      this.writeTo = { sink ->
+        sink.write(byteArray)
+      }
       this.contentLength = byteArray.size.toLong()
     }
 
@@ -82,12 +99,23 @@ class DefaultUpload internal constructor(
 
     fun build(): DefaultUpload {
       return DefaultUpload(
-          bufferedSource,
-          byteString,
+          writeTo ?: error("DefaultUpload content is missing"),
           contentType ?: "application/octet-stream",
           contentLength,
           fileName
       )
     }
   }
+}
+
+fun Path.toUpload(contentType: String, fileSystem: FileSystem = systemFileSystem): Upload {
+  return DefaultUpload.Builder()
+      .content { sink ->
+        fileSystem.openReadOnly(this).use {
+          sink.writeAll(it.source().buffer())
+        }
+      }
+      .contentType(contentType)
+      .contentLength(fileSystem.metadata(this).size ?: -1L)
+      .build()
 }
