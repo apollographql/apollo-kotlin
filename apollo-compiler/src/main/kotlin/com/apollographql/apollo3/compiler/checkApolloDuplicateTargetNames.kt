daee061111052db25efa629c0335c5b8a5295a33
BoD <BoD@JRAF.org>
2022-07-11 16:44:53 +0200

    Validation: duplicate target name

---
+++ apollo-compiler/src/main/kotlin/com/apollographql/apollo3/compiler/checkApolloDuplicateTargetNames.kt
@@ -0,0 +1,58 @@
+@file:JvmName("-checkApolloDuplicateTargetNames")
+
+package com.apollographql.apollo3.compiler
+
+import com.apollographql.apollo3.annotations.ApolloInternal
+import com.apollographql.apollo3.ast.GQLDirective
+import com.apollographql.apollo3.ast.GQLEnumTypeDefinition
+import com.apollographql.apollo3.ast.GQLInputObjectTypeDefinition
+import com.apollographql.apollo3.ast.GQLInterfaceTypeDefinition
+import com.apollographql.apollo3.ast.GQLObjectTypeDefinition
+import com.apollographql.apollo3.ast.GQLScalarTypeDefinition
+import com.apollographql.apollo3.ast.GQLTypeDefinition
+import com.apollographql.apollo3.ast.GQLUnionTypeDefinition
+import com.apollographql.apollo3.ast.Issue
+import com.apollographql.apollo3.ast.Schema
+import com.apollographql.apollo3.ast.findTargetName
+
+@ApolloInternal
+fun checkApolloDuplicateTargetNames(schema: Schema): List<Issue> {
+  val issues = mutableListOf<Issue>()
+
+  val visitedTypes = mutableListOf<GQLTypeDefinition>()
+  for (type in schema
+      .typeDefinitions
+      .values
+  ) {
+    val targetName = type.directives.findTargetName(schema)
+    if (targetName == null) {
+      visitedTypes += type
+      continue
+    }
+
+    val existingTypeWithSameName = visitedTypes.find { visitedType ->
+      val visitedTypeTargetName = visitedType.directives.findTargetName(schema)
+      (visitedTypeTargetName ?: visitedType.name) == targetName
+    }
+    if (existingTypeWithSameName != null) {
+      issues.add(
+          Issue.ReservedEnumValueName(
+              message = "A type named '${targetName}' is already defined, please use a different target name. First definition is: ${existingTypeWithSameName.sourceLocation.pretty()}",
+              sourceLocation = type.sourceLocation
+          )
+      )
+    }
+    visitedTypes.add(type)
+  }
+  return issues
+}
+
+private val GQLTypeDefinition.directives: List<GQLDirective>
+  get() = when (this) {
+    is GQLEnumTypeDefinition -> directives
+    is GQLInputObjectTypeDefinition -> directives
+    is GQLInterfaceTypeDefinition -> directives
+    is GQLObjectTypeDefinition -> directives
+    is GQLScalarTypeDefinition -> directives
+    is GQLUnionTypeDefinition -> directives
+  }
--- apollo-compiler/src/test/kotlin/com/apollographql/apollo3/compiler/ValidationTest.kt
+++ apollo-compiler/src/test/kotlin/com/apollographql/apollo3/compiler/ValidationTest.kt
@@ -3,7 +3,6 @@ package com.apollographql.apollo3.compiler
 import com.apollographql.apollo3.ast.Issue
 import com.apollographql.apollo3.ast.parseAsGQLDocument
 import com.apollographql.apollo3.ast.validateAsExecutable
-import com.apollographql.apollo3.ast.validateAsSchema
 import com.apollographql.apollo3.ast.validateAsSchemaAndAddApolloDefinition
 import com.apollographql.apollo3.compiler.TestUtils.checkExpected
 import com.apollographql.apollo3.compiler.TestUtils.testParametersForGraphQLFilesIn
@@ -45,6 +44,8 @@ class ValidationTest(name: String, private val graphQLFile: File) {
         val schemaResult = parseResult.valueAssertNoErrors().validateAsSchemaAndAddApolloDefinition()
         schemaResult.issues + if (graphQLFile.name == "reserved-enum-value-names.graphql") {
           checkApolloReservedEnumValueNames(schemaResult.value!!)
+        } else if (graphQLFile.name == "duplicate-target-name.graphql") {
+          checkApolloDuplicateTargetNames(schemaResult.value!!)
         } else {
           emptyList()
         }
