5f87c9e17e001927d4d135e37fae40274011b80f
BoD <BoD@JRAF.org>
2022-07-21 16:44:47 +0200

    Make Apple's runTest use runBlocking, and mark it as deprecated. Also add a test using kotlinx.coroutines.test.runTest

--- apollo-testing-support/src/commonMain/kotlin/com/apollographql/apollo3/testing/runTest.kt
+++ apollo-testing-support/src/commonMain/kotlin/com/apollographql/apollo3/testing/runTest.kt
@@ -1,11 +1,15 @@
 package com.apollographql.apollo3.testing
 
+import com.apollographql.apollo3.annotations.ApolloDeprecatedSince
+import com.apollographql.apollo3.annotations.ApolloDeprecatedSince.Version.v3_4_1
 import com.apollographql.apollo3.annotations.ApolloExperimental
 import kotlinx.coroutines.CoroutineScope
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
 @ApolloExperimental
+@Deprecated("Use kotlinx.coroutines.test.runTest from org.jetbrains.kotlinx:kotlinx-coroutines-test instead")
+@ApolloDeprecatedSince(v3_4_1)
 expect fun runTest(
     context: CoroutineContext = EmptyCoroutineContext,
     before: suspend CoroutineScope.() -> Unit = {},
