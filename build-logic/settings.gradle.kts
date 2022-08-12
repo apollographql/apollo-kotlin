db16ce1a792fc406745b6f37ae0fddde728da4f9
BoD <BoD@JRAF.org>
2022-08-12 14:48:06 +0200

    Add specific content section to gradlePluginPortal

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,15 +1,20 @@
 rootProject.name = "build-logic"
 
 dependencyResolutionManagement {
-    repositories {
-        mavenCentral()
-        google()
-        gradlePluginPortal()
+  repositories {
+    mavenCentral()
+    google()
+    gradlePluginPortal() {
+      content {
+        includeModule("me.champeau.gradle", "japicmp-gradle-plugin")
+        includeModule("me.tylerbwong.gradle", "metalava-gradle")
+      }
     }
+  }
 
-    versionCatalogs {
-        create("libs") {
-            from(files("../gradle/libs.versions.toml"))
-        }
+  versionCatalogs {
+    create("libs") {
+      from(files("../gradle/libs.versions.toml"))
     }
+  }
 }
