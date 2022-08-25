ff6c279bd10193f184f99ed3fb1ac74aa1a59f43
Benoit Lubek <BoD@JRAF.org>
2022-08-25 15:34:02 +0200

    Fix JS tests passing even when they should fail (#4357)

---
+++ apollo-testing-support/src/jsMain/kotlin/com/apollographql/apollo3/testing/internal/runTest.kt
@@ -0,0 +1,39 @@
+package com.apollographql.apollo3.testing.internal
+
+import com.apollographql.apollo3.annotations.ApolloInternal
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.promise
+import kotlin.coroutines.CoroutineContext
+
+@ApolloInternal
+@OptIn(ExperimentalCoroutinesApi::class, DelicateCoroutinesApi::class)
+actual fun runTest(
+    skipDelays: Boolean,
+    context: CoroutineContext,
+    before: suspend CoroutineScope.() -> Unit,
+    after: suspend CoroutineScope.() -> Unit,
+    block: suspend CoroutineScope.() -> Unit,
+): dynamic {
+  return if (skipDelays) {
+    kotlinx.coroutines.test.runTest(context) {
+      before()
+      try {
+        block()
+      } finally {
+        after()
+      }
+    }
+  } else {
+    GlobalScope.promise(context = context) {
+      before()
+      try {
+        block()
+      } finally {
+        after()
+      }
+    }
+  }
+}
