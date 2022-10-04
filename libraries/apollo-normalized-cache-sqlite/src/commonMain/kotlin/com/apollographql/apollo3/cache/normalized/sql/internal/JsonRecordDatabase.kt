2a8952f60fb5ec23227939102a05de5496ad9a19
Martin Bonnin <martin@mbonnin.net>
2022-10-04 19:41:43 +0200

    implementselectAll

--- libraries/apollo-normalized-cache-sqlite/src/commonMain/kotlin/com/apollographql/apollo3/cache/normalized/sql/internal/JsonRecordDatabase.kt
+++ libraries/apollo-normalized-cache-sqlite/src/commonMain/kotlin/com/apollographql/apollo3/cache/normalized/sql/internal/JsonRecordDatabase.kt
@@ -1,6 +1,5 @@
 package com.apollographql.apollo3.cache.normalized.sql.internal
 
-import com.apollographql.apollo3.annotations.ApolloInternal
 import com.apollographql.apollo3.cache.normalized.api.Record
 import com.apollographql.apollo3.cache.normalized.api.internal.JsonRecordSerializer
 import com.apollographql.apollo3.cache.normalized.sql.internal.json.JsonQueries
@@ -52,6 +51,8 @@ internal class JsonRecordDatabase(private val jsonQueries: JsonQueries): RecordD
   }
 
   override fun selectAll(): List<Record> {
-    TODO("Not yet implemented")
+    return jsonQueries.selectRecords().executeAsList().map {
+      JsonRecordSerializer.deserialize(it.key, it.record)
+    }
   }
 }
