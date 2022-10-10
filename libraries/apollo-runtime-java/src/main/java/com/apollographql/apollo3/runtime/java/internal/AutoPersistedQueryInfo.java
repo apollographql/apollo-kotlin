fb910cbece34601af1b761fa75b5816beb572593
Martin Bonnin <martin@mbonnin.net>
2022-10-10 11:00:32 +0200

    fix creating an ExecutionContext.Element from Java

--- libraries/apollo-runtime-java/src/main/java/com/apollographql/apollo3/runtime/java/internal/AutoPersistedQueryInfo.java
+++ libraries/apollo-runtime-java/src/main/java/com/apollographql/apollo3/runtime/java/internal/AutoPersistedQueryInfo.java
@@ -12,25 +12,25 @@ class AutoPersistedQueryInfo implements ExecutionContext.Element {
     this.hit = hit;
   }
 
-  @NotNull @Override public ExecutionContext plus(@NotNull ExecutionContext context) {
-    return Element.super.plus(context);
-  }
-
   @NotNull @Override public Key<?> getKey() {
     return KEY;
   }
 
+  static final ExecutionContext.Key<AutoPersistedQueryInfo> KEY = new ExecutionContext.Key<AutoPersistedQueryInfo>() {};
+
+  @NotNull @Override public ExecutionContext minusKey(@NotNull ExecutionContext.Key<?> key) {
+    return ExecutionContext.Element.DefaultImpls.minusKey(this, key);
+  }
+
   @Nullable @Override public <E extends Element> E get(@NotNull ExecutionContext.Key<E> key) {
-    return Element.super.get(key);
+    return ExecutionContext.Element.DefaultImpls.get(this, key);
   }
 
   @Override public <R> R fold(R initial, @NotNull Function2<? super R, ? super Element, ? extends R> operation) {
-    return Element.super.fold(initial, operation);
+    return ExecutionContext.Element.DefaultImpls.fold(this, initial, operation);
   }
 
-  @NotNull @Override public ExecutionContext minusKey(@NotNull ExecutionContext.Key<?> key) {
-    return Element.super.minusKey(key);
+  @NotNull @Override public ExecutionContext plus(@NotNull ExecutionContext context) {
+    return ExecutionContext.Element.DefaultImpls.plus(this, context);
   }
-
-  static final ExecutionContext.Key<AutoPersistedQueryInfo> KEY = new ExecutionContext.Key<AutoPersistedQueryInfo>() {};
 }
