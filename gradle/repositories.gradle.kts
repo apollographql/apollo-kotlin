0aa08512a886cbb1bab6a62dd9ccdae73f4ef00c
Martin Bonnin <martin@mbonnin.net>
2022-11-21 10:00:12 +0100

    factor the repositories even more (#4521)

--- gradle/repositories.gradle.kts
+++ gradle/repositories.gradle.kts
@@ -1,44 +1,27 @@
+// the plugin management block is evaluated first separately, do not change this to
+// listOf(pluginManagement.repositories, dependencyResolutionManagement.repositories)
+// instead that would change the semantics
+// See https://www.linen.dev/s/gradle-community/t/5100108/are-the-rules-for-how-gradle-parses-compiles-buildscript-and
 pluginManagement {
-  repositories {
-    mavenCentral()
-    google()
-    gradlePluginPortal {
-      content {
-        includeModule("org.gradle.kotlin.embedded-kotlin", "org.gradle.kotlin.embedded-kotlin.gradle.plugin")
-        includeGroup("org.gradle.kotlin")
-        includeModule("me.champeau.gradle", "japicmp-gradle-plugin")
-        includeModule("com.gradle.publish", "plugin-publish-plugin")
-        includeModule("com.github.ben-manes", "gradle-versions-plugin")
+  listOf(repositories, dependencyResolutionManagement.repositories).forEach {
+    it.apply {
+      mavenCentral()
+      google()
+      gradlePluginPortal {
+        content {
+          includeModule("org.gradle.kotlin.embedded-kotlin", "org.gradle.kotlin.embedded-kotlin.gradle.plugin")
+          includeGroup("org.gradle.kotlin")
+          includeModule("me.champeau.gradle", "japicmp-gradle-plugin")
+          includeModule("com.gradle.publish", "plugin-publish-plugin")
+          includeModule("com.github.ben-manes", "gradle-versions-plugin")
+        }
       }
-    }
-    @Suppress("DEPRECATION")
-    jcenter {
-      content {
-        // https://github.com/Kotlin/kotlinx-nodejs/issues/16
-        includeModule("org.jetbrains.kotlinx", "kotlinx-nodejs")
-      }
-    }
-  }
-}
-
-dependencyResolutionManagement {
-  repositories {
-    mavenCentral()
-    google()
-    gradlePluginPortal {
-      content {
-        includeModule("org.gradle.kotlin.embedded-kotlin", "org.gradle.kotlin.embedded-kotlin.gradle.plugin")
-        includeGroup("org.gradle.kotlin")
-        includeModule("me.champeau.gradle", "japicmp-gradle-plugin")
-        includeModule("com.gradle.publish", "plugin-publish-plugin")
-        includeModule("com.github.ben-manes", "gradle-versions-plugin")
-      }
-    }
-    @Suppress("DEPRECATION")
-    jcenter {
-      content {
-        // https://github.com/Kotlin/kotlinx-nodejs/issues/16
-        includeModule("org.jetbrains.kotlinx", "kotlinx-nodejs")
+      @Suppress("DEPRECATION")
+      jcenter {
+        content {
+          // https://github.com/Kotlin/kotlinx-nodejs/issues/16
+          includeModule("org.jetbrains.kotlinx", "kotlinx-nodejs")
+        }
       }
     }
   }
