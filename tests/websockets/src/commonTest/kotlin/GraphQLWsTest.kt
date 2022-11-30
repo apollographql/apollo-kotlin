eae682ddc5ad14d142ff019e07a4b4f56b9c4f6c
Martin Bonnin <martin@mbonnin.net>
2022-11-30 12:40:51 +0100

    fix graphql-ws errors

--- tests/websockets/src/commonTest/kotlin/GraphQLWsTest.kt
+++ tests/websockets/src/commonTest/kotlin/GraphQLWsTest.kt
@@ -5,19 +5,22 @@ import com.apollographql.apollo3.network.ws.WebSocketNetworkTransport
 import com.apollographql.apollo3.testing.internal.runTest
 import graphql.ws.GreetingsSubscription
 import graphql.ws.HelloQuery
+import graphql.ws.Hello2Query
 import graphql.ws.SetHelloMutation
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.toList
+import okio.use
 import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
 // Ignored because it depends on a local server
 // See https://github.com/martinbonnin/graphql-ws-server
 // These tests take a long time to execute, this is expected
 @Ignore
 class GraphQLWsTest {
-  @Test
-  fun queryOverWebSocket() = runTest {
+  private fun withApolloClient(block: suspend CoroutineScope.(ApolloClient) -> Unit) = runTest {
     val apolloClient = ApolloClient.Builder()
         .networkTransport(
             WebSocketNetworkTransport.Builder().serverUrl(
@@ -27,43 +30,33 @@ class GraphQLWsTest {
             ).build()
         )
         .build()
-
+    apolloClient.use {
+      block(it)
+    }
+  }
+  @Test
+  fun queryOverWebSocket() = withApolloClient { apolloClient ->
     assertEquals("Hello World!", apolloClient.query(HelloQuery()).execute().data?.hello)
   }
 
   @Test
-  fun mutationOverWebSocket() = runTest {
-    val apolloClient = ApolloClient.Builder()
-        .networkTransport(
-            WebSocketNetworkTransport.Builder().serverUrl(
-                serverUrl = "http://localhost:9090/graphql",
-            ).protocol(
-                protocolFactory = GraphQLWsProtocol.Factory()
-            ).build()
-        )
-        .build()
+  fun errorOverWebSocket()  = withApolloClient { apolloClient ->
+    val response = apolloClient.query(Hello2Query()).execute()
+    assertNull(response.data)
+    assertEquals("Cannot query field \"hello2\" on type \"Query\". Did you mean \"hello\"?", response.errors?.get(0)?.message)
+  }
 
+  @Test
+  fun mutationOverWebSocket() =  withApolloClient { apolloClient ->
     assertEquals("Hello Mutation!", apolloClient.mutation(SetHelloMutation()).execute().data?.hello)
   }
 
 
   @Test
-  fun subscriptionOverWebSocket() = runTest {
-    val apolloClient = ApolloClient.Builder()
-        .networkTransport(
-            WebSocketNetworkTransport.Builder().serverUrl(
-                serverUrl = "http://localhost:9090/graphql",
-            ).protocol(
-                protocolFactory = GraphQLWsProtocol.Factory()
-            ).build()
-        )
-        .build()
-
+  fun subscriptionOverWebSocket() = withApolloClient { apolloClient ->
     val list = apolloClient.subscription(GreetingsSubscription())
         .toFlow()
         .toList()
     assertEquals(listOf("Hi", "Bonjour", "Hola", "Ciao", "Zdravo"), list.map { it.data?.greetings })
-
-    apolloClient.dispose()
   }
 }
