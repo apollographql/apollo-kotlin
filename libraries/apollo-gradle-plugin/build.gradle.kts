a34022c2aeef0705254260ddbee23f2814583973
Martin Bonnin <martin@mbonnin.net>
2022-12-02 11:26:11 +0100

    Fix the below warning:
    ```
    Multiple publications with coordinates 'com.apollographql.apollo3:apollo-gradle-plugin-external:3.7.2-SNAPSHOT' are published to repository 'mavenLocal'. The publications 'default' in project ':libraries:apollo-gradle-plugin-external' and 'pluginMaven' in project ':libraries:apollo-gradle-plugin-external' will overwrite each other!
    ```
    
    The order of applying warning is important. Without this, the "apollo.library" plugin was missing the "java-gradle-plugin" plugin and creating too many publications

--- libraries/apollo-gradle-plugin/build.gradle.kts
+++ libraries/apollo-gradle-plugin/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 apolloLibrary {
-  treatWarningsAsErrors(false)
+  treatWarningsAsErrors(true)
 }
 
 // Configuration for extra jar to pass to R8 to give it more context about what can be relocated
