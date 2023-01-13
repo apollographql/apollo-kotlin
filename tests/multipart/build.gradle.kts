e25bc2f0b4f2158e9ec0cf4619f0ca689754aa69
Benoit Lubek <BoD@JRAF.org>
2023-01-13 10:03:00 +0100

    Allow having an empty last part in multipart (#4598)
    (cherry picked from commit 7e4550a5ec3b5bfbd93893ab331c11ab5716a19c)

---
+++ tests/multipart/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+  id("org.jetbrains.kotlin.multiplatform")
+  id("apollo.test")
+  id("com.apollographql.apollo3")
+}
+
+apolloTest {
+  mpp {}
+}
+
+kotlin {
+  sourceSets {
+    findByName("commonMain")?.apply {
+      dependencies {
+        implementation(golatac.lib("apollo.runtime"))
+      }
+    }
+
+    findByName("commonTest")?.apply {
+      dependencies {
+        implementation(golatac.lib("apollo.testingsupport"))
+      }
+    }
+  }
+}
+
+
+apollo {
+  service("service") {
+    packageName.set("multipart")
+  }
+}
