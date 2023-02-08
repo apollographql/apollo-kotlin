46f11579f8e80524dcd150fffaa5106a9114836e
BoD <BoD@JRAF.org>
2023-02-08 16:13:41 +0100

    Fix migration tests

--- intellij-plugin/src/test/testData/migration/v2-to-v3/upgradeGradlePluginInBuildGradleKts_after.gradle.kts
+++ intellij-plugin/src/test/testData/migration/v2-to-v3/upgradeGradlePluginInBuildGradleKts_after.gradle.kts
@@ -2,17 +2,17 @@ plugins {
   java
   kotlin("jvm") version "1.6.10"
   id("com.apollographql.apollo3")
-  id("com.apollographql.apollo3").version("3.7.1")
-  id("com.apollographql.apollo3").version("3.7.1").apply(false)
-  id("com.apollographql.apollo3").version("3.7.1") apply (false)
-  // TODO: Update version to 3.7.1
+  id("com.apollographql.apollo3").version("3.7.4")
+  id("com.apollographql.apollo3").version("3.7.4").apply(false)
+  id("com.apollographql.apollo3").version("3.7.4") apply (false)
+  // TODO: Update version to 3.7.4
   id("com.apollographql.apollo3").version(someClass.someConstant)
-  // TODO: Update version to 3.7.1
+  // TODO: Update version to 3.7.4
   id("com.apollographql.apollo3").version("${someClass.someConstant}")
-  id("com.apollographql.apollo3") version "3.7.1"
-  id("com.apollographql.apollo3") version "3.7.1" apply false
-  // TODO: Update version to 3.7.1
+  id("com.apollographql.apollo3") version "3.7.4"
+  id("com.apollographql.apollo3") version "3.7.4" apply false
+  // TODO: Update version to 3.7.4
   id("com.apollographql.apollo3") version someClass.someConstant
-  // TODO: Update version to 3.7.1
+  // TODO: Update version to 3.7.4
   id("com.apollographql.apollo3") version "${someClass.someConstant}"
 }
