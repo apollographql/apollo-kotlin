6d747312f29b9d449a89a3efbcc885700d34cd56
BoD <BoD@JRAF.org>
2023-02-09 18:25:47 +0100

    Remove composite builds workaround

--- tests/build.gradle.kts
+++ tests/build.gradle.kts
@@ -17,33 +17,3 @@ tasks.register("ciBuild") {
     checkGitStatus()
   }
 }
-
-/**
- * Workaround for composite builds not working super well with multiplatform
- * See https://youtrack.jetbrains.com/issue/KT-51970
- * See https://youtrack.jetbrains.com/issue/KT-52172
- */
-if (System.getProperty("idea.sync.active") == null) {
-  gradle.startParameter.setExcludedTaskNames(listOf(
-      ":coroutines-mt:compileAppleMainKotlinMetadata",
-      ":coroutines-mt:compileCommonMainKotlinMetadata",
-      ":defer:compileAppleMainKotlinMetadata",
-      ":defer:compileCommonMainKotlinMetadata",
-      ":gzip:compileCommonMainKotlinMetadata",
-      ":integration-tests:compileAppleMainKotlinMetadata",
-      ":integration-tests:compileCommonMainKotlinMetadata",
-      ":ios-test:compileCommonMainKotlinMetadata",
-      ":legacy-memory-model:compileCommonMainKotlinMetadata",
-      ":models-operation-based:compileAppleMainKotlinMetadata",
-      ":models-operation-based:compileCommonMainKotlinMetadata",
-      ":models-response-based:compileAppleMainKotlinMetadata",
-      ":models-response-based:compileCommonMainKotlinMetadata",
-      ":multipart:compileCommonMainKotlinMetadata",
-      ":native-benchmarks:compileAppleMainKotlinMetadata",
-      ":native-benchmarks:compileCommonMainKotlinMetadata",
-      ":pagination:compileCommonMainKotlinMetadata",
-      ":sqlite:compileCommonMainKotlinMetadata",
-      ":websockets:compileCommonMainKotlinMetadata",
-      ":js:compileCommonMainKotlinMetadata",
-  ))
-}
