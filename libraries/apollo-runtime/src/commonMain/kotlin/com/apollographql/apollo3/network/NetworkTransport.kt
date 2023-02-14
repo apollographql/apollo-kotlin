187d624d6e502dbc7c5a3ae87b533b7f8dcc486b
BoD <BoD@JRAF.org>
2023-02-14 17:53:36 +0100

    Clarify the new exception contract of NetworkTransport

--- libraries/apollo-runtime/src/commonMain/kotlin/com/apollographql/apollo3/network/NetworkTransport.kt
+++ libraries/apollo-runtime/src/commonMain/kotlin/com/apollographql/apollo3/network/NetworkTransport.kt
@@ -1,12 +1,20 @@
 package com.apollographql.apollo3.network
 
-import com.apollographql.apollo3.api.Operation
 import com.apollographql.apollo3.api.ApolloRequest
 import com.apollographql.apollo3.api.ApolloResponse
+import com.apollographql.apollo3.api.Operation
+import com.apollographql.apollo3.exception.ApolloException
 import kotlinx.coroutines.flow.Flow
 
 interface NetworkTransport {
 
+  /**
+   * Execute a request.
+   *
+   * @param request the request to execute.
+   * @return a flow of responses.
+   * [ApolloException]s must not be thrown from the flow. Instead, they must be emitted as [ApolloResponse.exception].
+   */
   fun <D : Operation.Data> execute(
       request: ApolloRequest<D>,
   ): Flow<ApolloResponse<D>>
