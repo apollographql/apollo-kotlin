88269da4978d7b889490d6ad56d80ee43fcfff34
Goooler <wangzongler@gmail.com>
2023-03-02 23:18:15 +0800

    Remove duplicates

--- intellij-plugin/src/test/testData/migration/v2-to-v3/upgradeGradlePluginInBuildGradleKts.gradle.kts
+++ intellij-plugin/src/test/testData/migration/v2-to-v3/upgradeGradlePluginInBuildGradleKts.gradle.kts
@@ -4,7 +4,6 @@ plugins {
   id("com.apollographql.apollo")
   id("com.apollographql.apollo") version "2.5.14"
   id("com.apollographql.apollo") version "2.5.14" apply false
-  id("com.apollographql.apollo") version "2.5.14" apply false
   id("com.apollographql.apollo") version someClass.someConstant
   id("com.apollographql.apollo") version "${someClass.someConstant}"
   id("com.apollographql.apollo") version "2.5.14"
--- intellij-plugin/src/test/testData/migration/v2-to-v3/upgradeGradlePluginInBuildGradleKts_after.gradle.kts
+++ intellij-plugin/src/test/testData/migration/v2-to-v3/upgradeGradlePluginInBuildGradleKts_after.gradle.kts
@@ -4,7 +4,6 @@ plugins {
   id("com.apollographql.apollo3")
   id("com.apollographql.apollo3") version "3.7.4"
   id("com.apollographql.apollo3") version "3.7.4" apply false
-  id("com.apollographql.apollo3") version "3.7.4" apply false
   // TODO: Update version to 3.7.4
   id("com.apollographql.apollo3") version someClass.someConstant
   // TODO: Update version to 3.7.4
