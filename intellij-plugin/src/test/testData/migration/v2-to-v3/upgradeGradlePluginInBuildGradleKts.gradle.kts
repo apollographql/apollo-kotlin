fbfbd2eccfe017cf1542c9eae9184f65310717c8
Goooler <wangzongler@gmail.com>
2023-03-02 22:33:28 +0800

    Fix

--- intellij-plugin/src/test/testData/migration/v2-to-v3/upgradeGradlePluginInBuildGradleKts.gradle.kts
+++ intellij-plugin/src/test/testData/migration/v2-to-v3/upgradeGradlePluginInBuildGradleKts.gradle.kts
@@ -3,7 +3,7 @@ plugins {
   kotlin("jvm") version "1.6.10"
   id("com.apollographql.apollo")
   id("com.apollographql.apollo") version "2.5.14"
-  id("com.apollographql.apollo") version "2.5.14" apply
+  id("com.apollographql.apollo") version "2.5.14" apply false
   id("com.apollographql.apollo") version "2.5.14" apply false
   id("com.apollographql.apollo") version someClass.someConstant
   id("com.apollographql.apollo") version "${someClass.someConstant}"
