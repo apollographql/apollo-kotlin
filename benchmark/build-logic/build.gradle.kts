8bd7493222218dda5c796cd6d67883f4fac8388e
Goooler <wangzongler@gmail.com>
2023-03-02 22:15:38 +0800

    Use infix version style for applying plugins
    Easy to use in both Groovy and Kotlin DSL

--- benchmark/build-logic/build.gradle.kts
+++ benchmark/build-logic/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   `embedded-kotlin`
-  id("net.mbonnin.golatac").version("0.0.3")
+  id("net.mbonnin.golatac") version "0.0.3"
 }
 
 golatac.init(file("../../gradle/libraries.toml"))
--- intellij-plugin/src/test/testData/migration/v2-to-v3/upgradeGradlePluginInBuildGradleKts.gradle.kts
+++ intellij-plugin/src/test/testData/migration/v2-to-v3/upgradeGradlePluginInBuildGradleKts.gradle.kts
@@ -2,11 +2,11 @@ plugins {
   java
   kotlin("jvm") version "1.6.10"
   id("com.apollographql.apollo")
-  id("com.apollographql.apollo").version("2.5.14")
-  id("com.apollographql.apollo").version("2.5.14").apply(false)
-  id("com.apollographql.apollo").version("2.5.14") apply (false)
-  id("com.apollographql.apollo").version(someClass.someConstant)
-  id("com.apollographql.apollo").version("${someClass.someConstant}")
+  id("com.apollographql.apollo") version "2.5.14"
+  id("com.apollographql.apollo") version "2.5.14" apply
+  id("com.apollographql.apollo") version "2.5.14" apply false
+  id("com.apollographql.apollo") version someClass.someConstant
+  id("com.apollographql.apollo") version "${someClass.someConstant}"
   id("com.apollographql.apollo") version "2.5.14"
   id("com.apollographql.apollo") version "2.5.14" apply false
   id("com.apollographql.apollo") version someClass.someConstant
