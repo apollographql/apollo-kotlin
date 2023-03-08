70cbe6dba1ed1c3c96d9bca1b3f64914affe47d3
Martin Bonnin <martin@mbonnin.net>
2023-03-08 14:20:51 +0100

    Add multipart subscriptions test (#4738)
    * add router multipart subscriptions test
    
    * make private
    
    * update apiDump

---
+++ tests/multipart/src/commonTest/kotlin/test/MultipartSubscriptionsRouterTest.kt
@@ -0,0 +1,46 @@
+package test
+
+import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.network.http.HttpNetworkTransport
+import com.apollographql.apollo3.testing.internal.runTest
+import router.DieSubscription
+import kotlin.test.Ignore
+import kotlin.test.Test
+
+/**
+ * A test that runs against https://github.com/apollographql/federated-subscriptions-poc
+ */
+@Ignore
+class MultipartSubscriptionsRouterTest {
+  @Test
+  fun routerTest() = runTest {
+    val apolloClient = ApolloClient.Builder()
+        .serverUrl("http://localhost:4040/")
+        .subscriptionNetworkTransport(
+            HttpNetworkTransport.Builder()
+                .serverUrl("http://localhost:4040/")
+                .build()
+        )
+        .build()
+
+    /**
+     * Should display something like this
+     *
+     * Got Data(aNewDieWasCreated=ANewDieWasCreated(die=Die(roll=3, sides=30, color=red)))
+     * Got Data(aNewDieWasCreated=ANewDieWasCreated(die=Die(roll=5, sides=31, color=blue)))
+     * Got Data(aNewDieWasCreated=ANewDieWasCreated(die=Die(roll=11, sides=32, color=red)))
+     * Got Data(aNewDieWasCreated=ANewDieWasCreated(die=Die(roll=12, sides=33, color=blue)))
+     * Got Data(aNewDieWasCreated=ANewDieWasCreated(die=Die(roll=21, sides=34, color=red)))
+     * Got Data(aNewDieWasCreated=ANewDieWasCreated(die=Die(roll=16, sides=35, color=blue)))
+     * Got Data(aNewDieWasCreated=ANewDieWasCreated(die=Die(roll=22, sides=36, color=red)))
+     * Got Data(aNewDieWasCreated=ANewDieWasCreated(die=Die(roll=11, sides=37, color=blue)))
+     */
+    apolloClient.subscription(DieSubscription()).toFlow().collect {
+      if (it.data != null) {
+        println("${it.data}")
+      } else {
+        it.exception!!.printStackTrace()
+      }
+    }
+  }
+}
