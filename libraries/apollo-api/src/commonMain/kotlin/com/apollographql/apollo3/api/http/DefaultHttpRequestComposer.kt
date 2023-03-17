c3ecc6831d2c3ae7c91a8be326d2b95447f15547
Martin Bonnin <martin@mbonnin.net>
2023-03-17 11:58:32 +0100

    forgot to remove one const

--- libraries/apollo-api/src/commonMain/kotlin/com/apollographql/apollo3/api/http/DefaultHttpRequestComposer.kt
+++ libraries/apollo-api/src/commonMain/kotlin/com/apollographql/apollo3/api/http/DefaultHttpRequestComposer.kt
@@ -72,7 +72,7 @@ class DefaultHttpRequestComposer(
   }
 
   companion object {
-    const val HEADER_APOLLO_OPERATION_ID = "X-APOLLO-OPERATION-ID"
+    val HEADER_APOLLO_OPERATION_ID = "X-APOLLO-OPERATION-ID"
 
     // Note: in addition to this being a generally useful header to send, Apollo
     // Server's CSRF prevention feature (introduced in AS3.7 and intended to be
