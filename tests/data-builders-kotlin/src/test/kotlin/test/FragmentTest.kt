25cdf708071d77c576670cf126e3723a1d064aa0
Martin Bonnin <martin@mbonnin.net>
2023-03-21 18:46:08 +0100

    fix test

--- tests/data-builders-kotlin/src/test/kotlin/test/FragmentTest.kt
+++ tests/data-builders-kotlin/src/test/kotlin/test/FragmentTest.kt
@@ -2,6 +2,8 @@ package test
 
 import data.builders.fragment.AnimalDetailsImpl
 import data.builders.fragment.CatDetailsImpl
+import data.builders.fragment.TrivialFragment
+import data.builders.fragment.TrivialFragmentImpl
 import data.builders.type.Animal
 import data.builders.type.buildLion
 import org.junit.Assert.assertNull
@@ -32,22 +34,18 @@ class FragmentTest {
     val data = AnimalDetailsImpl.Data(Animal) {
       __typename = "Brontaroc"
       species = "alien"
-      id = "43"
-      country = "Trisolaris"
     }
-    // The parsers know that onAnimal is always true despite the unknkown __typename
     assertEquals("alien", data.species)
-    assertEquals("43", data.onAnimal.id)
   }
 
   @Test
   fun polymorphicFragmentMissingFields() {
     // The fake resolvers are not aware that on onAnimal is always true because the
     // __typename is unknown so this fails
-    // XXX: we could be smarter about this
+    // XXX: we could be smarter about this (the parsers are
     // data.builders.fragment.AnimalDetailsImpl_ResponseAdapter$OnAnimal.fromJson(AnimalDetailsImpl_ResponseAdapter.kt:85)
     assertFailsWith(NullPointerException::class) {
-      AnimalDetailsImpl.Data(Animal) {
+      TrivialFragmentImpl.Data(Animal) {
         __typename = "Brontaroc"
         species = "alien"
       }
