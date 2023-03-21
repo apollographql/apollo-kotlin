4bed05a83453af368f6a025184de8259fe7fdfe9
BoD <BoD@JRAF.org>
2023-03-21 11:13:58 +0100

    Add compose support

---
+++ libraries/apollo-compose-support/build.gradle.kts
@@ -0,0 +1,41 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinCompile
+
+plugins {
+  id("com.android.library")
+  id("org.jetbrains.kotlin.android")
+  id("apollo.library")
+}
+
+dependencies {
+  api(golatac.lib("compose-runtime"))
+  api(project(":apollo-runtime"))
+  api(project(":apollo-normalized-cache"))
+}
+
+android {
+  compileSdk = golatac.version("android.sdkversion.compile").toInt()
+
+  defaultConfig {
+    minSdk = golatac.version("android.sdkversion.min").toInt()
+    targetSdk = golatac.version("android.sdkversion.target").toInt()
+  }
+
+  buildFeatures {
+    compose = true
+  }
+
+  composeOptions {
+    kotlinCompilerExtensionVersion = golatac.version("compose.compiler")
+  }
+}
+
+// TODO: needed as long as we use a non supported version of Kotlin
+// See https://developer.android.com/jetpack/androidx/releases/compose-kotlin
+tasks.withType(KotlinCompile::class.java).configureEach {
+  kotlinOptions {
+    freeCompilerArgs += listOf(
+        "-P",
+        "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=1.8.20-RC"
+    )
+  }
+}
---
+++ libraries/apollo-compose-support/src/main/java/com/apollographql/apollo3/compose/State.kt
@@ -0,0 +1,64 @@
+package com.apollographql.apollo3.compose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
+import com.apollographql.apollo3.ApolloCall
+import com.apollographql.apollo3.annotations.ApolloExperimental
+import com.apollographql.apollo3.api.ApolloResponse
+import com.apollographql.apollo3.api.Operation
+import com.apollographql.apollo3.api.Query
+import com.apollographql.apollo3.cache.normalized.watch
+import com.apollographql.apollo3.exception.ApolloException
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+
+/**
+ * Returns a State that produces [ApolloResponse]s for this [ApolloCall].
+ * Execution happens at first call.
+ * The initial value is null, which can be used to mean 'waiting for the first value'.
+ * [ApolloResponse.exception] can be used to check for [ApolloException]s thrown during execution.
+ * Example:
+ * ```
+ *     val response by apolloClient.query(MyQuery()).toState()
+ *     when {
+ *         response == null -> Loading()
+ *         response!!.exception != null -> NetworkError(response!!.exception!!)
+ *         response!!.hasErrors() -> BackendError(response!!.errors!!)
+ *         else -> Screen(response!!.data!!)
+ *     }
+ * ```
+ */
+@ApolloExperimental
+@Composable
+fun <D : Operation.Data> ApolloCall<D>.toState(context: CoroutineContext = EmptyCoroutineContext): State<ApolloResponse<D>?> {
+  val responseFlow = remember {
+    toFlow()
+  }
+  return responseFlow.collectAsState(initial = null, context = context)
+}
+
+/**
+ * Returns a State that produces [ApolloResponse]s by watching this [ApolloCall].
+ * The initial value is null, which can be used to mean 'waiting for the first value'.
+ * [ApolloResponse.exception] can be used to check for [ApolloException]s thrown during execution.
+ * Example:
+ * ```
+ *     val response by apolloClient.query(MyQuery()).watchAsState()
+ *     when {
+ *         response == null -> Loading()
+ *         response!!.exception != null -> NetworkError(response!!.exception!!)
+ *         response!!.hasErrors() -> BackendError(response!!.errors!!)
+ *         else -> Screen(response!!.data!!)
+ *     }
+ * ```
+ */
+@ApolloExperimental
+@Composable
+fun <D : Query.Data> ApolloCall<D>.watchAsState(context: CoroutineContext = EmptyCoroutineContext): State<ApolloResponse<D>?> {
+  val responseFlow = remember {
+    watch()
+  }
+  return responseFlow.collectAsState(initial = null, context = context)
+}
