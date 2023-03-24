f1abc913b21f1bb7a94f813406d106a37b0ddc24
Benoit Lubek <BoD@JRAF.org>
2023-03-24 11:29:04 +0100

    First drop of experimental Compose support libraries (#4783)
    * Add compose support
    
    * Update API dump
    
    * Fix compiling issue with 1.8.20-RC
    
    * Use 1.8.20-RC2
    
    (cherry picked from commit 4f96c1d866b53006f349b9b1c57c1977976212f2)

---
+++ libraries/apollo-compose-paging-support/build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinCompile
+
+plugins {
+  id("com.android.library")
+  id("org.jetbrains.kotlin.android")
+  id("apollo.library")
+}
+
+dependencies {
+  api(project(":apollo-compose-support"))
+  api("androidx.paging:paging-compose:1.0.0-alpha18")
+}
+
+android {
+  compileSdk = golatac.version("android.sdkversion.compile").toInt()
+
+  defaultConfig {
+    minSdk = golatac.version("android.sdkversion.min").toInt()
+    targetSdk = golatac.version("android.sdkversion.target").toInt()
+  }
+
+  buildFeatures {
+    compose = true
+  }
+
+  composeOptions {
+    kotlinCompilerExtensionVersion = golatac.version("compose.compiler")
+  }
+
+  // TODO: compiling fails only with the debug variant currently, due to using a version of Kotlin non supported by Compose.
+  // For now, disabling the debug variant works around the issue.
+  variantFilter {
+    ignore = name == "debug"
+  }
+}
+
+// TODO: needed as long as we use a non supported version of Kotlin
+// See https://developer.android.com/jetpack/androidx/releases/compose-kotlin
+tasks.withType(KotlinCompile::class.java).configureEach {
+  kotlinOptions {
+    freeCompilerArgs += listOf(
+        "-P",
+        "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=1.8.20-RC2"
+    )
+  }
+}
