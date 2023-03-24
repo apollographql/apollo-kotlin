c4caa90c51cac2c7b1168850f0eeaf75d68bdedd
Benoit Lubek <BoD@JRAF.org>
2023-03-24 16:23:42 +0100

    Fix minSdk for compose support (#4793)
    * Compose needs minSdk 21
    
    * Use libraries.toml
    
    * Toml trouble

--- libraries/apollo-compose-paging-support/build.gradle.kts
+++ libraries/apollo-compose-paging-support/build.gradle.kts
@@ -15,7 +15,7 @@ android {
   compileSdk = golatac.version("android.sdkversion.compile").toInt()
 
   defaultConfig {
-    minSdk = golatac.version("android.sdkversion.min").toInt()
+    minSdk = golatac.version("android.sdkversion.compose.min").toInt()
     targetSdk = golatac.version("android.sdkversion.target").toInt()
   }
 
