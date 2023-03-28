c5d69d1932b5197d0da4903d450f58944734befb
Martin Bonnin <martin@mbonnin.net>
2023-03-28 16:19:54 +0200

    more urlencode tests

---
+++ libraries/apollo-api/src/jvmTest/kotlin/UrlEncodeTest.kt
@@ -0,0 +1,18 @@
+import com.apollographql.apollo3.api.http.internal.urlEncode
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class UrlEncodeTest {
+
+  @Test
+  fun test() {
+    listOf(
+        "%" to "%25",
+        " " to "%20",
+        "{}" to "%7B%7D",
+        "e30=" to "e30%3D", // {} base64 encoded
+    ).forEach {
+      assertEquals(it.second, it.first.urlEncode())
+    }
+  }
+}
