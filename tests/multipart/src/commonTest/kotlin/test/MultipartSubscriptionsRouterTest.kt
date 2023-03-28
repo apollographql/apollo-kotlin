306ab19b9f6cc2bca6d58b596189c2d679e8b89e
Martin Bonnin <martin@mbonnin.net>
2023-03-28 13:03:17 +0200

    add a test for multipart subscriptions

--- tests/multipart/src/commonTest/kotlin/test/MultipartSubscriptionsRouterTest.kt
+++ tests/multipart/src/commonTest/kotlin/test/MultipartSubscriptionsRouterTest.kt
@@ -1,10 +1,12 @@
 package test
 
 import com.apollographql.apollo3.ApolloClient
-import com.apollographql.apollo3.api.http.DefaultHttpRequestComposer
 import com.apollographql.apollo3.network.http.HttpNetworkTransport
+import com.apollographql.apollo3.network.http.LoggingInterceptor
 import com.apollographql.apollo3.testing.internal.runTest
+import okio.use
 import router.DieSubscription
+import router.ReviewSubscription
 import kotlin.test.Ignore
 import kotlin.test.Test
 
@@ -13,34 +15,59 @@ import kotlin.test.Test
  */
 @Ignore
 class MultipartSubscriptionsRouterTest {
-  @Test
-  fun routerTest() = runTest {
-    val apolloClient = ApolloClient.Builder()
-        .serverUrl("http://localhost:4040/")
-        .subscriptionNetworkTransport(
-            HttpNetworkTransport.Builder()
-                .serverUrl("http://localhost:4040/")
-                .build()
-        )
-        .build()
+  private fun client(log: Boolean) = ApolloClient.Builder()
+      .serverUrl("http://localhost:4040/")
+      .subscriptionNetworkTransport(
+          HttpNetworkTransport.Builder()
+              .apply {
+                if (log) {
+                  addInterceptor(
+                      LoggingInterceptor(LoggingInterceptor.Level.BODY) {
+                        println(it.replace("\r", "\\r").replace("\n", "\\n"))
+                      },
+                  )
+                }
+              }
+              .serverUrl("http://localhost:4040/")
+              .build()
+      )
+      .build()
 
-    /**
-     * Should display something like this
-     *
-     * Got Data(aNewDieWasCreated=ANewDieWasCreated(die=Die(roll=3, sides=30, color=red)))
-     * Got Data(aNewDieWasCreated=ANewDieWasCreated(die=Die(roll=5, sides=31, color=blue)))
-     * Got Data(aNewDieWasCreated=ANewDieWasCreated(die=Die(roll=11, sides=32, color=red)))
-     * Got Data(aNewDieWasCreated=ANewDieWasCreated(die=Die(roll=12, sides=33, color=blue)))
-     * Got Data(aNewDieWasCreated=ANewDieWasCreated(die=Die(roll=21, sides=34, color=red)))
-     * Got Data(aNewDieWasCreated=ANewDieWasCreated(die=Die(roll=16, sides=35, color=blue)))
-     * Got Data(aNewDieWasCreated=ANewDieWasCreated(die=Die(roll=22, sides=36, color=red)))
-     * Got Data(aNewDieWasCreated=ANewDieWasCreated(die=Die(roll=11, sides=37, color=blue)))
-     */
-    apolloClient.subscription(DieSubscription()).toFlow().collect {
-      if (it.data != null) {
-        println("${it.data}")
-      } else {
-        it.exception!!.printStackTrace()
+  @Test
+  fun dieTest() = runTest {
+    client(log = true).use { apolloClient ->
+      /**
+       * Should display something like this
+       *
+       * Got Data(aNewDieWasCreated=ANewDieWasCreated(die=Die(roll=3, sides=30, color=red)))
+       * Got Data(aNewDieWasCreated=ANewDieWasCreated(die=Die(roll=5, sides=31, color=blue)))
+       * Got Data(aNewDieWasCreated=ANewDieWasCreated(die=Die(roll=11, sides=32, color=red)))
+       * Got Data(aNewDieWasCreated=ANewDieWasCreated(die=Die(roll=12, sides=33, color=blue)))
+       * Got Data(aNewDieWasCreated=ANewDieWasCreated(die=Die(roll=21, sides=34, color=red)))
+       * Got Data(aNewDieWasCreated=ANewDieWasCreated(die=Die(roll=16, sides=35, color=blue)))
+       * Got Data(aNewDieWasCreated=ANewDieWasCreated(die=Die(roll=22, sides=36, color=red)))
+       * Got Data(aNewDieWasCreated=ANewDieWasCreated(die=Die(roll=11, sides=37, color=blue)))
+       */
+      apolloClient.subscription(DieSubscription()).toFlow().collect {
+        if (it.data != null) {
+          println("${it.data}")
+        } else {
+          it.exception!!.printStackTrace()
+        }
+      }
+    }
+  }
+
+  @Test
+  fun reviewTest() = runTest {
+    client(log = true).use { apolloClient ->
+      apolloClient.subscription(ReviewSubscription()).toFlow().collect {
+        if (it.data != null) {
+          println("${it.data} isLast=${it.isLast}")
+        } else {
+          println("exception: ${it.exception}")
+          it.exception?.printStackTrace()
+        }
       }
     }
   }
