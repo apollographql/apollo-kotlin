674a0c3453d2bb174c88bf605f23d98b2138e0bf
BoD <BoD@JRAF.org>
2023-03-29 17:02:08 +0200

    Make more sense in canUseTheSameIntrospectionQueryAsVersion3

--- libraries/apollo-tooling/src/test/kotlin/com/apollographql/apollo3/tooling/IntrospectionQueryTest.kt
+++ libraries/apollo-tooling/src/test/kotlin/com/apollographql/apollo3/tooling/IntrospectionQueryTest.kt
@@ -99,115 +99,16 @@ class IntrospectionQueryTest {
       }
     }""".trimIndent()
 
-  private val normalizedV3IntrospectionQuery = """
-    query IntrospectionQuery {
-      __schema {
-        queryType {
-          name
-        }
-        mutationType {
-          name
-        }
-        subscriptionType {
-          name
-        }
-        types {
-          ...FullType
-        }
-        directives {
-          name
-          description
-          locations
-          args {
-            ...InputValue
-          }
-        }
-      }
-    }
-
-    fragment FullType on __Type {
-      kind
-      name
-      description
-      fields(includeDeprecated: true) {
-        name
-        description
-        args {
-          ...InputValue
-        }
-        type {
-          ...TypeRef
-        }
-        isDeprecated
-        deprecationReason
-      }
-      inputFields {
-        ...InputValue
-      }
-      interfaces {
-        ...TypeRef
-      }
-      enumValues(includeDeprecated: true) {
-        name
-        description
-        isDeprecated
-        deprecationReason
-      }
-      possibleTypes {
-        ...TypeRef
-      }
-    }
-
-    fragment InputValue on __InputValue {
-      name
-      description
-      type {
-        ...TypeRef
-      }
-      defaultValue
-    }
-
-    fragment TypeRef on __Type {
-      kind
-      name
-      ofType {
-        kind
-        name
-        ofType {
-          kind
-          name
-          ofType {
-            kind
-            name
-            ofType {
-              kind
-              name
-              ofType {
-                kind
-                name
-                ofType {
-                  kind
-                  name
-                  ofType {
-                    kind
-                    name
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }""".trimIndent()
-
-  private fun String.normalized(): String {
+  private fun String.asParsedGQLDocument(): String {
     return Buffer().writeUtf8(this).use {
       it.parseAsGQLDocument(null).getOrThrow().toUtf8().trimIndent()
     }
   }
 
+  private fun String.normalized() = this.trimIndent().replace(Regex("\\s+"), " ")
+
   @Test
   fun canUseTheSameIntrospectionQueryAsVersion3() {
-    assertEquals(normalizedV3IntrospectionQuery, v3IntrospectionQuery.normalized())
+    assertEquals(v3IntrospectionQuery.normalized(), v3IntrospectionQuery.asParsedGQLDocument().normalized())
   }
 }
