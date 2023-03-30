f3d5efe7e8b16daea6d70330b8ea856cdf41a378
BoD <BoD@JRAF.org>
2023-03-30 11:15:57 +0200

    Make even more sense in canUseTheSameIntrospectionQueryAsVersion3

--- libraries/apollo-tooling/src/test/kotlin/com/apollographql/apollo3/tooling/IntrospectionQueryTest.kt
+++ libraries/apollo-tooling/src/test/kotlin/com/apollographql/apollo3/tooling/IntrospectionQueryTest.kt
@@ -1,10 +1,15 @@
 package com.apollographql.apollo3.tooling
 
+import com.apollographql.apollo3.ast.GQLField
+import com.apollographql.apollo3.ast.GQLFragmentDefinition
+import com.apollographql.apollo3.ast.GQLOperationDefinition
+import com.apollographql.apollo3.ast.GQLSelectionSet
 import com.apollographql.apollo3.ast.parseAsGQLDocument
 import com.apollographql.apollo3.ast.toUtf8
 import okio.Buffer
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import com.apollographql.apollo3.tooling.graphql.june2018.IntrospectionQuery as GraphQLJune2018IntrospectionQuery
 
 class IntrospectionQueryTest {
 
@@ -99,16 +104,64 @@ class IntrospectionQueryTest {
       }
     }""".trimIndent()
 
-  private fun String.asParsedGQLDocument(): String {
+  private fun String.normalized(): String {
     return Buffer().writeUtf8(this).use {
-      it.parseAsGQLDocument(null).getOrThrow().toUtf8().trimIndent()
+      it.parseAsGQLDocument(null).getOrThrow()
+          .let {
+            it.copy(
+                definitions = it.definitions
+                    // Filter out __typename
+                    .map {
+                      when (it) {
+                        is GQLOperationDefinition -> {
+                          it.copy(selectionSet = it.selectionSet.withoutTypenames())
+                        }
+
+                        is GQLFragmentDefinition -> {
+                          it.copy(selectionSet = it.selectionSet.withoutTypenames())
+                        }
+
+                        else -> it
+                      }
+                    }
+                    // Sort fragments after operations
+                    .sortedWith { a, b ->
+                      if (a is GQLFragmentDefinition) {
+                        if (b is GQLFragmentDefinition) {
+                          a.name.compareTo(b.name)
+                        } else {
+                          1
+                        }
+                      } else {
+                        if (b is GQLFragmentDefinition) {
+                          -1
+                        } else {
+                          0
+                        }
+                      }
+                    }
+            )
+                .toUtf8()
+          }
     }
   }
 
-  private fun String.normalized() = this.trimIndent().replace(Regex("\\s+"), " ")
+  private fun GQLSelectionSet.withoutTypenames(): GQLSelectionSet = copy(
+      selections = selections.mapNotNull {
+        if (it is GQLField) {
+          if (it.name == "__typename") {
+            null
+          } else {
+            it.copy(selectionSet = it.selectionSet?.withoutTypenames())
+          }
+        } else {
+          it
+        }
+      }
+  )
 
   @Test
   fun canUseTheSameIntrospectionQueryAsVersion3() {
-    assertEquals(v3IntrospectionQuery.normalized(), v3IntrospectionQuery.asParsedGQLDocument().normalized())
+    assertEquals(v3IntrospectionQuery.normalized(), GraphQLJune2018IntrospectionQuery.OPERATION_DOCUMENT.normalized())
   }
 }
