f1b3060a2cddbb690ccd0236761efbb0e896e7f3
BoD <BoD@JRAF.org>
2023-04-12 09:52:16 +0200

    Remove KDoc since this is not to be implemented by users

--- libraries/apollo-api/src/commonMain/kotlin/com/apollographql/apollo3/api/ApolloAdapter.kt
+++ libraries/apollo-api/src/commonMain/kotlin/com/apollographql/apollo3/api/ApolloAdapter.kt
@@ -16,64 +16,9 @@ import okio.IOException
  * This class is implemented by the generated code, it shouldn't be used directly.
  */
 interface ApolloAdapter<T> {
-  /**
-   * Deserializes the given Json to the expected Kotlin type.
-   *
-   * implementations may throw [com.apollographql.apollo3.exception.JsonEncodingException] or [com.apollographql.apollo3.exception.JsonDataException]
-   * on unexpected incoming data
-   *
-   * @param [scalarAdapters] configured instance of GraphQL operation response adapters cache. A global empty instance will be used by default.
-   *
-   * Example:
-   * ```
-   * override fun fromJson(reader: JsonReader, scalarAdapters: ScalarAdapters): Hero {
-   *   var name: String? = null
-   *   var homeworld: String? = null
-   *
-   *   while (reader.hasNext()) {
-   *     when(reader.nextName()) {
-   *       "name" -> name = reader.nextString()
-   *       "homeworld" -> homeworld = reader.nextString()
-   *     }
-   *   }
-   *
-   *   return Hero(name!!, homeworld!!)
-   * }
-   * ```
-   *
-   * Alternatively, you can use the built-in [AnyApolloAdapter] to simplify the parsing loop:
-   * ```
-   * override fun fromJson(reader: JsonReader, scalarAdapters: ScalarAdapters): Hero {
-   *   val map = AnyApolloAdapter.fromJson(reader) as Map<String, String>
-   *
-   *   return Hero(map["name"]!!, map["homeworld"]!!)
-   * }
-   * ```
-   */
   @Throws(IOException::class)
   fun fromJson(reader: JsonReader, scalarAdapters: ScalarAdapters): T
 
-  /**
-   * Serializes a Kotlin type into its equivalent Json representation.
-   *
-   * Example:
-   * ```
-   * override fun toJson(writer: JsonWriter, scalarAdapters: ScalarAdapters, value: Hero) {
-   *   writer.name("name")
-   *   writer.value(value.name)
-   *   writer.name("homeworld")
-   *   writer.value(value.homeworld)
-   * }
-   *```
-   *
-   * Alternatively, you can use the built-in [AnyApolloAdapter]:
-   * ```
-   * override fun toJson(writer: JsonWriter, scalarAdapters: ScalarAdapters, value: Hero) {
-   *   val map = mapOf("name" to value.name, "homeworld" to value.homeworld)
-   *   AnyApolloAdapter.toJson(writer, scalarAdapters, map)
-   * }
-   * ```
-   */
   @Throws(IOException::class)
   fun toJson(writer: JsonWriter, scalarAdapters: ScalarAdapters, value: T)
 }
