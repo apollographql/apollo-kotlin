b9b192d3fce3acfd39bc153bf6e4c12f18665fca
Benoit Lubek <BoD@JRAF.org>
2023-05-15 09:33:16 +0200

    [IJ/AS plugin] Improve Kotlin -> GraphQL navigation (#4946)

--- intellij-plugin/src/main/kotlin/com/apollographql/ijplugin/navigation/Navigation.kt
+++ intellij-plugin/src/main/kotlin/com/apollographql/ijplugin/navigation/Navigation.kt
@@ -10,18 +10,22 @@ import com.intellij.lang.jsgraphql.psi.GraphQLFragmentDefinition
 import com.intellij.lang.jsgraphql.psi.GraphQLFragmentSpread
 import com.intellij.lang.jsgraphql.psi.GraphQLInlineFragment
 import com.intellij.lang.jsgraphql.psi.GraphQLOperationDefinition
-import com.intellij.lang.jsgraphql.psi.GraphQLTypeCondition
+import com.intellij.lang.jsgraphql.psi.GraphQLSelectionSet
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiManager
 import com.intellij.psi.search.FileTypeIndex
 import com.intellij.psi.search.GlobalSearchScope
 import org.jetbrains.kotlin.asJava.toLightClass
+import org.jetbrains.kotlin.idea.base.utils.fqname.fqName
+import org.jetbrains.kotlin.idea.base.utils.fqname.getKotlinFqName
 import org.jetbrains.kotlin.idea.references.KtSimpleNameReference
+import org.jetbrains.kotlin.nj2k.postProcessing.type
 import org.jetbrains.kotlin.psi.KtClass
 import org.jetbrains.kotlin.psi.KtConstructor
 import org.jetbrains.kotlin.psi.KtElement
 import org.jetbrains.kotlin.psi.KtNameReferenceExpression
 import org.jetbrains.kotlin.psi.KtParameter
+import org.jetbrains.kotlin.psi.KtProperty
 import org.jetbrains.kotlin.psi.psiUtil.containingClass
 import org.jetbrains.kotlin.utils.addToStdlib.firstIsInstanceOrNull
 
@@ -31,29 +35,34 @@ private val APOLLO_OPERATION_TYPES = setOf(
     "com.apollographql.apollo3.api.Subscription",
 )
 
-private val APOLLO_FRAGMENT_TYPE = "com.apollographql.apollo3.api.Fragment.Data"
+private const val APOLLO_FRAGMENT_TYPE = "com.apollographql.apollo3.api.Fragment.Data"
 
 fun KtNameReferenceExpression.isApolloOperationOrFragmentReference(): Boolean {
   val resolvedElement = references.firstIsInstanceOrNull<KtSimpleNameReference>()?.resolve()
   return (resolvedElement as? KtClass
       ?: (resolvedElement as? KtConstructor<*>)?.containingClass())
-      ?.implementsOperationOrFragment() == true
+      ?.isOperationOrFragment() == true
 }
 
 fun KtNameReferenceExpression.isApolloModelField(): Boolean {
-  return (references.firstIsInstanceOrNull<KtSimpleNameReference>()?.resolve() as? KtParameter)
-      ?.topMostContainingClass()
-      ?.implementsOperationOrFragment() == true
+  val resolved = references.firstIsInstanceOrNull<KtSimpleNameReference>()?.resolve()
+  // Parameter is for data classes, property is for interfaces
+  return (resolved is KtParameter || resolved is KtProperty) &&
+      (resolved as KtElement).topMostContainingClass()
+          ?.isOperationOrFragment() == true
 }
 
-private fun KtClass.implementsOperationOrFragment(): Boolean {
+private fun KtClass.isOperationOrFragment(): Boolean {
   return toLightClass()
       ?.implementsList
       ?.referencedTypes
       ?.any { classType ->
         classType.canonicalText.startsWith(APOLLO_FRAGMENT_TYPE) ||
             APOLLO_OPERATION_TYPES.any { classType.canonicalText.startsWith(it) }
-      } == true
+      } == true ||
+      // Fallback for fragments in responseBased codegen: they are interfaces generated in a .fragment package.
+      // This can lead to false positives, but consequences are not dire.
+      isInterface() && getKotlinFqName()?.parent()?.shortName()?.asString() == "fragment"
 }
 
 private fun KtElement.topMostContainingClass(): KtClass? {
@@ -81,36 +90,140 @@ fun findOperationOrFragmentGraphQLDefinitions(project: Project, name: String): L
   return definitions
 }
 
-fun findGraphQLFields(nameReferenceExpression: KtNameReferenceExpression): List<GraphQLElement> {
-  val fields = mutableListOf<GraphQLElement>()
+fun findGraphQLElements(nameReferenceExpression: KtNameReferenceExpression): List<GraphQLElement> {
+  val elements = mutableListOf<GraphQLElement>()
   val project = nameReferenceExpression.project
-  val containingClassName = (nameReferenceExpression.references.firstIsInstanceOrNull<KtSimpleNameReference>()?.resolve() as? KtElement)
-      ?.topMostContainingClass()
-      ?.name
-      ?: return emptyList()
-  val operationOrFragmentDefinitions = findOperationOrFragmentGraphQLDefinitions(project, containingClassName)
+  val resolved = nameReferenceExpression.references.firstIsInstanceOrNull<KtSimpleNameReference>()?.resolve()
+  // Parameter is for data classes, property is for interfaces
+  val ktElement = if (resolved is KtParameter || resolved is KtProperty) resolved as KtElement else return emptyList()
+  val operationOrFragmentClass = ktElement.topMostContainingClass() ?: return emptyList()
+  val fieldPath = operationOrFragmentClass.elementPath(ktElement)
+  val operationOrFragmentDefinitions = findOperationOrFragmentGraphQLDefinitions(project, operationOrFragmentClass.name!!)
   for (operationOrFragmentDefinition in operationOrFragmentDefinitions) {
-    fields +=
-        // Fields (including aliases)
-        operationOrFragmentDefinition.findChildrenOfType<GraphQLField> { field ->
-          field.name == nameReferenceExpression.text ||
-              field.alias?.identifier?.referenceName == nameReferenceExpression.text
-        } +
-            // Fragment spreads
-            operationOrFragmentDefinition.findChildrenOfType<GraphQLFragmentSpread> { fragmentSpread ->
-              fragmentSpread.name.equals(nameReferenceExpression.text, ignoreCase = true)
-            } +
-            // Inline fragments
-            operationOrFragmentDefinition.findChildrenOfType<GraphQLInlineFragment> { inlineFragment ->
-              inlineFragment.typeCondition?.typeName?.name?.let { "on$it" } == nameReferenceExpression.text
-            }
-                // Only keep the type condition
-                .flatMap { inlineFragment ->
-                  inlineFragment.findChildrenOfType<GraphQLTypeCondition>()
-                }
+    val elementsAtPath = operationOrFragmentDefinition.findElementAtPath(fieldPath)
+    if (elementsAtPath != null) {
+      elements += elementsAtPath
+    } else {
+      // 'Best effort' fallback for responseBased codegen: all fields with the name
+      elements += operationOrFragmentDefinition.findElementsNamed(ktElement.name!!)
+    }
   }
-  return fields
+  return elements
 }
+
+/**
+ * For a given [element], return its path in the given operation class.
+ *
+ * For instance if the operation class is:
+ * ```
+ * class MyQuery : Query {
+ *   data class Data(val user: User)
+ *                              ↖
+ *                  data class User(val address: Address)
+ *                                                ↖
+ *                                    data class Address(val street: String)
+ * }
+ * ```
+ *
+ * the path for `Address.street` will be `["user", "address", "street"]`.
+ */
+private fun KtClass.elementPath(element: KtElement): List<String> {
+  var modelClass = element.containingClass()!!
+  val parameterPath = mutableListOf(element.name!!)
+  while (true) {
+    var found = false
+    findClass@ for (candidateModelClass in findChildrenOfType<KtClass>(withSelf = true)) {
+      // Look for the parameter in the constructor (for data classes) and in the properties (for interfaces)
+      for (property in candidateModelClass.primaryConstructor?.valueParameters.orEmpty() + candidateModelClass.getProperties()) {
+        val parameterType = property.type()
+        if (parameterType?.fqName == modelClass.fqName ||
+            // For lists
+            parameterType?.arguments?.firstOrNull()?.type?.fqName == modelClass.fqName
+        ) {
+          parameterPath.add(0, property.name!!)
+          modelClass = candidateModelClass
+          found = true
+          break@findClass
+        }
+      }
+    }
+    if (!found) break
+  }
+  return parameterPath
+}
+
+/**
+ * Get the element (field, fragment spread, or inline fragment type condition) at the given path.
+ */
+private fun GraphQLDefinition.findElementAtPath(path: List<String>): GraphQLElement? {
+  var element: GraphQLElement? = null
+  var selectionSet = findChildrenOfType<GraphQLSelectionSet>(recursive = false).firstOrNull() ?: return null
+  for (pathElement in path) {
+    var found = false
+    for (selection in selectionSet.selectionList) {
+      if (selection.field != null) {
+        // Field
+        val field = selection.field!!
+        if (field.name == pathElement ||
+            field.alias?.identifier?.referenceName == pathElement) {
+          element = field
+          field.selectionSet?.let { selectionSet = it }
+          found = true
+          break
+        }
+      } else if (selection.fragmentSelection != null) {
+        // Fragment
+        if (selection.fragmentSelection!!.inlineFragment != null) {
+          // Inline
+          val inlineFragment = selection.fragmentSelection!!.inlineFragment!!
+          if (inlineFragment.typeCondition?.typeName?.name?.let { "on$it" } == pathElement) {
+            // Point to the type condition
+            element = inlineFragment.typeCondition
+            inlineFragment.selectionSet?.let { selectionSet = it }
+            found = true
+            break
+          }
+        } else if (selection.fragmentSelection!!.fragmentSpread != null) {
+          // Spread
+          val fragmentSpread = selection.fragmentSelection!!.fragmentSpread!!
+          if (fragmentSpread.name.equals(pathElement, ignoreCase = true)) {
+            element = fragmentSpread
+            found = true
+            break
+          }
+        }
+      }
+    }
+    if (!found) {
+      element = null
+      break
+    }
+  }
+  return element
+}
+
+/**
+ * Get all elements (field, fragment spread, or inline fragment type condition) with the given name.
+ */
+private fun GraphQLDefinition.findElementsNamed(name: String): List<GraphQLElement> {
+  // Fields
+  return findChildrenOfType<GraphQLField> { field ->
+    field.name == name || field.alias?.identifier?.referenceName == name
+  } +
+      // Fragment spreads
+      findChildrenOfType<GraphQLFragmentSpread> { fragmentSpread ->
+        fragmentSpread.name.equals(name, ignoreCase = true)
+      } +
+      // Inline fragments
+      findChildrenOfType<GraphQLInlineFragment> { inlineFragment ->
+        inlineFragment.typeCondition?.typeName?.name?.let { "on$it" } == name
+      }
+          .map { inlineFragment ->
+            // Point to the type condition
+            inlineFragment.typeCondition!!
+          }
+}
+
 
 // Remove "Query", "Mutation", or "Subscription" from the end of the operation name
 private fun String.minusOperationTypeSuffix(): String {
