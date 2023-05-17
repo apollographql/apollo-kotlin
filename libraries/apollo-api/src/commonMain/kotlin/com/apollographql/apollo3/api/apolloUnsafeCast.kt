fd47a8b88dc3b2dc760f97193bda9870e23ed04d
Martin Bonnin <martin@mbonnin.net>
2023-05-17 10:42:12 +0200

    mark apolloUnsafeCast as @ApolloExperimental and tweak kdoc
    rename tests

--- libraries/apollo-api/src/commonMain/kotlin/com/apollographql/apollo3/api/apolloUnsafeCast.kt
+++ libraries/apollo-api/src/commonMain/kotlin/com/apollographql/apollo3/api/apolloUnsafeCast.kt
@@ -1,14 +1,17 @@
 package com.apollographql.apollo3.api
 
-expect inline fun <reified T>  Any.apolloUnsafeCast(): T
+import com.apollographql.apollo3.annotations.ApolloExperimental
 
 /**
- * Uses `unsafeCast` on JS or a regular `as?` cast on other platforms. Note that
- * this means that JS platforms will crash at runtime if the cast fails, whereas
- * other (statically) typed targets will return a null value.
+ * Uses [unsafeCast] on JS or a regular `as` unsafe cast on other platforms.
  *
- * @return
+ * On JS targets [apolloUnsafeCast] bypasses the Kotlin type system. If the target class or interface is missing some fields, the cast will succeed but an exception will be thrown later when accessing the missing field.
+ *
+ * On non-JS targets, [apolloUnsafeCast] uses the Kotlin type system and will throw an exception if the receiver is not of the expected type.
  */
+@ApolloExperimental
+expect inline fun <reified T>  Any.apolloUnsafeCast(): T
+
 inline fun <reified T> Any.defaultApolloUnsafeCast(): T {
   return this as T
 }
