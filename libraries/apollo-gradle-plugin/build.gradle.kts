75425d9304fd18523114737a639272431ab68939
Martin Bonnin <martin@mbonnin.net>
2023-05-22 18:26:07 +0200

    Revert "Share Gradle Tests Home with the main build (#4900)" (#4976)
    This reverts commit 8b0b1363d29683bafb196e28be87833970d18ceb.

--- libraries/apollo-gradle-plugin/build.gradle.kts
+++ libraries/apollo-gradle-plugin/build.gradle.kts
@@ -113,32 +113,6 @@ configure<PublishingExtension> {
   }
 }
 
-tasks.register("wrapInitScripts") {
-  doLast {
-    val initDir = File(System.getenv("HOME")).resolve(".gradle/init.d/")
-
-    val buildCapture = initDir.resolve("build-result-capture.init.gradle")
-    if (!buildCapture.exists()) {
-      // Running on a local machine or wrapping is already done
-      println("~/.gradle/init.d/build-result-capture.init.gradle not found")
-      return@doLast
-    }
-    println("wrapping init scrips")
-
-    // Rename the script
-    buildCapture.renameTo(initDir.resolve("build-result-capture"))
-
-    initDir.resolve("wrapper.init.gradle.kts").writeText("""
-      if (System.getProperty("skipBuildCollection") == null) {
-          println("Init scripts: capturing builds")
-          apply(from = "build-result-capture")
-      } else {
-          println("Init scripts: skipping build capture")
-      }
-    """.trimIndent())
-  }
-}
-
 tasks.register("cleanStaleTestProjects") {
   doFirst {
     /**
@@ -162,11 +136,6 @@ tasks.withType<Test> {
   dependsOn(":apollo-gradle-plugin-external:publishAllPublicationsToPluginTestRepository")
   dependsOn(":apollo-tooling:publishAllPublicationsToPluginTestRepository")
   dependsOn("publishAllPublicationsToPluginTestRepository")
-  if (System.getenv("CI") != null) {
-    dependsOn("wrapInitScripts")
-  }
-  // Because we run the GradleRunner in process, this gets forwarded to invocation
-  systemProperty("skipBuildCollection", "true")
 
   dependsOn("cleanStaleTestProjects")
 
