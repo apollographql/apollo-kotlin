76214b9267cf6f46fca3906fa37b6736e2f9d666
BoD <BoD@JRAF.org>
2023-06-05 18:20:23 +0200

    Use 1.9.0-RC-257

--- libraries/apollo-compose-support/build.gradle.kts
+++ libraries/apollo-compose-support/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
   id("com.android.library")
   id("org.jetbrains.kotlin.android")
@@ -28,12 +30,12 @@ android {
 }
 
 // Uncomment when using a non supported version of Kotlin
-//// See https://developer.android.com/jetpack/androidx/releases/compose-kotlin
-//tasks.withType(KotlinCompile::class.java).configureEach {
-//  kotlinOptions {
-//    freeCompilerArgs += listOf(
-//        "-P",
-//        "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=1.8.20"
-//    )
-//  }
-//}
+// See https://developer.android.com/jetpack/androidx/releases/compose-kotlin
+tasks.withType(KotlinCompile::class.java).configureEach {
+  kotlinOptions {
+    freeCompilerArgs += listOf(
+        "-P",
+        "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=1.9.0-RC-257"
+    )
+  }
+}
