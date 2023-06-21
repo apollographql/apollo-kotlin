1c406ddf9315ef426a0a2f1bc69e8b6fd607b0b4
BoD <BoD@JRAF.org>
2023-06-21 09:48:02 +0200

    Use Kotlin 1.9-RC

--- intellij-plugin/build.gradle.kts
+++ intellij-plugin/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 // XXX: this should use the settings repositories instead
 repositories {
   // Uncomment this one to use the Kotlin "dev" repository
-  maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/") }
+  // maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/") }
   mavenCentral()
 }
 
