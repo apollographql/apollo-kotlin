7be377a10dadf22cf4acde0540cfcba2e4a0cebc
Benoit Lubek <BoD@JRAF.org>
2023-06-29 11:00:39 +0200

    [IJ plugin] "Expensive field" inspection (#5044)

---
+++ intellij-plugin/src/main/kotlin/com/apollographql/ijplugin/studio/fieldinsights/FieldInsightsService.kt
@@ -0,0 +1,43 @@
+package com.apollographql.ijplugin.studio.fieldinsights
+
+import com.apollographql.apollo3.annotations.ApolloExperimental
+import com.apollographql.apollo3.tooling.FieldInsights
+import com.apollographql.ijplugin.util.logd
+import com.intellij.codeInsight.hints.InlayHintsPassFactory
+import com.intellij.codeInsight.hints.settings.InlayHintsConfigurable
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.project.Project
+
+@OptIn(ApolloExperimental::class)
+class FieldInsightsService(private val project: Project) : Disposable {
+  /**
+   * Keys are the project name in the form "projectName/apolloServiceName", same format used
+   * in [com.apollographql.ijplugin.graphql.GraphQLProjectFiles.name]
+   */
+  private val fieldLatenciesByProject = mutableMapOf<String, FieldInsights.FieldLatencies>()
+
+  init {
+    // TODO Random values for testing
+    fieldLatenciesByProject["intellij-plugin-test-project/main"] = FieldInsights.FieldLatencies(
+        listOf(
+            FieldInsights.FieldLatencies.FieldLatency("Query", "animals", 42.0),
+            FieldInsights.FieldLatencies.FieldLatency("Animal", "id", 0.5),
+            FieldInsights.FieldLatencies.FieldLatency("Computer", "screen", 123.0),
+        ),
+    )
+  }
+
+  fun getLatency(graphQLProjectFilesName: String, typeName: String, fieldName: String): Double? {
+    return fieldLatenciesByProject[graphQLProjectFilesName]?.getLatency(parentType = typeName, fieldName = fieldName)
+  }
+
+  fun refreshHints() {
+    @Suppress("UnstableApiUsage")
+    InlayHintsPassFactory.forceHintsUpdateOnNextPass()
+    InlayHintsConfigurable.updateInlayHintsUI()
+  }
+
+  override fun dispose() {
+    logd("project=${project.name}")
+  }
+}
