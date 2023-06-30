09645fb7061f6b1f5875f7ed69f06fa373c19193
BoD <BoD@JRAF.org>
2023-06-30 16:25:02 +0200

    Request v4 to configure GraphOS API keys

--- intellij-plugin/src/main/kotlin/com/apollographql/ijplugin/settings/SettingsComponent.kt
+++ intellij-plugin/src/main/kotlin/com/apollographql/ijplugin/settings/SettingsComponent.kt
@@ -1,9 +1,12 @@
 package com.apollographql.ijplugin.settings
 
 import com.apollographql.ijplugin.ApolloBundle
+import com.apollographql.ijplugin.project.apolloProjectService
 import com.apollographql.ijplugin.settings.studio.ApiKeyDialog
+import com.intellij.openapi.observable.properties.PropertyGraph
 import com.intellij.openapi.project.Project
 import com.intellij.ui.AddEditRemovePanel
+import com.intellij.ui.dsl.builder.bindSelected
 import com.intellij.ui.dsl.builder.panel
 import com.intellij.ui.dsl.gridLayout.HorizontalAlign
 import javax.swing.JCheckBox
@@ -12,94 +15,92 @@ import javax.swing.JPanel
 
 
 class SettingsComponent(private val project: Project) {
+  private val propertyGraph = PropertyGraph()
+  private val automaticCodegenTriggeringProperty = propertyGraph.property(false)
+  private val contributeConfigurationToGraphqlPluginProperty = propertyGraph.property(false)
+
+  var automaticCodegenTriggering: Boolean by automaticCodegenTriggeringProperty
+  var contributeConfigurationToGraphqlPlugin: Boolean by contributeConfigurationToGraphqlPluginProperty
+  var apolloKotlinServiceConfigurations: List<ApolloKotlinServiceConfiguration>
+    get() = addEditRemovePanel?.data?.toList()?: emptyList()
+    set(value) {
+      addEditRemovePanel?.data = value.toMutableList()
+    }
+
   private lateinit var chkAutomaticCodegenTriggering: JCheckBox
-  private lateinit var chkContributeConfigurationToGraphqlPlugin: JCheckBox
-  private lateinit var addEditRemovePanel: AddEditRemovePanel<ApolloKotlinServiceConfiguration>
+  private var addEditRemovePanel: AddEditRemovePanel<ApolloKotlinServiceConfiguration>? = null
 
   val panel: JPanel = panel {
     group(ApolloBundle.message("settings.codegen.title")) {
       row {
         chkAutomaticCodegenTriggering = checkBox(ApolloBundle.message("settings.codegen.automaticCodegenTriggering.text"))
             .comment(ApolloBundle.message("settings.codegen.automaticCodegenTriggering.comment"))
+            .bindSelected(automaticCodegenTriggeringProperty)
             .component
       }
     }
     group(ApolloBundle.message("settings.graphqlPlugin.title")) {
       row {
-        chkContributeConfigurationToGraphqlPlugin = checkBox(ApolloBundle.message("settings.graphqlPlugin.contributeConfigurationToGraphqlPlugin.text"))
+        checkBox(ApolloBundle.message("settings.graphqlPlugin.contributeConfigurationToGraphqlPlugin.text"))
             .comment(ApolloBundle.message("settings.graphqlPlugin.contributeConfigurationToGraphqlPlugin.comment"))
-            .component
+            .bindSelected(contributeConfigurationToGraphqlPluginProperty)
       }
     }
     group(ApolloBundle.message("settings.studio.title")) {
-      row {
-        addEditRemovePanel = object : AddEditRemovePanel<ApolloKotlinServiceConfiguration>(
-            ApiKeysModel(),
-            emptyList(),
-            ApolloBundle.message("settings.studio.apiKeys.text")
-        ) {
-          override fun addItem(): ApolloKotlinServiceConfiguration? {
-            val apiKeyDialog = ApiKeyDialog(project)
-            if (!apiKeyDialog.showAndGet()) return null
-            return ApolloKotlinServiceConfiguration(
-                id = apiKeyDialog.apolloKotlinServiceId,
-                graphOsGraphName = apiKeyDialog.graphOsServiceName,
-            ).apply {
-              graphOsApiKey = apiKeyDialog.graphOsApiKey
+      if (!project.apolloProjectService.apolloVersion.isAtLeastV4) {
+        row { label(ApolloBundle.message("settings.studio.apiKeys.needV4.message")) }
+      } else {
+        row {
+          addEditRemovePanel = object : AddEditRemovePanel<ApolloKotlinServiceConfiguration>(
+              ApiKeysModel(),
+              emptyList(),
+              ApolloBundle.message("settings.studio.apiKeys.text")
+          ) {
+            override fun addItem(): ApolloKotlinServiceConfiguration? {
+              val apiKeyDialog = ApiKeyDialog(project)
+              if (!apiKeyDialog.showAndGet()) return null
+              return ApolloKotlinServiceConfiguration(
+                  id = apiKeyDialog.apolloKotlinServiceId,
+                  graphOsGraphName = apiKeyDialog.graphOsServiceName,
+              ).apply {
+                graphOsApiKey = apiKeyDialog.graphOsApiKey
+              }
             }
+
+            override fun editItem(o: ApolloKotlinServiceConfiguration): ApolloKotlinServiceConfiguration? {
+              val apiKeyDialog = ApiKeyDialog(
+                  project,
+                  apolloKotlinServiceId = o.apolloKotlinServiceId,
+                  graphOsApiKey = o.graphOsApiKey ?: "",
+                  graphOsServiceName = o.graphOsGraphName
+              )
+              if (!apiKeyDialog.showAndGet()) return null
+              return ApolloKotlinServiceConfiguration(
+                  id = apiKeyDialog.apolloKotlinServiceId,
+                  graphOsGraphName = apiKeyDialog.graphOsServiceName,
+              ).apply {
+                graphOsApiKey = apiKeyDialog.graphOsApiKey
+              }
+            }
+
+            override fun removeItem(o: ApolloKotlinServiceConfiguration?): Boolean {
+              return true
+            }
+          }.apply {
+            table.setShowGrid(false)
+            table.setShowColumns(true)
+            emptyText.text = ApolloBundle.message("settings.studio.apiKeys.empty")
           }
 
-          override fun editItem(o: ApolloKotlinServiceConfiguration): ApolloKotlinServiceConfiguration? {
-            val apiKeyDialog = ApiKeyDialog(
-                project,
-                apolloKotlinServiceId = o.apolloKotlinServiceId,
-                graphOsApiKey = o.graphOsApiKey ?: "",
-                graphOsServiceName = o.graphOsGraphName
-            )
-            if (!apiKeyDialog.showAndGet()) return null
-            return ApolloKotlinServiceConfiguration(
-                id = apiKeyDialog.apolloKotlinServiceId,
-                graphOsGraphName = apiKeyDialog.graphOsServiceName,
-            ).apply {
-              graphOsApiKey = apiKeyDialog.graphOsApiKey
-            }
-          }
-
-          override fun removeItem(o: ApolloKotlinServiceConfiguration?): Boolean {
-            return true
-          }
-        }.apply {
-          table.setShowGrid(false)
-          table.setShowColumns(true)
-          emptyText.text = ApolloBundle.message("settings.studio.apiKeys.empty")
+          cell(addEditRemovePanel!!)
+              .horizontalAlign(HorizontalAlign.FILL)
+              .comment(ApolloBundle.message("settings.studio.apiKeys.comment"))
         }
-
-        cell(addEditRemovePanel)
-            .horizontalAlign(HorizontalAlign.FILL)
-            .comment(ApolloBundle.message("settings.studio.apiKeys.comment"))
       }
     }
   }
 
   val preferredFocusedComponent: JComponent = chkAutomaticCodegenTriggering
-
-  var automaticCodegenTriggering: Boolean
-    get() = chkAutomaticCodegenTriggering.isSelected
-    set(value) {
-      chkAutomaticCodegenTriggering.isSelected = value
-    }
-
-  var contributeConfigurationToGraphqlPlugin: Boolean
-    get() = chkContributeConfigurationToGraphqlPlugin.isSelected
-    set(value) {
-      chkContributeConfigurationToGraphqlPlugin.isSelected = value
-    }
-
-  var apolloKotlinServiceConfigurations: List<ApolloKotlinServiceConfiguration>
-    get() = addEditRemovePanel.data.toList()
-    set(value) {
-      addEditRemovePanel.data = value.toMutableList()
-    }
 }
 
 class ApiKeysModel : AddEditRemovePanel.TableModel<ApolloKotlinServiceConfiguration>() {
