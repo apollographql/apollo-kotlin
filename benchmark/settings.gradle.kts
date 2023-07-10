5cd99747acb41a88f000ee395f7e07ac3af35c8a
Martin Bonnin <martin@mbonnin.net>
2023-07-10 15:54:30 +0200

    also update benchmark

--- benchmark/settings.gradle.kts
+++ benchmark/settings.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+  id("org.gradle.toolchains.foojay-resolver-convention") version "0.6.0"
 }
 
 include(":app", ":microbenchmark")
