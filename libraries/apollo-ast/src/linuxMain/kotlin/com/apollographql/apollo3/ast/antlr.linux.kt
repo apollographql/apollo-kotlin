42aadb6e0bdebfee7638ae2ee97b715ba37fe126
Martin Bonnin <martin@mbonnin.net>
2023-07-10 10:57:54 +0200

    Update libraries/apollo-ast/src/linuxMain/kotlin/com/apollographql/apollo3/ast/antlr.linux.kt
    Co-authored-by: Benoit Lubek <BoD@JRAF.org>

--- libraries/apollo-ast/src/linuxMain/kotlin/com/apollographql/apollo3/ast/antlr.linux.kt
+++ libraries/apollo-ast/src/linuxMain/kotlin/com/apollographql/apollo3/ast/antlr.linux.kt
@@ -6,26 +6,26 @@ internal actual fun parseDocumentWithAntlr(
     source: BufferedSource,
     filePath: String?,
 ): GQLResult<GQLDocument> {
-  TODO("Not yet implemented")
+  throw UnsupportedOperationException("Antlr parser implementation is for the JVM")
 }
 
 internal actual fun parseValueWithAntlr(
     source: BufferedSource,
     filePath: String?,
 ): GQLResult<GQLValue> {
-  TODO("Not yet implemented")
+  throw UnsupportedOperationException("Antlr parser implementation is for the JVM")
 }
 
 internal actual fun parseTypeWithAntlr(
     source: BufferedSource,
     filePath: String?,
 ): GQLResult<GQLType> {
-  TODO("Not yet implemented")
+  throw UnsupportedOperationException("Antlr parser implementation is for the JVM")
 }
 
 internal actual fun parseSelectionsWithAntlr(
     source: BufferedSource,
     filePath: String?,
 ): GQLResult<List<GQLSelection>> {
-  TODO("Not yet implemented")
+  throw UnsupportedOperationException("Antlr parser implementation is for the JVM")
 }
