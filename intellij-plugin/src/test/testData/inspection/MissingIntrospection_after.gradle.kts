f85ec4543affac4e4abb9b3841198040aeb72c2a
BoD <BoD@JRAF.org>
2023-08-03 10:49:19 +0200

    Add a test

---
+++ intellij-plugin/src/test/testData/inspection/MissingIntrospection_after.gradle.kts
@@ -0,0 +1,51 @@
+dependencies {
+  implementation("org.example:somelibrary:1.0.0")
+}
+
+apollo {
+  service("a") { // Should highlight
+    packageName.set("com.example")
+    // Some comment
+    codegenModels.set("operationBased")
+    srcDir("src/main/graphql")
+    introspection {
+      endpointUrl.set("https://example.com/graphql")
+      schemaFile.set(file("src/main/graphql/schema.graphqls"))
+    }
+  }
+
+  service("b") { // Should highlight
+    packageName.set("com.example")
+    // Some comment
+    codegenModels.set("operationBased")
+    srcDir("src/main/graphql")
+    registry {
+      key.set(System.getenv("APOLLO_KEY"))
+      graph.set(System.getenv("APOLLO_GRAPH"))
+      schemaFile.set(file("src/main/graphql/schema.graphqls"))
+    }
+  }
+
+  service("c") {
+    packageName.set("com.example")
+    // Some comment
+    codegenModels.set("operationBased")
+    srcDir("src/main/graphql")
+    introspection {
+      endpointUrl.set("https://example.com/graphql")
+      schemaFile.set(file("src/main/graphql/schema.graphqls"))
+    }
+  }
+
+  service("d") {
+    packageName.set("com.example")
+    // Some comment
+    codegenModels.set("operationBased")
+    srcDir("src/main/graphql")
+    registry {
+      key.set(System.getenv("APOLLO_KEY"))
+      graph.set(System.getenv("APOLLO_GRAPH"))
+      schemaFile.set(file("src/main/graphql/schema.graphqls"))
+    }
+  }
+}
