d5129cacf81a884064022b3c801fb88cb7f8a5cd
BoD <BoD@JRAF.org>
2023-08-04 11:47:36 +0200

    Workaround for KT-60741

--- libraries/apollo-normalized-cache/src/appleMain/kotlin/com/apollographql/apollo3/cache/normalized/internal/Lock.kt
+++ libraries/apollo-normalized-cache/src/appleMain/kotlin/com/apollographql/apollo3/cache/normalized/internal/Lock.kt
@@ -5,6 +5,7 @@ import kotlinx.atomicfu.locks.reentrantLock
 import kotlinx.atomicfu.locks.withLock
 
 internal actual class Lock {
+  // https://youtrack.jetbrains.com/issue/KT-60741
   private val lock: ReentrantLock = reentrantLock() as ReentrantLock
 
   actual fun <T> read(block: () -> T): T {
