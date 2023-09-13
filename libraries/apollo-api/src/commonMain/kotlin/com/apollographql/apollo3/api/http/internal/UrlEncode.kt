d4eff8736325e4801888c53906801f104a2d0fb4
Martin Bonnin <martin@mbonnin.net>
2023-09-13 10:27:21 +0200

    Tweak the urlencode algorithm (#5234)
    * add tests for 5233
    
    * use correct quote
    
    * Tweak the url encoding algorithm according to RFC 3986
    
    * typo

--- libraries/apollo-api/src/commonMain/kotlin/com/apollographql/apollo3/api/http/internal/UrlEncode.kt
+++ libraries/apollo-api/src/commonMain/kotlin/com/apollographql/apollo3/api/http/internal/UrlEncode.kt
@@ -1,24 +1,63 @@
 package com.apollographql.apollo3.api.http.internal
 
-
-private val RESERVED_CHARS = "!#\$&'\"()*+,/:;=?@[]{}% "
+/**
+ * Return true if the unicode code point is an unreserved char as in
+ * https://datatracker.ietf.org/doc/html/rfc3986#section-2.3
+ */
+private fun Int.isUnreserved(): Boolean {
+  return when (this) {
+    in 'a'.code..'z'.code -> true
+    in 'A'.code..'Z'.code -> true
+    in '0'.code..'9'.code -> true
+    '-'.code -> true
+    '.'.code -> true
+    '_'.code -> true
+    '~'.code -> true
+    else -> false
+  }
+}
 
 /**
- * A very simple urlEncode.
- * Note that spaces are encoded as `%20` and not `+`. See:
- * - https://stackoverflow.com/a/47188851/15695
- * - https://www.rfc-editor.org/rfc/rfc3986#section-2.1
- * - https://github.com/apollographql/apollo-kotlin/pull/4567
+ * From https://datatracker.ietf.org/doc/html/rfc3986#autoid-12:
+ *
+ * ```
+ *    When a new URI scheme defines a component that represents textual
+ *    data consisting of characters from the Universal Character Set [UCS],
+ *    the data should first be encoded as octets according to the UTF-8
+ *    character encoding [STD63]; then only those octets that do not
+ *    correspond to characters in the unreserved set should be percent-
+ *    encoded.  For example, the character A would be represented as "A",
+ *    the character LATIN CAPITAL LETTER A WITH GRAVE would be represented
+ *    as "%C3%80", and the character KATAKANA LETTER A would be represented
+ *    as "%E3%82%A2".
+ * ```
+ *
+ * If I'm reading that right, data must be first encoded to UTF8 then percent-encoded
+ * as needed.
+ *
+ * This is especially important for older iOS versions where NSURL(String) returns
+ * null (and crashes with a NullPointerException) if invalid characters are
+ * encountered
+ *
+ * Somewhere in 2023-2024, NSURL will begin percent-encoding those characters:
+ * https://developer.apple.com/documentation/foundation/nsurl
  */
 internal fun String.urlEncode(): String = buildString {
-  this@urlEncode.forEach { char ->
-    when (char) {
-      in RESERVED_CHARS -> append(char.percentEncode())
-      else -> append(char)
+  this@urlEncode.encodeToByteArray().forEach { byte ->
+    val b = byte.toInt().and(0xff)
+
+    if (b.isUnreserved()) {
+      append(b.toChar())
+    } else {
+      append(b.percentEncode())
     }
   }
 }
 
-private fun Char.percentEncode(): String {
-  return "%${code.toString(16)}".uppercase()
+private fun Int.percentEncode(): String {
+  var hex = this.toString(16).uppercase()
+  if (hex.length == 1) {
+    hex = "0$hex"
+  }
+  return "%$hex"
 }
