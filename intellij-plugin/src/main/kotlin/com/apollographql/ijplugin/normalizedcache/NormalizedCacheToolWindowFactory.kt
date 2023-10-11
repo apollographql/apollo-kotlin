aee8416dc3c4383c62b5e5f06bdbff07939155e5
BoD <BoD@JRAF.org>
2023-10-11 18:49:39 +0200

    Tab handling

--- intellij-plugin/src/main/kotlin/com/apollographql/ijplugin/normalizedcache/NormalizedCacheToolWindowFactory.kt
+++ intellij-plugin/src/main/kotlin/com/apollographql/ijplugin/normalizedcache/NormalizedCacheToolWindowFactory.kt
@@ -1,24 +1,40 @@
 package com.apollographql.ijplugin.normalizedcache
 
+import com.apollographql.ijplugin.ApolloBundle
+import com.intellij.icons.AllIcons
+import com.intellij.ide.CommonActionsManager
+import com.intellij.ide.DefaultTreeExpander
+import com.intellij.ide.TreeExpander
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.SimpleToolWindowPanel
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.openapi.wm.ex.ToolWindowEx
+import com.intellij.openapi.wm.ex.ToolWindowManagerListener
 import com.intellij.ui.OnePixelSplitter
 import com.intellij.ui.ScrollPaneFactory
 import com.intellij.ui.ScrollingUtil
 import com.intellij.ui.SimpleListCellRenderer
+import com.intellij.ui.SimpleTextAttributes
 import com.intellij.ui.components.JBList
+import com.intellij.ui.components.JBPanelWithEmptyText
 import com.intellij.ui.components.JBTreeTable
 import com.intellij.ui.content.ContentFactory
+import com.intellij.ui.content.ContentManager
 import com.intellij.ui.treeStructure.treetable.ListTreeTableModel
 import com.intellij.ui.treeStructure.treetable.TreeTableModel
 import com.intellij.util.ui.ColumnInfo
+import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.ListUiUtil
 import java.awt.Cursor
 import java.awt.Point
+import java.awt.event.KeyEvent
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import javax.swing.DefaultListModel
@@ -29,49 +45,106 @@ import javax.swing.tree.DefaultMutableTreeNode
 
 class NormalizedCacheToolWindowFactory : ToolWindowFactory, DumbAware, Disposable {
   override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-    return toolWindow.contentManager.addContent(
+    createNewTab(toolWindow.contentManager)
+
+    val newTabAction = object : DumbAwareAction(ApolloBundle.messagePointer("normalizedCacheViewer.newTab"), AllIcons.General.Add) {
+      override fun actionPerformed(e: AnActionEvent) {
+        createNewTab(toolWindow.contentManager)
+      }
+    }.apply {
+      registerCustomShortcutSet(KeyEvent.VK_T, KeyEvent.CTRL_DOWN_MASK, toolWindow.component)
+    }
+    (toolWindow as ToolWindowEx).setTabActions(newTabAction)
+
+    // Open a new tab if all tabs were closed
+    project.messageBus.connect().subscribe(
+        ToolWindowManagerListener.TOPIC,
+        object : ToolWindowManagerListener {
+          override fun toolWindowShown(shownToolWindow: ToolWindow) {
+            if (toolWindow === shownToolWindow && toolWindow.isVisible && toolWindow.contentManager.isEmpty) {
+              createNewTab(toolWindow.contentManager)
+            }
+          }
+        }
+    )
+  }
+
+  private fun createNewTab(contentManager: ContentManager) {
+    val setTabName: (String) -> Unit = {
+      contentManager.selectedContent?.displayName = it
+    }
+    contentManager.addContent(
         ContentFactory.getInstance().createContent(
-            NormalizedCacheValuesPanel(NormalizedCache.getFakeNormalizedCache()),
-            "",
+            NormalizedCacheWindowPanel(setTabName),
+            "Empty", // TODO
             false
         )
     )
+    contentManager.setSelectedContent(contentManager.contents.last())
   }
 
   override fun dispose() {}
 }
 
-class NormalizedCacheValuesPanel(
-    private val normalizedCache: NormalizedCache,
-) : SimpleToolWindowPanel(false, true) {
+class NormalizedCacheWindowPanel(private val setTabName: (String) -> Unit) : SimpleToolWindowPanel(false, true) {
+  private lateinit var normalizedCache: NormalizedCache
+
   private val fieldTreeTableModel = ListTreeTableModel(
       DefaultMutableTreeNode(),
       arrayOf(
-          object : ColumnInfo<Unit, Unit>("Key") { //TODO
-            override fun valueOf(item: Unit) = Unit
+          object : ColumnInfo<Unit, Unit>(ApolloBundle.message("normalizedCacheViewer.fields.column.key")) {
             override fun getColumnClass() = TreeTableModel::class.java
+            override fun valueOf(item: Unit) = Unit
           },
-          object : ColumnInfo<NormalizedCacheFieldTreeNode, NormalizedCache.Field>("Value") { // TODO
-            override fun valueOf(item: NormalizedCacheFieldTreeNode) = item.field
+          object : ColumnInfo<NormalizedCacheFieldTreeNode, NormalizedCache.Field>(ApolloBundle.message("normalizedCacheViewer.fields.column.value")) {
             override fun getColumnClass() = NormalizedCache.Field::class.java
+            override fun valueOf(item: NormalizedCacheFieldTreeNode) = item.field
           },
       ),
   )
 
+  private lateinit var fieldTreeExpander: TreeExpander
   private lateinit var keyList: JBList<NormalizedCache.Record>
 
   init {
-    // TODO
-    // setEmptyState("This is empty!")
-
-    setContent(createSplitter())
+    setContent(createEmptyContent())
   }
 
-  // TODO use DSL?
-  private fun createSplitter(): JComponent {
+  private fun createEmptyContent(): JComponent {
+    return JBPanelWithEmptyText().apply {
+      emptyText.text = ApolloBundle.message("normalizedCacheViewer.empty.message")
+      emptyText.appendLine(ApolloBundle.message("normalizedCacheViewer.empty.openFile"), SimpleTextAttributes.LINK_PLAIN_ATTRIBUTES) {
+        openFile()
+      }
+    }
+  }
+
+  private fun openFile() {
+    // TODO
+    normalizedCache = NormalizedCache.getFakeNormalizedCache()
+    setContent(createNormalizedCacheContent())
+    toolbar = createToolbar()
+    setTabName("xxx-aaa-bbb.db")
+  }
+
+  private fun createToolbar(): JComponent {
+    val group = DefaultActionGroup()
+    group.add(CommonActionsManager.getInstance().createExpandAllAction(fieldTreeExpander, this).apply {
+      getTemplatePresentation().setDescription(ApolloBundle.message("normalizedCacheViewer.toolbar.expandAll"))
+    })
+    group.add(CommonActionsManager.getInstance().createCollapseAllAction(fieldTreeExpander, this).apply {
+      getTemplatePresentation().setDescription(ApolloBundle.message("normalizedCacheViewer.toolbar.collapseAll"))
+    })
+
+    val actionToolBar = ActionManager.getInstance().createActionToolbar(NormalizedCacheWindowPanel::class.java.name, group, false);
+    actionToolBar.targetComponent = this
+    return JBUI.Panels.simplePanel(actionToolBar.component)
+  }
+
+  private fun createNormalizedCacheContent(): JComponent {
     val splitter = OnePixelSplitter(false, .25F).apply {
       firstComponent = createKeyList()
-      secondComponent = createFieldTree()
+      secondComponent = createFieldTreeTable()
       setResizeEnabled(true)
       splitterProportionKey = "${NormalizedCacheToolWindowFactory::class.java}.splitterProportionKey"
     }
@@ -100,8 +173,8 @@ class NormalizedCacheValuesPanel(
   }
 
   @Suppress("UnstableApiUsage")
-  private fun createFieldTree(): JComponent {
-    val tree = JBTreeTable(fieldTreeTableModel).apply {
+  private fun createFieldTreeTable(): JComponent {
+    val treeTable = JBTreeTable(fieldTreeTableModel).apply {
       columnProportion = .75F
       setDefaultRenderer(NormalizedCache.Field::class.java, object : DefaultTableCellRenderer() {
         override fun setValue(value: Any?) {
@@ -119,8 +192,9 @@ class NormalizedCacheValuesPanel(
         }
       })
 
+      // Handle reference clicks and cursor changes
       val mouseAdapter = object : MouseAdapter() {
-        private fun getFieldAtPoint(e: MouseEvent): NormalizedCache.Field? {
+        private fun getFieldUnderPointer(e: MouseEvent): NormalizedCache.Field? {
           val point = Point(e.x, e.y)
           val row = table.rowAtPoint(point)
           // Add 1 to account for the tree column
@@ -129,9 +203,9 @@ class NormalizedCacheValuesPanel(
         }
 
         override fun mouseClicked(e: MouseEvent) {
-          val field = getFieldAtPoint(e) ?: return
+          table.cursor = Cursor(Cursor.DEFAULT_CURSOR)
+          val field = getFieldUnderPointer(e) ?: return
           if (field.value is NormalizedCache.FieldValue.Reference) {
-            table.cursor = Cursor(Cursor.DEFAULT_CURSOR)
             val index = normalizedCache.records.indexOfFirst { it.key == field.value.key }
             if (index == -1) return
             keyList.selectedIndex = index
@@ -141,7 +215,7 @@ class NormalizedCacheValuesPanel(
 
         override fun mouseMoved(e: MouseEvent) {
           table.cursor = Cursor(
-              if (getFieldAtPoint(e)?.value is NormalizedCache.FieldValue.Reference) {
+              if (getFieldUnderPointer(e)?.value is NormalizedCache.FieldValue.Reference) {
                 Cursor.HAND_CURSOR
               } else {
                 Cursor.DEFAULT_CURSOR
@@ -155,8 +229,10 @@ class NormalizedCacheValuesPanel(
       }
       table.addMouseListener(mouseAdapter)
       table.addMouseMotionListener(mouseAdapter)
+
+      fieldTreeExpander = DefaultTreeExpander { tree }
     }
-    return tree
+    return treeTable
   }
 
   private fun getRootNodeForRecord(record: NormalizedCache.Record) = DefaultMutableTreeNode().apply {
@@ -175,7 +251,6 @@ class NormalizedCacheValuesPanel(
     }
   }
 }
-
 
 private class NormalizedCacheFieldTreeNode(val field: NormalizedCache.Field) : DefaultMutableTreeNode() {
   init {
