863e9c831979c765a9f198a00cbbce0a4a107328
BoD <BoD@JRAF.org>
2023-10-18 12:55:39 +0200

    Use apollo-normalized-cache-sqlite

--- intellij-plugin/src/main/kotlin/com/apollographql/ijplugin/normalizedcache/DatabaseNormalizedCacheProvider.kt
+++ intellij-plugin/src/main/kotlin/com/apollographql/ijplugin/normalizedcache/DatabaseNormalizedCacheProvider.kt
@@ -1,5 +1,7 @@
 package com.apollographql.ijplugin.normalizedcache
 
+import com.apollographql.apollo3.cache.normalized.api.CacheKey
+import com.apollographql.apollo3.cache.normalized.sql.SqlNormalizedCacheFactory
 import com.apollographql.ijplugin.normalizedcache.NormalizedCache.Field
 import com.apollographql.ijplugin.normalizedcache.NormalizedCache.FieldValue
 import com.apollographql.ijplugin.normalizedcache.NormalizedCache.FieldValue.BooleanValue
@@ -9,57 +11,52 @@ import com.apollographql.ijplugin.normalizedcache.NormalizedCache.FieldValue.Nul
 import com.apollographql.ijplugin.normalizedcache.NormalizedCache.FieldValue.NumberValue
 import com.apollographql.ijplugin.normalizedcache.NormalizedCache.FieldValue.Reference
 import com.apollographql.ijplugin.normalizedcache.NormalizedCache.FieldValue.StringValue
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonArray
-import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.JsonNull
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.JsonPrimitive
-import kotlinx.serialization.json.jsonObject
 import java.io.File
 import java.sql.DriverManager
-
-private const val REFERENCE_PREFIX = "ApolloCacheReference{"
+import com.apollographql.apollo3.cache.normalized.api.NormalizedCache as ApolloNormalizedCache
+import com.apollographql.apollo3.cache.normalized.api.Record as ApolloRecord
 
 class DatabaseNormalizedCacheProvider : NormalizedCacheProvider<File> {
-  override fun provide(parameters: File): Result<NormalizedCache> {
+  private fun checkDatabase(url: String) {
     Class.forName("org.sqlite.JDBC")
-    return runCatching {
-      DriverManager.getConnection("jdbc:sqlite:${parameters.absolutePath}").use { connection ->
-        val resultSet = connection.createStatement().executeQuery("SELECT key, record FROM records ORDER BY key COLLATE NOCASE")
-        val records = mutableListOf<NormalizedCache.Record>()
-        while (resultSet.next()) {
-          val key = resultSet.getString(1)
-          val recordJsonStr = resultSet.getString(2)
-          val recordJson = Json.parseToJsonElement(recordJsonStr).jsonObject
-          records.add(NormalizedCache.Record(key, recordJson.map {
-            Field(it.key, it.value.toFieldValue())
-          }))
+    DriverManager.getConnection(url).use { connection ->
+      connection.createStatement().executeQuery("SELECT key, record FROM records").use { resultSet ->
+        if (!resultSet.next()) {
+          error("No records found")
         }
-        NormalizedCache(records)
       }
+    }
+  }
+
+  override fun provide(parameters: File): Result<NormalizedCache> {
+    val url = "jdbc:sqlite:${parameters.absolutePath}"
+    return runCatching {
+      checkDatabase(url)
+      val apolloNormalizedCache: ApolloNormalizedCache = SqlNormalizedCacheFactory(url).create()
+      val apolloRecords: Map<String, ApolloRecord> = apolloNormalizedCache.dump().values.first()
+      NormalizedCache(
+          apolloRecords.map { (key, apolloRecord) ->
+            NormalizedCache.Record(
+                key,
+                apolloRecord.map { (fieldName, fieldValue) ->
+                  Field(fieldName, fieldValue.toFieldValue())
+                }
+            )
+          }
+      )
     }
   }
 }
 
-private fun JsonElement.toFieldValue(): FieldValue {
+private fun Any?.toFieldValue(): FieldValue {
   return when (this) {
-    is JsonNull -> Null
-    is JsonPrimitive -> {
-      when {
-        isString -> if (content.startsWith(REFERENCE_PREFIX)) {
-          Reference(content.substringAfter(REFERENCE_PREFIX).substringBeforeLast('}'))
-        } else {
-          StringValue(content)
-        }
-
-        content == "true" || content == "false" -> BooleanValue(content.toBooleanStrict())
-        else -> NumberValue(content.toIntOrNull() ?: content.toDoubleOrNull()
-        ?: error("Could not parse number '$content'"))
-      }
-    }
-
-    is JsonArray -> ListValue(map { it.toFieldValue() })
-    is JsonObject -> CompositeValue(map { Field(it.key, it.value.toFieldValue()) })
+    null -> Null
+    is String -> StringValue(this)
+    is Number -> NumberValue(this)
+    is Boolean -> BooleanValue(this)
+    is List<*> -> ListValue(map { it.toFieldValue() })
+    is Map<*, *> -> CompositeValue(map { Field(it.key as String, it.value.toFieldValue()) })
+    is CacheKey -> Reference(this.key)
+    else -> error("Unsupported type ${this::class}")
   }
 }
