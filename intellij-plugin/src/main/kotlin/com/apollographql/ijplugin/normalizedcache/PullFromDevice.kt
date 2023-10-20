699b82367817a5f2d5b031f94d17260bf41b8076
BoD <BoD@JRAF.org>
2023-10-20 11:24:31 +0200

    More robust error handling

--- intellij-plugin/src/main/kotlin/com/apollographql/ijplugin/normalizedcache/PullFromDevice.kt
+++ intellij-plugin/src/main/kotlin/com/apollographql/ijplugin/normalizedcache/PullFromDevice.kt
@@ -1,5 +1,6 @@
 package com.apollographql.ijplugin.normalizedcache
 
+import android.annotation.SuppressLint
 import com.android.ddmlib.AndroidDebugBridge
 import com.android.ddmlib.IDevice
 import com.android.ddmlib.SyncService
@@ -35,13 +36,14 @@ fun createPullFromDeviceActionGroup(project: Project, onFilePullError: (Throwabl
     try {
       val adb = AndroidDebugBridge.createBridge(1, TimeUnit.SECONDS)
       if (adb.devices.isEmpty()) {
-        add(DisabledAction(ApolloBundle.message("normalizedCacheViewer.pullFromDevice.noDevices")))
+        add(DisabledAction(ApolloBundle.message("normalizedCacheViewer.pullFromDevice.listDevices.empty")))
         return@apply
       }
       for (device in adb.devices) {
         add(DefaultActionGroup.createPopupGroup { device.name }.apply {
           // Add running apps
           for (client in device.clients) {
+            if (!client.isValid) continue
             val appPackageName = client.clientData.clientDescription
             val databasesDir = client.clientData.dataDir + "/databases"
             add(PackageDatabasesActionGroup(project, device, appPackageName, databasesDir, onFilePulled, onFilePullError))
@@ -52,7 +54,8 @@ fun createPullFromDeviceActionGroup(project: Project, onFilePullError: (Throwabl
         })
       }
     } catch (e: Exception) {
-      add(DisabledAction(ApolloBundle.message("normalizedCacheViewer.pullFromDevice.list.error", e.message ?: "")))
+      logd(e, "Could not list devices")
+      add(DisabledAction(ApolloBundle.message("normalizedCacheViewer.pullFromDevice.listDevices.error", e.message?.let { ": $it" } ?: "")))
     }
   }
 }
@@ -69,33 +72,41 @@ private class PackageDatabasesActionGroup(
     templatePresentation.isDisableGroupIfEmpty = false
   }
 
-  val databaseFilesResult by lazy {
-    AdbShellCommandsUtil.create(device).executeCommandBlocking("run-as $appPackageName ls $databasesDir")
+  val databaseFilesResult: Result<AdbShellCommandResult> by lazy {
+    runCatching {
+      AdbShellCommandsUtil.create(device).executeCommandBlocking(
+          "run-as $appPackageName ls $databasesDir"
+      )
+    }
   }
 
   override fun getActionUpdateThread() = ActionUpdateThread.BGT
 
-  override fun getChildren(e: AnActionEvent?) = arrayOf(DisabledAction("Loading..."))
+  override fun getChildren(e: AnActionEvent?) = arrayOf(LoadingAction)
 
   override fun postProcessVisibleChildren(visibleChildren: List<AnAction>, updateSession: UpdateSession): List<AnAction> {
-    if (databaseFilesResult.isError) {
+    if (databaseFilesResult.isFailure) {
+      return listOf(DisabledAction(ApolloBundle.message("normalizedCacheViewer.pullFromDevice.listDatabases.error")))
+    }
+    val result = databaseFilesResult.getOrThrow()
+    if (result.isError) {
       return listOf(DisabledAction(ApolloBundle.message(
-          if (databaseFilesResult.output.any { it.contains("No such file or directory") }) {
-            "normalizedCacheViewer.pullFromDevice.listDatabases.noDatabases"
+          if (result.output.any { it.contains("No such file or directory") }) {
+            "normalizedCacheViewer.pullFromDevice.listDatabases.empty"
           } else {
             "normalizedCacheViewer.pullFromDevice.listDatabases.error"
           }
       )))
     }
-    val databaseFileNames = databaseFilesResult.output.filter { it.endsWith(".db") }
+    val databaseFileNames = result.output.filter { it.endsWith(".db") }
     if (databaseFileNames.isEmpty()) {
-      return listOf(DisabledAction(ApolloBundle.message("normalizedCacheViewer.pullFromDevice.listDatabases.noDatabases")))
+      return listOf(DisabledAction(ApolloBundle.message("normalizedCacheViewer.pullFromDevice.listDatabases.empty")))
     }
     return databaseFileNames.map { databaseFileName ->
       DumbAwareAction.create(databaseFileName) {
         object : Task.Backgroundable(
             project,
-            ApolloBundle.message("normalizedCacheViewer.pullFromDevice.pulling"),
+            ApolloBundle.message("normalizedCacheViewer.pullFromDevice.pull.ongoing"),
             false,
         ) {
           override fun run(indicator: ProgressIndicator) {
@@ -120,31 +131,39 @@ private class DebuggablePackagesActionGroup(
     private val device: IDevice,
     private val onFilePulled: (File) -> Unit,
     private val onFilePullError: (Throwable) -> Unit,
-) : DefaultActionGroup("All packages", true), DumbAware {
+) : DefaultActionGroup(@Suppress("DialogTitleCapitalization") ApolloBundle.message("normalizedCacheViewer.pullFromDevice.listDebuggablePackages.title"), true), DumbAware {
   init {
     templatePresentation.isDisableGroupIfEmpty = false
   }
 
-  private val debuggablePackagesResult: AdbShellCommandResult by lazy {
-    // List all packages, and try run-as on them - if it succeeds, it's debuggable
-    AdbShellCommandsUtil.create(device).executeCommandBlocking("for p in \$(pm list packages -3 | cut -d : -f 2); do (run-as \$p true >/dev/null 2>&1 && echo \$p); done; true")
+  private val debuggablePackagesResult: Result<AdbShellCommandResult> by lazy {
+    runCatching {
+      AdbShellCommandsUtil.create(device).executeCommandBlocking(
+          // List all packages, and try run-as on them - if it succeeds, the package is debuggable
+          "for p in \$(pm list packages -3 | cut -d : -f 2); do (run-as \$p true >/dev/null 2>&1 && echo \$p); done; true"
+      )
+    }
   }
 
   override fun getActionUpdateThread() = ActionUpdateThread.BGT
 
-  override fun getChildren(e: AnActionEvent?): Array<AnAction> {
-    return arrayOf(DisabledAction("Loading..."))
-  }
+  override fun getChildren(e: AnActionEvent?) = arrayOf(LoadingAction)
 
+  @SuppressLint("SdCardPath")
   override fun postProcessVisibleChildren(visibleChildren: List<AnAction>, updateSession: UpdateSession): List<AnAction> {
-    return when {
-      debuggablePackagesResult.isError -> listOf(DisabledAction("Could not list debuggable apps"))
-      debuggablePackagesResult.output.filter { it.isNotEmpty() }.isEmpty() -> listOf(DisabledAction("No debuggable apps"))
-      else -> {
-        debuggablePackagesResult.output.filter { it.isNotEmpty() }.map { packageName ->
-          PackageDatabasesActionGroup(project, device, packageName, "/data/data/$packageName/databases", onFilePulled, onFilePullError)
-        }
-      }
+    if (debuggablePackagesResult.isFailure) {
+      return listOf(DisabledAction(ApolloBundle.message("normalizedCacheViewer.pullFromDevice.listDebuggablePackages.error")))
+    }
+    val result = debuggablePackagesResult.getOrThrow()
+    if (result.isError) {
+      return listOf(DisabledAction(ApolloBundle.message("normalizedCacheViewer.pullFromDevice.listDebuggablePackages.error")))
+    }
+    val packageNames = result.output.filterNot { it.isEmpty() }
+    if (packageNames.isEmpty()) {
+      return listOf(DisabledAction(ApolloBundle.message("normalizedCacheViewer.pullFromDevice.listDebuggablePackages.empty")))
+    }
+    return packageNames.map { packageName ->
+      PackageDatabasesActionGroup(project, device, packageName, "/data/data/$packageName/databases", onFilePulled, onFilePullError)
     }
   }
 }
@@ -155,21 +174,21 @@ private fun pullFile(device: IDevice, appPackageName: String, databasesDir: Stri
   logd("Pulling $remoteFilePath to ${localFile.absolutePath}")
   val intermediateRemoteFilePath = "/data/local/tmp/${localFile.name}"
   val shellCommandsUtil = AdbShellCommandsUtil.create(device)
-  var commandResult = shellCommandsUtil.executeCommandBlocking("touch $intermediateRemoteFilePath")
-  if (commandResult.isError) {
-    return Result.failure(Exception("'touch' command failed"))
-  }
-  commandResult = shellCommandsUtil.executeCommandBlocking("run-as $appPackageName sh -c 'cp $remoteFilePath $intermediateRemoteFilePath'")
-  if (commandResult.isError) {
-    return Result.failure(Exception("'copy' command failed"))
-  }
   return runCatching {
+    var commandResult = shellCommandsUtil.executeCommandBlocking("touch $intermediateRemoteFilePath")
+    if (commandResult.isError) {
+      throw Exception("'touch' command failed")
+    }
+    commandResult = shellCommandsUtil.executeCommandBlocking("run-as $appPackageName sh -c 'cp $remoteFilePath $intermediateRemoteFilePath'")
+    if (commandResult.isError) {
+      throw Exception("'copy' command failed")
+    }
     try {
       device.syncService.pullFile(intermediateRemoteFilePath, localFile.absolutePath, NullSyncProgressMonitor)
     } finally {
       commandResult = shellCommandsUtil.executeCommandBlocking("rm $intermediateRemoteFilePath")
       if (commandResult.isError) {
-        logw("rm failed")
+        logw("'rm' command failed")
       }
     }
     localFile
@@ -193,3 +212,5 @@ private class DisabledAction(text: String) : AnAction(text) {
 
   override fun getActionUpdateThread() = ActionUpdateThread.BGT
 }
+
+private val LoadingAction = DisabledAction(ApolloBundle.message("normalizedCacheViewer.pullFromDevice.loading"))
