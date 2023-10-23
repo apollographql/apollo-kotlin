4017ff29de5414b735eb0f3d860331cec6611999
Martin Bonnin <martin@mbonnin.net>
2023-10-23 10:29:36 +0200

    Fix NSURL tests on recent apple OSes (#5315)
    * fix NSURL tests on recent apple OSes
    
    * better fix
    
    * prepare for macos 14

--- libraries/apollo-api/src/appleTest/kotlin/NSURLTest.kt
+++ libraries/apollo-api/src/appleTest/kotlin/NSURLTest.kt
@@ -1,17 +1,42 @@
 import com.apollographql.apollo3.api.http.internal.urlEncode
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.UnsafeNumber
+import kotlinx.cinterop.useContents
+import platform.Foundation.NSProcessInfo
 import platform.Foundation.NSURL
+import kotlin.experimental.ExperimentalNativeApi
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.fail
 
+
 class NSURLTest {
+  @OptIn(ExperimentalForeignApi::class, UnsafeNumber::class, ExperimentalNativeApi::class)
   @Test
   fun invalidCharThrows() {
-    try {
-      NSURL(string = "https://example.com/?a=À")
-      fail("An exception was expected")
-    } catch (e: NullPointerException) {
+    // Behaviour changed in newer OS versions. See https://developer.apple.com/documentation/foundation/nsurl
+    val isFixed = NSProcessInfo.processInfo().operatingSystemVersion().useContents {
+      val fixVersion = when(Platform.osFamily) {
+        OsFamily.IOS -> 17
+        OsFamily.TVOS -> 17
+        OsFamily.WATCHOS -> 10
+        OsFamily.MACOSX -> 14
+        else -> Int.MAX_VALUE
+      }
 
+      majorVersion >= fixVersion
+    }
+
+    if (!isFixed) {
+      try {
+        NSURL(string = "https://example.com/?a=À")
+        fail("An exception was expected")
+      } catch (e: NullPointerException) {
+
+      }
+    } else {
+      // New versions do percent encoding automatically
+      assertEquals("https://example.com/?a=%C3%80", NSURL(string = "https://example.com/?a=À").description)
     }
   }
 
