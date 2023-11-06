a6698f731d9f8e686e23866e2bd8a86dd7fbae21
BoD <BoD@JRAF.org>
2023-11-06 12:46:15 +0100

    Add a lock for safety

--- libraries/apollo-debug-server/src/commonMain/kotlin/com/apollographql/apollo3/debugserver/ApolloDebugServer.kt
+++ libraries/apollo-debug-server/src/commonMain/kotlin/com/apollographql/apollo3/debugserver/ApolloDebugServer.kt
@@ -3,26 +3,34 @@ package com.apollographql.apollo3.debugserver
 import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.debugserver.internal.server.Server
 import com.apollographql.apollo3.debugserver.internal.server.createServer
+import okio.withLock
+import java.util.concurrent.locks.ReentrantLock
 
 object ApolloDebugServer {
   private val apolloClients = mutableMapOf<ApolloClient, String>()
   private var server: Server? = null
+  private val lock = ReentrantLock()
 
   fun registerApolloClient(apolloClient: ApolloClient, id: String = "client") {
-    if (apolloClients.containsKey(apolloClient)) error("Client '$apolloClient' already registered")
-    if (apolloClients.containsValue(id)) error("Name '$id' already registered")
-    apolloClients[apolloClient] = id
-    startOrStopServer()
+    lock.withLock {
+      if (apolloClients.containsKey(apolloClient)) error("Client '$apolloClient' already registered")
+      if (apolloClients.containsValue(id)) error("Name '$id' already registered")
+      apolloClients[apolloClient] = id
+      startOrStopServer()
+    }
   }
 
   fun unregisterApolloClient(apolloClient: ApolloClient) {
-    apolloClients.remove(apolloClient)
-    startOrStopServer()
+    lock.withLock {
+      apolloClients.remove(apolloClient)
+      startOrStopServer()
+    }
   }
 
   private fun startOrStopServer() {
     if (apolloClients.isEmpty()) {
       server?.stop()
+      server = null
     } else {
       if (server == null) {
         server = createServer(apolloClients).apply {
