90ff731331eecaa379051d93128cedda50cc8dd6
BoD <BoD@JRAF.org>
2023-11-22 10:10:48 +0100

    Fix API breakage and update API dump

--- libraries/apollo-ast/src/commonMain/kotlin/com/apollographql/apollo3/ast/VariableUsage.kt
+++ libraries/apollo-ast/src/commonMain/kotlin/com/apollographql/apollo3/ast/VariableUsage.kt
@@ -1,5 +1,8 @@
 package com.apollographql.apollo3.ast
 
+import com.apollographql.apollo3.annotations.ApolloDeprecatedSince
+import com.apollographql.apollo3.annotations.ApolloDeprecatedSince.Version.v4_0_0
+
 /**
  * A variable used in a [GQLValue]
  */
@@ -8,7 +11,24 @@ class VariableUsage(
     val locationType: GQLType,
     val hasLocationDefaultValue: Boolean,
     val isOneOfInputObject: Boolean,
-)
+) {
+  @Deprecated(
+      message = "Use the constructor with isOneOfInputObject instead",
+      replaceWith = ReplaceWith("VariableUsage(variable, locationType, hasLocationDefaultValue, isOneOfInputObject = false)"),
+      level = DeprecationLevel.ERROR
+  )
+  @ApolloDeprecatedSince(v4_0_0)
+  constructor(
+      variable: GQLVariableValue,
+      locationType: GQLType,
+      hasLocationDefaultValue: Boolean,
+  ) : this(
+      variable = variable,
+      locationType = locationType,
+      hasLocationDefaultValue = hasLocationDefaultValue,
+      isOneOfInputObject = false,
+  )
+}
 
 /**
  * A variable that is inferred from its usages in fragments
