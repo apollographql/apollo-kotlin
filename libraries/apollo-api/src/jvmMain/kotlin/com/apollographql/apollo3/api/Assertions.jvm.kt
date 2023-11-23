947fd2328a9f01c6b85d0ba29205c32e2cbdb6fb
BoD <BoD@JRAF.org>
2023-11-23 20:05:16 +0100

    Add a comment about @Suppress("NewApi")

--- libraries/apollo-api/src/jvmMain/kotlin/com/apollographql/apollo3/api/Assertions.jvm.kt
+++ libraries/apollo-api/src/jvmMain/kotlin/com/apollographql/apollo3/api/Assertions.jvm.kt
@@ -4,6 +4,8 @@
 package com.apollographql.apollo3.api
 
 @SafeVarargs
+// Optional and its methods generate 'NewApi' warnings since it's Android API level 24+. It can safely be suppressed as this method
+// is only referenced in projects that use Optional already.
 @Suppress("NewApi")
 fun assertOneOf(vararg args: java.util.Optional<out java.util.Optional<*>>) {
   val presentArgs = args.filter { it.isPresent }
