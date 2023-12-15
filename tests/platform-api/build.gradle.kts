7d7d41ce074106ca5e89724248f97bec954ff495
BoD <BoD@JRAF.org>
2023-12-15 16:46:16 +0100

    Fix gradle build

--- tests/platform-api/build.gradle.kts
+++ tests/platform-api/build.gradle.kts
@@ -13,10 +13,10 @@ apollo {
   service("service") {
     packageName.set("com.example")
 
-    val key = System.getenv("PLATFORM_API_TESTS_KEY")
+    val apiKey = System.getenv("PLATFORM_API_TESTS_KEY")
     if (key != null) {
       registry {
-        key.set(key)
+        key.set(apiKey)
         graph.set("Apollo-Kotlin-CI-tests")
         graphVariant.set("current")
         schemaFile.set(file("src/main/graphql/schema.graphqls"))
@@ -24,7 +24,7 @@ apollo {
 
       operationManifestFormat.set("persistedQueryManifest")
       registerOperations {
-        key.set(key)
+        key.set(apiKey)
         graph.set("Apollo-Kotlin-CI-tests")
         graphVariant.set("current")
         listId.set("2ad55629-f473-4d72-9897-2dd2096540f4")
