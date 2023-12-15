1a4eb0c109e11f55a839c1df86cd8004a3fc89e7
BoD <BoD@JRAF.org>
2023-12-15 16:38:09 +0100

    Don't configure anything if there's no key

--- tests/platform-api/build.gradle.kts
+++ tests/platform-api/build.gradle.kts
@@ -13,19 +13,22 @@ apollo {
   service("service") {
     packageName.set("com.example")
 
-    registry {
-      key.set(System.getenv("PLATFORM_API_TESTS_KEY"))
-      graph.set("Apollo-Kotlin-CI-tests")
-      graphVariant.set("current")
-      schemaFile.set(file("src/main/graphql/schema.graphqls"))
-    }
+    val key = System.getenv("PLATFORM_API_TESTS_KEY")
+    if (key != null) {
+      registry {
+        key.set(key)
+        graph.set("Apollo-Kotlin-CI-tests")
+        graphVariant.set("current")
+        schemaFile.set(file("src/main/graphql/schema.graphqls"))
+      }
 
-    operationManifestFormat.set("persistedQueryManifest")
-    registerOperations {
-      key.set(System.getenv("PLATFORM_API_TESTS_KEY"))
-      graph.set("Apollo-Kotlin-CI-tests")
-      graphVariant.set("current")
-      listId.set("2ad55629-f473-4d72-9897-2dd2096540f4")
+      operationManifestFormat.set("persistedQueryManifest")
+      registerOperations {
+        key.set(key)
+        graph.set("Apollo-Kotlin-CI-tests")
+        graphVariant.set("current")
+        listId.set("2ad55629-f473-4d72-9897-2dd2096540f4")
+      }
     }
   }
 }
