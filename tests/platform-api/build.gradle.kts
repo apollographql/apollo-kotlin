7f0171dd7a046132902a0df04cab003cdc37e452
BoD <BoD@JRAF.org>
2023-12-15 16:15:04 +0100

    Use a real account

--- tests/platform-api/build.gradle.kts
+++ tests/platform-api/build.gradle.kts
@@ -15,7 +15,7 @@ apollo {
 
     registry {
       key.set(System.getenv("PLATFORM_API_TESTS_KEY"))
-      graph.set("changeme")
+      graph.set("Apollo-Kotlin-CI-tests")
       graphVariant.set("current")
       schemaFile.set(file("src/main/graphql/schema.graphqls"))
     }
@@ -23,9 +23,9 @@ apollo {
     operationManifestFormat.set("persistedQueryManifest")
     registerOperations {
       key.set(System.getenv("PLATFORM_API_TESTS_KEY"))
-      graph.set("changeme")
+      graph.set("Apollo-Kotlin-CI-tests")
       graphVariant.set("current")
-      listId.set("changeme")
+      listId.set("2ad55629-f473-4d72-9897-2dd2096540f4")
     }
   }
 }
