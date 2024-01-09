fa99bdb029350dc54b84fa371452fe2b80e8c173
Martin Bonnin <martin@mbonnin.net>
2024-01-10 00:25:09 +0100

    fix location of the kdoc

--- build-logic/src/main/kotlin/Publishing.kt
+++ build-logic/src/main/kotlin/Publishing.kt
@@ -147,6 +147,13 @@ fun Project.configureDokkaAggregate() {
     versionsOrdering.set((olderVersions + currentVersion).reversed())
     olderVersionsDir.fileProvider(downloadKDocVersions.map { it.outputs.files.singleFile })
   }
+  tasks.withType(DokkatooGenerateTask::class.java).configureEach {
+    /**
+     * The Apollo docs website expect the contents to be in a `kdoc` subfolder
+     * See https://github.com/apollographql/website-router/blob/389d6748c592ac88411ceb15c93965d2b800d9b3/_redirects#L105
+     */
+    outputDirectory.set(layout.buildDirectory.asFile.get().resolve("dokka/html/kdoc"))
+  }
 }
 
 private fun Project.getOssStagingUrl(): String {
