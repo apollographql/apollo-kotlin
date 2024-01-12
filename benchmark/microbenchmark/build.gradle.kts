c1a2a00d51c9525176a31fc3e0cc6eee31231904
Martin Bonnin <martin@mbonnin.net>
2024-01-12 17:53:19 +0100

    fix SDK version

--- benchmark/microbenchmark/build.gradle.kts
+++ benchmark/microbenchmark/build.gradle.kts
@@ -68,10 +68,10 @@ dependencies {
 
 configure<com.android.build.gradle.LibraryExtension> {
   namespace = "com.apollographql.apollo3.benchmark"
-  compileSdk = libs.versions.android.sdkversion.compile.get().toInt()
+  compileSdk = libs.versions.android.sdkversion.compilebenchmark.get().toInt()
 
   defaultConfig {
-    minSdk = libs.versions.android.sdkversion.min.get().toInt()
+    minSdk = libs.versions.android.sdkversion.compilebenchmark.get().toInt()
     testInstrumentationRunner = "androidx.benchmark.junit4.AndroidBenchmarkRunner"
   }
 
