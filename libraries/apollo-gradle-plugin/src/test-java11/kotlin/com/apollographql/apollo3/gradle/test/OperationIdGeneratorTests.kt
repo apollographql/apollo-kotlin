e321d7e27e8f660e82e20db45779eec59978b264
Martin Bonnin <martin@mbonnin.net>
2024-02-01 18:09:51 +0100

    adapt operationIds test

--- libraries/apollo-gradle-plugin/src/test-java11/kotlin/com/apollographql/apollo3/gradle/test/OperationIdGeneratorTests.kt
+++ libraries/apollo-gradle-plugin/src/test-java11/kotlin/com/apollographql/apollo3/gradle/test/OperationIdGeneratorTests.kt
@@ -1,15 +1,15 @@
 package com.apollographql.apollo3.gradle.test
 
-import util.TestUtils
-import util.TestUtils.withSimpleProject
-import util.TestUtils.withTestProject
-import util.generatedChild
-import util.replaceInText
 import com.google.common.truth.Truth
 import org.gradle.testkit.runner.TaskOutcome
 import org.junit.Assert
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import util.TestUtils
+import util.TestUtils.withSimpleProject
+import util.TestUtils.withTestProject
+import util.generatedChild
+import util.replaceInText
 import java.io.File
 
 class OperationIdGeneratorTests {
@@ -147,6 +147,30 @@ class OperationIdGeneratorTests {
       Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":generateServiceApolloSources")!!.outcome)
 
       Truth.assertThat(dir.generatedChild("service/com/example/GreetingQuery.kt").readText())
+          .contains("anotherCustomId")
+    }
+  }
+
+  @Test
+  fun `operationOutputGenerator is working as expected with classloader isolation`() {
+    withTestProject("operationIdsWithIsolation") { dir ->
+
+      var result = TestUtils.executeTask("generateApolloSources", dir)
+
+      val appDir = dir.resolve("app")
+      Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":app:generateServiceApolloSources")!!.outcome)
+
+      Truth.assertThat(appDir.generatedChild("service/com/example/GreetingQuery.kt").readText())
+          .contains("OPERATION_ID: String = \"GreetingCustomId\"")
+
+      // Change the implementation of the operation ID generator and check again
+      File(dir,"apollo-compiler-plugin/src/main/kotlin/apollo/plugin/MyPlugin.kt").replaceInText("CustomId", "anotherCustomId")
+
+      result = TestUtils.executeTask("generateApolloSources", dir)
+
+      Assert.assertEquals(TaskOutcome.SUCCESS, result.task(":app:generateServiceApolloSources")!!.outcome)
+
+      Truth.assertThat(appDir.generatedChild("service/com/example/GreetingQuery.kt").readText())
           .contains("anotherCustomId")
     }
   }
