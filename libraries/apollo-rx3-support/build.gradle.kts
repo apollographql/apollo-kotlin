aa0f04a8aabb6107d685e7466e095156422a5353
Martin Bonnin <martin@mbonnin.net>
2024-02-08 17:08:35 +0100

    Keep rx-support as DeprecationLevel.Error (#5610)

---
+++ libraries/apollo-rx3-support/build.gradle.kts
@@ -0,0 +1,17 @@
+/*
+ * This file is auto generated from apollo-rx2-support by rxjava3.main.kts, do not edit manually.
+ */
+plugins {
+  id("org.jetbrains.kotlin.jvm")
+}
+
+apolloLibrary(javaModuleName = "com.apollographql.apollo3.rx3")
+
+dependencies {
+  implementation(project(":apollo-api"))
+  api(libs.rx.java3)
+  api(libs.kotlinx.coroutines.rx3)
+
+  api(project(":apollo-runtime"))
+  api(project(":apollo-normalized-cache"))
+}
