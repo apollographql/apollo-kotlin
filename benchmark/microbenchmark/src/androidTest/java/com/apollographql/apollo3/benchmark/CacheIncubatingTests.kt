b12e4d9d80511e5af8db2734c205d63d42331384
BoD <BoD@JRAF.org>
2024-02-13 14:39:51 +0100

    Add a benchmark: CacheIncubatingTests.concurrentReadWriteFromCache

--- benchmark/microbenchmark/src/androidTest/java/com/apollographql/apollo3/benchmark/CacheIncubatingTests.kt
+++ benchmark/microbenchmark/src/androidTest/java/com/apollographql/apollo3/benchmark/CacheIncubatingTests.kt
@@ -9,8 +9,6 @@ import com.apollographql.apollo3.api.Operation
 import com.apollographql.apollo3.api.Query
 import com.apollographql.apollo3.api.json.jsonReader
 import com.apollographql.apollo3.api.parseJsonResponse
-import com.apollographql.apollo3.benchmark.Utils.checkOperationBased
-import com.apollographql.apollo3.benchmark.Utils.checkResponseBased
 import com.apollographql.apollo3.benchmark.Utils.operationBasedQuery
 import com.apollographql.apollo3.benchmark.Utils.registerCacheSize
 import com.apollographql.apollo3.benchmark.Utils.resource
@@ -26,9 +24,11 @@ import com.apollographql.apollo3.cache.normalized.incubating.api.Record
 import com.apollographql.apollo3.cache.normalized.incubating.api.TypePolicyCacheKeyGenerator
 import com.apollographql.apollo3.cache.normalized.incubating.sql.SqlNormalizedCacheFactory
 import kotlinx.coroutines.runBlocking
+import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 import java.lang.reflect.Method
+import java.util.concurrent.Executors
 
 @Suppress("UNCHECKED_CAST")
 class CacheIncubatingTests {
@@ -54,6 +54,27 @@ class CacheIncubatingTests {
   fun cacheResponseSql() {
     readFromCache("cacheResponseSql", responseBasedQuery, sql = true, ::checkResponseBased)
   }
+
+  @Test
+  fun concurrentCacheOperationMemory() {
+    concurrentReadWriteFromCache(operationBasedQuery, sql = false)
+  }
+
+  @Test
+  fun concurrentCacheOperationSql() {
+    concurrentReadWriteFromCache(operationBasedQuery, sql = true)
+  }
+
+  @Test
+  fun concurrentCacheResponseMemory() {
+    concurrentReadWriteFromCache(responseBasedQuery, sql = false)
+  }
+
+  @Test
+  fun concurrentCacheResponseSql() {
+    concurrentReadWriteFromCache(responseBasedQuery, sql = true)
+  }
+
 
   private fun <D : Query.Data> readFromCache(testName: String, query: Query<D>, sql: Boolean, check: (D) -> Unit) {
     val cache = if (sql) {
@@ -93,7 +114,55 @@ class CacheIncubatingTests {
     }
   }
 
+  private fun <D : Query.Data> concurrentReadWriteFromCache(query: Query<D>, sql: Boolean) {
+    val cache = if (sql) {
+      Utils.dbFile.delete()
+      // Pass context explicitly here because androidx.startup fails due to relocation
+      SqlNormalizedCacheFactory(InstrumentationRegistry.getInstrumentation().context, Utils.dbName, withDates = true).create()
+    } else {
+      MemoryCacheFactory().create()
+    }
+    val data = query.parseJsonResponse(resource(R.raw.calendar_response_simple).jsonReader()).data!!
+
+    val records = normalizeMethod.invoke(
+        null,
+        query,
+        data,
+        CustomScalarAdapters.Empty,
+        TypePolicyCacheKeyGenerator,
+    ) as Map<String, Record>
+
+    val threadPool = Executors.newFixedThreadPool(CONCURRENCY)
+    benchmarkRule.measureRepeated {
+      val futures = (1..CONCURRENCY).map {
+        threadPool.submit {
+          // Let each thread execute a few writes/reads
+          repeat(WORK_LOAD) {
+            cache.merge(records.values.toList(), CacheHeaders.NONE)
+
+            val data2 = readDataFromCacheMethod.invoke(
+                null,
+                query,
+                CustomScalarAdapters.Empty,
+                cache,
+                FieldPolicyCacheResolver,
+                CacheHeaders.NONE
+            ) as D
+
+            Assert.assertEquals(data, data2)
+          }
+        }
+      }
+      // Wait for all threads to finish
+      futures.forEach { it.get() }
+    }
+  }
+
+
   companion object {
+    private const val CONCURRENCY = 15
+    private const val WORK_LOAD = 15
+
     /**
      * There doesn't seem to be a way to relocate Kotlin metdata and kotlin_module files so we rely on reflection to call top-level
      * methods
