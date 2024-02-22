375483aa0d7389c589c3a12ed5cdd11437a5be58
martinbonnin <martin@mbonnin.net>
2024-02-22 15:58:01 +0000

    Deploying to gh-pages from @ apollographql/apollo-kotlin@3acfc47f75ba3f8e0c2fb393a171c676245121c3 🚀

--- kdoc/scripts/navigation-loader.js
+++ kdoc/scripts/navigation-loader.js
@@ -1,7 +1,3 @@
-/*
- * Copyright 2014-2023 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
- */
-
 navigationPageText = fetch(pathToRoot + "navigation.html").then(response => response.text())
 
 displayNavigationFromPage = () => {
