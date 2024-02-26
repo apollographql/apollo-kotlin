3fd856a934005308769c82edc4d4f874d06cd7d8
Benoit 'BoD' Lubek <BoD@JRAF.org>
2024-02-26 10:11:24 +0100

    Update libraries/apollo-normalized-cache-api-incubating/src/wasmJsMain/kotlin/com/apollographql/apollo3/cache/normalized/api/internal/ConcurrentMap.wasmJs.kt
    Co-authored-by: Martin Bonnin <martin@mbonnin.net>

--- libraries/apollo-normalized-cache-api-incubating/src/wasmJsMain/kotlin/com/apollographql/apollo3/cache/normalized/api/internal/ConcurrentMap.wasmJs.kt
+++ libraries/apollo-normalized-cache-api-incubating/src/wasmJsMain/kotlin/com/apollographql/apollo3/cache/normalized/api/internal/ConcurrentMap.wasmJs.kt
@@ -1,3 +1,3 @@
 package com.apollographql.apollo3.cache.normalized.api.internal
 
-internal actual fun <K, V> ConcurrentMap(): MutableMap<K, V> = CommonConcurrentMap()
+internal actual fun <K, V> ConcurrentMap(): MutableMap<K, V> = mutableMapOf()
