e67a1d515b33c69421c25e9ced7367f0777ca8db
Martin Bonnin <martin@mbonnin.net>
2024-02-28 12:49:53 +0100

    make more Android things internal

--- libraries/apollo-gradle-plugin-external/src/main/kotlin/com/apollographql/apollo3/gradle/internal/AndroidPluginFacade.kt
+++ libraries/apollo-gradle-plugin-external/src/main/kotlin/com/apollographql/apollo3/gradle/internal/AndroidPluginFacade.kt
@@ -4,12 +4,14 @@ import com.android.build.gradle.AppExtension
 import com.android.build.gradle.BaseExtension
 import com.android.build.gradle.LibraryExtension
 import com.android.build.gradle.TestedExtension
+import com.android.build.gradle.api.ApplicationVariant
 import com.android.build.gradle.api.BaseVariant
+import com.android.build.gradle.api.LibraryVariant
 import com.android.build.gradle.api.TestVariant
 import com.android.build.gradle.api.UnitTestVariant
 import com.apollographql.apollo3.compiler.capitalizeFirstLetter
-import com.apollographql.apollo3.gradle.api.androidExtensionOrThrow
 import com.apollographql.apollo3.gradle.api.kotlinProjectExtension
+import org.gradle.api.DomainObjectSet
 import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Project
 import org.gradle.api.Task
@@ -18,6 +20,56 @@ import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.TaskProvider
 import java.lang.reflect.Method
 import java.lang.reflect.ParameterizedType
+
+internal object AndroidProject {
+  fun onEachVariant(project: Project, withTestVariants: Boolean = false, block: (BaseVariant) -> Unit) {
+    project.applicationVariants?.configureEach {
+      block(it)
+    }
+    project.libraryVariants?.configureEach {
+      block(it)
+    }
+
+    if (withTestVariants) {
+      project.testVariants?.configureEach {
+        block(it)
+      }
+      project.unitTestVariants?.configureEach {
+        block(it)
+      }
+    }
+  }
+}
+
+internal val Project.androidExtension
+  get() = extensions.findByName("android") as? BaseExtension
+
+internal val Project.androidExtensionOrThrow
+  get() = androidExtension ?: throw IllegalStateException("Apollo: no 'android' extension found. Did you apply the Android plugin?")
+
+internal val Project.libraryVariants: DomainObjectSet<LibraryVariant>?
+  get() {
+    return (androidExtensionOrThrow as? LibraryExtension)
+        ?.libraryVariants
+  }
+
+internal val Project.applicationVariants: DomainObjectSet<ApplicationVariant>?
+  get() {
+    return (androidExtensionOrThrow as? AppExtension)
+        ?.applicationVariants
+  }
+
+internal val Project.unitTestVariants: DomainObjectSet<UnitTestVariant>?
+  get() {
+    return (androidExtensionOrThrow as? TestedExtension)
+        ?.unitTestVariants
+  }
+
+internal val Project.testVariants: DomainObjectSet<TestVariant>?
+  get() {
+    return (androidExtensionOrThrow as? TestedExtension)
+        ?.testVariants
+  }
 
 private fun Project.getVariants(): NamedDomainObjectContainer<BaseVariant> {
   val container = project.container(BaseVariant::class.java)
