efd428fc146c3c97c21c9dc34800042d85c83d16
BoD <BoD@JRAF.org>
2024-03-01 10:29:15 +0100

    Be exhaustive in the kdoc

--- libraries/apollo-api/src/commonMain/kotlin/com/apollographql/apollo3/api/ApolloResponse.kt
+++ libraries/apollo-api/src/commonMain/kotlin/com/apollographql/apollo3/api/ApolloResponse.kt
@@ -17,9 +17,10 @@ import kotlin.jvm.JvmName
  * - [exception] contains an exception if a GraphQL response wasn't received
  *
  * GraphQL responses can contain partial data, so it is possible to have both [data] != null and [errors] != null. An [ApolloResponse] may be in any of these states:
- * - exception == null && data != null && errors == null: complete data with no error
+ * - exception == null && data != null && errors == null: complete data with no errors
  * - exception == null && data != null && errors != null: partial data with field errors
  * - exception == null && data == null && errors != null: no data, only field errors
+ * - exception == null && data == null && errors == null: no data and no errors - while technically possible, this should not happen with a spec-compliant server
  * - exception != null && data == null && errors == null: no GraphQL response was received due to a network error or otherwise
  */
 class ApolloResponse<D : Operation.Data>
