f5e1fbba02affc56ba60265d9b6a2997d9ea2629
Martin Bonnin <martin@mbonnin.net>
2024-03-14 15:38:44 +0100

    Update Subscription.kt

--- libraries/apollo-api/src/commonMain/kotlin/com/apollographql/apollo3/api/Subscription.kt
+++ libraries/apollo-api/src/commonMain/kotlin/com/apollographql/apollo3/api/Subscription.kt
@@ -1,7 +1,7 @@
 package com.apollographql.apollo3.api
 
 /**
- * Type safe representation of a [GraphQL mutation](https://spec.graphql.org/October2021/#sec-Subscription).
+ * Type safe representation of a [GraphQL subscription](https://spec.graphql.org/October2021/#sec-Subscription).
  */
 interface Subscription<D : Subscription.Data> : Operation<D> {
   interface Data: Operation.Data
