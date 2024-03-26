530cee49a99ec6bc7ffe098ee2ee91c1c4ced2c8
Martin Bonnin <martin@mbonnin.net>
2024-03-26 11:28:32 +0100

    disable debug

--- libraries/apollo-runtime-java/src/main/java/com/apollographql/apollo3/runtime/java/network/ws/WebSocketConnection.java
+++ libraries/apollo-runtime-java/src/main/java/com/apollographql/apollo3/runtime/java/network/ws/WebSocketConnection.java
@@ -17,7 +17,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 public class WebSocketConnection {
-  private static final boolean DEBUG = true; // TODO
+  private static final boolean DEBUG = false;
   private static final long OPEN_TIMEOUT_MS = 10_000;
 
   private static final int CLOSE_NORMAL = 1000;
