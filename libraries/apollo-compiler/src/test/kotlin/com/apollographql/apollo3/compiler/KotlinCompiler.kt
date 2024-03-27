3e0db872a69d77e018728bf55c24edee7ef5239d
BoD <BoD@JRAF.org>
2024-03-27 13:29:32 +0100

    Add -Xskip-prerelease-check for Gradle tests

--- libraries/apollo-compiler/src/test/kotlin/com/apollographql/apollo3/compiler/KotlinCompiler.kt
+++ libraries/apollo-compiler/src/test/kotlin/com/apollographql/apollo3/compiler/KotlinCompiler.kt
@@ -19,7 +19,7 @@ object KotlinCompiler {
     val result = KotlinCompilation().apply {
       sources = kotlinFiles
 
-      kotlincArguments = kotlincArguments + "-opt-in=kotlin.RequiresOptIn"
+      kotlincArguments = kotlincArguments + "-opt-in=kotlin.RequiresOptIn" + "-Xskip-prerelease-check"
       inheritClassPath = true
       verbose = false
       messageOutputStream = okio.blackholeSink().buffer().outputStream()
--- libraries/apollo-gradle-plugin/testProjects/kotlin-android-plugin-version/build.gradle.kts
+++ libraries/apollo-gradle-plugin/testProjects/kotlin-android-plugin-version/build.gradle.kts
@@ -1,4 +1,3 @@
-import com.android.build.gradle.BaseExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -41,5 +40,6 @@ tasks.withType<KotlinCompile>().configureEach {
     // Required for Kotlin < 1.6.10
     // See https://kotlinlang.org/docs/whatsnew1620.html#compatibility-changes-in-the-xjvm-default-modes
     freeCompilerArgs += "-Xjvm-default=all"
+    freeCompilerArgs += "-Xskip-prerelease-check"
   }
 }
