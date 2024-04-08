ae11d8e528873a4ee5f3fedd65338c2dac1c1fa9
BoD <BoD@JRAF.org>
2024-04-08 19:47:06 +0200

    Update com.gradle.enterprise

--- benchmark/settings.gradle.kts
+++ benchmark/settings.gradle.kts
@@ -2,7 +2,7 @@ pluginManagement {
   includeBuild("build-logic")
 }
 plugins {
-  id("com.gradle.enterprise") version "3.16.2" // sync with libraries.toml
+  id("com.gradle.enterprise") version "3.17" // sync with libraries.toml
   id("com.gradle.common-custom-user-data-gradle-plugin") version "2.0"
   id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
