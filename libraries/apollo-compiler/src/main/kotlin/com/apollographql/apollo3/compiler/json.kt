30662ff6135c37a16f225e5562ff9ae9e1d55d4d
BoD <BoD@JRAF.org>
2024-04-12 09:58:51 +0200

    Indent the used-coordinates.json.expected files

--- libraries/apollo-compiler/src/main/kotlin/com/apollographql/apollo3/compiler/json.kt
+++ libraries/apollo-compiler/src/main/kotlin/com/apollographql/apollo3/compiler/json.kt
@@ -20,6 +20,8 @@ private val json = Json {
   classDiscriminator = "#class"
 }
 
+private val prettyPrintJson = Json { prettyPrint = true }
+
 private inline fun <reified T> T.encodeToJson(file: File) {
   // XXX: use a streaming API when they are not experimental anymore
   file.writeText(json.encodeToString(this))
@@ -35,8 +37,10 @@ private inline fun <reified T> File.parseFromJson(): T {
  */
 @JvmName("readCodegenSchemaOptions")
 fun File.toCodegenSchemaOptions(): CodegenSchemaOptions = parseFromJson()
+
 @JvmName("readIrOptions")
 fun File.toIrOptions(): IrOptions = parseFromJson()
+
 @JvmName("readCodegenOptions")
 fun File.toCodegenOptions(): CodegenOptions = parseFromJson()
 
@@ -45,8 +49,10 @@ fun File.toCodegenOptions(): CodegenOptions = parseFromJson()
  */
 @JvmName("writeCodegenSchemaOptions")
 fun CodegenSchemaOptions.writeTo(file: File) = encodeToJson(file)
+
 @JvmName("writeIrOptions")
 fun IrOptions.writeTo(file: File) = encodeToJson(file)
+
 @JvmName("writeCodegenOptions")
 fun CodegenOptions.writeTo(file: File) = encodeToJson(file)
 
@@ -55,14 +61,19 @@ fun CodegenOptions.writeTo(file: File) = encodeToJson(file)
  */
 @JvmName("readCodegenSchema")
 fun File.toCodegenSchema(): CodegenSchema = parseFromJson()
+
 @JvmName("readIrOperations")
 fun File.toIrOperations(): IrOperations = parseFromJson<IrOperations>()
+
 @JvmName("readIrSchema")
 fun File.toIrSchema(): IrSchema = parseFromJson<DefaultIrSchema>()
+
 @JvmName("readCodegenMetadata")
 fun File.toCodegenMetadata(): CodegenMetadata = parseFromJson()
+
 @JvmName("readOperationOutput")
 fun File.toOperationOutput(): OperationOutput = parseFromJson<Map<String, OperationDescriptor>>()
+
 @JvmName("readPersistedQueryManifest")
 fun File.toPersistedQueryManifest(): PersistedQueryManifest = parseFromJson()
 
@@ -71,15 +82,20 @@ fun File.toPersistedQueryManifest(): PersistedQueryManifest = parseFromJson()
  */
 @JvmName("writeCodegenSchema")
 fun CodegenSchema.writeTo(file: File) = encodeToJson(file)
+
 @JvmName("writeIrOperations")
 fun IrOperations.writeTo(file: File) = this.encodeToJson(file)
+
 @JvmName("writeIrSchema")
 fun IrSchema.writeTo(file: File) = (this as DefaultIrSchema).encodeToJson(file)
+
 @JvmName("writeCodegenMetadata")
 fun CodegenMetadata.writeTo(file: File) = encodeToJson(file)
+
 @JvmName("writePersistedQueryManifest")
 fun PersistedQueryManifest.writeTo(file: File) = encodeToJson(file)
+
 @JvmName("writeOperationOutput")
 fun OperationOutput.writeTo(file: File) = this.encodeToJson(file)
 
-internal fun UsedCoordinates.writeTo(file: File) = encodeToJson(file)
+internal fun UsedCoordinates.writeTo(file: File) = file.writeText(prettyPrintJson.encodeToString (this))
