40aea47eb050426c0ab19eb624f447e59ee8676e
BoD <BoD@JRAF.org>
2024-04-12 11:27:46 +0200

    Bump to Kotlin 2.0.0-RC1

--- libraries/apollo-compose-paging-support-incubating/build.gradle.kts
+++ libraries/apollo-compose-paging-support-incubating/build.gradle.kts
@@ -13,22 +13,3 @@ dependencies {
   api(project(":apollo-compose-support-incubating"))
   api(libs.androidx.paging.compose)
 }
-
-//android {
-//  // TODO: compiling fails only with the debug variant currently, due to using a version of Kotlin non supported by Compose.
-//  // For now, disabling the debug variant works around the issue.
-//  variantFilter {
-//    ignore = name == "debug"
-//  }
-//}
-
-// Uncomment when using a non supported version of Kotlin
-// See https://developer.android.com/jetpack/androidx/releases/compose-kotlin
-//tasks.withType(KotlinCompile::class.java).configureEach {
-//  kotlinOptions {
-//    freeCompilerArgs += listOf(
-//        "-P",
-//        "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=1.9.0"
-//    )
-//  }
-//}
--- libraries/apollo-compose-support-incubating/build.gradle.kts
+++ libraries/apollo-compose-support-incubating/build.gradle.kts
@@ -13,14 +13,3 @@ dependencies {
   api(project(":apollo-runtime"))
   api(project(":apollo-normalized-cache"))
 }
-
-// Uncomment when using a non supported version of Kotlin
-// See https://developer.android.com/jetpack/androidx/releases/compose-kotlin
-//tasks.withType(KotlinCompile::class.java).configureEach {
-//  kotlinOptions {
-//    freeCompilerArgs += listOf(
-//        "-P",
-//        "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=1.9.0"
-//    )
-//  }
-//}
