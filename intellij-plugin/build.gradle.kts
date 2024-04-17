e89a428dd2125ece7068ab5315db7672993fafd6
BoD <BoD@JRAF.org>
2024-04-17 15:59:06 +0200

    Add zipSigner() to dependencies

--- intellij-plugin/build.gradle.kts
+++ intellij-plugin/build.gradle.kts
@@ -195,6 +195,7 @@ dependencies {
     instrumentationTools()
     pluginVerifier()
     testFramework(TestFrameworkType.Plugin.Java)
+    zipSigner()
   }
   implementation(project(":apollo-gradle-plugin-external"))
   implementation(project(":apollo-ast"))
