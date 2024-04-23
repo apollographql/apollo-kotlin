a118e3d9c9eff8fc49a0e2c39d8c3bb60f37a5fc
Martin Bonnin <martin@mbonnin.net>
2024-04-23 19:01:58 +0200

    release 4.0.0-beta.6 (#5844)

--- intellij-plugin/src/test/testData/migration/v3-to-v4/upgradeGradlePluginInBuildGradleKts_after.gradle.kts
+++ intellij-plugin/src/test/testData/migration/v3-to-v4/upgradeGradlePluginInBuildGradleKts_after.gradle.kts
@@ -2,16 +2,16 @@ plugins {
   java
   kotlin("jvm") version "1.6.10"
   id("com.apollographql.apollo3")
-  id("com.apollographql.apollo3") version "4.0.0-beta.5"
-  id("com.apollographql.apollo3") version "4.0.0-beta.5" apply false
-  // TODO: Update version to 4.0.0-beta.5
+  id("com.apollographql.apollo3") version "4.0.0-beta.6"
+  id("com.apollographql.apollo3") version "4.0.0-beta.6" apply false
+  // TODO: Update version to 4.0.0-beta.6
   id("com.apollographql.apollo3") version someClass.someConstant
-  // TODO: Update version to 4.0.0-beta.5
+  // TODO: Update version to 4.0.0-beta.6
   id("com.apollographql.apollo3") version "${someClass.someConstant}"
-  id("com.apollographql.apollo3") version "4.0.0-beta.5"
-  id("com.apollographql.apollo3") version "4.0.0-beta.5" apply false
-  // TODO: Update version to 4.0.0-beta.5
+  id("com.apollographql.apollo3") version "4.0.0-beta.6"
+  id("com.apollographql.apollo3") version "4.0.0-beta.6" apply false
+  // TODO: Update version to 4.0.0-beta.6
   id("com.apollographql.apollo3") version someClass.someConstant
-  // TODO: Update version to 4.0.0-beta.5
+  // TODO: Update version to 4.0.0-beta.6
   id("com.apollographql.apollo3") version "${someClass.someConstant}"
 }
