131596c81326d60ae5ebba3982da4b06f7626c21
BoD <BoD@JRAF.org>
2024-06-03 11:21:54 +0200

    Use apiVersion KOTLIN_1_9 in apollo-debug-server du to KSP 1

--- tests/sample-server/build.gradle.kts
+++ tests/sample-server/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
+
 plugins {
   id("org.jetbrains.kotlin.jvm")
   id("com.google.devtools.ksp")
@@ -26,4 +29,12 @@ dependencies {
 
 application {
   mainClass.set("com.apollographql.apollo.sample.server.MainKt")
+}
+
+// Can't use apiVersion KOTLIN_2_0 when using languageVersion KOTLIN_1_9, which is the case here because we're using KSP 1
+// TODO: move to KSP 2 and remove this when https://github.com/google/ksp/issues/1823 is resolved
+tasks.withType(KotlinCompilationTask::class.java).configureEach {
+  compilerOptions {
+    apiVersion.set(KotlinVersion.KOTLIN_1_9)
+  }
 }
