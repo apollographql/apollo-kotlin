3c48f3c4b757097185f9a8a7b2aa72f4521a5ae2
Eduard-Cristian Boloș <eduardb@users.noreply.github.com>
2024-06-04 16:08:17 +0200

    Add unit tests

--- libraries/apollo-api/src/commonTest/kotlin/test/BufferedSourceJsonReaderTest.kt
+++ libraries/apollo-api/src/commonTest/kotlin/test/BufferedSourceJsonReaderTest.kt
@@ -17,4 +17,70 @@ class BufferedSourceJsonReaderTest {
 
     assertEquals(json, number.value)
   }
+
+  @Test
+  fun intmin() {
+    val json = "-2147483648"
+
+    val jsonReader = BufferedSourceJsonReader(Buffer().writeUtf8(json))
+
+    val number = jsonReader.nextInt()
+
+    assertEquals(Int.MIN_VALUE, number)
+  }
+
+  @Test
+  fun intmax() {
+    val json = "2147483647"
+
+    val jsonReader = BufferedSourceJsonReader(Buffer().writeUtf8(json))
+
+    val number = jsonReader.nextInt()
+
+    assertEquals(Int.MAX_VALUE, number)
+  }
+
+  @Test
+  fun longmin() {
+    val json = "-9223372036854775808"
+
+    val jsonReader = BufferedSourceJsonReader(Buffer().writeUtf8(json))
+
+    val number = jsonReader.nextLong()
+
+    assertEquals(Long.MIN_VALUE, number)
+  }
+
+  @Test
+  fun longmax() {
+    val json = "9223372036854775807"
+
+    val jsonReader = BufferedSourceJsonReader(Buffer().writeUtf8(json))
+
+    val number = jsonReader.nextLong()
+
+    assertEquals(Long.MAX_VALUE, number)
+  }
+
+  @Test
+  fun doublemin() {
+    val json = "4.9E-324"
+
+    val jsonReader = BufferedSourceJsonReader(Buffer().writeUtf8(json))
+
+    val number = jsonReader.nextDouble()
+
+    assertEquals(Double.MIN_VALUE, number)
+  }
+
+  @Test
+  fun doublemax() {
+    val json = "1.7976931348623157E308"
+
+    val jsonReader = BufferedSourceJsonReader(Buffer().writeUtf8(json))
+
+    val number = jsonReader.nextDouble()
+
+    assertEquals(Double.MAX_VALUE, number)
+  }
 }
