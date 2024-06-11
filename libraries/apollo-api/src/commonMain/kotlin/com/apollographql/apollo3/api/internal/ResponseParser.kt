78ae284506e5bf44fd8f0dd1a59c81cbd1989730
Jan Duzinkiewicz <duzinkie@twitch.tv>
2024-06-11 16:56:33 -0700

    making not correctly ending json responses throw an exception instead of logging a warning, as per PR feedback

--- libraries/apollo-api/src/commonMain/kotlin/com/apollographql/apollo3/api/internal/ResponseParser.kt
+++ libraries/apollo-api/src/commonMain/kotlin/com/apollographql/apollo3/api/internal/ResponseParser.kt
@@ -8,6 +8,7 @@ import com.apollographql.apollo3.api.json.JsonReader
 import com.apollographql.apollo3.api.json.MapJsonReader
 import com.apollographql.apollo3.api.json.readAny
 import com.apollographql.apollo3.api.nullable
+import com.apollographql.apollo3.exception.JsonDataException
 import com.benasher44.uuid.uuid4
 import okio.use
 
@@ -40,7 +41,7 @@ internal object ResponseParser {
       jsonReader.endObject()
 
       if (jsonReader.peek() != JsonReader.Token.END_DOCUMENT) {
-        println("Apollo: extra tokens after payload")
+        throw JsonDataException("Expected END_DOCUMENT but was ${jsonReader.peek()}")
       }
 
       ApolloResponse.Builder(requestUuid = uuid4(), operation = operation, data = data).errors(errors)
