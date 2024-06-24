1a88fbac1b130a298eb03ed431b24a11eb87e173
BoD <BoD@JRAF.org>
2024-06-24 19:02:28 +0200

    Remove integration tests that were moved to the java-support repo

--- tests/jpms/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module com.example.app {
-    requires com.apollographql.apollo3.api;
-    requires com.apollographql.java.rx2;
-    requires com.apollographql.java.client;
-    requires kotlin.stdlib;
-}
