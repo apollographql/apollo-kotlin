d018d9944a433734a6665c1e6ab99230e23b7393
Benoit 'BoD' Lubek <BoD@JRAF.org>
2024-06-27 14:29:55 +0200

    Kotlin nightlies: filter on 2.0.20, and trigger workflow (#6003)
    * Get the latest 2.0.20 dev version
    
    * Run workflow

--- scripts/bump-kotlin-nightlies.main.kts
+++ scripts/bump-kotlin-nightlies.main.kts
@@ -6,7 +6,7 @@ import javax.xml.parsers.DocumentBuilderFactory
 
 fun main() {
   // Update versions in libraries.toml
-  val kotlinVersion = getLatestVersion("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/org/jetbrains/kotlin/kotlin-stdlib/maven-metadata.xml")
+  val kotlinVersion = getLatestVersion("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/org/jetbrains/kotlin/kotlin-stdlib/maven-metadata.xml", prefix = "2.0.20")
   val kspVersion = getLatestVersion("https://oss.sonatype.org/content/repositories/snapshots/com/google/devtools/ksp/com.google.devtools.ksp.gradle.plugin/maven-metadata.xml")
   File("gradle/libraries.toml").let { file ->
     file.writeText(
@@ -41,17 +41,29 @@ fun String.replaceVersion(key: String, version: String): String {
   return replace(Regex("""$key = ".*""""), """$key = "$version"""")
 }
 
-fun getLatestVersion(url: String): String {
-  return URL(url)
+fun getLatestVersion(url: String, prefix: String? = null): String {
+  val document = URL(url)
       .openConnection()
       .getInputStream().use { inputStream ->
         DocumentBuilderFactory.newInstance()
             .newDocumentBuilder()
             .parse(inputStream)
       }
-      .getElementsByTagName("latest")
-      .item(0)
-      .textContent
+  return if (prefix != null) {
+    document
+        .getElementsByTagName("version")
+        .let {
+          (0 until it.length)
+              .map { i -> it.item(i).textContent }
+              .filter { it.startsWith(prefix) }
+              .first() // Assumes they are sorted by most recent first, which is true on Kotlin's repo, false on Sonatype
+        }
+  } else {
+    document
+        .getElementsByTagName("latest")
+        .item(0)
+        .textContent
+  }
 }
 
 main()
