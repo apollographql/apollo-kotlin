5ac64f89b791ad13713006d8efd120914a62f5e2
Benoit 'BoD' Lubek <BoD@JRAF.org>
2024-06-27 09:30:37 +0200

    Add bump kotlin shadow job (#5999)
    * Add bump kotlin shadow job
    
    * Update job name
    
    * Fetch latest versions

---
+++ scripts/bump-kotlin-nightlies.main.kts
@@ -0,0 +1,57 @@
+#!/usr/bin/env kotlin
+
+import java.io.File
+import java.net.URL
+import javax.xml.parsers.DocumentBuilderFactory
+
+fun main() {
+  // Update versions in libraries.toml
+  val kotlinVersion = getLatestVersion("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/org/jetbrains/kotlin/kotlin-stdlib/maven-metadata.xml")
+  val kspVersion = getLatestVersion("https://oss.sonatype.org/content/repositories/snapshots/com/google/devtools/ksp/com.google.devtools.ksp.gradle.plugin/maven-metadata.xml")
+  File("gradle/libraries.toml").let { file ->
+    file.writeText(
+        file.readText()
+            .replaceVersion("kotlin-plugin", kotlinVersion)
+            .replaceVersion("kotlin-plugin-max", kotlinVersion)
+            .replaceVersion("kotlin-stdlib", kotlinVersion)
+            .replaceVersion("ksp", kspVersion)
+    )
+  }
+
+  // Uncomment Kotlin dev maven repository, add Sonatype snapshots repository
+  setOf(
+      File("gradle/repositories.gradle.kts"),
+      File("intellij-plugin/build.gradle.kts"),
+  )
+      .forEach { file ->
+        file.writeText(
+            file.readText()
+                .replace(
+                    """// maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/") }""",
+                    """
+                      maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/") }
+                      maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
+                    """.trimIndent()
+                )
+        )
+      }
+}
+
+fun String.replaceVersion(key: String, version: String): String {
+  return replace(Regex("""$key = ".*""""), """$key = "$version"""")
+}
+
+fun getLatestVersion(url: String): String {
+  return URL(url)
+      .openConnection()
+      .getInputStream().use { inputStream ->
+        DocumentBuilderFactory.newInstance()
+            .newDocumentBuilder()
+            .parse(inputStream)
+      }
+      .getElementsByTagName("latest")
+      .item(0)
+      .textContent
+}
+
+main()
