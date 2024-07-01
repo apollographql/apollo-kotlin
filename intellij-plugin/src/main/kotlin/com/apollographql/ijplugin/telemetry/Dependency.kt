71a2db7933b2481d5866c0c5b880478536247829
BoD <BoD@JRAF.org>
2024-07-01 16:56:45 +0200

    Adjust telemetry

--- intellij-plugin/src/main/kotlin/com/apollographql/ijplugin/telemetry/Dependency.kt
+++ intellij-plugin/src/main/kotlin/com/apollographql/ijplugin/telemetry/Dependency.kt
@@ -1,9 +1,13 @@
 package com.apollographql.ijplugin.telemetry
 
 import com.apollographql.ijplugin.util.MavenCoordinates
+import com.apollographql.ijplugin.util.apollo2
+import com.apollographql.ijplugin.util.apollo3
+import com.apollographql.ijplugin.util.apollo4
 
+@Suppress("KotlinConstantConditions")
 fun MavenCoordinates.toTelemetryProperty(): TelemetryProperty? = when {
-  group == "com.apollographql.apollo" || group == "com.apollographql.apollo3" -> TelemetryProperty.Dependency(group, artifact, version)
+  group == apollo2 || group == apollo3 || group == apollo4 -> TelemetryProperty.Dependency(group, artifact, version)
   group == "org.jetbrains.kotlin" && artifact == "kotlin-stdlib" -> TelemetryProperty.KotlinVersion(version)
   group == "androidx.compose.runtime" && artifact == "runtime" -> TelemetryProperty.ComposeVersion(version)
   else -> null
