5c5d0b069b53b78cb9836651745d432c057ffb84
BoD <BoD@JRAF.org>
2024-07-04 16:14:40 +0200

    Disable the Optimize Imports Refactoring Helper.

--- intellij-plugin/src/main/kotlin/com/apollographql/ijplugin/refactoring/migration/item/MigrationItemUsageInfo.kt
+++ intellij-plugin/src/main/kotlin/com/apollographql/ijplugin/refactoring/migration/item/MigrationItemUsageInfo.kt
@@ -1,6 +1,7 @@
 package com.apollographql.ijplugin.refactoring.migration.item
 
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiReference
 import com.intellij.usageView.UsageInfo
 
@@ -38,6 +39,25 @@ open class MigrationItemUsageInfo : UsageInfo {
 
   override fun isValid(): Boolean {
     return super.getElement() != null && super.isValid()
+  }
+
+  /**
+   * Taken from https://github.com/JetBrains/android/blob/b9cc41149734794ca6746711b1fb5a2fb6d06ec4/project-system-gradle/src/org/jetbrains/android/refactoring/MigrateToAndroidxProcessor.kt
+   *
+   * Verifies if one of the calls on the stack comes from the Optimize Imports Refactoring Helper.
+   * We check the last 5 elements to allow for some future flow changes.
+   *
+   * This avoids the helper kicking as it can remove imports we're trying to add.
+   */
+  private fun isKotlinOptimizerCall(): Boolean = Thread.currentThread().stackTrace
+      .take(5)
+      .map { it.className }
+      .any { it.contains("OptimizeImports", ignoreCase = true) }
+
+  override fun getFile(): PsiFile? = if (isKotlinOptimizerCall()) {
+    null
+  } else {
+    super.getFile()
   }
 }
 
