22a62be960c1159479b62930a8e50e9b13449470
Benoit 'BoD' Lubek <BoD@JRAF.org>
2024-07-22 11:12:50 +0200

    release 4.0.0-rc.2 (#6065)

--- intellij-plugin/src/test/testData/migration/v3-to-v4/upgradeGradlePluginInBuildGradleKts_after.gradle.kts
+++ intellij-plugin/src/test/testData/migration/v3-to-v4/upgradeGradlePluginInBuildGradleKts_after.gradle.kts
@@ -2,16 +2,16 @@ plugins {
   java
   kotlin("jvm") version "1.6.10"
   id("com.apollographql.apollo")
-  id("com.apollographql.apollo") version "4.0.0-rc.1"
-  id("com.apollographql.apollo") version "4.0.0-rc.1" apply false
-  // TODO: Update version to 4.0.0-rc.1
+  id("com.apollographql.apollo") version "4.0.0-rc.2"
+  id("com.apollographql.apollo") version "4.0.0-rc.2" apply false
+  // TODO: Update version to 4.0.0-rc.2
   id("com.apollographql.apollo") version someClass.someConstant
-  // TODO: Update version to 4.0.0-rc.1
+  // TODO: Update version to 4.0.0-rc.2
   id("com.apollographql.apollo") version "${someClass.someConstant}"
-  id("com.apollographql.apollo") version "4.0.0-rc.1"
-  id("com.apollographql.apollo") version "4.0.0-rc.1" apply false
-  // TODO: Update version to 4.0.0-rc.1
+  id("com.apollographql.apollo") version "4.0.0-rc.2"
+  id("com.apollographql.apollo") version "4.0.0-rc.2" apply false
+  // TODO: Update version to 4.0.0-rc.2
   id("com.apollographql.apollo") version someClass.someConstant
-  // TODO: Update version to 4.0.0-rc.1
+  // TODO: Update version to 4.0.0-rc.2
   id("com.apollographql.apollo") version "${someClass.someConstant}"
 }
