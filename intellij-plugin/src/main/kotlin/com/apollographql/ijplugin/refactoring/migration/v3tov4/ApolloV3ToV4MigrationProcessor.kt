3660af8e6b086e71882197f4f21eb11f651fea83
BoD <BoD@JRAF.org>
2024-07-22 11:11:38 +0200

    release 4.0.0-rc.2

--- intellij-plugin/src/main/kotlin/com/apollographql/ijplugin/refactoring/migration/v3tov4/ApolloV3ToV4MigrationProcessor.kt
+++ intellij-plugin/src/main/kotlin/com/apollographql/ijplugin/refactoring/migration/v3tov4/ApolloV3ToV4MigrationProcessor.kt
@@ -34,7 +34,7 @@ import com.intellij.openapi.project.Project
  */
 class ApolloV3ToV4MigrationProcessor(project: Project) : ApolloMigrationRefactoringProcessor(project) {
   companion object {
-    const val apollo4LatestVersion = "4.0.0-rc.1"
+    const val apollo4LatestVersion = "4.0.0-rc.2"
   }
 
   override val refactoringName = ApolloBundle.message("ApolloV3ToV4MigrationProcessor.title")
