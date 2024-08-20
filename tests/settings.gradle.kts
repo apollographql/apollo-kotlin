c256009a90e673fab82dad0501c1e150db578117
BoD <BoD@JRAF.org>
2024-08-20 10:10:29 +0200

    Bump develocity

--- tests/settings.gradle.kts
+++ tests/settings.gradle.kts
@@ -3,7 +3,7 @@ pluginManagement {
 }
 
 plugins {
-  id("com.gradle.develocity") version "3.17.6" // sync with libraries.toml
+  id("com.gradle.develocity") version "3.18" // sync with libraries.toml
   id("com.gradle.common-custom-user-data-gradle-plugin") version "2.0.2"
   id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
