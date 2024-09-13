01f9542d57cdf0faebe1761392cf3a1032e942a8
BoD <BoD@JRAF.org>
2024-09-13 16:00:54 +0200

    `analyze` must be called from a read action and not from the EDT

--- intellij-plugin/src/main/kotlin/com/apollographql/ijplugin/util/Analysis.kt
+++ intellij-plugin/src/main/kotlin/com/apollographql/ijplugin/util/Analysis.kt
@@ -1,23 +1,44 @@
 package com.apollographql.ijplugin.util
 
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.runReadAction
+import org.jetbrains.kotlin.analysis.api.KaSession
 import org.jetbrains.kotlin.analysis.api.analyze
 import org.jetbrains.kotlin.analysis.api.types.KaClassType
+import org.jetbrains.kotlin.idea.util.application.isUnitTestMode
 import org.jetbrains.kotlin.psi.KtDeclaration
+import org.jetbrains.kotlin.psi.KtElement
 import org.jetbrains.kotlin.psi.KtExpression
+import java.util.concurrent.Callable
 
-fun KtExpression.canBeNull(): Boolean? = analyze(this) {
+fun KtExpression.canBeNull(): Boolean? = runAnalyze(this) {
   expressionType?.nullability?.isNullable
 }
 
-fun KtExpression.className(): String? = analyze(this) {
+fun KtExpression.className(): String? = runAnalyze(this) {
   expressionType.cast<KaClassType>()?.classId?.asFqNameString()
 }
 
-fun KtDeclaration.className(): String? = analyze(this) {
+fun KtDeclaration.className(): String? = runAnalyze(this) {
   returnType.cast<KaClassType>()?.classId?.asFqNameString()
 }
 
-fun KtDeclaration.typeArgumentClassName(index: Int): String? = analyze(this) {
+fun KtDeclaration.typeArgumentClassName(index: Int): String? = runAnalyze(this) {
   returnType.cast<KaClassType>()?.typeArguments?.getOrNull(index)?.type?.cast<KaClassType>()?.classId?.asFqNameString()
 }
 
+/**
+ * `analyze` must be called from a read action and not from the EDT.
+ * In the IDE that's already the case but not in tests.
+ */
+private inline fun <R> runAnalyze(useSiteElement: KtElement, crossinline action: KaSession.() -> R): R {
+  return if (isUnitTestMode()) {
+    ApplicationManager.getApplication().executeOnPooledThread(Callable {
+      runReadAction {
+        analyze(useSiteElement, action)
+      }
+    }).get()
+  } else {
+    analyze(useSiteElement, action)
+  }
+}
