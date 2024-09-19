ccabd361af5b2ac78d93f289046ba1ff688ca5d5
BoD <BoD@JRAF.org>
2024-09-19 19:16:21 +0200

    Bump Kotlin to 2.1.0-Beta1
    (cherry picked from commit 5cbb969d9dc04ea137abeb26bd3f1d35b6e12211)

--- tests/sample-server/build.gradle.kts
+++ tests/sample-server/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
-
 plugins {
   id("org.jetbrains.kotlin.jvm")
   id("com.google.devtools.ksp")
@@ -8,13 +6,7 @@ plugins {
   id("application")
 }
 
-apolloTest(
-    // Can't use apiVersion KOTLIN_2_0 when using languageVersion KOTLIN_1_9, which is the case here because we're using KSP 1
-    // TODO: move to KSP 2 and remove this when https://github.com/google/ksp/issues/1823 is resolved
-    kotlinCompilerOptions = KotlinCompilerOptions(
-        version = KotlinVersion.KOTLIN_1_9,
-    )
-)
+apolloTest()
 
 dependencies {
   implementation(libs.apollo.annotations)
@@ -39,4 +31,3 @@ apolloExecution {
 application {
   mainClass.set("com.apollographql.apollo.sample.server.MainKt")
 }
-
