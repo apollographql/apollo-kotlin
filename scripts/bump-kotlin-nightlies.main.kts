426364ea73ae178c3b3a762bd610f844023da97d
Benoit 'BoD' Lubek <BoD@JRAF.org>
2024-09-23 10:04:21 +0200

    Make the Kotlin nightlies test against 2.1.0 (#6157)

--- scripts/bump-kotlin-nightlies.main.kts
+++ scripts/bump-kotlin-nightlies.main.kts
@@ -8,7 +8,7 @@ val BRANCH_NAME = "kotlin-nightlies"
 
 fun bumpVersions() {
   val kotlinVersion =
-    getLatestVersion("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/org/jetbrains/kotlin/kotlin-stdlib/maven-metadata.xml", prefix = "2.0.20")
+    getLatestVersion("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/org/jetbrains/kotlin/kotlin-stdlib/maven-metadata.xml", prefix = "2.1.0")
   val kspVersion =
     getLatestVersion("https://oss.sonatype.org/content/repositories/snapshots/com/google/devtools/ksp/com.google.devtools.ksp.gradle.plugin/maven-metadata.xml")
   File("gradle/libraries.toml").let { file ->
