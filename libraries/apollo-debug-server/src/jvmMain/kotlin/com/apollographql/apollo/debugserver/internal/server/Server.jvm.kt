5f4dba52eaab900b35ecd26a898834ccec978c3c
Benoit 'BoD' Lubek <BoD@JRAF.org>
2024-10-29 10:57:32 +0100

    Remove Ktor dependency from apollo-debug-server JVM (#6224)

--- libraries/apollo-debug-server/src/jvmMain/kotlin/com/apollographql/apollo/debugserver/internal/server/Server.jvm.kt
+++ libraries/apollo-debug-server/src/jvmMain/kotlin/com/apollographql/apollo/debugserver/internal/server/Server.jvm.kt
@@ -1,30 +1,49 @@
 package com.apollographql.apollo.debugserver.internal.server
 
 import com.apollographql.apollo.ApolloClient
-import com.apollographql.apollo.debugserver.internal.graphql.ApolloDebugServerExecutableSchemaBuilder
-import com.apollographql.apollo.debugserver.internal.graphql.Query
-import com.apollographql.execution.ktor.apolloModule
-import io.ktor.server.cio.CIO
-import io.ktor.server.engine.embeddedServer
+import com.apollographql.apollo.debugserver.internal.graphql.GraphQL
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import java.net.ServerSocket
+import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicReference
 
-internal actual fun createServer(apolloClients: AtomicReference<Map<ApolloClient, String>>): Server = KtorServer(apolloClients)
+internal actual fun createServer(
+    apolloClients: AtomicReference<Map<ApolloClient, String>>,
+): Server = JvmServer(apolloClients)
 
-internal class KtorServer(private val apolloClients: AtomicReference<Map<ApolloClient, String>>) : Server {
-  private val server = embeddedServer(CIO, port = 8081) {
-    val schema = ApolloDebugServerExecutableSchemaBuilder()
-        .queryRoot {
-          Query(apolloClients)
-        }.build()
+private class JvmServer(
+    apolloClients: AtomicReference<Map<ApolloClient, String>>,
+) : Server {
+  private var serverSocket: ServerSocket? = null
+  private val dispatcher = Executors.newCachedThreadPool().asCoroutineDispatcher()
+  private val coroutineScope = CoroutineScope(SupervisorJob() + dispatcher)
 
-    apolloModule(schema)
-  }
+  private val graphQL = GraphQL(apolloClients)
 
   override fun start() {
-    server.start()
+    if (serverSocket != null) error("Already started")
+    coroutineScope.launch {
+      val localServerSocket = ServerSocket(8081)
+      this@JvmServer.serverSocket = localServerSocket
+      while (true) {
+        val clientSocket = try {
+          localServerSocket.accept()
+        } catch (_: Exception) {
+          // Server socket has been closed (stop() was called)
+          break
+        }
+        launch { handleClient(graphQL, clientSocket, clientSocket.inputStream, clientSocket.outputStream) }
+      }
+    }
   }
 
   override fun stop() {
-    server.stop()
+    runCatching { serverSocket?.close() }
+    coroutineScope.cancel()
+    dispatcher.close()
   }
 }
