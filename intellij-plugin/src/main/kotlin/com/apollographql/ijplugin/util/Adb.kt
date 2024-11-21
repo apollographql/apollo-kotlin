a4210425c6b79a878da8b5fd9761da0f8b34a662
BoD <BoD@JRAF.org>
2024-11-21 13:26:18 +0100

    Don't reference AdbShellCommandsUtil.executeCommandBlocking that's been removed

--- intellij-plugin/src/main/kotlin/com/apollographql/ijplugin/util/Adb.kt
+++ intellij-plugin/src/main/kotlin/com/apollographql/ijplugin/util/Adb.kt
@@ -2,10 +2,11 @@ package com.apollographql.ijplugin.util
 
 import com.android.tools.idea.adb.AdbShellCommandResult
 import com.android.tools.idea.adb.AdbShellCommandsUtil
+import kotlinx.coroutines.runBlocking
 
 fun AdbShellCommandsUtil.execute(command: String): AdbShellCommandResult {
   logd("Executing adb shell command: '$command'")
-  val result = executeCommandBlocking(command)
+  val result = runBlocking { executeCommand(command) }
   logd("adb shell command result: isError=${result.isError}, isEmpty=${result.isEmpty}, output=${result.output.joinToString("\n")}")
   return result
 }
