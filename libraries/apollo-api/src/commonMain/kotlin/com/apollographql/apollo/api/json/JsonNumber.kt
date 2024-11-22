e8da44db9781c4079febdeccd4e5f6bd4bcf1f08
Joseph Van der Wee <jvanderwee@users.noreply.github.com>
2024-11-22 18:23:15 +0000

    Override JsonNumber.toString() (#6273)
    * Override JsonNumber.toString()
    
    * Update apollo-api.api
    
    * update klib ABI dump
    
    ---------
    
    Co-authored-by: Martin Bonnin <martin@mbonnin.net>

--- libraries/apollo-api/src/commonMain/kotlin/com/apollographql/apollo/api/json/JsonNumber.kt
+++ libraries/apollo-api/src/commonMain/kotlin/com/apollographql/apollo/api/json/JsonNumber.kt
@@ -3,4 +3,6 @@ package com.apollographql.apollo.api.json
 /**
  * A simple wrapper class that can be put in [Map] to indicate an arbitrary precision json number
  */
-class JsonNumber(val value: String)
+class JsonNumber(val value: String) {
+  override fun toString(): String = value
+}
