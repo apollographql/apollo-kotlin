7785334592bd8ca9557e01c838af548bbb8e258d
Martin Bonnin <martin@mbonnin.net>
2024-11-27 17:06:37 +0100

    Remove CloseableBackgroundDispatcher

--- libraries/apollo-runtime/src/jsMain/kotlin/com/apollographql/apollo/internal/dispatchers.js.kt
+++ libraries/apollo-runtime/src/jsMain/kotlin/com/apollographql/apollo/internal/dispatchers.js.kt
@@ -7,10 +7,3 @@ import okio.Closeable
 internal actual val defaultDispatcher: CoroutineDispatcher
   get() = Dispatchers.Default
 
-// We can't use threads in JS, so just fallback to defaultDispatcher()
-internal actual class CloseableSingleThreadDispatcher : Closeable {
-  actual val coroutineDispatcher: CoroutineDispatcher = Dispatchers.Default
-
-  actual override fun close() {
-  }
-}
