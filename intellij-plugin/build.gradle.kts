6c950b3868bffeb0ef2d4f36bd45ad165db98323
BoD <BoD@JRAF.org>
2024-11-28 09:40:00 +0100

    Use stable versions

--- intellij-plugin/build.gradle.kts
+++ intellij-plugin/build.gradle.kts
@@ -27,8 +27,9 @@ commonSetup()
 // XXX: this should use the settings repositories instead
 repositories {
   // Uncomment this one to use the Kotlin "dev" repository
-  maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/") }
-  maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
+  // maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/") }
+  // Uncomment this one to use the Sonatype OSSRH snapshots repository
+  // maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
   mavenCentral()
 
   intellijPlatform {
