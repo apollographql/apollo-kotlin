d8c857318b5d802fcfa42b8411448f929e09d19d
Martin Bonnin <martin@mbonnin.net>
2024-12-09 16:20:11 +0100

    Fix classloader caching. Many thanks @scana for catching this (#6309)

--- libraries/apollo-gradle-plugin-external/src/main/kotlin/com/apollographql/apollo/gradle/internal/ApolloBuildService.kt
+++ libraries/apollo-gradle-plugin-external/src/main/kotlin/com/apollographql/apollo/gradle/internal/ApolloBuildService.kt
@@ -3,17 +3,19 @@ package com.apollographql.apollo.gradle.internal
 import org.gradle.api.file.FileCollection
 import org.gradle.api.services.BuildService
 import org.gradle.api.services.BuildServiceParameters
+import java.net.URI
 import java.net.URL
 import java.net.URLClassLoader
 
 abstract class ApolloBuildService: BuildService<BuildServiceParameters.None>, AutoCloseable {
-  private val classloaders = mutableMapOf<Array<URL>, ClassLoader>()
+  private val classloaders = mutableMapOf<List<URI>, ClassLoader>()
 
+  @Synchronized
   fun classloader(classpath: FileCollection): ClassLoader {
-    val urls = classpath.map { it.toURI().toURL() }.toTypedArray()
+    val urls = classpath.map { it.toURI() }
     return classloaders.getOrPut(urls) {
       URLClassLoader(
-          urls,
+          urls.map { it.toURL() }.toTypedArray(),
           ClassLoader.getPlatformClassLoader()
       )
     }
