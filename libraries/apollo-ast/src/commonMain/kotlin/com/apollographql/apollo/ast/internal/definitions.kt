9af60c942002763c255bf3afcabd51bd158ec443
Martin Bonnin <martin@mbonnin.net>
2024-12-09 19:39:59 +0100

    Make deprecation.reason non-nullable (#6311)
    * Make deprecation.reason non-nullable
    
    * update apolloDump
    
    * fix test fixture

--- libraries/apollo-ast/src/commonMain/kotlin/com/apollographql/apollo/ast/internal/definitions.kt
+++ libraries/apollo-ast/src/commonMain/kotlin/com/apollographql/apollo/ast/internal/definitions.kt
@@ -310,7 +310,7 @@ internal val builtinsDefinitionsStr = """
   directive @include(if: Boolean!) on FIELD | FRAGMENT_SPREAD | INLINE_FRAGMENT
 
   directive @deprecated(
-    reason: String = "No longer supported"
+    reason: String! = "No longer supported"
   ) on FIELD_DEFINITION | ARGUMENT_DEFINITION | INPUT_FIELD_DEFINITION | ENUM_VALUE
 
   directive @defer(
