3527e1c21655709c12c06d0fa260fdfbcb8092d9
BoD <BoD@JRAF.org>
2024-12-17 16:08:43 +0100

    Fix missed test

--- tests/defer/src/jvmTest/kotlin/test/DeferJvmTest.kt
+++ tests/defer/src/jvmTest/kotlin/test/DeferJvmTest.kt
@@ -4,11 +4,11 @@ import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.cache.http.HttpFetchPolicy
 import com.apollographql.apollo.cache.http.httpCache
 import com.apollographql.apollo.cache.http.httpFetchPolicy
-import com.apollographql.mockserver.MockServer
-import com.apollographql.mockserver.enqueueMultipart
 import com.apollographql.apollo.mpp.currentTimeMillis
 import com.apollographql.apollo.testing.awaitElement
 import com.apollographql.apollo.testing.internal.runTest
+import com.apollographql.mockserver.MockServer
+import com.apollographql.mockserver.enqueueMultipart
 import defer.WithFragmentSpreadsQuery
 import defer.fragment.ComputerFields
 import defer.fragment.ScreenFields
@@ -60,11 +60,8 @@ class DeferJvmTest {
     }
 
     val jsonList = listOf(
-        """{"data":{"computers":[{"__typename":"Computer","id":"Computer1"},{"__typename":"Computer","id":"Computer2"}]},"hasNext":true}""",
-        """{"incremental":[{"data":{"cpu":"386","year":1993,"screen":{"__typename":"Screen","resolution":"640x480"}},"path":["computers",0]}],"hasNext":true}""",
-        """{"incremental":[{"data":{"cpu":"486","year":1996,"screen":{"__typename":"Screen","resolution":"800x600"}},"path":["computers",1]}],"hasNext":true}""",
-        """{"incremental":[{"data":{"isColor":false},"path":["computers",0,"screen"],"label":"a"}],"hasNext":true}""",
-        """{"incremental":[{"data":{"isColor":true},"path":["computers",1,"screen"],"label":"a"}],"hasNext":false}""",
+        """{"data":{"computers":[{"__typename":"Computer","id":"Computer1"},{"__typename":"Computer","id":"Computer2"}]},"pending":[{"id":"0","path":["computers",0]},{"id":"1","path":["computers",1]}],"hasNext":true}""",
+        """{"hasNext":true,"pending":[{"id":"2","path":["computers",0,"screen"],"label":"a"},{"id":"3","path":["computers",1,"screen"],"label":"a"}],"incremental":[{"data":{"cpu":"386","year":1993,"screen":{"__typename":"Screen","resolution":"640x480"}},"id":"0"},{"data":{"cpu":"486","year":1996,"screen":{"__typename":"Screen","resolution":"800x600"}},"id":"1"},{"data":{"isColor":false},"id":"2"},{"data":{"isColor":true},"id":"3"}],"completed":[{"id":"0"},{"id":"1"},{"id":"2"},{"id":"3"}]}""",
     )
 
     for ((index, json) in jsonList.withIndex()) {
@@ -83,10 +80,14 @@ class DeferJvmTest {
         listOf(
             WithFragmentSpreadsQuery.Computer("Computer", "Computer1", ComputerFields("386", 1993,
                 ComputerFields.Screen("Screen", "640x480",
-                    ScreenFields(false)))),
+                    ScreenFields(false)
+                )
+            )
+            ),
             WithFragmentSpreadsQuery.Computer("Computer", "Computer2", ComputerFields("486", 1996,
-                ComputerFields.Screen("Screen", "800x600",
-                    ScreenFields(true)))),
+                ComputerFields.Screen("Screen", "800x600", ScreenFields(true))
+            )
+            ),
         )
     )
 
