bb9494c783fa4c6dab3611c3dcb2863a662fc5b9
Martin Bonnin <martin@mbonnin.net>
2025-01-15 17:49:08 +0100

    update comment

--- tests/build.gradle.kts
+++ tests/build.gradle.kts
@@ -4,7 +4,8 @@ buildscript {
   }
   repositories {
     /*
-     * This duplicates the repositories in
+     * This duplicates the repositories in repositories.gradle.kts but I haven't found a way to make it work otherwise
+     * See https://github.com/gradle/gradle/issues/32045
      */
     mavenCentral()
     google()
