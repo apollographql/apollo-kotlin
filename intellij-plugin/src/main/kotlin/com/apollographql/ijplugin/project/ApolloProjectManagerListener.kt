99f863d65afd3722c6fa40e5c7d0b48baef85919
BoD <BoD@JRAF.org>
2025-01-20 16:25:55 +0100

    Mark the Gradle plugin as optional, and move all components specific to Apollo Kotlin to the Kotlin plugin dependency file.

--- intellij-plugin/src/main/kotlin/com/apollographql/ijplugin/project/ApolloProjectManagerListener.kt
+++ intellij-plugin/src/main/kotlin/com/apollographql/ijplugin/project/ApolloProjectManagerListener.kt
@@ -9,6 +9,8 @@ import com.apollographql.ijplugin.settings.ProjectSettingsService
 import com.apollographql.ijplugin.studio.fieldinsights.FieldInsightsService
 import com.apollographql.ijplugin.studio.sandbox.SandboxService
 import com.apollographql.ijplugin.telemetry.TelemetryService
+import com.apollographql.ijplugin.util.isGradlePluginPresent
+import com.apollographql.ijplugin.util.isKotlinPluginPresent
 import com.apollographql.ijplugin.util.isLspAvailable
 import com.apollographql.ijplugin.util.logd
 import com.intellij.openapi.components.service
@@ -23,15 +25,18 @@ internal class ApolloProjectManagerListener : ProjectManagerListener {
 
     // Initialize all services on project open.
     // But wait for 'smart mode' to do it.
+    // Most of these services can't operate without the Kotlin and Gradle plugins (e.g. in RustRover).
     DumbService.getInstance(project).runWhenSmart {
       logd("apolloVersion=" + project.apolloProjectService.apolloVersion)
-      project.service<ApolloCodegenService>()
-      project.service<GraphQLConfigService>()
-      project.service<GradleToolingModelService>()
-      project.service<ProjectSettingsService>()
-      project.service<SandboxService>()
-      project.service<FieldInsightsService>()
-      project.service<TelemetryService>()
+      if (isKotlinPluginPresent && isGradlePluginPresent) {
+        project.service<ApolloCodegenService>()
+        project.service<GraphQLConfigService>()
+        project.service<GradleToolingModelService>()
+        project.service<ProjectSettingsService>()
+        project.service<SandboxService>()
+        project.service<FieldInsightsService>()
+        project.service<TelemetryService>()
+      }
       if (isLspAvailable) {
         project.service<ApolloLspProjectService>()
         application.service<ApolloLspAppService>()
