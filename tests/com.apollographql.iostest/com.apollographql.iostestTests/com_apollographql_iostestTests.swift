35231ce56071226bb320da639c1bbef2a8ab9e57
Martin Bonnin <martin@mbonnin.net>
2025-03-04 18:01:57 +0100

    add iOS test

---
+++ tests/com.apollographql.iostest/com.apollographql.iostestTests/com_apollographql_iostestTests.swift
@@ -0,0 +1,25 @@
+import Testing
+import shared_framework
+@testable import com_apollographql_iostest
+
+struct com_apollographql_iostestTests {
+
+    final class AuthenticationInterceptor: Apollo_runtimeHttpInterceptor {
+
+        func intercept(request: HttpRequest, chain: Apollo_runtimeHttpInterceptorChain) async throws -> HttpResponse {
+            //throw DefaultApolloException(message: "interceptor error", cause: nil)
+            try await chain.proceed(request: request)
+        }
+        
+        func dispose() {
+            // No op
+        }
+    }
+
+    @Test func example() async throws {
+        try MainKt.testInterceptor(interceptor: AuthenticationInterceptor())
+    }
+
+}
+
+extension DefaultApolloException: Swift.Error {}
