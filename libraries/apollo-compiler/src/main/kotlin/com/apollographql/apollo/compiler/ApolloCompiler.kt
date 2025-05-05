4af21484bd1ea4383416d9168b3c397fd3b6ec21
Benoit 'BoD' Lubek <BoD@JRAF.org>
2025-05-05 16:48:24 +0200

    Call DocumentTransform.transform after processing (#6510)

--- libraries/apollo-compiler/src/main/kotlin/com/apollographql/apollo/compiler/ApolloCompiler.kt
+++ libraries/apollo-compiler/src/main/kotlin/com/apollographql/apollo/compiler/ApolloCompiler.kt
@@ -16,7 +16,6 @@ import com.apollographql.apollo.ast.Issue
 import com.apollographql.apollo.ast.ParserOptions
 import com.apollographql.apollo.ast.QueryDocumentMinifier
 import com.apollographql.apollo.ast.Schema
-import com.apollographql.apollo.ast.UnknownDirective
 import com.apollographql.apollo.ast.UnusedFragment
 import com.apollographql.apollo.ast.UnusedVariable
 import com.apollographql.apollo.ast.builtinForeignSchemas
@@ -275,7 +274,7 @@ object ApolloCompiler {
     val operations = definitions.filterIsInstance<GQLOperationDefinition>().map {
       var operation = addRequiredFields(it, addTypename, schema, fragmentDefinitions)
       if (documentTransform != null) {
-        operation = documentTransform.transform(schema, it)
+        operation = documentTransform.transform(schema, operation)
       }
       if (schema.directiveDefinitions.containsKey(Schema.DISABLE_ERROR_PROPAGATION)
           && schema.schemaDefinition?.directives?.any { schema.originalDirectiveName(it.name) == Schema.CATCH_BY_DEFAULT } == true) {
