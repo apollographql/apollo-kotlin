b24cc1db0c49275c94283333e869e2564bbae856
Martin Bonnin <martin@mbonnin.net>
2025-05-13 19:45:38 +0200

    fix task name

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,7 @@ tasks.register("ciPublishRelease") {
 
 tasks.register("ciTestsGradle") {
   description = "Execute the Gradle tests (slow)"
-  dependsOn(":apollo-gradle-plugin:allTests")
+  dependsOn(":apollo-gradle-plugin:test")
 }
 
 tasks.register("ciTestsNoGradle") {
