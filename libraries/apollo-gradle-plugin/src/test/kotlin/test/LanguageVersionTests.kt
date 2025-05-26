1eb7eea7677f1809024fc09e6e0fd8e5e7130a4a
BoD <BoD@JRAF.org>
2025-05-26 11:46:12 +0200

    Use compilerOptions instead of deprecated kotlinOptions

--- libraries/apollo-gradle-plugin/src/test/kotlin/test/LanguageVersionTests.kt
+++ libraries/apollo-gradle-plugin/src/test/kotlin/test/LanguageVersionTests.kt
@@ -15,7 +15,7 @@ import kotlin.test.assertEquals
 class LanguageVersionTests {
   @Test
   fun `compiling with 1_5 features with Kotlin 1_5 is working`() {
-    withProject(kotlinLanguageVersion = "1.5", apolloLanguageVersion = "1.5") { dir ->
+    withProject(kotlinLanguageVersion = "org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_5", apolloLanguageVersion = "1.5") { dir ->
       dir.disableIsolatedProjects() // old KGP versions do not support isolated projects
       TestUtils.executeGradleWithVersion(dir, "8.10", ":assemble").apply {
         assertEquals(TaskOutcome.SUCCESS, task(":assemble")!!.outcome)
@@ -25,7 +25,7 @@ class LanguageVersionTests {
 
   @Test
   fun `compiling with 1_5 features with Kotlin 1_4 is not working`() {
-    withProject(kotlinLanguageVersion = "1.4", apolloLanguageVersion = "1.5") { dir ->
+    withProject(kotlinLanguageVersion = "org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_4", apolloLanguageVersion = "1.5") { dir ->
       try {
         TestUtils.executeGradleWithVersion(dir, "8.10", ":assemble")
         Assert.fail("Compiling with incompatible languageVersion should fail")
@@ -37,7 +37,7 @@ class LanguageVersionTests {
 
   @Test
   fun `using bogus languageVersion fails`() {
-    withProject(kotlinLanguageVersion = "1.5", apolloLanguageVersion = "3.14") { dir ->
+    withProject(kotlinLanguageVersion = "org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_5", apolloLanguageVersion = "3.14") { dir ->
       try {
         TestUtils.executeGradleWithVersion(dir, "8.10", ":assemble")
         Assert.fail("Compiling with incompatible languageVersion should fail")
@@ -98,9 +98,9 @@ class LanguageVersionTests {
   ): String {
     val kotlinConfiguration = """
       tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-        kotlinOptions {
-          ${if (kotlinLanguageVersion == null) "" else "languageVersion = \"$kotlinLanguageVersion\""}
-          ${if (kotlinApiVersion == null) "" else "apiVersion = \"$kotlinApiVersion\""}
+        compilerOptions {
+          ${if (kotlinLanguageVersion == null) "" else "languageVersion.set($kotlinLanguageVersion)"}
+          ${if (kotlinApiVersion == null) "" else "apiVersion.set(\"$kotlinApiVersion\")"}
         }
       }      
       """.trimIndent()
