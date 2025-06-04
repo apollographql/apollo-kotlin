21036877ba8513273bac6da769d7c500e24422c6
Martin Bonnin <martin@mbonnin.net>
2025-06-04 17:45:28 +0200

    mark more things as experimental

--- libraries/apollo-compiler/src/main/kotlin/com/apollographql/apollo/compiler/codegen/SchemaAndOperationsLayout.kt
+++ libraries/apollo-compiler/src/main/kotlin/com/apollographql/apollo/compiler/codegen/SchemaAndOperationsLayout.kt
@@ -1,10 +1,14 @@
 package com.apollographql.apollo.compiler.codegen
 
+import com.apollographql.apollo.annotations.ApolloExperimental
+
+@ApolloExperimental
 interface CommonLayout {
   fun className(name: String): String
   fun propertyName(name: String): String
 }
 
+@ApolloExperimental
 interface SchemaLayout : CommonLayout {
   fun schemaPackageName(): String
   fun schemaTypeName(schemaTypeName: String): String
@@ -13,10 +17,12 @@ interface SchemaLayout : CommonLayout {
   fun paginationName(): String
 }
 
+@ApolloExperimental
 interface OperationsLayout: CommonLayout {
   fun executableDocumentPackageName(filePath: String?): String
   fun operationName(name: String, capitalizedOperationType: String): String
   fun fragmentName(name: String): String
 }
 
+@ApolloExperimental
 interface SchemaAndOperationsLayout : SchemaLayout, OperationsLayout
