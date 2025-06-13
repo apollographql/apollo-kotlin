35fe9085ad17ad6cd41f03e43533f4f5ee95c4c8
Martin Bonnin <martin@mbonnin.net>
2025-06-13 16:03:12 +0200

    remove unused file

--- libraries/apollo-gradle-plugin/src/main/kotlin/com/apollographql/apollo/gradle/internal/ApolloBuildService.kt
+++
@@ -1,8 +0,0 @@
-package com.apollographql.apollo.gradle.internal
-
-import org.gradle.api.file.FileCollection
-import org.gradle.api.services.BuildService
-import org.gradle.api.services.BuildServiceParameters
-import java.net.URI
-import java.net.URLClassLoader
-
