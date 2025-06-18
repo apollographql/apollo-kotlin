d9c48a60fc8d54421fc700ea9eb378a8e1992e5c
BoD <BoD@JRAF.org>
2025-06-18 11:26:50 +0200

    release 4.3.1

--- intellij-plugin/src/test/testData/migration/v3-to-v4/upgradeGradlePluginInBuildGradleKts_after.gradle.kts
+++ intellij-plugin/src/test/testData/migration/v3-to-v4/upgradeGradlePluginInBuildGradleKts_after.gradle.kts
@@ -2,16 +2,16 @@ plugins {
   java
   kotlin("jvm") version "1.6.10"
   id("com.apollographql.apollo")
-  id("com.apollographql.apollo") version "4.3.0"
-  id("com.apollographql.apollo") version "4.3.0" apply false
-  // TODO: Update version to 4.3.0
+  id("com.apollographql.apollo") version "4.3.1"
+  id("com.apollographql.apollo") version "4.3.1" apply false
+  // TODO: Update version to 4.3.1
   id("com.apollographql.apollo") version someClass.someConstant
-  // TODO: Update version to 4.3.0
+  // TODO: Update version to 4.3.1
   id("com.apollographql.apollo") version "${someClass.someConstant}"
-  id("com.apollographql.apollo") version "4.3.0"
-  id("com.apollographql.apollo") version "4.3.0" apply false
-  // TODO: Update version to 4.3.0
+  id("com.apollographql.apollo") version "4.3.1"
+  id("com.apollographql.apollo") version "4.3.1" apply false
+  // TODO: Update version to 4.3.1
   id("com.apollographql.apollo") version someClass.someConstant
-  // TODO: Update version to 4.3.0
+  // TODO: Update version to 4.3.1
   id("com.apollographql.apollo") version "${someClass.someConstant}"
 }
