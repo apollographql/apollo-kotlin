9964f42dc7b002e4c68e6940e6d3b79175145716
Martin Bonnin <martin@mbonnin.net>
2025-07-30 11:24:12 +0200

    Also sort the implemented interfaces

--- libraries/apollo-ast/src/commonMain/kotlin/com/apollographql/apollo/ast/internal/semanticEquals.kt
+++ libraries/apollo-ast/src/commonMain/kotlin/com/apollographql/apollo/ast/internal/semanticEquals.kt
@@ -22,12 +22,12 @@ internal fun GQLNode.toSemanticSdl(): String {
   return transform2 { node ->
     when (node) {
       is GQLInputValueDefinition -> node.copy(description = null)
-      is GQLObjectTypeDefinition -> node.copy(description = null, fields = node.fields.sortedBy { it.name })
+      is GQLObjectTypeDefinition -> node.copy(description = null, fields = node.fields.sortedBy { it.name }, implementsInterfaces = node.implementsInterfaces.sortedBy { it })
       is GQLScalarTypeDefinition -> node.copy(description = null)
       is GQLInputObjectTypeDefinition -> node.copy(description = null, inputFields = node.inputFields.sortedBy { it.name })
       is GQLUnionTypeDefinition -> node.copy(description = null, memberTypes = node.memberTypes.sortedBy { it.name })
       is GQLEnumTypeDefinition -> node.copy(description = null, enumValues = node.enumValues.sortedBy { it.name })
-      is GQLInterfaceTypeDefinition -> node.copy(description = null, fields = node.fields.sortedBy { it.name })
+      is GQLInterfaceTypeDefinition -> node.copy(description = null, fields = node.fields.sortedBy { it.name }, implementsInterfaces = node.implementsInterfaces.sortedBy { it })
       is GQLFieldDefinition -> node.copy(description = null, arguments = node.arguments.sortedBy { it.name })
       is GQLDirectiveDefinition -> node.copy(description = null, arguments = node.arguments.sortedBy { it.name })
       is GQLEnumValueDefinition -> node.copy(description = null)
