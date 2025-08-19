77388646b852928d0ce27dd21e74c335d6b26395
BoD <BoD@JRAF.org>
2025-08-19 10:45:18 +0200

    Don't make toAny public, in fact remove it since it's unused.

--- libraries/apollo-gradle-plugin-tasks/src/main/kotlin/com/apollographql/apollo/gradle/task/apolloGenerateCompilationUnitModel.kt
+++ libraries/apollo-gradle-plugin-tasks/src/main/kotlin/com/apollographql/apollo/gradle/task/apolloGenerateCompilationUnitModel.kt
@@ -2,10 +2,14 @@ package com.apollographql.apollo.gradle.task
 
 import com.apollographql.apollo.compiler.model.CompilationUnitModel
 import com.apollographql.apollo.compiler.model.writeTo
-import com.apollographql.apollo.tooling.toJsonElement
 import gratatouille.tasks.GAny
 import gratatouille.tasks.GOutputFile
 import gratatouille.tasks.GTask
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonNull
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
 
 @GTask
 internal fun apolloGenerateCompilationUnitModel(
@@ -37,4 +41,15 @@ internal fun apolloGenerateCompilationUnitModel(
       pluginArguments = pluginArguments.mapValues { (_, v) -> v.toJsonElement() },
   )
       .writeTo(compilationUnitModel)
+}
+
+@Suppress("UNCHECKED_CAST")
+private fun Any?.toJsonElement(): JsonElement = when (this) {
+  is Map<*, *> -> JsonObject(mapValues { it.value.toJsonElement() } as Map<String, JsonElement>)
+  is List<*> -> JsonArray(map { it.toJsonElement() })
+  is Boolean -> JsonPrimitive(this)
+  is Number -> JsonPrimitive(this)
+  is String -> JsonPrimitive(this)
+  null -> JsonNull
+  else -> error("cannot convert $this to JsonElement")
 }
--- libraries/apollo-tooling/src/main/kotlin/com/apollographql/apollo/tooling/serialization.kt
+++ libraries/apollo-tooling/src/main/kotlin/com/apollographql/apollo/tooling/serialization.kt
@@ -1,29 +1,11 @@
 package com.apollographql.apollo.tooling
 
-import com.apollographql.apollo.annotations.ApolloInternal
 import com.apollographql.apollo.tooling.SchemaDownloader.cast
 import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonNull
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.JsonPrimitive
-import kotlinx.serialization.json.booleanOrNull
-import kotlinx.serialization.json.doubleOrNull
-import kotlinx.serialization.json.intOrNull
-import kotlinx.serialization.json.longOrNull
-
-@ApolloInternal
-fun JsonElement.toAny(): Any? = when (this) {
-  is JsonObject -> this.mapValues { it.value.toAny() }
-  is JsonArray -> this.map { it.toAny() }
-  is JsonPrimitive -> {
-    when {
-      isString -> this.content
-      this is JsonNull -> null
-      else -> booleanOrNull ?: intOrNull ?: longOrNull ?: doubleOrNull ?: error("cannot decode $this")
-    }
-  }
-}
 
 fun Any?.toJsonElement(): JsonElement = when (this) {
   is Map<*, *> -> JsonObject(this.cast<Map<String, *>>()!!.mapValues { it.value.toJsonElement() })
