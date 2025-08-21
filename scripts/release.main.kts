9f02138a265c6f232f5c414b58be14dd4743879b
BoD <BoD@JRAF.org>
2025-08-21 12:22:14 +0200

    Release script: intellij plugin files no longer exist

--- scripts/release.main.kts
+++ scripts/release.main.kts
@@ -47,7 +47,6 @@ val releaseBranchName = "release-$versionToRelease"
 runCommand("git", "checkout", "-b", releaseBranchName)
 setCurrentVersion(versionToRelease)
 setVersionInDocs(versionToRelease, nextSnapshot)
-setVersionInIntelliJPlugin(versionToRelease)
 runCommand("git", "commit", "-a", "-m", "release $versionToRelease")
 runCommand("git", "push", "origin", releaseBranchName)
 runCommand("gh", "pr", "create", "--base", startBranch, "--fill")
@@ -210,37 +209,6 @@ fun setVersionInDocs(version: String, nextSnapshot: String) {
           "And then use the `$nextSnapshot` version for the plugin and libraries."
         }
     file.writeText(content)
-  }
-}
-
-fun setVersionInIntelliJPlugin(version: String) {
-  File("intellij-plugin/src/main/kotlin/com/apollographql/ijplugin/refactoring/migration/v3tov4/ApolloV3ToV4MigrationProcessor.kt").let { file ->
-    file.writeText(file.readText().replace(Regex("""apollo4LatestVersion = "(.+)"""")) {
-      """apollo4LatestVersion = "$version""""
-    })
-  }
-  File("intellij-plugin/src/test/testData/migration/v3-to-v4/updateGradleDependenciesInLibsVersionsToml_after.versions.toml").let { file ->
-    file.writeText(file.readText()
-        .replace(Regex(""""com\.apollographql\.apollo:apollo-runtime:4(.+)"""")) {
-          """"com.apollographql.apollo:apollo-runtime:$version""""
-        }
-        .replace(Regex(""""com\.apollographql\.apollo:4(.+)"""")) {
-          """"com.apollographql.apollo:$version""""
-        }
-        .replace(Regex(""""4(.+)"""")) {
-          """"$version""""
-        }
-    )
-  }
-  File("intellij-plugin/src/test/testData/migration/v3-to-v4/upgradeGradlePluginInBuildGradleKts_after.gradle.kts").let { file ->
-    file.writeText(file.readText()
-        .replace(Regex(""""4(.+)"""")) {
-          """"$version""""
-        }
-        .replace(Regex("""// TODO: Update version to 4(.+)""")) {
-          """// TODO: Update version to $version"""
-        }
-    )
   }
 }
 
