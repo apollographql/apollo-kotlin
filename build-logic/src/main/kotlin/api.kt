c1b119f61de075b73f4d8584fb41490ff964510b
Martin Bonnin <martin@mbonnin.net>
2025-09-22 22:10:54 +0200

    fix licensee

--- build-logic/src/main/kotlin/api.kt
+++ build-logic/src/main/kotlin/api.kt
@@ -86,14 +86,13 @@ fun Project.apolloLibrary(
 
     allow("Apache-2.0")
     allow("MIT")
-    allow("CC0-1.0")
     allow("MIT-0")
+    allow("CC0-1.0")
+
     // remove when https://github.com/apollographql/apollo-kotlin-execution/pull/64 is released
     allowDependency("com.apollographql.execution", "apollo-execution-runtime", "0.1.1")
     allowDependency("com.apollographql.execution", "apollo-execution-runtime-jvm", "0.1.1")
 
-    allowUrl("https://raw.githubusercontent.com/apollographql/apollo-kotlin-mockserver/main/LICENSE")
-    allowUrl("https://raw.githubusercontent.com/apollographql/apollo-kotlin/main/LICENSE")
     allowUrl("https://asm.ow2.io/license.html")
     allowUrl("https://spdx.org/licenses/MIT.txt")
   }
