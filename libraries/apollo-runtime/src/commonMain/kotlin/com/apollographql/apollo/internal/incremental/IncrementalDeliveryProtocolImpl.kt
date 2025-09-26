658187da65842b2b851891a926f19e5a4da4a9ee
BoD <BoD@JRAF.org>
2025-09-26 11:40:33 +0200

    Rename Defer20220824 protocol to GraphQL17Alpha2 for consistency

--- libraries/apollo-runtime/src/commonMain/kotlin/com/apollographql/apollo/internal/incremental/IncrementalDeliveryProtocolImpl.kt
+++ libraries/apollo-runtime/src/commonMain/kotlin/com/apollographql/apollo/internal/incremental/IncrementalDeliveryProtocolImpl.kt
@@ -7,25 +7,12 @@ internal sealed interface IncrementalDeliveryProtocolImpl {
 
   fun newIncrementalResultsMerger(): IncrementalResultsMerger
 
-  /**
-   * Format specified in this historical commit:
-   * https://github.com/graphql/graphql-spec/tree/48cf7263a71a683fab03d45d309fd42d8d9a6659/spec
-   *
-   * Only `@defer` is supported with this format.
-   *
-   * This is the default.
-   */
-  object Defer20220824 : IncrementalDeliveryProtocolImpl {
+  object GraphQL17Alpha2 : IncrementalDeliveryProtocolImpl {
     override val acceptHeader: String = "multipart/mixed;deferSpec=20220824, application/graphql-response+json, application/json"
 
     override fun newIncrementalResultsMerger(): IncrementalResultsMerger = GraphQL17Alpha2IncrementalResultsMerger()
   }
 
-  /**
-   * Newer format as implemented by graphql.js version `17.0.0-alpha.9`.
-   *
-   * Both `@defer` and `@stream` are supported with this format.
-   */
   object GraphQL17Alpha9 : IncrementalDeliveryProtocolImpl {
     // TODO To be agreed upon with the router and other clients
     override val acceptHeader: String =
@@ -37,6 +24,6 @@ internal sealed interface IncrementalDeliveryProtocolImpl {
 
 internal val HttpNetworkTransport.IncrementalDeliveryProtocol.impl: IncrementalDeliveryProtocolImpl
   get() = when (this) {
-    HttpNetworkTransport.IncrementalDeliveryProtocol.GraphQL17Alpha2 -> IncrementalDeliveryProtocolImpl.Defer20220824
+    HttpNetworkTransport.IncrementalDeliveryProtocol.GraphQL17Alpha2 -> IncrementalDeliveryProtocolImpl.GraphQL17Alpha2
     HttpNetworkTransport.IncrementalDeliveryProtocol.GraphQL17Alpha9 -> IncrementalDeliveryProtocolImpl.GraphQL17Alpha9
   }
