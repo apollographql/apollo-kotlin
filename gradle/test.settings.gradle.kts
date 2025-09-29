e5f347dd33edf498b62a8735d8340d3a8431eec5
BoD <BoD@JRAF.org>
2025-09-29 18:59:43 +0200

    Revert "Use Apollo's KSP fork"
    This reverts commit 26760fbd2f25ee8a52749ef62b1b5f4cd9cf10bc.

--- gradle/test.settings.gradle.kts
+++ gradle/test.settings.gradle.kts
@@ -12,7 +12,6 @@ pluginManagement {
   listOf(repositories, dependencyResolutionManagement.repositories).forEach {
     // Uncomment this one to use the Kotlin "dev" repository
     it.maven("https://redirector.kotlinlang.org/maven/dev/")
-    maven("https://storage.googleapis.com/apollo-previews/m2")
     it.mavenCentral()
     it.google()
     it.maven("../../../../build/localMaven")
