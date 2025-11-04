5623ef26f994f75bbf33322f33f8fa502169339c
Martin Bonnin <martin@mbonnin.net>
2025-11-04 16:15:30 +0100

    Add a test for executionContext

---
+++ libraries/apollo-runtime/src/jvmTest/kotlin/ExecutionContextTest.kt
@@ -0,0 +1,53 @@
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.ApolloRequest
+import com.apollographql.apollo.api.ApolloResponse
+import com.apollographql.apollo.api.ExecutionContext
+import com.apollographql.apollo.api.Operation
+import com.apollographql.apollo.api.http.HttpRequest
+import com.apollographql.apollo.api.http.HttpResponse
+import com.apollographql.apollo.exception.DefaultApolloException
+import com.apollographql.apollo.interceptor.ApolloInterceptor
+import com.apollographql.apollo.interceptor.ApolloInterceptorChain
+import com.apollographql.apollo.network.http.HttpEngine
+import com.apollographql.apollo.testing.internal.runTest
+import com.apollographql.mockserver.MockServer
+import com.apollographql.mockserver.enqueueString
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
+import okio.Buffer
+import okio.use
+import test.FooQuery
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertIs
+import kotlin.test.assertNotNull
+
+class ExecutionContextTest {
+  @Test
+  fun test() = runTest {
+    MockServer().use { mockServer ->
+      ApolloClient.Builder()
+          .serverUrl(mockServer.url())
+          .build()
+          .use { apolloClient ->
+
+            mockServer.enqueueString(FooQuery.successResponse)
+            val response = apolloClient.query(FooQuery())
+                .addExecutionContext(MyExecutionContext())
+                .execute()
+
+            assertNotNull(response.executionContext[MyExecutionContext])
+          }
+    }
+  }
+}
+
+internal class MyExecutionContext : ExecutionContext.Element {
+  override val key: ExecutionContext.Key<*>
+    get() = Key
+
+  companion object Key : ExecutionContext.Key<MyExecutionContext>
+}
