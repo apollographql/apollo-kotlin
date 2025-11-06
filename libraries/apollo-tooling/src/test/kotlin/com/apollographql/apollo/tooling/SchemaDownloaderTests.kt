a96e7b3330c78b5960056302f130f2094f2d75da
BoD <BoD@JRAF.org>
2025-11-06 16:19:17 +0100

    Add a SchemaDownloader test that converts to SDL

--- libraries/apollo-tooling/src/test/kotlin/com/apollographql/apollo/tooling/SchemaDownloaderTests.kt
+++ libraries/apollo-tooling/src/test/kotlin/com/apollographql/apollo/tooling/SchemaDownloaderTests.kt
@@ -16,29 +16,37 @@ private val introspectionRequestJune2018 = pathToUtf8("apollo-tooling/src/test/f
 private val preIntrospectionResponseOctober2021 = pathToUtf8("apollo-tooling/src/test/fixtures/pre-introspection-response-october2021.json")
 private val introspectionRequestOctober2021 = pathToUtf8("apollo-tooling/src/test/fixtures/introspection-request-october2021.json")
 
-private val preIntrospectionResponseDraft = pathToUtf8("apollo-tooling/src/test/fixtures/pre-introspection-response-draft.json")
-private val introspectionRequestDraft = pathToUtf8("apollo-tooling/src/test/fixtures/introspection-request-draft.json")
+private val preIntrospectionResponseSeptember2025 =
+  pathToUtf8("apollo-tooling/src/test/fixtures/pre-introspection-response-september2025.json")
+private val introspectionRequestSeptember2025 = pathToUtf8("apollo-tooling/src/test/fixtures/introspection-request-september2025.json")
 
-private val preIntrospectionResponseOneOf = pathToUtf8("apollo-tooling/src/test/fixtures/pre-introspection-response-oneOf.json")
-private val introspectionRequestOneOf = pathToUtf8("apollo-tooling/src/test/fixtures/introspection-request-oneOf.json")
+private val preIntrospectionResponseSeptember2025OneOf =
+  pathToUtf8("apollo-tooling/src/test/fixtures/pre-introspection-response-september2025-oneOf.json")
+private val introspectionRequestSeptember2025OneOf =
+  pathToUtf8("apollo-tooling/src/test/fixtures/introspection-request-september2025-oneOf.json")
 
 private val introspectionRequestFailSafe = pathToUtf8("apollo-tooling/src/test/fixtures/introspection-request-failSafe.json")
 
 private val introspectionResponseSuccess = pathToUtf8("apollo-tooling/src/test/fixtures/introspection-response-success.json")
+private val introspectionResponseSuccessFullJson = pathToUtf8("apollo-tooling/src/test/fixtures/introspection-response-success-full.json")
+private val introspectionResponseSuccessFullSdl =
+  pathToUtf8("apollo-tooling/src/test/fixtures/introspection-response-success-full.graphqls")
 private val introspectionResponseFail = pathToUtf8("apollo-tooling/src/test/fixtures/introspection-response-fail.json")
 
 class SchemaDownloaderTests {
   private lateinit var mockServer: MockServer
-  private lateinit var tempFile: File
+  private lateinit var tempFileJson: File
+  private lateinit var tempFileSdl: File
 
   private fun setUp() {
     mockServer = MockServer()
-    tempFile = File.createTempFile("schema", ".json")
+    tempFileJson = File.createTempFile("schema", ".json")
+    tempFileSdl = File.createTempFile("schema", ".graphqls")
   }
 
   private fun tearDown() {
     mockServer.close()
-    tempFile.delete()
+    tempFileJson.delete()
   }
 
   @Test
@@ -51,13 +59,13 @@ class SchemaDownloaderTests {
         graph = null,
         key = null,
         graphVariant = "",
-        schema = tempFile,
+        schema = tempFileJson,
     )
 
     mockServer.takeRequest()
     val introspectionRequest = mockServer.takeRequest().body.utf8()
     assertEquals(introspectionRequestJune2018, introspectionRequest)
-    assertEquals(introspectionResponseSuccess, tempFile.readText())
+    assertEquals(introspectionResponseSuccess, tempFileJson.readText())
   }
 
   @Test
@@ -70,18 +78,18 @@ class SchemaDownloaderTests {
         graph = null,
         key = null,
         graphVariant = "",
-        schema = tempFile,
+        schema = tempFileJson,
     )
 
     mockServer.takeRequest()
     val introspectionRequest = mockServer.takeRequest().body.utf8()
     assertEquals(introspectionRequestOctober2021, introspectionRequest)
-    assertEquals(introspectionResponseSuccess, tempFile.readText())
+    assertEquals(introspectionResponseSuccess, tempFileJson.readText())
   }
 
   @Test
-  fun `schema is downloaded correctly when server supports Draft spec as of 2023-11-15`() = runTest(before = { setUp() }, after = { tearDown() }) {
-    mockServer.enqueueString(preIntrospectionResponseDraft)
+  fun `schema is downloaded correctly when server supports September 2025 spec`() = runTest(before = { setUp() }, after = { tearDown() }) {
+    mockServer.enqueueString(preIntrospectionResponseSeptember2025)
     mockServer.enqueueString(introspectionResponseSuccess)
 
     SchemaDownloader.download(
@@ -89,37 +97,58 @@ class SchemaDownloaderTests {
         graph = null,
         key = null,
         graphVariant = "",
-        schema = tempFile,
+        schema = tempFileJson,
     )
 
     mockServer.takeRequest()
     val introspectionRequest = mockServer.takeRequest().body.utf8()
-    assertEquals(introspectionRequestDraft, introspectionRequest)
-    assertEquals(introspectionResponseSuccess, tempFile.readText())
+    assertEquals(introspectionRequestSeptember2025, introspectionRequest)
+    assertEquals(introspectionResponseSuccess, tempFileJson.readText())
   }
 
   @Test
-  fun `schema is downloaded correctly when server supports oneOf`() = runTest(before = { setUp() }, after = { tearDown() }) {
-    mockServer.enqueueString(preIntrospectionResponseOneOf)
-    mockServer.enqueueString(introspectionResponseSuccess)
+  fun `full schema is downloaded and converted correctly to SDL when server supports September 2025 spec`() =
+    runTest(before = { setUp() }, after = { tearDown() }) {
+      mockServer.enqueueString(preIntrospectionResponseSeptember2025)
+      mockServer.enqueueString(introspectionResponseSuccessFullJson)
 
-    SchemaDownloader.download(
-        endpoint = mockServer.url(),
-        graph = null,
-        key = null,
-        graphVariant = "",
-        schema = tempFile,
-    )
+      SchemaDownloader.download(
+          endpoint = mockServer.url(),
+          graph = null,
+          key = null,
+          graphVariant = "",
+          schema = tempFileSdl,
+      )
 
-    mockServer.takeRequest()
-    val introspectionRequest = mockServer.takeRequest().body.utf8()
-    assertEquals(introspectionRequestOneOf, introspectionRequest)
-    assertEquals(introspectionResponseSuccess, tempFile.readText())
-  }
+      mockServer.takeRequest()
+      val introspectionRequest = mockServer.takeRequest().body.utf8()
+      assertEquals(introspectionRequestSeptember2025, introspectionRequest)
+      assertEquals(introspectionResponseSuccessFullSdl, tempFileSdl.readText())
+    }
+
+  @Test
+  fun `schema is downloaded correctly when server supports September 2025 spec including oneOf`() =
+    runTest(before = { setUp() }, after = { tearDown() }) {
+      mockServer.enqueueString(preIntrospectionResponseSeptember2025OneOf)
+      mockServer.enqueueString(introspectionResponseSuccess)
+
+      SchemaDownloader.download(
+          endpoint = mockServer.url(),
+          graph = null,
+          key = null,
+          graphVariant = "",
+          schema = tempFileJson,
+      )
+
+      mockServer.takeRequest()
+      val introspectionRequest = mockServer.takeRequest().body.utf8()
+      assertEquals(introspectionRequestSeptember2025OneOf, introspectionRequest)
+      assertEquals(introspectionResponseSuccess, tempFileJson.readText())
+    }
 
   @Test
   fun `schema is downloaded correctly in fail-safe mode after 2-step fails`() = runTest(before = { setUp() }, after = { tearDown() }) {
-    mockServer.enqueueString(preIntrospectionResponseDraft)
+    mockServer.enqueueString(preIntrospectionResponseSeptember2025)
     mockServer.enqueueString(introspectionResponseFail)
     mockServer.enqueueString(introspectionResponseSuccess)
 
@@ -128,14 +157,14 @@ class SchemaDownloaderTests {
         graph = null,
         key = null,
         graphVariant = "",
-        schema = tempFile,
+        schema = tempFileJson,
     )
 
     mockServer.takeRequest()
     mockServer.takeRequest()
     val introspectionRequest = mockServer.takeRequest().body.utf8()
     assertEquals(introspectionRequestFailSafe, introspectionRequest)
-    assertEquals(introspectionResponseSuccess, tempFile.readText())
+    assertEquals(introspectionResponseSuccess, tempFileJson.readText())
   }
 
 }
