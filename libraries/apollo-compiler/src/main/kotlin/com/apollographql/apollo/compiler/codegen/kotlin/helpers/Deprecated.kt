a27e20772dfe4c4dc15ffa320463318c7ac29629
Martin Bonnin <martin@mbonnin.net>
2025-11-06 20:06:53 +0100

    Allow empty deprecation messages (#6779)
    * Allow empty deprecation messages
    
    * add test

--- libraries/apollo-compiler/src/main/kotlin/com/apollographql/apollo/compiler/codegen/kotlin/helpers/Deprecated.kt
+++ libraries/apollo-compiler/src/main/kotlin/com/apollographql/apollo/compiler/codegen/kotlin/helpers/Deprecated.kt
@@ -6,8 +6,6 @@ import com.squareup.kotlinpoet.AnnotationSpec
 internal fun deprecatedAnnotation(message: String) = AnnotationSpec
     .builder(KotlinSymbols.Deprecated)
     .apply {
-      if (message.isNotBlank()) {
-        addMember("message = %S", message)
-      }
+      addMember("message = %S", message)
     }
     .build()
