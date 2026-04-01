de5fa4f545660e9c5aba3620b8eba17536d948dd
Martin Bonnin <martin@mbonnin.net>
2026-04-01 14:24:14 +0200

    Remove legacy JS target check

--- libraries/apollo-gradle-plugin-external/src/main/kotlin/com/apollographql/apollo/gradle/internal/DefaultApolloExtension.kt
+++ libraries/apollo-gradle-plugin-external/src/main/kotlin/com/apollographql/apollo/gradle/internal/DefaultApolloExtension.kt
@@ -40,7 +40,6 @@ import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.TaskProvider
 import org.gradle.util.GradleVersion
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-import org.jetbrains.kotlin.gradle.targets.js.KotlinJsTarget
 import java.io.File
 import java.util.concurrent.Callable
 import javax.inject.Inject
@@ -261,9 +260,7 @@ abstract class DefaultApolloExtension(
       }
 
       maybeLinkSqlite()
-      checkForLegacyJsTarget()
       checkApolloMetadataIsEmpty()
-
     }
   }
 
@@ -289,15 +286,6 @@ abstract class DefaultApolloExtension(
       """.trimIndent()
     }
   }
-
-  private fun checkForLegacyJsTarget() {
-    val kotlin = project.extensions.findByName("kotlin") as? KotlinMultiplatformExtension
-    val hasLegacyJsTarget = kotlin?.targets?.any { target -> target is KotlinJsTarget && target.irTarget == null } == true
-    check(!hasLegacyJsTarget) {
-      "Apollo: LEGACY js target is not supported by Apollo, please use IR."
-    }
-  }
-
   private fun maybeLinkSqlite() {
     when (linkSqlite.orNull) {
       false -> return // explicit opt-out
--- libraries/apollo-gradle-plugin/testProjects/legacyJsTarget/settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-apply(from = "../../../../gradle/test.settings.gradle.kts")
