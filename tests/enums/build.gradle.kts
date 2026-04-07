f554cd774580f19e8003cdcdafed284ce8147a6c
Benoit 'BoD' Lubek <BoD@JRAF.org>
2026-04-07 18:42:26 +0200

    Remove Kotlin 1.9 specific Enum test

--- tests/enums/build.gradle.kts
+++ tests/enums/build.gradle.kts
@@ -1,5 +1,6 @@
-import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
+@file:OptIn(ApolloExperimental::class)
+
+import com.apollographql.apollo.annotations.ApolloExperimental
 
 plugins {
   id("org.jetbrains.kotlin.jvm")
@@ -14,13 +15,8 @@ dependencies {
 }
 
 apollo {
-  service("kotlin15") {
-    packageName.set("enums.kotlin15")
-    sealedClassesForEnumsMatching.set(listOf(".*avity", "FooSealed"))
-  }
-
-  service("kotlin19") {
-    packageName.set("enums.kotlin19")
+  service("kotlin") {
+    packageName.set("enums.kotlin")
     sealedClassesForEnumsMatching.set(listOf(".*avity", "FooSealed"))
   }
 
@@ -35,23 +31,5 @@ apollo {
   service("apollo") {
     packageName.set("enums.apollo")
     generateApolloEnums.set(true)
-  }
-}
-
-//kotlin {
-//  compilerOptions {
-//    languageVersion.set(KotlinVersion.KOTLIN_1_9)
-//    apiVersion.set(KotlinVersion.KOTLIN_1_9)
-//  }
-//}
-
-
-tasks.withType(KotlinCompilationTask::class.java).configureEach {
-  compilerOptions {
-    apiVersion.set(KotlinVersion.KOTLIN_1_9)
-    languageVersion.set(KotlinVersion.KOTLIN_1_9)
-
-    // Suppress "Language version 1.9 is deprecated and its support will be removed in a future version of Kotlin"
-    freeCompilerArgs.add("-Xsuppress-version-warnings")
   }
 }
