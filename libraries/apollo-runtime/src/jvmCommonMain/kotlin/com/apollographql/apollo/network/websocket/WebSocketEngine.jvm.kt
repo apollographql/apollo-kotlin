6da476ee152100e61689679143f06342867042b5
Martin Bonnin <martin@mbonnin.net>
2026-09-07 11:25:21 +0200

    Allow custom JVM HTTP engines without OkHttp - #7018 (#7020)
    * Allow custom JVM HTTP engines without OkHttp
    
    * Simplify the WebSocketEngine
    
    ---------
    
    Co-authored-by: GT <gregjotau@gmail.com>

--- libraries/apollo-runtime/src/jvmCommonMain/kotlin/com/apollographql/apollo/network/websocket/WebSocketEngine.jvm.kt
+++ libraries/apollo-runtime/src/jvmCommonMain/kotlin/com/apollographql/apollo/network/websocket/WebSocketEngine.jvm.kt
@@ -14,7 +14,11 @@ import java.util.concurrent.atomic.AtomicReference
 import okhttp3.WebSocket as PlatformWebSocket
 import okhttp3.WebSocketListener as PlatformWebSocketListener
 
-internal class JvmWebSocketEngine(webSocketFactory: () -> PlatformWebSocket.Factory) : WebSocketEngine {
+/**
+ * Use `Any` in the constructor signature to avoid linking OkHttp classes.
+ * See https://github.com/apollographql/apollo-kotlin/pull/7018
+ */
+internal class JvmWebSocketEngine(webSocketFactory: () -> Any) : WebSocketEngine {
   private val webSocketFactory by lazy { webSocketFactory() }
 
   constructor(webSocketFactory: PlatformWebSocket.Factory) : this({ webSocketFactory })
@@ -24,7 +28,7 @@ internal class JvmWebSocketEngine(webSocketFactory: () -> PlatformWebSocket.Fact
     require(!closed) {
       "JvmWebSocketEngine is closed"
     }
-    return JvmWebSocket(webSocketFactory, url, headers, listener)
+    return JvmWebSocket(webSocketFactory as PlatformWebSocket.Factory, url, headers, listener)
   }
 
   override fun close() {
