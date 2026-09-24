823151f1b7aed9c78c6d7b7a937e0d4b8735ed6f
Martin Bonnin <martin@mbonnin.net>
2026-09-24 18:23:01 +0200

    Fix Java codegen referencing unavaiable symbols

--- gradle/repositories.gradle.kts
+++ gradle/repositories.gradle.kts
@@ -42,12 +42,5 @@ listOf(pluginManagement.repositories, dependencyResolutionManagement.repositorie
         includeModule("com.gradle", "develocity-gradle-plugin")
       }
     }
-
-    exclusiveContent {
-      forRepository { maven("https://storage.googleapis.com/gradleup/m2") }
-      filter {
-        includeGroup("com.gradleup.librarian")
-      }
-    }
   }
 }
